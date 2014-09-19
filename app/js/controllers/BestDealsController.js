6fe1b5c3d260197e2e01c70ae70630b8e3ca6fbb
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-09-19 19:06:56 +0200

    product details

--- app/js/controllers/BestDealsController.js
+++ app/js/controllers/BestDealsController.js
@@ -4,6 +4,24 @@ angular.module('myApp').controller('BestDealsController', [
     '$modal',
     function ($scope, productService, $modal) {
 
+        $scope.showDetail = function (id) {
+
+            var productDetailsController = function ($scope) {
+
+                productService.get(id).success(function (data) {
+                    $scope.product = data.data;
+                }).error(function (data) {
+                    console.log(data);
+                });
+            }
+
+            $modal.open({
+                templateUrl: 'views/ProductDetail.html',
+                controller: productDetailsController,
+                size: 'lg'
+            });
+        }
+
         productService.find().success(function (data) {
             $scope.products = data.data;
         }).error(function (data) {
