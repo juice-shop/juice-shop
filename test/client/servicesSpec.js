04050d58068de9cb84e3be15747cc0ccb3832d81
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-09-21 21:20:09 +0200

    expect service methods to be defined

--- test/client/servicesSpec.js
+++ test/client/servicesSpec.js
@@ -4,12 +4,16 @@ describe('service', function () {
     describe('UserService', function () {
         it('should be defined', inject(function (UserService) {
             expect(UserService).toBeDefined();
+            expect(UserService.find).toBeDefined();
+            expect(UserService.get).toBeDefined();
         }));
     });
 
     describe('ProductService', function () {
         it('should be defined', inject(function (ProductService) {
             expect(ProductService).toBeDefined();
+            expect(ProductService.find).toBeDefined();
+            expect(ProductService.get).toBeDefined();
         }));
     });
 
