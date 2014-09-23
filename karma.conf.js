fad9286eaa5f3827156c91f4c0a6de381df3487a
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-09-23 19:49:48 +0200

    ngGrid removed from karma

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,6 @@ module.exports = function (config) {
             'app/bower_components/angular-bootstrap/ui-bootstrap.js',
             'app/bower_components/underscore/underscore.js',
             'app/bower_components/jquery/dist/jquery.js',
-            'app/bower_components/angular-grid/build/ng-grid.js',
             'app/js/app.js',
             'app/js/**/*.js',
             'test/client/**/*.js'
