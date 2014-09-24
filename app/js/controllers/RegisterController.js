b4b120519feef463e2e17312c239d7bc06ddb542
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-09-24 10:45:25 +0200

    setting admin flag to false on save

--- app/js/controllers/RegisterController.js
+++ app/js/controllers/RegisterController.js
@@ -6,6 +6,7 @@ angular.module('myApp').controller('RegisterController', [
         'use strict';
 
         $scope.save = function () {
+            $scope.user.admin = false;
             userService.save($scope.user).success(function () {
                 $scope.user = {};
                 $location.path( "/login" );
