9e3e7e851893984dd21b2f97b0309b46e4dbc146
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-09-25 16:14:19 +0200

    ignoring jshint for SQL query line

--- server.js
+++ server.js
@@ -119,9 +119,7 @@ app.use(bodyParser.json());
 /* Restful APIs */
 app.use(restful(sequelize, { endpoint: '/api' }));
 app.post('/rest/user/login', function(req, res, next){
-    /*jshint -W110 */
-    sequelize.query("SELECT * FROM Users WHERE email = '" + req.body.email +
-        "' AND password = '" + req.body.password + "'", User, {plain: true})
+    sequelize.query("SELECT * FROM Users WHERE email = '" + req.body.email + "' AND password = '" + req.body.password + "'", User, {plain: true}) // jshint ignore:line
         .success(function(data) {
             var user = toJSON(data);
             req.session.user = user;
