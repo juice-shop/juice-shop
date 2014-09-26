e7827ac5c3fa6cacf01d4a47abbaab708d384649
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-09-26 22:59:31 +0200

    making code climate happy

--- app/js/controllers/SearchResultController.js
+++ app/js/controllers/SearchResultController.js
@@ -2,8 +2,8 @@ angular.module('myApp').controller('SearchResultController', [
     '$scope',
     '$location',
     '$sce',
-    'ProductService',
-    function ($scope, $location, $sce, productService) {
+    /*'ProductService',*/
+    function ($scope, $location, $sce/*, productService*/) {
         'use strict';
 
         $scope.searchQuery = $sce.trustAsHtml($location.search().q);
