271dc34474d8f835655fbb6bf6746c05b96c5102
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-09-28 16:36:30 +0200

    added angular cookies for testing

--- karma.conf.js
+++ karma.conf.js
@@ -10,6 +10,7 @@ module.exports = function (config) {
             'app/bower_components/angular/angular.js',
             'app/bower_components/angular-route/angular-route.js',
             'app/bower_components/angular-mocks/angular-mocks.js',
+            'app/bower_components/angular-cookies/angular-cookies.js',
             'app/bower_components/angular-bootstrap/ui-bootstrap.js',
             'app/bower_components/underscore/underscore.js',
             'app/bower_components/jquery/dist/jquery.js',
