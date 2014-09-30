00eafb71ed0bbf73c03e698d71acdbc1d1e48a83
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-09-30 14:10:55 +0200

    showing login error

--- app/js/controllers/LoginController.js
+++ app/js/controllers/LoginController.js
@@ -1,19 +1,19 @@
 angular.module('myApp').controller('LoginController', [
     '$scope',
     '$window',
+    '$location',
     'UserService',
-    function ($scope, $window, userService) {
+    function ($scope, $window, $location, userService) {
         'use strict';
 
         $scope.login = function () {
             userService.login($scope.user).success(function (data) {
                 $window.sessionStorage.token = data.token;
-                $scope.message = 'Welcome ' + $scope.user.email;
+                $location.path( '/' );
             }).error(function(error) {
                 delete $window.sessionStorage.token;
-                $scope.message = error;
+                $scope.error = error;
             });
         };
-
 
     }]);
