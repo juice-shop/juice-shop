4e1f840fc3169e967d6c3677913923478f0bee16
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-10-01 15:00:44 +0200

    passing auth token with every request

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,27 @@
-angular.module('myApp', [
+var myApp = angular.module('myApp', [
     'ngRoute',
     'ngCookies',
     'ui.bootstrap'
 ]);
+
+myApp.factory('authInterceptor', function ($rootScope, $q, $window) {
+    return {
+        request: function (config) {
+            config.headers = config.headers || {};
+            if ($window.sessionStorage.token) {
+                config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
+            }
+            return config;
+        },
+        response: function (response) {
+            if (response.status === 401) {
+                console.err('401: ' + response.statusText);
+            }
+            return response || $q.when(response);
+        }
+    };
+});
+
+myApp.config(function ($httpProvider) {
+    $httpProvider.interceptors.push('authInterceptor');
+});
