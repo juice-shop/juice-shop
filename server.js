880d19e6d80945702ce9c20645aaa81302096bab
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-10-03 23:09:08 +0200

    deleting products is now entirely forbidden via API calls

--- server.js
+++ server.js
@@ -309,7 +309,7 @@ app.delete('/api/Users/:id', insecurity.denyAll()); // Deleting users is forbidd
 /* Products: Only GET is allowed in order to view products */
 app.post('/api/Products', insecurity.isAuthorized());
 //app.put('/api/Products/:id', insecurity.isAuthorized()); // = missing function-level access control vulnerability
-app.delete('/api/Products/:id', insecurity.isAuthorized());
+app.delete('/api/Products/:id', insecurity.denyAll()); // Deleting products is forbidden entirely to keep the O-Saft url-change challenge solvable
 
 /* Challenges: GET list of challenges allowed. Everything else forbidden independent of authorization (hence the random secret) */
 app.post('/api/Challenges', insecurity.denyAll());
