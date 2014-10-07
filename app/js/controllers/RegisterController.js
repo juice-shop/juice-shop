eb1f4e6605142c3bde309ce7b88d823e743f44a1
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-10-07 10:52:50 +0200

    removed admin flag

--- app/js/controllers/RegisterController.js
+++ app/js/controllers/RegisterController.js
@@ -6,7 +6,6 @@ angular.module('myApp').controller('RegisterController', [
         'use strict';
 
         $scope.save = function () {
-            $scope.user.admin = false;
             userService.save($scope.user).success(function () {
                 $scope.user = {};
                 $location.path( '/login' );
