0bb26176745ac827e1fabaf78d6743109c410de5
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2014-10-21 01:42:31 +0200

    added test for ChangePasswordController

--- test/client/changepasswordControllerSpec.js
+++ test/client/changepasswordControllerSpec.js
@@ -7,8 +7,9 @@ describe('controllers', function () {
     }));
 
     describe('ChangePasswordController', function () {
-        beforeEach(inject(function ($rootScope, $controller) {
+        beforeEach(inject(function ($rootScope, $location, $controller) {
             scope = $rootScope.$new();
+            location = $location;
             controller = $controller('ChangePasswordController', {
                 '$scope': scope
             });
@@ -18,6 +19,31 @@ describe('controllers', function () {
             expect(controller).toBeDefined();
             expect(scope.changePassword).toBeDefined();
         }));
+
+        it('should forward to main page after changing password', inject(function ($controller) {
+            $httpBackend.whenGET('/rest/user/change-password?current=old&new=foobar&repeat=foobar').respond(200);
+            scope.currentPassword = 'old';
+            scope.newPassword = 'foobar';
+            scope.newPasswordRepeat = 'foobar';
+
+            scope.changePassword();
+            $httpBackend.flush();
+
+            expect(location.path()).toBe('/');
+        }));
+
+        it('should gracefully handle error on password change', inject(function ($controller) {
+            $httpBackend.whenGET('/rest/user/change-password?current=old&new=foobar&repeat=foobar').respond(500, 'error');
+            scope.currentPassword = 'old';
+            scope.newPassword = 'foobar';
+            scope.newPasswordRepeat = 'foobar';
+
+            scope.changePassword();
+            $httpBackend.flush();
+
+            expect(scope.error).toBe('error');
+        }));
+
     });
 
 });
