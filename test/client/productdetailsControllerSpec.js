1bb49abb2069076262d1f8e20c09c82a5eb3cf53
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2014-10-20 23:46:22 +0200

    added tests for modal overlay dialogs

--- test/client/productdetailsControllerSpec.js
+++ test/client/productdetailsControllerSpec.js
@@ -1,24 +1,51 @@
 describe('controllers', function () {
-    var scope, location, controller, window, cookieStore, $httpBackend;
+    var scope, controller, $httpBackend, $sce;
 
     beforeEach(module('myApp'));
     beforeEach(inject(function($injector) {
         $httpBackend = $injector.get('$httpBackend');
+        $sce = $injector.get('$sce');
     }));
 
     describe('ProductDetailsController', function () {
         beforeEach(inject(function ($rootScope, $controller) {
             scope = $rootScope.$new();
-            var id;
             controller = $controller('ProductDetailsController', {
                 '$scope': scope,
-                'id': id
+                'id': 42
             });
         }));
 
         it('should be defined', inject(function ($controller) {
             expect(controller).toBeDefined();
         }));
+
+        it('should hold single product with given id', inject(function ($controller) {
+            $httpBackend.whenGET('/api/Products/42').respond(200, {data: {name: 'Test Juice'}});
+
+            $httpBackend.flush();
+
+            expect(scope.product).toBeDefined();
+            expect(scope.product.name).toBe('Test Juice');
+        }));
+
+        it('should render product description as trusted HTML', inject(function ($controller) {
+            $httpBackend.whenGET('/api/Products/42').respond(200, {data: {description: '<script>alert("XSS4")</script>'}});
+            spyOn($sce, 'trustAsHtml');
+
+            $httpBackend.flush();
+
+            expect($sce.trustAsHtml).toHaveBeenCalledWith('<script>alert("XSS4")</script>');
+        }));
+
+        it('should hold no product if API call fails', inject(function ($controller) {
+            $httpBackend.whenGET('/api/Products/42').respond(500);
+
+            $httpBackend.flush();
+
+            expect(scope.product).toBeUndefined();
+        }));
+
     });
 
 });
--- test/client/userdetailsControllerSpec.js
+++ test/client/userdetailsControllerSpec.js
@@ -1,5 +1,5 @@
 describe('controllers', function () {
-    var scope, location, controller, window, cookieStore, $httpBackend;
+    var scope, controller, $httpBackend;
 
     beforeEach(module('myApp'));
     beforeEach(inject(function($injector) {
@@ -9,16 +9,33 @@ describe('controllers', function () {
     describe('UserDetailsController', function () {
         beforeEach(inject(function ($rootScope, $controller) {
             scope = $rootScope.$new();
-            var id;
             controller = $controller('UserDetailsController', {
                 '$scope': scope,
-                'id': id
+                'id': 42
             });
         }));
 
         it('should be defined', inject(function ($controller) {
             expect(controller).toBeDefined();
         }));
+
+        it('should hold single product with given id', inject(function ($controller) {
+            $httpBackend.whenGET('/api/Users/42').respond(200, {data: {email: 'test@juice-sh.op'}});
+
+            $httpBackend.flush();
+
+            expect(scope.user).toBeDefined();
+            expect(scope.user.email).toBe('test@juice-sh.op');
+        }));
+
+        it('should hold no product if API call fails', inject(function ($controller) {
+            $httpBackend.whenGET('/api/Users/42').respond(500);
+
+            $httpBackend.flush();
+
+            expect(scope.user).toBeUndefined();
+        }));
+
     });
 
 });
