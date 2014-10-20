c17a2afaeafc194fc33ec4fcfb3395aa88440204
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2014-10-21 01:50:28 +0200

    added test for ChallengeController

--- test/client/challengeControllerSpec.js
+++ test/client/challengeControllerSpec.js
@@ -1,9 +1,10 @@
 describe('controllers', function () {
-    var scope, location, controller, window, cookieStore, $httpBackend;
+    var scope, controller, $httpBackend, $sce;
 
     beforeEach(module('myApp'));
     beforeEach(inject(function($injector) {
         $httpBackend = $injector.get('$httpBackend');
+        $sce = $injector.get('$sce');
     }));
 
     describe('ChallengeController', function () {
@@ -17,6 +18,42 @@ describe('controllers', function () {
         it('should be defined', inject(function ($controller) {
             expect(controller).toBeDefined();
         }));
+
+        it('should hold existing challenges', inject(function ($controller) {
+            $httpBackend.whenGET('/api/Challenges/').respond(200, {data: [{description: 'XSS'},{description: 'CSRF'}]});
+
+            $httpBackend.flush();
+
+            expect(scope.challenges.length).toBe(2);
+            expect(scope.challenges[0].description).toBeDefined();
+            expect(scope.challenges[1].description).toBeDefined();
+        }));
+
+        it('should consider challenge description as trusted HTML', inject(function ($controller) {
+            $httpBackend.whenGET('/api/Challenges/').respond(200, {data: [{description: '<a src="link">Link</a>'}]});
+            spyOn($sce, 'trustAsHtml');
+
+            $httpBackend.flush();
+
+            expect($sce.trustAsHtml).toHaveBeenCalledWith('<a src="link">Link</a>');
+        }));
+
+        it('should hold nothing when no challenges exists', inject(function ($controller) {
+            $httpBackend.whenGET('/api/Challenges/').respond(200, {data: {}});
+
+            $httpBackend.flush();
+
+            expect(scope.challenges).toEqual({});
+        }));
+
+        it('should hold nothing on error from backend API', inject(function ($controller) {
+            $httpBackend.whenGET('/api/Challenges/').respond(500);
+
+            $httpBackend.flush();
+
+            expect(scope.challenges).toBeUndefined();
+        }));
+
     });
 
 });
