ce558fee34b86b3280a9f1db3f2d3e0cbbb64239
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2014-10-27 18:28:24 +0100

    kicking protractor on travis manually II

--- protractor.conf.js
+++ protractor.conf.js
@@ -21,4 +21,17 @@ exports.config = {
         showColors: true,
         defaultTimeoutInterval: 30000
     }
+
 };
+
+if (process.env.TRAVIS_BUILD_NUMBER) {
+    config.seleniumAddress = 'http://localhost:4445/wd/hub';
+    config.capabilities = {
+        'username': process.env.SAUCE_USERNAME,
+        'accessKey': process.env.SAUCE_ACCESS_KEY,
+        'browserName': 'chrome',
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+        'build': process.env.TRAVIS_BUILD_NUMBER,
+        'name': 'Juice Shop e2e Protractor Tests'
+    }
+}
