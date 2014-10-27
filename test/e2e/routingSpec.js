c710d85e6a36fec4f3abd0550bfa5b42bbdca194
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-10-27 10:48:17 +0100

    e2e tests
    - regexes delimited by /.../ instead of '...'
    - empty search test

--- test/e2e/routingSpec.js
+++ test/e2e/routingSpec.js
@@ -4,12 +4,12 @@ describe('routing', function () {
 
     it('should automatically route to /search when location hash/fragment is empty', function () {
         browser.get('/');
-        expect(browser.getLocationAbsUrl()).toMatch("/search");
+        expect(browser.getLocationAbsUrl()).toMatch(/\/search/);
     });
 
     it('should automatically route to /search when location hash/fragment is not defined', function () {
         browser.get('/#/undefined');
-        expect(browser.getLocationAbsUrl()).toMatch("/search");
+        expect(browser.getLocationAbsUrl()).toMatch(/\/search/);
     });
 
 });
--- test/e2e/searchSpec.js
+++ test/e2e/searchSpec.js
@@ -6,14 +6,20 @@ describe('search', function () {
         browser.get('/#/search'); // not really necessary as search field is part of navbar on every dialog
     });
 
+    it('should show all products performing search without criteria', function () {
+        element(by.id('searchButton')).click();
+
+        expect(browser.getLocationAbsUrl()).toMatch(/\/search\?q=$/);
+    });
+
     it('should show matching products for name when performing search with criteria', function () {
         element(by.model('searchQuery')).sendKeys('Apple');
 
         element(by.id('searchButton')).click();
 
+        expect(browser.getLocationAbsUrl()).toMatch(/\/search\?q=Apple/);
         var productNames = element.all(by.repeater('product in products').column('name'));
-        expect(productNames.count()).toBe(1);
-        expect(productNames.first().getText()).toMatch('Apple');
+        expect(productNames.first().getText()).toMatch(/Apple/);
     });
 
     it('should show matching products for description when performing search with criteria', function () {
@@ -21,9 +27,9 @@ describe('search', function () {
 
         element(by.id('searchButton')).click();
 
+        expect(browser.getLocationAbsUrl()).toMatch(/\/search\?q=hand-picked/);
         var productDescriptions = element.all(by.repeater('product in products').column('description'));
-        expect(productDescriptions.count()).toBe(1);
-        expect(productDescriptions.first().getText()).toMatch('hand-picked');
+        expect(productDescriptions.first().getText()).toMatch(/hand-picked/);
     });
 
 });
