f5c457422a2e44b1f984e03539c6c7052c83107f
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-10-27 07:12:04 +0100

    added scenario

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,9 +2,13 @@
 
 describe('scenarios', function () {
 
-    browser.get('/');
-
     it('should automatically redirect to /search when location hash/fragment is empty', function () {
+        browser.get('/');
+        expect(browser.getLocationAbsUrl()).toMatch("/search");
+    });
+
+    it('should automatically redirect to /search when location hash/fragment is not defined', function () {
+        browser.get('/#/undefined');
         expect(browser.getLocationAbsUrl()).toMatch("/search");
     });
 
