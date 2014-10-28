5e3895452c38f775f98c628747212eead076e41e
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2014-10-28 11:18:22 +0100

    reverted broken approach to result determination
    - parent process does not have required env-variables from sauce and travis

--- test/e2eTests.js
+++ test/e2eTests.js
@@ -2,7 +2,6 @@
 'use strict';
 
 var spawn = require('win-spawn'),
-    SauceLabs = require('saucelabs'),
     server = require('./../server.js');
 
 server.start({ port: 3000 }, function () {
@@ -17,35 +16,6 @@ server.start({ port: 3000 }, function () {
 
     protractor.on('exit', function (exitCode) {
         console.log('Protractor exited with code ' + exitCode + '.');
-        console.log(process.env.SAUCE_USERNAME);
-        console.log(process.env.TRAVIS_BUILD_NUMBER);
-        setSaucelabJobResult(exitCode);
         process.exit(exitCode);
     });
 });
-
-function setSaucelabJobResult(exitCode) {
-    if (process.env.SAUCE_USERNAME && process.env.TRAVIS_BUILD_NUMBER) {
-        var sauceLabs = new SauceLabs({ username: process.env.SAUCE_USERNAME, password: process.env.SAUCE_ACCESS_KEY });
-        sauceLabs.getJobs(function (err, jobs) {
-            console.log(jobs);
-            for (var j in jobs) {
-                if (jobs.hasOwnProperty(j)) {
-                    sauceLabs.showJob(jobs[j].id, function (err, job) {
-                        console.log(job);
-                        var tags = job.tags;
-                        console.log(tags);
-                        for (var i = 0; i < tags.length; i++) {
-                            if (tags[i] === process.env.TRAVIS_BUILD_NUMBER) {
-                                sauceLabs.updateJob(job.id, 'passed=' + exitCode === 0 ? 'true' : 'false', function (err, res) {
-                                    console.log('Marked job ' + job.id + ' for build #' + process.env.TRAVIS_BUILD_NUMBER + ' as ' + exitCode === 0 ? 'PASSED.' : 'FAILED.')
-                                    process.exit(exitCode);
-                                });
-                            }
-                        }
-                    });
-                }
-            }
-        });
-    }
-}
