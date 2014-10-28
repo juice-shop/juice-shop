929114690e0477771d56e13ac1c16c8bd47b765d
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2014-10-28 09:02:08 +0100

    put tunnel-id back in

--- protractor.conf.js
+++ protractor.conf.js
@@ -28,11 +28,12 @@ exports.config = {
 if (process.env.TRAVIS_BUILD_NUMBER) {
     exports.config.seleniumAddress = 'http://localhost:4445/wd/hub';
     exports.config.capabilities = {
-        name: 'Juice-Shop Protractor e2e Tests',
-        browserName: 'firefox',
-        username: process.env.SAUCE_USERNAME,
-        accessKey: process.env.SAUCE_ACCESS_KEY,
-        build: process.env.TRAVIS_BUILD_NUMBER,
-        tags: [process.env.TRAVIS_BRANCH, process.env.TRAVIS_BUILD_NUMBER]
+        'name': 'Juice-Shop Protractor e2e Tests',
+        'browserName': 'firefox',
+        'username': process.env.SAUCE_USERNAME,
+        'accessKey': process.env.SAUCE_ACCESS_KEY,
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+        'build': process.env.TRAVIS_BUILD_NUMBER,
+        'tags': [process.env.TRAVIS_BRANCH, process.env.TRAVIS_BUILD_NUMBER]
     }
 }
