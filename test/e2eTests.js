155d1726569ba3de76dd0337c21fdb371c5cd1a4
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2014-10-28 09:09:34 +0100

    logging job data (debug)

--- test/e2eTests.js
+++ test/e2eTests.js
@@ -27,10 +27,13 @@ server.start({ port: 3000 }, function () {
 function setSaucelabJobResult(exitCode) {
     var sauceLabs = new SauceLabs({ username: process.env.SAUCE_USERNAME, password: process.env.SAUCE_ACCESS_KEY });
     sauceLabs.getJobs(function (err, jobs) {
+        console.log(jobs);
         for (var j in jobs) {
             if (jobs.hasOwnProperty(j)) {
                 sauceLabs.showJob(jobs[j].id, function (err, job) {
+                    console.log(job);
                     var tags = job.tags;
+                    console.log(tags);
                     for (var i = 0; i < tags.length; i++) {
                         if (tags[i] === process.env.TRAVIS_BUILD_NUMBER) {
                             sauceLabs.updateJob(job.id, 'passed=' + exitCode === 0 ? 'true' : 'false', function(err, res) {
