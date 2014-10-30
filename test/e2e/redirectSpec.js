026b220277d140af78cdb7667ce7c718924feeeb
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-10-30 17:01:48 +0100

    added redirect challenge test

--- test/e2e/redirectSpec.js
+++ test/e2e/redirectSpec.js
@@ -4,19 +4,23 @@ describe('/redirect', function () {
 
     describe('challenge "redirect"', function () {
 
-        xit('should redirect to https://github.com/bkimminich/juice-shop when supplying any other target URL', function () {
-
+        it('should redirect to https://github.com/bkimminich/juice-shop when supplying any other target URL', function () {
+            browser.driver.get(browser.baseUrl + '/redirect?to=http://kimminich.de').then(function() {
+                expect(browser.driver.getCurrentUrl()).toBe('https://github.com/bkimminich/juice-shop');
+            });
         });
 
     });
 
     describe('challenge "redirect"', function () {
 
-        xit('should redirect to target URL if https://github.com/bkimminich/juice-shop is contained in it as parameter', function () {
-
+        it('should redirect to target URL if https://github.com/bkimminich/juice-shop is contained in it as parameter', function () {
+            browser.driver.get(browser.baseUrl + '/redirect?to=http://kimminich.de?trickIndexOf=https://github.com/bkimminich/juice-shop').then(function() {
+                expect(browser.driver.getCurrentUrl()).toMatch(/http:\/\/kimminich\.de/);
+            });
         });
 
-        //protractor.expect.challengeSolved({challenge: 'redirect'});
+        protractor.expect.challengeSolved({challenge: 'redirect'});
 
     });
 
