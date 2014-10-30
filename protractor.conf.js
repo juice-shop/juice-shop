3fe4ddd85b1f100cc17d5cf973e09d2a6fbf04b0
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-10-30 19:38:32 +0100

    using win 8.1 on sauce labs

--- protractor.conf.js
+++ protractor.conf.js
@@ -31,7 +31,7 @@ if (process.env.TRAVIS_BUILD_NUMBER) {
     exports.config.capabilities = {
         'name': 'Juice-Shop e2e Tests (Protractor)',
         'browserName': 'chrome',
-        'selenium-version': '2.43.0',
+        'platform': 'Windows 8.1',
         'username': process.env.SAUCE_USERNAME,
         'accessKey': process.env.SAUCE_ACCESS_KEY,
         'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
