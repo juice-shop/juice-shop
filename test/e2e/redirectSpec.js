534ed3397382f20e4ff0ba23945d02c9bbad1109
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-10-30 09:55:38 +0100

    added shells for remaining challenges

---
+++ test/e2e/redirectSpec.js
@@ -0,0 +1,23 @@
+'use strict';
+
+describe('/redirect', function () {
+
+    describe('challenge "redirect"', function () {
+
+        xit('should redirect to https://github.com/bkimminich/juice-shop when supplying any other target URL', function () {
+
+        });
+
+    });
+
+    describe('challenge "redirect"', function () {
+
+        xit('should redirect to target URL if https://github.com/bkimminich/juice-shop is contained in it as parameter', function () {
+
+        });
+
+        //protractor.expect.challengeSolved({challenge: 'redirect'});
+
+    });
+
+});
---
+++ test/e2e/restApiSpec.js
@@ -0,0 +1,25 @@
+'use strict';
+
+describe('/rest', function () {
+
+    describe('challenge "xss4"', function () {
+
+        xit('should be possible to create a new product when logged in', function () {
+
+        });
+
+        //protractor.expect.challengeSolved({challenge: 'xss4'});
+
+    });
+
+    describe('challenge "changeProduct"', function () {
+
+        xit('should be possible to change product via PUT request without being logged in', function () {
+
+        });
+
+        //protractor.expect.challengeSolved({challenge: 'changeProduct'});
+
+    });
+
+});
