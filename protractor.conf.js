08d6d8c7e764e22f1ce6e7e61c182b2f8ebd05f7
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-11-18 14:08:01 +0100

    fixed style issues

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,4 +1,7 @@
 /*jslint node: true */
+/*global jasmine*/
+'use strict';
+
 exports.config = {
     directConnect: true,
 
@@ -44,6 +47,6 @@ if (process.env.TRAVIS_BUILD_NUMBER) {
         'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
         'build': process.env.TRAVIS_BUILD_NUMBER,
         'tags': [process.env.TRAVIS_BRANCH, process.env.TRAVIS_BUILD_NUMBER]
-    }
+    };
     
-}
+};
