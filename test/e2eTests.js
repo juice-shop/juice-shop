55a2d1a52132be82c74746623c4149ff70e63bae
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-11-19 11:19:45 +0100

    retrying saucelabs result determination

--- test/e2eTests.js
+++ test/e2eTests.js
@@ -2,6 +2,7 @@
 'use strict';
 
 var spawn = require('win-spawn'),
+    SauceLabs = require('saucelabs'),
     colors = require('colors/safe'),
     server = require('./../server.js');
 
@@ -17,6 +18,33 @@ server.start({ port: 3000 }, function () {
 
     protractor.on('exit', function (exitCode) {
         console.log('Protractor exited with code ' + exitCode + ' (' + (exitCode === 0 ? colors.green('SUCCESS') : colors.red('FAILED')) + ')');
+        if (process.env.TRAVIS_BUILD_NUMBER && process.env.SAUCE_USERNAME) {
+            console.log(colors.rainbow('TRAVIS_BUILD_NUMBER' + process.env.TRAVIS_BUILD_NUMBER));
+            console.log(colors.rainbow('SAUCE_USERNAME' + process.env.SAUCE_USERNAME));
+            console.log(colors.rainbow('SAUCE_ACCESS_KEY' + process.env.SAUCE_ACCESS_KEY));
+            setSaucelabJobResult(exitCode);
+        }
         server.close(exitCode);
     });
 });
+
+function setSaucelabJobResult(exitCode) {
+    var sauceLabs = new SauceLabs({ username: process.env.SAUCE_USERNAME, password: process.env.SAUCE_ACCESS_KEY });
+    sauceLabs.getJobs(function (err, jobs) {
+        for (var j in jobs) {
+            if (jobs.hasOwnProperty(j)) {
+                sauceLabs.showJob(jobs[j].id, function (err, job) {
+                    var tags = job.tags;
+                    for (var i = 0; i < tags.length; i++) {
+                        if (tags[i] === process.env.TRAVIS_BUILD_NUMBER) {
+                            sauceLabs.updateJob(job.id, 'passed=' + exitCode === 0 ? 'true' : 'false', function(err, res) {
+                                console.log('Marked job ' + job.id + ' for build #' + process.env.TRAVIS_BUILD_NUMBER + ' as ' + (exitCode === 0 ? colors.green('PASSED') : colors.red('FAILED')) + '.');
+                                process.exit(exitCode);
+                            });
+                        }
+                    }
+                });
+            }
+        }
+    });
+}
