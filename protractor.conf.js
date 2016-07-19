55ae275f9a176425b4568b6d14d444ff903f4443
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2016-07-19 15:50:59 +0200

    Use Firefox@Win10 for Protractor on Travis-CI

--- protractor.conf.js
+++ protractor.conf.js
@@ -39,8 +39,8 @@ if (process.env.TRAVIS_BUILD_NUMBER) {
     exports.config.seleniumAddress = 'http://localhost:4445/wd/hub';
     exports.config.capabilities = {
         'name': 'Juice-Shop e2e Tests (Protractor)',
-        'browserName': 'chrome',
-        'platform': 'Windows 7',
+        'browserName': 'firefox',
+        'platform': 'Windows 10',
         'screen-resolution': '1920x1200',
         'username': process.env.SAUCE_USERNAME,
         'accessKey': process.env.SAUCE_ACCESS_KEY,
