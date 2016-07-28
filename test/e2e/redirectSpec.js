3c4eeeefd965f574b65b155b0b8c8d8ae95dd510
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2016-07-28 17:05:31 +0200

    Add PayPal donation button
    (using paypal@owasp.org earmarked for "Juice Shop")

--- test/e2e/redirectSpec.js
+++ test/e2e/redirectSpec.js
@@ -38,6 +38,12 @@ describe('/redirect', function () {
             });
         });
 
+        it('should redirect to target URL if https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=paypal%40owasp%2eorg&lc=US&item_name=Juice%20Shop&no_note=0&currency_code=USD&bn=PP%2dDonationsBF is contained in it as encoded parameter', function () {
+            browser.driver.get(browser.baseUrl + '/redirect?to=https://www.owasp.org?trickIndexOf=https%3A%2F%2Fwww.paypal.com%2Fcgi-bin%2Fwebscr%3Fcmd%3D_donations%26business%3Dpaypal%2540owasp%252eorg%26lc%3DUS%26item_name%3DJuice%2520Shop%26no_note%3D0%26currency_code%3DUSD%26bn%3DPP%252dDonationsBF').then(function() {
+                expect(browser.driver.getCurrentUrl()).toMatch(/https:\/\/www\.owasp\.org/);
+            });
+        });
+
         protractor.expect.challengeSolved({challenge: 'redirect'});
 
     });
