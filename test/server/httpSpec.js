2c339124c1dca30e172f1bdbe5c493c29bf8dff5
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2016-09-03 00:26:08 +0200

    Add tests for security HTTP headers

--- test/server/httpSpec.js
+++ test/server/httpSpec.js
@@ -5,7 +5,19 @@ var frisby = require('frisby');
 var URL = 'http://localhost:3000';
 
 frisby.create('GET response must contain CORS header allowing all origins')
-    .get(URL + "/dist/juice-shop.min.js")
+    .get(URL)
     .expectStatus(200)
     .expectHeaderContains('Access-Control-Allow-Origin', '*')
+    .toss();
+
+frisby.create('GET response must contain sameorigin frameguard header')
+    .get(URL)
+    .expectStatus(200)
+    .expectHeaderContains('X-Frame-Options', 'sameorigin')
+    .toss();
+
+frisby.create('GET response must contain nosniff content type header')
+    .get(URL)
+    .expectStatus(200)
+    .expectHeaderContains('X-Content-Type-Options', 'nosniff')
     .toss();
