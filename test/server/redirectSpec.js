3b529c7182e37d76dfc48448e1c8979243c8c0ae
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2016-09-20 18:03:14 +0200

    Test without following redirects

--- test/server/redirectSpec.js
+++ test/server/redirectSpec.js
@@ -2,109 +2,45 @@ var frisby = require('frisby')
 
 var URL = 'http://localhost:3000'
 
-frisby.create('GET availability of https://github.com/bkimminich/juice-shop before executing redirect test')
-  .get('https://github.com/bkimminich/juice-shop')
-  .after(function (err, res) {
-    if (err != null) {
-      console.log('Problem with HTTP connection (' + err + '). Skipping redirect test to https://github.com/bkimminich/juice-shop')
-    } else {
-      frisby.create('GET redirected to https://github.com/bkimminich/juice-shop when this URL is passed as "to" parameter')
-        .get(URL + '/redirect?to=https://github.com/bkimminich/juice-shop')
-        .expectStatus(res.statusCode)
-        .toss()
-    }
-  }).toss()
+frisby.create('GET redirected to https://github.com/bkimminich/juice-shop when this URL is passed as "to" parameter')
+  .get(URL + '/redirect?to=https://github.com/bkimminich/juice-shop', { followRedirect: false })
+  .expectStatus(302)
+  .toss()
 
-frisby.create('GET availability of https://gratipay.com/juice-shop before executing redirect test')
-  .get('https://gratipay.com/juice-shop')
-  .after(function (err, res) {
-    if (err != null) {
-      console.log('Problem with HTTP connection (' + err + '). Skipping redirect test to https://gratipay.com/juice-shop')
-    } else {
-      frisby.create('GET redirected to https://gratipay.com/juice-shop when this URL is passed as "to" parameter')
-        .get(URL + '/redirect?to=https://gratipay.com/juice-shop')
-        .expectStatus(res.statusCode)
-        .toss()
-    }
-  }).toss()
+frisby.create('GET redirected to https://gratipay.com/juice-shop when this URL is passed as "to" parameter')
+  .get(URL + '/redirect?to=https://gratipay.com/juice-shop', { followRedirect: false })
+  .expectStatus(302)
+  .toss()
 
-frisby.create('GET availability of https://blockchain.info/address/1FXJq5yVANLzR6ZWfqPKhJU3zWT3apnxmN before executing redirect test')
-  .get('https://blockchain.info/address/1FXJq5yVANLzR6ZWfqPKhJU3zWT3apnxmN')
-  .after(function (err, res) {
-    if (err != null) {
-      console.log('Problem with HTTP connection (' + err + '). Skipping redirect test to https://blockchain.info/address/1FXJq5yVANLzR6ZWfqPKhJU3zWT3apnxmN')
-    } else {
-      frisby.create('GET redirected to https://blockchain.info/address/1FXJq5yVANLzR6ZWfqPKhJU3zWT3apnxmN when this URL is passed as "to" parameter')
-        .get(URL + '/redirect?to=https://blockchain.info/address/1FXJq5yVANLzR6ZWfqPKhJU3zWT3apnxmN')
-        .expectStatus(res.statusCode)
-        .toss()
-    }
-  }).toss()
+frisby.create('GET redirected to https://blockchain.info/address/1FXJq5yVANLzR6ZWfqPKhJU3zWT3apnxmN when this URL is passed as "to" parameter')
+  .get(URL + '/redirect?to=https://blockchain.info/address/1FXJq5yVANLzR6ZWfqPKhJU3zWT3apnxmN', { followRedirect: false })
+  .expectStatus(302)
+  .toss()
 
-frisby.create('GET availability of http://flattr.com/thing/3856930/bkimminichjuice-shop-on-GitHub before executing redirect test')
-  .get('http://flattr.com/thing/3856930/bkimminichjuice-shop-on-GitHub')
-  .after(function (err, res) {
-    if (err != null) {
-      console.log('Problem with HTTP connection (' + err + '). Skipping redirect test to http://flattr.com/thing/3856930/bkimminichjuice-shop-on-GitHub')
-    } else {
-      frisby.create('GET redirected to http://flattr.com/thing/3856930/bkimminichjuice-shop-on-GitHub when this URL is passed as "to" parameter')
-        .get(URL + '/redirect?to=http://flattr.com/thing/3856930/bkimminichjuice-shop-on-GitHub')
-        .expectStatus(res.statusCode)
-        .toss()
-    }
-  }).toss()
+frisby.create('GET redirected to http://flattr.com/thing/3856930/bkimminichjuice-shop-on-GitHub when this URL is passed as "to" parameter')
+  .get(URL + '/redirect?to=http://flattr.com/thing/3856930/bkimminichjuice-shop-on-GitHub', { followRedirect: false })
+  .expectStatus(302)
+  .toss()
 
-frisby.create('GET availability of http://shop.spreadshirt.com/juiceshop before executing redirect test')
-  .get('http://shop.spreadshirt.com/juiceshop')
-  .after(function (err, res) {
-    if (err != null) {
-      console.log('Problem with HTTP connection (' + err + '). Skipping redirect test to http://shop.spreadshirt.com/juiceshop')
-    } else {
-      frisby.create('GET redirected to http://shop.spreadshirt.com/juiceshop when this URL is passed as "to" parameter')
-        .get(URL + '/redirect?to=http://shop.spreadshirt.com/juiceshop')
-        .expectStatus(res.statusCode)
-        .toss()
-    }
-  }).toss()
+frisby.create('GET redirected to http://shop.spreadshirt.com/juiceshop when this URL is passed as "to" parameter')
+  .get(URL + '/redirect?to=http://shop.spreadshirt.com/juiceshop', { followRedirect: false })
+  .expectStatus(302)
+  .toss()
 
-frisby.create('GET availability of http://shop.spreadshirt.de/juiceshop before executing redirect test')
-  .get('http://shop.spreadshirt.de/juiceshop')
-  .after(function (err, res) {
-    if (err != null) {
-      console.log('Problem with HTTP connection (' + err + '). Skipping redirect test to http://shop.spreadshirt.de/juiceshop')
-    } else {
-      frisby.create('GET redirected to http://shop.spreadshirt.de/juiceshop when this URL is passed as "to" parameter')
-        .get(URL + '/redirect?to=http://shop.spreadshirt.de/juiceshop')
-        .expectStatus(res.statusCode)
-        .toss()
-    }
-  }).toss()
+frisby.create('GET redirected to http://shop.spreadshirt.de/juiceshop when this URL is passed as "to" parameter')
+  .get(URL + '/redirect?to=http://shop.spreadshirt.de/juiceshop', { followRedirect: false })
+  .expectStatus(302)
+  .toss()
 
-frisby.create('GET availability of https://www.stickermule.com/user/1070702817/stickers before executing redirect test')
-  .get('https://www.stickermule.com/user/1070702817/stickers')
-  .after(function (err, res) {
-    if (err != null) {
-      console.log('Problem with HTTP connection (' + err + '). Skipping redirect test to https://www.stickermule.com/user/1070702817/stickers')
-    } else {
-      frisby.create('GET redirected to https://www.stickermule.com/user/1070702817/stickers when this URL is passed as "to" parameter')
-        .get(URL + '/redirect?to=https://www.stickermule.com/user/1070702817/stickers')
-        .expectStatus(res.statusCode)
-        .toss()
-    }
-  }).toss()
+frisby.create('GET redirected to https://www.stickermule.com/user/1070702817/stickers when this URL is passed as "to" parameter')
+  .get(URL + '/redirect?to=https://www.stickermule.com/user/1070702817/stickers', { followRedirect: false })
+  .expectStatus(302)
+  .toss()
 
-frisby.create('GET availability of https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=paypal%40owasp%2eorg&lc=US&item_name=Juice%20Shop&no_note=0&currency_code=USD&bn=PP%2dDonationsBF before executing redirect test')
-  .get('https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=paypal%40owasp%2eorg&lc=US&item_name=Juice%20Shop&no_note=0&currency_code=USD&bn=PP%2dDonationsBF')
-  .after(function (err, res) {
-    if (err != null) {
-      console.log('Problem with HTTP connection (' + err + '). Skipping redirect test to https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=paypal%40owasp%2eorg&lc=US&item_name=Juice%20Shop&no_note=0&currency_code=USD&bn=PP%2dDonationsBF')
-    } else {
-      frisby.create('GET redirected to https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=paypal%40owasp%2eorg&lc=US&item_name=Juice%20Shop&no_note=0&currency_code=USD&bn=PP%2dDonationsBF when this URL is passed as URL-encoded "to" parameter')
-        .get(URL + '/redirect?to=https%3A%2F%2Fwww.paypal.com%2Fcgi-bin%2Fwebscr%3Fcmd%3D_donations%26business%3Dpaypal%2540owasp%252eorg%26lc%3DUS%26item_name%3DJuice%2520Shop%26no_note%3D0%26currency_code%3DUSD%26bn%3DPP%252dDonationsBF')
-        .expectStatus(res.statusCode)
-        .toss()
-    }
-  }).toss()
+frisby.create('GET redirected to https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=paypal%40owasp%2eorg&lc=US&item_name=Juice%20Shop&no_note=0&currency_code=USD&bn=PP%2dDonationsBF when this URL is passed as URL-encoded "to" parameter')
+  .get(URL + '/redirect?to=https%3A%2F%2Fwww.paypal.com%2Fcgi-bin%2Fwebscr%3Fcmd%3D_donations%26business%3Dpaypal%2540owasp%252eorg%26lc%3DUS%26item_name%3DJuice%2520Shop%26no_note%3D0%26currency_code%3DUSD%26bn%3DPP%252dDonationsBF', { followRedirect: false })
+  .expectStatus(302)
+  .toss()
 
 frisby.create('GET error message with information leakage when calling /redirect without query parameter')
   .get(URL + '/redirect')
