a631070174a92d9e63cb88088ee4cd121100802e
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2016-09-29 13:37:26 +0200

    Only run SauceLabs for non-PRs and PRs onto master

--- scripts/test-on-saucelabs-with-node4.sh
+++ scripts/test-on-saucelabs-with-node4.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_NODE_VERSION" == "4" ]; then
-    if [ "$TRAVIS_PULL_REQUEST" == "false" ] || [ "$TRAVIS_BRANCH" != "develop" ] && [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    if [ "$TRAVIS_PULL_REQUEST" == "false" ] || [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
         echo "Running unit tests on SauceLabs..."
         karma start karma.conf-ci.js
         echo "Running e2e tests on SauceLabs..."
         npm run protractor
     else
-        echo "Skipping SauceLabs tests for PR #$TRAVIS_PULL_REQUEST onto develop branch (only executed for PRs onto master)"
+        echo "Skipping SauceLabs tests for PR #$TRAVIS_PULL_REQUEST onto $TRAVIS_BRANCH branch (only executed for PRs onto master)"
     fi
   else
     echo "Skipping SauceLabs tests for Node.js v$TRAVIS_NODE_VERSION (only executed on v4)"
