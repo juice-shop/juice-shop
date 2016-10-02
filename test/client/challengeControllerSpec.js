d78be5ec5f29034161a72258142fe4ff4fa602b8
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2016-10-02 06:04:37 +0200

    Fix UI test

--- test/client/challengeControllerSpec.js
+++ test/client/challengeControllerSpec.js
@@ -5,6 +5,7 @@ describe('controllers', function () {
   beforeEach(inject(function ($injector) {
     $httpBackend = $injector.get('$httpBackend')
     $httpBackend.whenGET(/\/i18n\/.*\.json/).respond(200, {})
+    $httpBackend.whenGET('/rest/continue-code').respond(200, {})
     $sce = $injector.get('$sce')
   }))
 
