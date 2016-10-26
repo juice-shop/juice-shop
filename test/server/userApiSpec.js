427238b6233e7826ad622bfa966bc510786ae4fb
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2016-10-26 20:13:32 +0200

    Solve async bug on login (confused user data)

--- test/server/userApiSpec.js
+++ test/server/userApiSpec.js
@@ -329,7 +329,7 @@ frisby.create('GET who-am-i request returns nothing on broken auth token')
   .expectJSONTypes({})
   .toss()
 
-/* frisby.create('POST OAuth login as admin@juice-sh.op with "Remember me" exploit to log in as ciso@juice-sh.op')
+frisby.create('POST OAuth login as admin@juice-sh.op with "Remember me" exploit to log in as ciso@juice-sh.op')
   .post(REST_URL + '/user/login', {
     email: 'admin@juice-sh.op',
     password: 'admin123',
@@ -341,4 +341,4 @@ frisby.create('GET who-am-i request returns nothing on broken auth token')
   .expectJSON({
     umail: 'ciso@juice-sh.op'
   })
-  .toss() */
+  .toss()
