a6cea4898147b3aa02a5793dad6b249d8540691c
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2017-01-24 00:30:20 +0100

    Add flag to challenge notification (resolves #260)

--- app/js/controllers/ChallengeSolvedNotificationController.js
+++ app/js/controllers/ChallengeSolvedNotificationController.js
@@ -14,9 +14,9 @@ angular.module('juiceShop').controller('ChallengeSolvedNotificationController', 
     socket.on('challenge solved', function (data) {
       if (data && data.challenge) {
         $translate('CHALLENGE_SOLVED', { challenge: data.challenge }).then(function (challengeSolved) {
-          $scope.notifications.push({message: challengeSolved})
+          $scope.notifications.push({message: challengeSolved, flag: data.flag})
         }, function (translationId) {
-          $scope.notifications.push({message: translationId})
+          $scope.notifications.push({message: translationId, flag: data.flag})
         })
         socket.emit('notification received', data.challenge)
       }
