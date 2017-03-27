c365c062f0044b20cbaff757aabe8ae21e1f7533
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2017-03-28 01:38:25 +0200

    Fix behavior on missing custom product images

--- data/datacreator.js
+++ data/datacreator.js
@@ -553,7 +553,7 @@ module.exports = function () {
       var description = product.description || 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. '
       var price = product.price || Math.floor(Math.random())
       var imageFileName = 'undefined.jpg'
-      if (product.imageUrl !== '') {
+      if (product.imageUrl) {
         imageFileName = makeRandomString(5) + '-' + product.imageUrl.substring(product.imageUrl.lastIndexOf('/') + 1)
         var imageFilePath = 'app/public/images/products/' + imageFileName
         utils.downloadToFile(product.imageUrl, imageFilePath)
