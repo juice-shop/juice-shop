ca780340fb79cb322796f7fa9b45d8b6011e261e
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2017-03-29 16:33:15 +0200

    Add $http mocking for config service

--- test/client/basketControllerSpec.js
+++ test/client/basketControllerSpec.js
@@ -12,6 +12,7 @@ describe('controllers', function () {
   beforeEach(inject(function ($injector) {
     $httpBackend = $injector.get('$httpBackend')
     $httpBackend.whenGET(/\/i18n\/.*\.json/).respond(200, {})
+    $httpBackend.whenGET('/rest/admin/application-configuration').respond(200)
     $sce = $injector.get('$sce')
     $uibModal = $injector.get('$uibModal')
   }))
