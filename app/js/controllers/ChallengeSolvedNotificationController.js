abed89032acbf287e8340cc73ce345ca0c00d992
Jannik Hollenbach <jannik.hollenbach@iteratec.de>
2017-04-05 16:11:31 +0200

    Added clipboard button

--- app/js/controllers/ChallengeSolvedNotificationController.js
+++ app/js/controllers/ChallengeSolvedNotificationController.js
@@ -14,9 +14,17 @@ angular.module('juiceShop').controller('ChallengeSolvedNotificationController', 
     socket.on('challenge solved', function (data) {
       if (data && data.challenge && !data.hidden) {
         $translate('CHALLENGE_SOLVED', { challenge: data.challenge }).then(function (challengeSolved) {
-          $scope.notifications.push({message: challengeSolved, flag: data.flag})
+          $scope.notifications.push({
+            message: challengeSolved,
+            flag: data.flag,
+            copyText: 'Copy to Clipboard'
+          })
         }, function (translationId) {
-          $scope.notifications.push({message: translationId, flag: data.flag})
+          $scope.notifications.push({
+              message: translationId,
+              flag: data.flag,
+              copyText: 'Copy to Clipboard'
+            })
         })
         socket.emit('notification received', data.flag)
       }
