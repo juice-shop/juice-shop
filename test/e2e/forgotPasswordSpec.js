368247683b7f3e6c8d60e3b8bbd6255a0fd08fa2
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2017-05-31 19:35:54 +0200

    Add safety against visibility issues

--- test/e2e/forgotPasswordSpec.js
+++ test/e2e/forgotPasswordSpec.js
@@ -5,6 +5,8 @@ var config = require('config')
 describe('/#/forgot-password', function () {
   var email, securityAnswer, newPassword, newPasswordRepeat, resetButton
 
+  var EC = protractor.ExpectedConditions
+
   beforeEach(function () {
     browser.get('/#/logout')
     browser.get('/#/forgot-password')
@@ -18,6 +20,7 @@ describe('/#/forgot-password', function () {
   describe('as Jim', function () {
     it('should be able to reset password with his security answer', function () {
       email.sendKeys('jim@' + config.get('application.domain'))
+      browser.wait(EC.visibilityOf(securityAnswer), 1000, 'Security answer field did not become visible')
       securityAnswer.sendKeys('Samuel')
       newPassword.sendKeys('I <3 Spock')
       newPasswordRepeat.sendKeys('I <3 Spock')
@@ -32,6 +35,7 @@ describe('/#/forgot-password', function () {
   describe('as Bender', function () {
     it('should be able to reset password with his security answer', function () {
       email.sendKeys('bender@' + config.get('application.domain'))
+      browser.wait(EC.visibilityOf(securityAnswer), 1000, 'Security answer field did not become visible')
       securityAnswer.sendKeys('Stop\'n\'Drop')
       newPassword.sendKeys('Brannigan 8=o Leela')
       newPasswordRepeat.sendKeys('Brannigan 8=o Leela')
@@ -46,6 +50,7 @@ describe('/#/forgot-password', function () {
   describe('as Bjoern', function () {
     it('should be able to reset password with his security answer', function () {
       email.sendKeys('bjoern.kimminich@googlemail.com')
+      browser.wait(EC.visibilityOf(securityAnswer), 1000, 'Security answer field did not become visible')
       securityAnswer.sendKeys('West-2082')
       newPassword.sendKeys('YmpvZXJuLmtpbW1pbmljaEBnb29nbGVtYWlsLmNvbQ==')
       newPasswordRepeat.sendKeys('YmpvZXJuLmtpbW1pbmljaEBnb29nbGVtYWlsLmNvbQ==')
