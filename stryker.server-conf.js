1de524e5fa70a317507680e247b27f0159ec3c50
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2017-06-02 00:24:01 +0200

    Add stryker-config for server side unit tests

---
+++ stryker.server-conf.js
@@ -0,0 +1,25 @@
+module.exports = function (config) {
+  'use strict'
+
+  config.set({
+    files: [
+      { pattern: 'data/*.js', mutated: true, included: false },
+      { pattern: 'lib/*.js', mutated: true, included: false },
+      { pattern: 'models/*.js', mutated: true, included: false },
+      { pattern: 'routes/*.js', mutated: true, included: false },
+      { pattern: 'package.json', included: false, mutated: false },
+      { pattern: 'node_modules/**/*.js', included: false, mutated: false },
+      'test/server/*Spec.js'
+    ],
+    testRunner: 'mocha',
+    testFramework: 'mocha',
+    coverageAnalysis: 'perTest',
+    reporter: ['html', 'progress'],
+    htmlReporter: {
+      baseDir: 'build/reports/mutation/server'
+    }
+  })
+  if (process.env.TRAVIS_BUILD_NUMBER) {
+    config.reporter = ['clear-text', 'progress']
+  }
+}
