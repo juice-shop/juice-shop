6c39620232b24a22d131128104df8af067fde9da
tpagel <timo.pagel@fhunii-media.com>
2017-06-14 15:02:43 +0100

    Implent awareness training with shakes.js(resolves #316)

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-sudo apt-get install -y nodejs
+apt-get install -y nodejs php libapache2-mod-php apache2 git
 
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
-sudo apt-get update
-sudo apt-get install -y docker-engine
+apt-get update
+apt-get install -y docker-engine
 
-docker run --restart=always -d -p 80:3000 bkimminich/juice-shop
+a2enmod proxy_http
+systemctl restart apache2
+
+docker run --restart=always -d -p 3000:3000 bkimminich/juice-shop
