2eeef5c7e6de27fd5061a9d169f1f644a7722d5d
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2017-07-06 20:03:53 +0200

    Add security question tests (partially disabled)

--- test/client/forgotpasswordControllerSpec.js
+++ test/client/forgotpasswordControllerSpec.js
@@ -64,5 +64,34 @@ describe('controllers', function () {
       expect(scope.newPasswordRepeat).toBeUndefined()
       expect(scope.error).toBe('error')
     }))
+
+    xit('should find the security question of a user with a known email address', inject(function () {
+      $httpBackend.whenGET('rest/user/security-question?email=known@user.test').respond(200, { question: 'What is your favorite test tool?' })
+      scope.email = 'known@user.test'
+
+      scope.findSecurityQuestion()
+      $httpBackend.flush()
+
+      expect(scope.securityQuestion).toBe('What is your favorite test tool?')
+    }))
+
+    xit('should not find the security question for an email address not bound to a user', inject(function () {
+      $httpBackend.whenGET('rest/user/security-question?email=unknown@user.test').respond(200, {})
+      scope.email = 'unknown@user.test'
+
+      scope.findSecurityQuestion()
+      $httpBackend.flush()
+
+      expect(scope.securityQuestion).toBeUndefined()
+    }))
+
+    it('should find not attempt to find security question for empty email address', inject(function () {
+      scope.email = undefined
+
+      scope.findSecurityQuestion()
+      $httpBackend.flush()
+
+      expect(scope.securityQuestion).toBeUndefined()
+    }))
   })
 })
