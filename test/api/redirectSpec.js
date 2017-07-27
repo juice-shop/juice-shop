8ebba658d94b83007b77dc92e8351a0792ee6353
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2017-07-27 18:15:59 +0200

    Migrate redirect tests to frisby2

---
+++ test/api/redirectSpec.js
@@ -0,0 +1,90 @@
+const frisby = require('frisby')
+
+const URL = 'http://localhost:3000'
+
+describe('/redirect', function () {
+  it('GET redirected to https://github.com/bkimminich/juice-shop when this URL is passed as "to" parameter', function (done) {
+    frisby.get(URL + '/redirect?to=https://github.com/bkimminich/juice-shop', { redirect: 'manual' })
+      .expect('status', 302)
+      .done(done)
+  })
+
+  it('GET redirected to https://gratipay.com/juice-shop when this URL is passed as "to" parameter', function (done) {
+    frisby.get(URL + '/redirect?to=https://gratipay.com/juice-shop', { redirect: 'manual' })
+      .expect('status', 302)
+      .done(done)
+  })
+
+  it('GET redirected to https://blockchain.info/address/1AbKfgvw9psQ41NbLi8kufDQTezwG8DRZm when this URL is passed as "to" parameter', function (done) {
+    frisby.get(URL + '/redirect?to=https://blockchain.info/address/1AbKfgvw9psQ41NbLi8kufDQTezwG8DRZm', { redirect: 'manual' })
+    .expect('status', 302)
+    .done(done)
+  })
+
+  it('GET redirected to http://flattr.com/thing/3856930/bkimminichjuice-shop-on-GitHub when this URL is passed as "to" parameter', function (done) {
+    frisby.get(URL + '/redirect?to=http://flattr.com/thing/3856930/bkimminichjuice-shop-on-GitHub', { redirect: 'manual' })
+    .expect('status', 302)
+    .done(done)
+  })
+
+  it('GET redirected to http://shop.spreadshirt.com/juiceshop when this URL is passed as "to" parameter', function (done) {
+    frisby.get(URL + '/redirect?to=http://shop.spreadshirt.com/juiceshop', { redirect: 'manual' })
+    .expect('status', 302)
+    .done(done)
+  })
+
+  it('GET redirected to http://shop.spreadshirt.de/juiceshop when this URL is passed as "to" parameter', function (done) {
+    frisby.get(URL + '/redirect?to=http://shop.spreadshirt.de/juiceshop', { redirect: 'manual' })
+    .expect('status', 302)
+    .done(done)
+  })
+
+  it('GET redirected to https://www.stickermule.com/user/1070702817/stickers when this URL is passed as "to" parameter', function (done) {
+    frisby.get(URL + '/redirect?to=https://www.stickermule.com/user/1070702817/stickers', { redirect: 'manual' })
+    .expect('status', 302)
+    .done(done)
+  })
+
+  it('GET redirected to https://explorer.dash.org/address/Xr556RzuwX6hg5EGpkybbv5RanJoZN17kW when this URL is passed as "to" parameter', function (done) {
+    frisby.get(URL + '/redirect?to=https://explorer.dash.org/address/Xr556RzuwX6hg5EGpkybbv5RanJoZN17kW', { redirect: 'manual' })
+    .expect('status', 302)
+    .done(done)
+  })
+
+  it('GET error message with information leakage when calling /redirect without query parameter', function (done) {
+    frisby.get(URL + '/redirect')
+    .expect('status', 500)
+    .expect('header', 'content-type', /text\/html/)
+    .expect('bodyContains', '<h1>Juice Shop (Express ~')
+    .expect('bodyContains', 'TypeError')
+    .expect('bodyContains', '&#39;indexOf&#39; of undefined')
+    .done(done)
+  })
+
+  it('GET error message with information leakage when calling /redirect with unrecognized query parameter', function (done) {
+    frisby.get(URL + '/redirect?x=y')
+    .expect('status', 500)
+    .expect('header', 'content-type', /text\/html/)
+    .expect('bodyContains', '<h1>Juice Shop (Express ~')
+    .expect('bodyContains', 'TypeError')
+    .expect('bodyContains', '&#39;indexOf&#39; of undefined')
+    .done(done)
+  })
+
+  it('GET error message hinting at whitelist validation when calling /redirect with an unrecognized "to" target', function (done) {
+    frisby.get(URL + '/redirect?to=whatever')
+    .expect('status', 406)
+    .expect('header', 'content-type', /text\/html/)
+    .expect('bodyContains', '<h1>Juice Shop (Express ~')
+    .expect('bodyContains', 'Unrecognized target URL for redirect: whatever')
+    .done(done)
+  })
+
+  it('GET redirected to target URL in "to" parameter when a white-listed URL is part of the query string', function (done) {
+    frisby.get(URL + '/redirect?to=/score-board?satisfyIndexOf=https://github.com/bkimminich/juice-shop')
+    .expect('status', 200)
+    .expect('header', 'content-type', /text\/html/)
+    .expect('bodyContains', 'dist/juice-shop.min.js')
+    .done(done)
+  })
+})
