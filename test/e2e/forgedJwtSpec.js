1a06b799a415d9f6c8eebd49589a3c2aa943b4ee
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2017-10-30 01:05:26 +0100

    Enable e2e tests for JWT challenges

--- test/e2e/forgedJwtSpec.js
+++ test/e2e/forgedJwtSpec.js
@@ -5,15 +5,15 @@ describe('/', () => {
       browser.get('/#/')
     })
 
-    // protractor.expect.challengeSolved({challenge: 'JWT Issues Tier 1'})
+    protractor.expect.challengeSolved({challenge: 'JWT Issues Tier 1'})
   })
 
   describe('challenge "jwtTier2"', () => {
-    xit('should accept a token signed with public RSA key with email rsa_lord@juice-sh.op in the payload ', () => {
+    it('should accept a token HMAC-signed with public RSA key with email rsa_lord@juice-sh.op in the payload ', () => {
       browser.manage().addCookie({ name: 'token', value: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImVtYWlsIjoicnNhX2xvcmRAanVpY2Utc2gub3AifSwiaWF0IjoxNTA4NjM5NjEyLCJleHAiOjk5OTk5OTk5OTl9.dFeqI0EGsOecwi5Eo06dFUBtW5ziRljFgMWOCYeA8yw' })
       browser.get('/#/')
     })
 
-    // protractor.expect.challengeSolved({challenge: 'JWT Issues Tier 2'})
+    protractor.expect.challengeSolved({challenge: 'JWT Issues Tier 2'})
   })
 })
