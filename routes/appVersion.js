243c91abf6f47f10d62209040a179e9b42a48099
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2017-11-02 13:33:48 +0100

    The version number can be hidden via a config option

--- routes/appVersion.js
+++ routes/appVersion.js
@@ -1,7 +1,11 @@
 const utils = require('../lib/utils')
+const config = require('config');
+
 
 exports = module.exports = function retrieveAppVersion () {
   return (req, res) => {
-    res.json({ version: utils.version() })
+    res.json({
+      version: config.get('application.showVersionNumber') ? utils.version() : ''
+    })
   }
 }
