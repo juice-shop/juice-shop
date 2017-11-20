b1d588a0b0ef0afa3751bde072950b9a081ffdba
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2017-11-20 17:15:10 +0100

    Add owasp-juice.shop subdomains for OAuth
    and remove unused manicode.us/juice-shop mapping

--- app/js/controllers/LoginController.js
+++ app/js/controllers/LoginController.js
@@ -42,14 +42,15 @@ angular.module('juiceShop').controller('LoginController', [
     var clientId = '1005568560502-6hm16lef8oh46hr2d98vf2ohlnj4nfhq.apps.googleusercontent.com'
 
     var authorizedRedirectURIs = {
+      'http://demo.owasp-juice.shop': 'http://demo.owasp-juice.shop',
       'https://juice-shop.herokuapp.com': 'https://juice-shop.herokuapp.com',
       'http://juice-shop.herokuapp.com': 'http://juice-shop.herokuapp.com',
+      'http://preview.owasp-juice.shop': 'http://preview.owasp-juice.shop',
       'https://juice-shop-staging.herokuapp.com': 'https://juice-shop-staging.herokuapp.com',
       'http://juice-shop-staging.herokuapp.com': 'http://juice-shop-staging.herokuapp.com',
       'http://localhost:3000': 'http://localhost:3000',
       'http://juice.sh': 'http://juice.sh',
-      'http://192.168.99.100:3000': 'http://tinyurl.com/ipMacLocalhost',
-      'http://manicode.us/juice-shop': 'http://manicode.us/juice-shop'
+      'http://192.168.99.100:3000': 'http://tinyurl.com/ipMacLocalhost'
     }
     var redirectUri = $location.protocol() + '://' + location.host
     $scope.oauthUnavailable = !authorizedRedirectURIs[redirectUri]
