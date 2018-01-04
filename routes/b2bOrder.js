f5c0a1c844fc64351e48aaccc5fb36e3de45829c
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2018-01-04 22:21:24 +0100

    Switch to node-serialize module (makes RCE work but timeout still does not trigger)

--- routes/b2bOrder.js
+++ routes/b2bOrder.js
@@ -1,16 +1,14 @@
 const utils = require('../lib/utils')
 const challenges = require('../data/datacache').challenges
-const serializer = require('serialize-to-js')
+const serializer = require('node-serialize')
 const vm = require('vm')
 
 exports = module.exports = function b2bOrder () {
   return (req, res) => {
     const orderLines = req.body.orderLinesData
     orderLines.forEach(orderLine => {
-      const sandbox = { serializer, orderLine }
-      vm.createContext(sandbox)
       try {
-        vm.runInContext('serializer.deserialize(orderLine)', sandbox, { timeout: 2000 })
+        vm.runInNewContext('serializer.unserialize(orderLine)', { serializer, orderLine }, { timeout: 2000 })
       } catch (err) {
         console.log(err)
         if (utils.notSolved(challenges.rceChallenge) && err.message === 'Script execution timed out.') {
