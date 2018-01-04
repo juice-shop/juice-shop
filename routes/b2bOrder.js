fb28076a21b54e1a5b56a7b40cadefe8acbfed29
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2018-01-04 23:48:32 +0100

    Use safe-eval for RCE challenge (resolves #269)

--- routes/b2bOrder.js
+++ routes/b2bOrder.js
@@ -1,21 +1,20 @@
 const utils = require('../lib/utils')
+const safeEval = require('safe-eval')
 const challenges = require('../data/datacache').challenges
-const serializer = require('node-serialize')
-const vm = require('vm')
 
 exports = module.exports = function b2bOrder () {
   return (req, res) => {
     const orderLines = req.body.orderLinesData
+    let orderConfirmation = { cid: req.body.cid }
     orderLines.forEach(orderLine => {
       try {
-        vm.runInNewContext('serializer.unserialize(orderLine)', { serializer, orderLine }, { timeout: 2000 })
+        safeEval(orderLine, {}, { timeout: 1000 })
       } catch (err) {
-        console.log(err)
         if (utils.notSolved(challenges.rceChallenge) && err.message === 'Script execution timed out.') {
           utils.solve(challenges.rceChallenge)
         }
       }
     })
-    res.end()
+    res.json(orderConfirmation)
   }
 }
