1966f7eaddefd90b8c8c8d5fdd7145cee2da8bc8
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2018-01-30 18:15:01 +0100

    Add e2e test for Blockchain Tier 1 challenge

---
+++ test/e2e/tokenSaleSpec.js
@@ -0,0 +1,10 @@
+describe('/#/tokensale-ico-ea', () => {
+  describe('challenge "tokenSale"', () => {
+    it('should be possible to access token sale section even when not authenticated', () => {
+      browser.get('/#/tokensale-ico-ea')
+      expect(browser.getCurrentUrl()).toMatch(/\/tokensale-ico-ea/)
+    })
+
+    protractor.expect.challengeSolved({challenge: 'Blockchain Tier 1'})
+  })
+})
