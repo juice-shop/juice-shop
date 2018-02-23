a944c6ec2be01c55cf4c79c4145dbc71e29fd8b3
CaptainFreak <captainfreak@localhost.localdomain>
2018-02-24 02:22:37 +0530

    Add CAPTCHA on feedback form

---
+++ models/captcha.js
@@ -0,0 +1,8 @@
+module.exports = (sequelize, {INTEGER, STRING}) => {
+  const Challenge = sequelize.define('Captcha', {
+    captchaId: INTEGER,
+    captcha: STRING,
+    answer: STRING
+  })
+  return Challenge
+}
