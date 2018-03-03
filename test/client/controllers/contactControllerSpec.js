20f8d02cef161a97b1bd1eb5e295879b08c901fc
CaptainFreak <captainfreak@localhost.localdomain>
2018-03-03 11:49:17 +0530

    fixed unit-tests for expecting captcha request

--- test/client/controllers/contactControllerSpec.js
+++ test/client/controllers/contactControllerSpec.js
@@ -22,7 +22,7 @@ describe('controllers', function () {
 
     it('should be defined', inject(function () {
       $httpBackend.whenGET('/rest/user/whoami').respond(200, {user: {}})
-
+      $httpBackend.whenGET('/rest/captcha/').respond(200, {'captchaId': 1, 'captcha': '5-1-1', 'answer': '3'})
       $httpBackend.flush()
 
       expect(controller).toBeDefined()
@@ -31,7 +31,7 @@ describe('controllers', function () {
 
     it('should hold the user id of the currently logged in user', inject(function () {
       $httpBackend.whenGET('/rest/user/whoami').respond(200, {user: {id: 42}})
-
+      $httpBackend.whenGET('/rest/captcha/').respond(200, {'captchaId': 1, 'captcha': '5-1-1', 'answer': '3'})
       $httpBackend.flush()
 
       expect(scope.feedback.UserId).toBe(42)
@@ -39,7 +39,7 @@ describe('controllers', function () {
 
     it('should hold no user id if current user is not logged in', inject(function () {
       $httpBackend.whenGET('/rest/user/whoami').respond(200, {user: {}})
-
+      $httpBackend.whenGET('/rest/captcha/').respond(200, {'captchaId': 1, 'captcha': '5-1-1', 'answer': '3'})
       $httpBackend.flush()
 
       expect(scope.feedback.UserId).toBeUndefined()
@@ -47,7 +47,7 @@ describe('controllers', function () {
 
     it('should miss feedback object if retrieving currently logged in user fails', inject(function () {
       $httpBackend.whenGET('/rest/user/whoami').respond(500)
-
+      $httpBackend.whenGET('/rest/captcha/').respond(500, {})
       $httpBackend.flush()
 
       expect(scope.feedback).toBeUndefined()
@@ -55,7 +55,7 @@ describe('controllers', function () {
 
     it('should hold the user email of the currently logged in user', inject(function () {
       $httpBackend.whenGET('/rest/user/whoami').respond(200, {user: {email: 'x@x.xx'}})
-
+      $httpBackend.whenGET('/rest/captcha/').respond(200, {'captchaId': 1, 'captcha': '5-1-1', 'answer': '3'})
       $httpBackend.flush()
 
       expect(scope.userEmail).toBe('x@x.xx')
@@ -63,7 +63,7 @@ describe('controllers', function () {
 
     it('should hold anonymous placeholder for email if current user is not logged in', inject(function () {
       $httpBackend.whenGET('/rest/user/whoami').respond(200, {user: {}})
-
+      $httpBackend.whenGET('/rest/captcha/').respond(200, {'captchaId': 1, 'captcha': '5-1-1', 'answer': '3'})
       $httpBackend.flush()
 
       expect(scope.userEmail).toBe('anonymous')
@@ -71,7 +71,7 @@ describe('controllers', function () {
 
     it('should display thank-you message and reset feedback form on saving feedback', inject(function () {
       $httpBackend.whenGET('/rest/user/whoami').respond(200, {user: {}})
-
+      $httpBackend.whenGET('/rest/captcha/').respond(200, {'captchaId': 1, 'captcha': '5-1-1', 'answer': '3'})
       $httpBackend.whenPOST('/api/Feedbacks/').respond(200, {data: {comment: 'Test', rating: 4}})
       scope.feedback = {comment: 'Test', rating: 4}
       scope.form = {$setPristine: function () {}}
@@ -85,7 +85,7 @@ describe('controllers', function () {
 
     it('should display 5-star thank-you message and reset feedback form on saving 5-star feedback', inject(function () {
       $httpBackend.whenGET('/rest/user/whoami').respond(200, {user: {}})
-
+      $httpBackend.whenGET('/rest/captcha/').respond(200, {'captchaId': 1, 'captcha': '5-1-1', 'answer': '3'})
       $httpBackend.whenPOST('/api/Feedbacks/').respond(200, {data: {comment: 'Praise', rating: 5}})
       scope.feedback = {comment: 'Praise', rating: 5}
       scope.form = {$setPristine: function () {}}
