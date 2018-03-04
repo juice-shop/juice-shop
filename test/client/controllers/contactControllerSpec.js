08de26da16ca13406f5d6ac5916f5931665cf127
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2018-03-04 14:46:48 +0100

    Reduce HTTP mock duplication for captcha retrieval

--- test/client/controllers/contactControllerSpec.js
+++ test/client/controllers/contactControllerSpec.js
@@ -5,6 +5,7 @@ describe('controllers', function () {
   beforeEach(inject(function ($injector) {
     $httpBackend = $injector.get('$httpBackend')
     $httpBackend.whenGET(/\/i18n\/.*\.json/).respond(200, {})
+    $httpBackend.whenGET('/rest/captcha/').respond(200, {'captchaId': 1, 'captcha': '5-1-1', 'answer': '3'})
   }))
 
   afterEach(function () {
@@ -22,7 +23,6 @@ describe('controllers', function () {
 
     it('should be defined', inject(function () {
       $httpBackend.whenGET('/rest/user/whoami').respond(200, {user: {}})
-      $httpBackend.whenGET('/rest/captcha/').respond(200, {'captchaId': 1, 'captcha': '5-1-1', 'answer': '3'})
       $httpBackend.flush()
 
       expect(controller).toBeDefined()
@@ -31,7 +31,6 @@ describe('controllers', function () {
 
     it('should hold the user id of the currently logged in user', inject(function () {
       $httpBackend.whenGET('/rest/user/whoami').respond(200, {user: {id: 42}})
-      $httpBackend.whenGET('/rest/captcha/').respond(200, {'captchaId': 1, 'captcha': '5-1-1', 'answer': '3'})
       $httpBackend.flush()
 
       expect(scope.feedback.UserId).toBe(42)
@@ -39,7 +38,6 @@ describe('controllers', function () {
 
     it('should hold no user id if current user is not logged in', inject(function () {
       $httpBackend.whenGET('/rest/user/whoami').respond(200, {user: {}})
-      $httpBackend.whenGET('/rest/captcha/').respond(200, {'captchaId': 1, 'captcha': '5-1-1', 'answer': '3'})
       $httpBackend.flush()
 
       expect(scope.feedback.UserId).toBeUndefined()
@@ -47,7 +45,6 @@ describe('controllers', function () {
 
     it('should miss feedback object if retrieving currently logged in user fails', inject(function () {
       $httpBackend.whenGET('/rest/user/whoami').respond(500)
-      $httpBackend.whenGET('/rest/captcha/').respond(500, {})
       $httpBackend.flush()
 
       expect(scope.feedback).toBeUndefined()
@@ -55,7 +52,6 @@ describe('controllers', function () {
 
     it('should hold the user email of the currently logged in user', inject(function () {
       $httpBackend.whenGET('/rest/user/whoami').respond(200, {user: {email: 'x@x.xx'}})
-      $httpBackend.whenGET('/rest/captcha/').respond(200, {'captchaId': 1, 'captcha': '5-1-1', 'answer': '3'})
       $httpBackend.flush()
 
       expect(scope.userEmail).toBe('x@x.xx')
@@ -63,7 +59,6 @@ describe('controllers', function () {
 
     it('should hold anonymous placeholder for email if current user is not logged in', inject(function () {
       $httpBackend.whenGET('/rest/user/whoami').respond(200, {user: {}})
-      $httpBackend.whenGET('/rest/captcha/').respond(200, {'captchaId': 1, 'captcha': '5-1-1', 'answer': '3'})
       $httpBackend.flush()
 
       expect(scope.userEmail).toBe('anonymous')
@@ -71,7 +66,6 @@ describe('controllers', function () {
 
     it('should display thank-you message and reset feedback form on saving feedback', inject(function () {
       $httpBackend.whenGET('/rest/user/whoami').respond(200, {user: {}})
-      $httpBackend.whenGET('/rest/captcha/').respond(200, {'captchaId': 1, 'captcha': '5-1-1', 'answer': '3'})
       $httpBackend.whenPOST('/api/Feedbacks/').respond(200, {data: {comment: 'Test', rating: 4}})
       scope.feedback = {comment: 'Test', rating: 4}
       scope.form = {$setPristine: function () {}}
@@ -85,7 +79,6 @@ describe('controllers', function () {
 
     it('should display 5-star thank-you message and reset feedback form on saving 5-star feedback', inject(function () {
       $httpBackend.whenGET('/rest/user/whoami').respond(200, {user: {}})
-      $httpBackend.whenGET('/rest/captcha/').respond(200, {'captchaId': 1, 'captcha': '5-1-1', 'answer': '3'})
       $httpBackend.whenPOST('/api/Feedbacks/').respond(200, {data: {comment: 'Praise', rating: 5}})
       scope.feedback = {comment: 'Praise', rating: 5}
       scope.form = {$setPristine: function () {}}
