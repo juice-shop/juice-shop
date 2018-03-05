c0a40eb77481731d0583b44e973f605c043f653f
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2018-03-05 21:40:26 +0100

    Make cookie consent popup fully customizable

--- server.js
+++ server.js
@@ -302,6 +302,9 @@ function populateIndexTemplate () {
     if (config.get('application.theme')) {
       replaceTheme()
     }
+    if (config.get('application.cookieConsent')) {
+      replaceCookieConsent()
+    }
   })
 }
 
@@ -343,6 +346,34 @@ function replaceTheme () {
     silent: true
   })
 }
+
+function replaceCookieConsent () {
+  const popupProperty = '"popup": { "background": "' + config.get('application.cookieConsent.backgroundColor') + '", "text": "' + config.get('application.cookieConsent.textColor') + '" }'
+  replace({
+    regex: /"popup": { "background": ".*", "text": ".*" }/,
+    replacement: popupProperty,
+    paths: ['app/index.html'],
+    recursive: false,
+    silent: true
+  })
+  const buttonProperty = '"button": { "background": "' + config.get('application.cookieConsent.buttonColor') + '", "text": "' + config.get('application.cookieConsent.buttonTextColor') + '" }'
+  replace({
+    regex: /"button": { "background": ".*", "text": ".*" }/,
+    replacement: buttonProperty,
+    paths: ['app/index.html'],
+    recursive: false,
+    silent: true
+  })
+  const contentProperty = '"content": { "message": "'+ config.get('application.cookieConsent.message') +'", "dismiss": "'+ config.get('application.cookieConsent.dismissText') +'", "link": "'+ config.get('application.cookieConsent.linkText') +'", "href": "'+ config.get('application.cookieConsent.linkUrl') +'" }'
+  replace({
+    regex: /"content": { "message": ".*", "dismiss": ".*", "link": ".*", "href": ".*" }/,
+    replacement: contentProperty,
+    paths: ['app/index.html'],
+    recursive: false,
+    silent: true
+  })
+}
+
 
 function replaceImagePath (overlay) {
   replace({
