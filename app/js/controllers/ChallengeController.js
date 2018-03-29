341dc88e9597c1ab07e0b09b82c82649734ad9a9
Aashish683 <aashishsingh2907@gmail.com>
2018-03-29 13:42:02 +0530

    Corrected glitch and added test

--- app/js/controllers/ChallengeController.js
+++ app/js/controllers/ChallengeController.js
@@ -12,6 +12,7 @@ angular.module('juiceShop').controller('ChallengeController', [
     'use strict'
 
     $scope.scoreBoardTablesExpanded = $window.localStorage.scoreBoardTablesExpanded ? JSON.parse($window.localStorage.scoreBoardTablesExpanded) : [null, true, false, false, false, false, false]
+    $scope.offsetValue = ['100%', '100%', '100%', '100%', '100%', '100%']
 
     $scope.toggleDifficulty = function () {
       $window.localStorage.scoreBoardTablesExpanded = JSON.stringify($scope.scoreBoardTablesExpanded)
@@ -57,16 +58,25 @@ angular.module('juiceShop').controller('ChallengeController', [
       }
     }
 
-    $scope.getOffset = function (challenges, difficulty) {
-      var solved = $scope.challenges.filter(function (challenge) { return challenge.difficulty === difficulty && challenge.solved }).length
-      var total = $scope.challenges.filter(function (challenge) { return challenge.difficulty === difficulty }).length
-      var offset = Math.round(solved * 100 / total)
-      offset = 100 - offset
-      if (offset === 0) {
-        challenges[difficulty].levelComplete = true
+    $scope.setOffset = function (challenges) {
+      for (var difficulty = 1; difficulty <= 6; difficulty++) {
+        var solved = 0
+        var total = 0
+
+        for (var i = 0; i < challenges.length; i++) {
+          if (challenges[i].difficulty === difficulty) {
+            total++
+            if (challenges[i].solved) {
+              solved++
+            }
+          }
+        }
+
+        var offset = Math.round(solved * 100 / total)
+        offset = 100 - offset
+        offset = +offset + '%'
+        $scope.offsetValue[difficulty - 1] = offset
       }
-      offset = +offset + '%'
-      return offset
     }
 
     challengeService.find().then(function (challenges) {
@@ -82,6 +92,7 @@ angular.module('juiceShop').controller('ChallengeController', [
       }
       $scope.trustDescriptionHtml()
       $scope.calculateProgressPercentage()
+      $scope.setOffset(challenges)
     }).catch(function (err) {
       console.log(err)
     })
@@ -95,6 +106,7 @@ angular.module('juiceShop').controller('ChallengeController', [
           }
         }
         $scope.calculateProgressPercentage()
+        $scope.setOffset($scope.challenges)
       }
     })
   }])
