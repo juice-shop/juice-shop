e9676e5a58590462ac69a2e29e6e6d4c2147ba22
CaptainFreak <patelshoeb4@gmail.com>
2018-04-06 08:02:33 +0530

    Add reflected XSS challenge

--- routes/trackOrder.js
+++ routes/trackOrder.js
@@ -1,6 +1,13 @@
+const utils = require('../lib/utils')
+const challenges = require('../data/datacache').challenges
+
 module.exports = function trackOrder () {
   return (req, res) => {
-    // TODO tracking functionality
-    res.send('Tracking info. Coming Soon.')
+    if (utils.notSolved(challenges.reflectedXssChallenge) && utils.contains(req.query.id, '<img src=x onerror=alert("XSS")>')) {
+      utils.solve(challenges.reflectedXssChallenge)
+    }
+    res.json({'id': req.query.id,
+      'more': 'Coming Soon.'
+    })
   }
 }
