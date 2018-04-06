9769420adc9392650c8de176aa1a82b883a40470
CaptainFreak <patelshoeb4@gmail.com>
2018-04-06 11:00:39 +0530

    Changes for xss challenge

--- routes/trackOrder.js
+++ routes/trackOrder.js
@@ -3,7 +3,7 @@ const challenges = require('../data/datacache').challenges
 
 module.exports = function trackOrder () {
   return (req, res) => {
-    if (utils.notSolved(challenges.reflectedXssChallenge) && utils.contains(req.query.id, '<img src=x onerror=alert("XSS")>')) {
+    if (utils.notSolved(challenges.reflectedXssChallenge) && utils.contains(req.query.id, '<script>alert("XSS")</script>')) {
       utils.solve(challenges.reflectedXssChallenge)
     }
     res.json({'id': req.query.id,
