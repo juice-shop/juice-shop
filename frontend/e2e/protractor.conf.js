7d35d7f4a0530bceb16a032750f1cb8741a97ee4
Aashish683 <aashishsingh2907@gmail.com>
2018-05-22 19:42:50 +0530

    fix standard styling

--- frontend/e2e/protractor.conf.js
+++ frontend/e2e/protractor.conf.js
@@ -1,7 +1,7 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const { SpecReporter } = require('jasmine-spec-reporter');
+const { SpecReporter } = require('jasmine-spec-reporter')
 
 exports.config = {
   allScriptsTimeout: 11000,
@@ -17,12 +17,12 @@ exports.config = {
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print: function() {}
+    print: function () {}
   },
-  onPrepare() {
+  onPrepare () {
     require('ts-node').register({
       project: require('path').join(__dirname, './tsconfig.e2e.json')
-    });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    })
+    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }))
   }
-};
+}
