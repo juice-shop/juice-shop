7ee9281869b9fa517d33e1326320ad6045ad2344
Aashish683 <aashishsingh2907@gmail.com>
2018-05-26 18:54:40 +0530

    Minor fix

--- app/js/controllers/LoginController.js
+++ app/js/controllers/LoginController.js
@@ -18,7 +18,6 @@ angular.module('juiceShop').controller('LoginController', [
     }
 
     $scope.login = function () {
-      console.log($scope.user)
       userService.login($scope.user).then(function (authentication) {
         $cookies.put('token', authentication.token)
         $window.sessionStorage.bid = authentication.bid
