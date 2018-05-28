81bcba005fcddfb18dd555d4446805df6b4aef8f
Aashish683 <aashishsingh2907@gmail.com>
2018-05-29 02:15:18 +0530

    Removed redundant comments

--- frontend/src/app/Services/feedback.service.ts
+++ frontend/src/app/Services/feedback.service.ts
@@ -8,40 +8,6 @@ import { map, catchError } from 'rxjs/operators';
 })
 export class FeedbackService {
 
-  /*var host = '/api/Feedbacks'
-
-  function find (params) {
-    var feedbacks = $q.defer()
-    $http.get(host + '/', {
-      params: params
-    }).then(function (response) {
-      feedbacks.resolve(response.data.data)
-    }).catch(function (response) {
-      feedbacks.reject(response.data)
-    })
-    return feedbacks.promise
-  }
-
-  function save (params) {
-    var createdFeedback = $q.defer()
-    $http.post(host + '/', params).then(function (response) {
-      createdFeedback.resolve(response.data.data)
-    }).catch(function (response) {
-      createdFeedback.reject(response.data)
-    })
-    return createdFeedback.promise
-  }
-
-  function del (id) {
-    var deletedFeedback = $q.defer()
-    $http.delete(host + '/' + id).then(function (response) {
-      deletedFeedback.resolve(response.data.data)
-    }).catch(function (response) {
-      deletedFeedback.reject(response.data)
-    })
-    return deletedFeedback.promise
-  }*/
-
   private hostServer = environment.hostServer;
   private host = this.hostServer + '/api/Feedbacks';
 
