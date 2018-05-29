4921b875da69191f4341d96495f0177ae26f4b2e
CaptainFreak <patelshoeb4@gmail.com>
2018-05-29 20:24:15 +0530

    Fixed test

--- test/e2e/impersonationSpec.js
+++ test/e2e/impersonationSpec.js
@@ -1,5 +1,5 @@
 const config = require('config')
-const http = require('http');
+const http = require('http')
 
 const REST_URL = 'http://localhost:3000/rest'
 
@@ -8,26 +8,31 @@ describe('/#/search', () => {
     browser.get('/#/search')
   })
 
+  protractor.beforeEach.login({ email: 'bender@' + config.get('application.domain'), password: 'OhG0dPlease1nsertLiquor!' })
+
   describe('challenge "User Impersonation"', () => {
-    fit('should be possible to edit any review', () => {
-      browser.waitForAngularEnabled(false)
-      let reviewId
+    let reviewId
 
+    beforeAll((done) => {
       http.get(REST_URL + '/product/1/reviews', (res) => {
-      let body = ''
+        let body = ''
 
-      res.on('data', chunk => {
-        body += chunk
-      })
+        res.on('data', chunk => {
+          body += chunk
+        })
 
-      res.on('end', () => {
-        const response = JSON.parse(body)
-        reviewId = response.data[0]._id
+        res.on('end', () => {
+          const response = JSON.parse(body)
+          reviewId = response.data[0]._id
+          done()
+        })
       })
     })
 
-      browser.executeScript('var $http = angular.element(document.body).injector().get(\'$http\'); $http.patch(\'/rest/product/reviews\', { "id": \"'+reviewId+'\", "message": "injected" });')
-      browser.driver.sleep(1000)
+    it('should be possible to edit any review', () => {
+      browser.waitForAngularEnabled(false)
+      browser.executeScript('var $http = angular.element(document.body).injector().get(\'$http\'); $http.patch(\'/rest/product/reviews\', { "id": "' + reviewId + '", "message": "injected" });')
+      browser.driver.sleep(5000)
       browser.waitForAngularEnabled(true)
     })
     protractor.expect.challengeSolved({ challenge: 'User Impersonation' })
