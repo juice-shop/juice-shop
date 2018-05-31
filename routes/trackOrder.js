68005b2c2bd1765d934af00be063356b7c9f3af1
CaptainFreak <patelshoeb4@gmail.com>
2018-05-31 07:44:06 +0530

    fixes for xss0

--- routes/trackOrder.js
+++ routes/trackOrder.js
@@ -4,10 +4,10 @@ const db = require('../data/mongodb')
 
 module.exports = function trackOrder () {
   return (req, res) => {
+    req.params.id = decodeURIComponent(req.params.id)
     if (utils.notSolved(challenges.reflectedXssChallenge) && utils.contains(req.params.id, '<script>alert("XSS")</script>')) {
       utils.solve(challenges.reflectedXssChallenge)
     }
-    req.params.id = decodeURIComponent(req.params.id)
     db.orders.find({ $where: "this.orderId === '" + req.params.id + "'" }).then(order => {
       const result = utils.queryResultToJson(order)
       if (utils.notSolved(challenges.noSqlOrdersChallenge) && result.data.length > 1) {
