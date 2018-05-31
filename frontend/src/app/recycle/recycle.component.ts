9771f2cae989ea1d26db6b597576a27abb46d1b2
Aashish683 <aashishsingh2907@gmail.com>
2018-06-01 01:25:21 +0530

    Prepared recycle component and recycle service

---
+++ frontend/src/app/recycle/recycle.component.ts
@@ -0,0 +1,75 @@
+import { ConfigurationService } from './../Services/configuration.service'
+import { UserService } from './../Services/user.service'
+import { RecycleService } from './../Services/recycle.service'
+import { Component, OnInit } from '@angular/core'
+import { FormControl,Validators } from '@angular/forms'
+import fontawesome from '@fortawesome/fontawesome'
+import { faPaperPlane } from '@fortawesome/fontawesome-free-solid'
+fontawesome.library.add(faPaperPlane)
+
+@Component({
+  selector: 'app-recycle',
+  templateUrl: './recycle.component.html',
+  styleUrls: ['./recycle.component.css']
+})
+export class RecycleComponent implements OnInit {
+
+  public requestorControl: FormControl = new FormControl({ value: '', disabled: true }, [])
+  public recycleAddressControl: FormControl = new FormControl('',[Validators.required,Validators.minLength(20),Validators.maxLength(180)])
+  public recycleQuantityControl: FormControl = new FormControl('',[Validators.required,Validators.min(10),Validators.max(1000)])
+  public topImage: string
+  public bottomImage: string
+  public recycles: any
+  public recycle: any = {}
+  public userEmail: any
+  public confirmation: any
+
+  constructor (private recycleService: RecycleService, private userService: UserService, private configurationService: ConfigurationService) { }
+
+  ngOnInit () {
+
+    this.configurationService.getApplicationConfiguration().subscribe((config: any) => {
+      if (config && config.application && config.application.recyclePage) {
+        this.topImage = 'assets/public/images/products/' + config.application.recyclePage.topProductImage
+        this.bottomImage = 'assets/public/images/products/' + config.application.recyclePage.bottomProductImage
+      }
+    },(err) => console.log(err))
+
+    this.initRecycle()
+  }
+
+  initRecycle () {
+    this.userService.whoAmI().subscribe((data) => {
+      this.recycle = {}
+      this.recycle.UserId = data.id
+      this.userEmail = data.email
+      this.requestorControl.setValue(this.userEmail)
+    },(err) => console.log(err))
+  }
+
+  save () {
+    this.recycleService.save(this.recycle).subscribe((savedRecycle: any) => {
+      this.confirmation = 'Thank you for using our recycling service. We will ' + (savedRecycle.isPickup ? ('pick up your pomace on ' + savedRecycle.pickupDate) : 'deliver your recycle box asap') + '.'
+      this.initRecycle()
+      this.resetForm()
+    },(err) => console.log(err))
+  }
+
+  findAll () {
+    this.recycleService.find(null).subscribe((recycles) => {
+      this.recycles = recycles
+    }, (error) => {
+      console.log(error)
+    })
+  }
+
+  resetForm () {
+    this.recycleAddressControl.setValue('')
+    this.recycleAddressControl.markAsPristine()
+    this.recycleAddressControl.markAsUntouched()
+    this.recycleQuantityControl.setValue('')
+    this.recycleQuantityControl.markAsPristine()
+    this.recycleQuantityControl.markAsUntouched()
+  }
+
+}
