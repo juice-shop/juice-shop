da32dc03699b2cfc4e3cb91bc8a74df6221db6c6
Aashish683 <aashishsingh2907@gmail.com>
2018-06-08 06:34:06 +0530

    Set up Score Board Component

--- frontend/src/app/Services/challenge.service.spec.ts
+++ frontend/src/app/Services/challenge.service.spec.ts
@@ -1,15 +1,19 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http'
+import { TestBed, inject } from '@angular/core/testing'
 
-import { ChallengeService } from './challenge.service';
+import { ChallengeService } from './challenge.service'
 
 describe('ChallengeService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports : [
+        HttpClientModule
+      ],
       providers: [ChallengeService]
-    });
-  });
+    })
+  })
 
   it('should be created', inject([ChallengeService], (service: ChallengeService) => {
-    expect(service).toBeTruthy();
-  }));
-});
+    expect(service).toBeTruthy()
+  }))
+})
