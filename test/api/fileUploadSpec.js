37882fb051c627b2f57889bd4df07ec102010282
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2018-06-14 11:27:12 +0200

    Re-enable billion laughs attack test

--- test/api/fileUploadSpec.js
+++ test/api/fileUploadSpec.js
@@ -79,7 +79,7 @@ describe('/file-upload', () => {
       .done(done)
   })
 
-  xit('POST file type XML with Billion Laughs attack is caught by parser', done => {
+  it('POST file type XML with Billion Laughs attack is caught by parser', done => {
     file = path.resolve(__dirname, '../files/xxeBillionLaughs.xml')
     form = new FormData()
     form.append('file', fs.createReadStream(file))
