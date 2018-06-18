a512dcb9cee773d5880f090ccedf67d14d191557
Aashish683 <aashishsingh2907@gmail.com>
2018-06-18 16:10:36 +0530

    Added catchError blocks and tests for product service

--- frontend/src/app/Services/product.service.ts
+++ frontend/src/app/Services/product.service.ts
@@ -1,7 +1,7 @@
 import { environment } from './../../environments/environment'
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
-import { map } from 'rxjs/operators'
+import { map, catchError } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -13,16 +13,16 @@ export class ProductService {
   constructor (private http: HttpClient) { }
 
   search (criteria) {
-    return this.http.get(this.hostServer + '/rest/product/search?q=' + criteria).pipe(map((response: any) => response.data))
+    return this.http.get(this.hostServer + '/rest/product/search?q=' + criteria).pipe(map((response: any) => response.data), catchError((err) => { throw err }))
   }
 
   find (params) {
-    return this.http.get(this.host + '/', { params: params }).pipe(map((response: any) => response.data))
+    return this.http.get(this.host + '/', { params: params }).pipe(map((response: any) => response.data), catchError((err) => { throw err }))
   }
 
   get (id) {
     return this.http.get(this.host + '/' + id + '?d=' + encodeURIComponent(new Date().toDateString())).pipe(map((response: any) =>
-    response.data))
+    response.data), catchError((err) => { throw err }))
   }
 
 }
