48b5b8cc88c91797191cc3450f22ab871f6af63c
Aashish683 <aashishsingh2907@gmail.com>
2018-06-18 17:50:26 +0530

    Added tests for security answer service and renabled tests

--- frontend/src/app/Services/security-answer.service.spec.ts
+++ frontend/src/app/Services/security-answer.service.spec.ts
@@ -1,5 +1,5 @@
-import { HttpClientModule } from '@angular/common/http'
-import { TestBed, inject } from '@angular/core/testing'
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
+import { TestBed, inject, fakeAsync, tick } from '@angular/core/testing'
 
 import { SecurityAnswerService } from './security-answer.service'
 
@@ -7,7 +7,7 @@ describe('SecurityAnswerService', () => {
   beforeEach(() => {
 
     TestBed.configureTestingModule({
-      imports: [HttpClientModule],
+      imports: [HttpClientTestingModule],
       providers: [SecurityAnswerService]
     })
   })
@@ -15,4 +15,19 @@ describe('SecurityAnswerService', () => {
   it('should be created', inject([SecurityAnswerService], (service: SecurityAnswerService) => {
     expect(service).toBeTruthy()
   }))
+
+  it('should create feedback directly via the rest api', inject([SecurityAnswerService, HttpTestingController],
+    fakeAsync((service: SecurityAnswerService, httpMock: HttpTestingController) => {
+      let res
+      service.save(null).subscribe((data) => res = data)
+      const req = httpMock.expectOne('http://localhost:3000/api/SecurityAnswers/')
+      req.flush({ data: 'apiResponse' })
+
+      tick()
+      expect(req.request.method).toBe('POST')
+      expect(req.request.body).toBeFalsy()
+      expect(res).toBe('apiResponse')
+      httpMock.verify()
+    })
+  ))
 })
