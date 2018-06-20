d5126d40a71e5041a46c01bf3bc221ddaff3ac0b
CaptainFreak <patelshoeb4@gmail.com>
2018-06-20 08:18:39 +0530

    Basket Access Tier 2 challenge

--- routes/basketItems.js
+++ routes/basketItems.js
@@ -1,48 +1,61 @@
-const db = require('../data/mongodb')
 const utils = require('../lib/utils')
 const challenges = require('../data/datacache').challenges
 const insecurity = require('../lib/insecurity')
 const models = require('../models/index')
-const clarinet = require('clarinet');
+const clarinet = require('clarinet')
 
 module.exports = function addBasketItem () {
   return (req, res, next) => {
-    const parser = clarinet.parser();
-    const result = [];
+    const parser = clarinet.parser()
+    const result = []
     parser.onkey = parser.onopenobject = k => {
-        result.push({key: k, value: null});
-    };
+      result.push({key: k, value: null})
+    }
     parser.onvalue = v => {
-        result[result.length - 1].value = v;
-    };
-    parser.write(req.rawBody).close();
+      result[result.length - 1].value = v
+    }
+    parser.write(req.rawBody).close()
 
     var productIds = []
     var basketIds = []
     var quantities = []
 
-    for (var i = 0 ; i < result.length ; i++) {
-      if(result[i].key == 'ProductId') {
+    for (var i = 0; i < result.length; i++) {
+      if (result[i].key === 'ProductId') {
         productIds.push(result[i].value)
-      } else if (result[i].key == 'BasketId') {
+      } else if (result[i].key === 'BasketId') {
         basketIds.push(result[i].value)
-      } else if (result[i].key == 'quantity') {
+      } else if (result[i].key === 'quantity') {
         quantities.push(result[i].value)
       }
     }
 
-    const basketItem = {
-      ProductId: productIds[productIds.length - 1],
-      BasketId: basketIds[basketIds.length - 1],
-      quantity: quantities[quantities.length -1]
-    }
-    const basketItemInstance = models.BasketItem.build(basketItem)
-    basketItemInstance.save().then((basketItem) => {
-      basketItem = {
-        status: 'success',
-        data: basketItem
+    const user = insecurity.authenticatedUsers.from(req)
+    if (user && basketIds[0] && basketIds[0] !== 'undefined' && user.bid != basketIds[0]) { // eslint-disable-line eqeqeq
+      res.status(401).send('{\'error\' : \'Invalid BasketId\'}')
+    } else {
+      const basketItem = {
+        ProductId: productIds[productIds.length - 1],
+        BasketId: basketIds[basketIds.length - 1],
+        quantity: quantities[quantities.length - 1]
       }
-      res.json(basketItem)
-    })
+
+      if (utils.notSolved(challenges.basketManipulateChallenge)) {
+        if (user && basketItem.BasketId && basketItem.BasketId !== 'undefined' && user.bid != basketItem.BasketId) { // eslint-disable-line eqeqeq
+          utils.solve(challenges.basketManipulateChallenge)
+        }
+      }
+
+      const basketItemInstance = models.BasketItem.build(basketItem)
+      basketItemInstance.save().then((basketItem) => {
+        basketItem = {
+          status: 'success',
+          data: basketItem
+        }
+        res.json(basketItem)
+      }).catch(error => {
+        next(error)
+      })
+    }
   }
 }
