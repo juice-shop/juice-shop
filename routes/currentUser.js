cb994790942dd042edfee0de1c96d37003fef28c
CaptainFreak <patelshoeb4@gmail.com>
2018-06-26 02:33:42 +0530

    jsonp challenge

--- routes/currentUser.js
+++ routes/currentUser.js
@@ -1,8 +1,18 @@
 const insecurity = require('../lib/insecurity')
+const utils = require('../lib/utils')
+const cache = require('../data/datacache')
+const challenges = cache.challenges
 
 module.exports = function retrieveLoggedInUser () {
   return (req, res) => {
-    const user = insecurity.authenticatedUsers.from(req)
-    res.json({user: {id: (user && user.data ? user.data.id : undefined), email: (user && user.data ? user.data.email : undefined)}})
+    const user = insecurity.authenticatedUsers.get(req.cookies.token)
+    if (req.query.callback === undefined) {
+      res.json({user: {id: (user && user.data ? user.data.id : undefined), email: (user && user.data ? user.data.email : undefined)}})
+    } else {
+      if (utils.notSolved(challenges.emailLeakChallenge)) {
+        utils.solve(challenges.emailLeakChallenge)
+      }
+      res.jsonp({user: {id: (user && user.data ? user.data.id : undefined), email: (user && user.data ? user.data.email : undefined)}})
+    }
   }
 }
