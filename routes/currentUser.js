76c7f1d9c6155c1cd1effa0ecf1773a66fc5dace
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2018-06-29 18:24:51 +0200

    Extracted response object into variable

--- routes/currentUser.js
+++ routes/currentUser.js
@@ -6,13 +6,14 @@ const challenges = cache.challenges
 module.exports = function retrieveLoggedInUser () {
   return (req, res) => {
     const user = insecurity.authenticatedUsers.get(req.cookies.token)
+    const response = { user: { id: (user && user.data ? user.data.id : undefined), email: (user && user.data ? user.data.email : undefined) } };
     if (req.query.callback === undefined) {
-      res.json({user: {id: (user && user.data ? user.data.id : undefined), email: (user && user.data ? user.data.email : undefined)}})
+      res.json(response)
     } else {
       if (utils.notSolved(challenges.emailLeakChallenge)) {
         utils.solve(challenges.emailLeakChallenge)
       }
-      res.jsonp({user: {id: (user && user.data ? user.data.id : undefined), email: (user && user.data ? user.data.email : undefined)}})
+      res.jsonp(response)
     }
   }
 }
