be056c932c7a045e955f5eca70eb57cada72d580
Aashish683 <aashishsingh2907@gmail.com>
2018-06-30 23:23:03 +0530

    Added rememberMe as a formControl

--- test/e2e/loginSpec.js
+++ test/e2e/loginSpec.js
@@ -1,24 +1,24 @@
-/* const config = require('config')
+const config = require('config')
 
 describe('/#/login', () => {
   let email, password, rememberMeCheckbox, loginButton
 
   beforeEach(() => {
     browser.get('/#/login')
-    email = element(by.model('user.email'))
-    password = element(by.model('user.password'))
-    rememberMeCheckbox = element(by.model('rememberMe'))
+    email = element(by.id('email'))
+    password = element(by.id('password'))
+    rememberMeCheckbox = element(by.id('rememberMe'))
     loginButton = element(by.id('loginButton'))
   })
 
   describe('challenge "loginAdmin"', () => {
-    xit('should log in Admin with SQLI attack on email field using "\' or 1=1--"', () => {
+    it('should log in Admin with SQLI attack on email field using "\' or 1=1--"', () => {
       email.sendKeys('\' or 1=1--')
       password.sendKeys('a')
       loginButton.click()
     })
 
-    xit('should log in Admin with SQLI attack on email field using "admin@<juice-sh.op>\'--"', () => {
+    it('should log in Admin with SQLI attack on email field using "admin@<juice-sh.op>\'--"', () => {
       email.sendKeys('admin@' + config.get('application.domain') + '\'--')
       password.sendKeys('a')
       loginButton.click()
@@ -28,7 +28,7 @@ describe('/#/login', () => {
   })
 
   describe('challenge "loginJim"', () => {
-    xit('should log in Jim with SQLI attack on email field using "jim@<juice-sh.op>\'--"', () => {
+    it('should log in Jim with SQLI attack on email field using "jim@<juice-sh.op>\'--"', () => {
       email.sendKeys('jim@' + config.get('application.domain') + '\'--')
       password.sendKeys('a')
       loginButton.click()
@@ -38,7 +38,7 @@ describe('/#/login', () => {
   })
 
   describe('challenge "loginBender"', () => {
-    xit('should log in Bender with SQLI attack on email field using "bender@<juice-sh.op>\'--"', () => {
+    it('should log in Bender with SQLI attack on email field using "bender@<juice-sh.op>\'--"', () => {
       email.sendKeys('bender@' + config.get('application.domain') + '\'--')
       password.sendKeys('a')
       loginButton.click()
@@ -48,7 +48,7 @@ describe('/#/login', () => {
   })
 
   describe('challenge "adminCredentials"', () => {
-    xit('should be able to log in with original (weak) admin credentials', () => {
+    it('should be able to log in with original (weak) admin credentials', () => {
       email.sendKeys('admin@' + config.get('application.domain'))
       password.sendKeys('admin123')
       loginButton.click()
@@ -58,7 +58,7 @@ describe('/#/login', () => {
   })
 
   describe('challenge "loginSupport"', () => {
-    xit('should be able to log in with original support-team credentials', () => {
+    it('should be able to log in with original support-team credentials', () => {
       email.sendKeys('support@' + config.get('application.domain'))
       password.sendKeys('J6aVjTgOpRs$?5l+Zkq2AYnCE@RF§P')
       loginButton.click()
@@ -68,7 +68,7 @@ describe('/#/login', () => {
   })
 
   describe('challenge "loginRapper"', () => {
-    xit('should be able to log in with original MC SafeSearch credentials', () => {
+    it('should be able to log in with original MC SafeSearch credentials', () => {
       email.sendKeys('mc.safesearch@' + config.get('application.domain'))
       password.sendKeys('Mr. N00dles')
       loginButton.click()
@@ -78,7 +78,7 @@ describe('/#/login', () => {
   })
 
   describe('challenge "oauthUserPassword"', () => {
-    xit('should be able to log in as bjoern.kimminich@googlemail.com with base64-encoded email as password', () => {
+    it('should be able to log in as bjoern.kimminich@googlemail.com with base64-encoded email as password', () => {
       email.sendKeys('bjoern.kimminich@googlemail.com')
       password.sendKeys('YmpvZXJuLmtpbW1pbmljaEBnb29nbGVtYWlsLmNvbQ==')
       loginButton.click()
@@ -88,13 +88,13 @@ describe('/#/login', () => {
   })
 
   describe('challenge "loginCiso"', () => {
-    xit('should be able to log in as ciso@juice-sh.op by using "Remember me" in combination with (fake) OAuth login with another user', () => {
+    it('should be able to log in as ciso@juice-sh.op by using "Remember me" in combination with (fake) OAuth login with another user', () => {
       email.sendKeys('ciso@' + config.get('application.domain'))
       password.sendKeys('wrong')
       rememberMeCheckbox.click()
       loginButton.click()
 
-      browser.executeScript('var $http = angular.element(document.body).injector().get(\'$http\'); $http.post(\'/rest/user/login\', {email: \'admin@juice-sh.op\', password: \'admin123\', oauth: true});')
+      browser.executeScript('var xhttp = new XMLHttpRequest(); xhttp.onreadystatechange = function() { if (this.status == 200) { console.log("Success"); }}; xhttp.open("POST","http://localhost:3000/rest/user/login", true); xhttp.setRequestHeader("Content-type","application/json"); xhttp.setRequestHeader("Authorization",`Bearer ${localStorage.getItem("token")}`); xhttp.send(JSON.stringify({email: "admin@juice-sh.op", password: "admin123", oauth: true}));') // eslint-disable-line
 
       // Deselect to clear email field for subsequent tests
       rememberMeCheckbox.click()
@@ -103,4 +103,4 @@ describe('/#/login', () => {
 
     protractor.expect.challengeSolved({challenge: 'Login CISO'})
   })
-}) */
+})
