0c73040bad887cf5cf2958b75e6f2032ef2d282f
Aashish683 <aashishsingh2907@gmail.com>
2018-07-02 01:42:32 +0530

    Uncommented e2e specs in redirectSpec.js

--- test/e2e/redirectSpec.js
+++ test/e2e/redirectSpec.js
@@ -1,27 +1,27 @@
-// describe('/redirect', () => {
-//   describe('challenge "redirect"', () => {
-//     xit('should show error page when supplying an unrecognized target URL', () => {
-//       browser.driver.get(browser.baseUrl + '/redirect?to=http://kimminich.de').then(() => {
-//         expect(browser.driver.getPageSource()).toContain('Unrecognized target URL for redirect: http://kimminich.de')
-//       })
-//     })
-//   })
+describe('/redirect', () => {
+  describe('challenge "redirect"', () => {
+    it('should show error page when supplying an unrecognized target URL', () => {
+      browser.driver.get(browser.baseUrl + '/redirect?to=http://kimminich.de').then(() => {
+        expect(browser.driver.getPageSource()).toContain('Unrecognized target URL for redirect: http://kimminich.de')
+      })
+    })
+  })
 
-//   describe('challenge "redirect"', () => {
-//     xit('should redirect to target URL if whitelisted URL is contained in xit as parameter', () => {
-//       browser.driver.get(browser.baseUrl + '/redirect?to=https://www.owasp.org?trickIndexOf=https://github.com/bkimminich/juice-shop').then(() => {
-//         expect(browser.driver.getCurrentUrl()).toMatch(/https:\/\/www\.owasp\.org/)
-//       })
-//     })
+  describe('challenge "redirect"', () => {
+    it('should redirect to target URL if whitelisted URL is contained in xit as parameter', () => {
+      browser.driver.get(browser.baseUrl + '/redirect?to=https://www.owasp.org?trickIndexOf=https://github.com/bkimminich/juice-shop').then(() => {
+        expect(browser.driver.getCurrentUrl()).toMatch(/https:\/\/www\.owasp\.org/)
+      })
+    })
 
-//     protractor.expect.challengeSolved({challenge: 'Redirects Tier 2'})
-//   })
+    protractor.expect.challengeSolved({challenge: 'Redirects Tier 2'})
+  })
 
-//   describe('challenge "redirectGratipay"', () => {
-//     xit('should still redirect to forgotten entry https://gratipay.com/juice-shop on whitelist', () => {
-//       browser.driver.get(browser.baseUrl + '/redirect?to=https://gratipay.com/juice-shop')
-//     })
+  describe('challenge "redirectGratipay"', () => {
+    it('should still redirect to forgotten entry https://gratipay.com/juice-shop on whitelist', () => {
+      browser.driver.get(browser.baseUrl + '/redirect?to=https://gratipay.com/juice-shop')
+    })
 
-//     protractor.expect.challengeSolved({challenge: 'Redirects Tier 1'})
-//   })
-// })
+    protractor.expect.challengeSolved({challenge: 'Redirects Tier 1'})
+  })
+})
