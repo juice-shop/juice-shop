29716bfe261bfb653b9b71850e7401bbd1de3ecb
CaptainFreak <patelshoeb4@gmail.com>
2018-07-31 04:00:16 +0530

    SSTi and SSRF challenges added

--- routes/fileUpload.js
+++ routes/fileUpload.js
@@ -15,6 +15,9 @@ module.exports = function fileUpload () {
     const file = req.file
     if (file === undefined && req.body.imageUrl !== undefined) {
       var url = req.body.imageUrl
+      if (url.match(/(.)*solve\/challenges\/server-side(.)*/) !== null) {
+        req.app.locals.abused_ssrf_bug = true
+      }
       const loggedInUser = insecurity.authenticatedUsers.get(req.cookies.token)
       if (loggedInUser) {
         request
