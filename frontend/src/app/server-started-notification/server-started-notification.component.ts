576103bb0d2a8f072e1629534dd7ef1bf01b5a3f
Aashish683 <aashishsingh2907@gmail.com>
2018-08-04 13:57:37 +0530

    Added tests for server-started-notification component

--- frontend/src/app/server-started-notification/server-started-notification.component.ts
+++ frontend/src/app/server-started-notification/server-started-notification.component.ts
@@ -17,6 +17,7 @@ fontawesome.library.add(faTrash)
 })
 export class ServerStartedNotificationComponent implements OnInit {
 
+  public io = io
   public socket
   public hackingProgress: any = {}
 
@@ -26,7 +27,7 @@ export class ServerStartedNotificationComponent implements OnInit {
 
   ngOnInit () {
     this.ngZone.runOutsideAngular(() => {
-      this.socket = io.connect(environment.hostServer)
+      this.socket = this.io.connect(environment.hostServer)
       this.socket.on('server started', () => {
 
         let continueCode = this.cookieService.get('continueCode')
