98d77993c2f4fb7ac1d179a8bf12daf675c93812
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2018-08-24 17:10:58 +0200

    Fix import generated by IntelliJ(?)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -79,7 +79,7 @@ import { MatMenuModule } from '@angular/material/menu'
 import { MatListModule } from '@angular/material/list'
 import { TokenSaleComponent } from './token-sale/token-sale.component'
 import { ProductReviewEditComponent } from './product-review-edit/product-review-edit.component'
-import {MatButtonToggleModule} from "@angular/material";
+import { MatButtonToggleModule } from '@angular/material'
 
 export function HttpLoaderFactory (http: HttpClient) {
   return new TranslateHttpLoader(http,'./../assets/i18n/' , '.json')
