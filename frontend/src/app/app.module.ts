2469bed6597854ac9c3e2e3b6840172145718402
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2018-09-18 18:39:53 +0200

    Reduce to single-theme with properly styled links

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpClientModule, HttpClient, HTTP_INTERCEPTORS } from '@angular/common
 import { CookieModule, CookieService } from 'ngx-cookie'
 import { ReactiveFormsModule } from '@angular/forms'
 import { Routing } from './app.routing'
+import { OverlayContainer } from '@angular/cdk/overlay'
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core'
 import { TranslateHttpLoader } from '@ngx-translate/http-loader'
 import { QRCodeModule } from 'angularx-qrcode'
@@ -182,4 +183,11 @@ export function HttpLoaderFactory (http: HttpClient) {
 })
 
 export class AppModule {
+
+  constructor (configurationService: ConfigurationService,overlayContainer: OverlayContainer) {
+    configurationService.getApplicationConfiguration().subscribe((conf) => {
+      overlayContainer.getContainerElement().classList.add(conf.application.theme)
+    })
+  }
+
 }
