c80a93ba798c530f74585a2d24db56fbc0a7f736
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2018-09-20 18:07:11 +0200

    Add (dummy) Feedback slideshow and social links

--- frontend/src/app/about/about.component.ts
+++ frontend/src/app/about/about.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core'
+import { Component,OnInit } from '@angular/core'
+import { ConfigurationService } from "../Services/configuration.service";
+import fontawesome from '@fortawesome/fontawesome'
+import { faFacebook,faTwitter } from '@fortawesome/fontawesome-free-brands'
+fontawesome.library.add(faFacebook, faTwitter)
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+
+  public twitterUrl = null
+  public facebookUrl = null
+
+  constructor (private configurationService: ConfigurationService) {}
+
+  ngOnInit () {
+    this.configurationService.getApplicationConfiguration().subscribe((config) => {
+      if (config && config.application) {
+        if (config.application.twitterUrl !== null) {
+          this.twitterUrl = config.application.twitterUrl
+        }
+        if (config.application.facebookUrl !== null) {
+          this.facebookUrl = config.application.facebookUrl
+        }
+      }
+    },(err) => console.log(err))
+  }
 }
