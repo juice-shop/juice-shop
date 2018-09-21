c7239c0b28d56661b918f2e8eea08ec48281b04b
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2018-09-21 14:17:24 +0200

    Load feedback and support team photos into slideshow

--- frontend/src/app/about/about.component.ts
+++ frontend/src/app/about/about.component.ts
@@ -1,5 +1,8 @@
 import { Component,OnInit } from '@angular/core'
+import { DomSanitizer } from '@angular/platform-browser'
 import { ConfigurationService } from '../Services/configuration.service'
+import { FeedbackService } from './../Services/feedback.service'
+import { IImage } from "ng-simple-slideshow";
 import fontawesome from '@fortawesome/fontawesome'
 import { faFacebook,faTwitter } from '@fortawesome/fontawesome-free-brands'
 fontawesome.library.add(faFacebook, faTwitter)
@@ -13,10 +16,22 @@ export class AboutComponent implements OnInit {
 
   public twitterUrl = null
   public facebookUrl = null
+  public slideshowDataSource: IImage[] = []
 
-  constructor (private configurationService: ConfigurationService) {}
+  private images = [
+    'assets/public/images/carousel/1.jpg',
+    'assets/public/images/carousel/2.jpg',
+    'assets/public/images/carousel/3.jpg',
+    'assets/public/images/carousel/4.jpg',
+    'assets/public/images/carousel/5.png',
+    'assets/public/images/carousel/6.jpg',
+    'assets/public/images/carousel/7.jpg'
+  ]
+
+  constructor (private configurationService: ConfigurationService, private feedbackService: FeedbackService, private sanitizer: DomSanitizer) {}
 
   ngOnInit () {
+    this.populateSlideshowFromFeedbacks()
     this.configurationService.getApplicationConfiguration().subscribe((config) => {
       if (config && config.application) {
         if (config.application.twitterUrl !== null) {
@@ -27,5 +42,16 @@ export class AboutComponent implements OnInit {
         }
       }
     },(err) => console.log(err))
+  }
+
+  populateSlideshowFromFeedbacks () {
+    this.feedbackService.find().subscribe((feedbacks) => {
+      for (let i = 0; i < feedbacks.length; i++) {
+        feedbacks[i].comment = this.sanitizer.bypassSecurityTrustHtml(feedbacks[i].comment)
+        this.slideshowDataSource.push({ url: this.images[i % this.images.length], caption: feedbacks[i].comment})
+      }
+    },(err) => {
+      console.log(err)
+    })
   }
 }
