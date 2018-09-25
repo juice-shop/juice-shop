00d92d6221977db1cfab29207ae5e549b5c4da6f
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2018-09-25 18:22:39 +0200

    Reenable XSS Tier 0 challenge

--- routes/trackOrder.js
+++ routes/trackOrder.js
@@ -5,7 +5,7 @@ const db = require('../data/mongodb')
 module.exports = function trackOrder () {
   return (req, res) => {
     req.params.id = decodeURIComponent(req.params.id)
-    if (utils.notSolved(challenges.reflectedXssChallenge) && utils.contains(req.params.id, '<script>alert("XSS")</script>')) {
+    if (utils.notSolved(challenges.reflectedXssChallenge) && utils.contains(req.params.id, '<iframe src="javascript:alert(`xss`)">')) {
       utils.solve(challenges.reflectedXssChallenge)
     }
     db.orders.find({ $where: "this.orderId === '" + req.params.id + "'" }).then(order => {
--- test/e2e/trackingOrderSpec.js
+++ test/e2e/trackingOrderSpec.js
@@ -3,23 +3,23 @@ describe('/#/track-order', () => {
 
   beforeEach(() => {
     browser.get('/#/track-order')
-    orderId = element(by.model('orderId'))
+    orderId = element(by.id('orderId'))
     trackButton = element(by.id('trackButton'))
   })
 
   describe('challenge "xss0"', () => {
-    xit('Order Id should be susceptible to reflected XSS attacks', () => {
+    it('Order Id should be susceptible to reflected XSS attacks', () => {
       const EC = protractor.ExpectedConditions
 
-      orderId.sendKeys('<script>alert("XSS")</script>')
+      orderId.sendKeys('<iframe src="javascript:alert(`xss`)">')
       trackButton.click()
-      browser.wait(EC.alertIsPresent(), 5000, "'XSS' alert is not present")
+      browser.wait(EC.alertIsPresent(), 5000, "'xss' alert is not present")
       browser.switchTo().alert().then(alert => {
-        expect(alert.getText()).toEqual('XSS')
+        expect(alert.getText()).toEqual('xss')
         alert.accept()
       })
     })
 
-    // protractor.expect.challengeSolved({ challenge: 'XSS Tier 0' })
+    protractor.expect.challengeSolved({ challenge: 'XSS Tier 0' })
   })
 })
