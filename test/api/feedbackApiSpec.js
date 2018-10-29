772ca9c0130d5d4107b6416a7c2a377ed1fa7b58
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2018-10-29 17:01:54 +0100

    Retrieve CAPTCHA individually per test
    and perform login before retrieving CAPTCHA
    and set 10sec timeout only on known slow tests

--- test/api/feedbackApiSpec.js
+++ test/api/feedbackApiSpec.js
@@ -9,137 +9,66 @@ const authHeader = { 'Authorization': 'Bearer ' + insecurity.authorize(), 'conte
 const jsonHeader = { 'content-type': 'application/json' }
 
 describe('/api/Feedbacks', () => {
-  let captchaId, captchaAnswer
-
-  beforeAll(() => {
-    return frisby.get(REST_URL + '/captcha')
-      .expect('status', 200)
-      .expect('header', 'content-type', /application\/json/)
-      .then(({ json }) => {
-        captchaId = json.captchaId
-        captchaAnswer = json.answer
-      })
-  })
-
   it('GET all feedback', () => {
     return frisby.get(API_URL + '/Feedbacks')
       .expect('status', 200)
   })
 
   it('POST sanitizes unsafe HTML from comment', () => {
-    return frisby.post(API_URL + '/Feedbacks', {
-      headers: jsonHeader,
-      body: {
-        comment: 'I am a harm<script>steal-cookie</script><img src="csrf-attack"/><iframe src="evil-content"></iframe>less comment.',
-        rating: 1,
-        captchaId: captchaId,
-        captcha: captchaAnswer
-      }
-    })
-      .expect('status', 201)
-      .expect('json', 'data', {
-        comment: 'I am a harmless comment.'
-      })
-  })
-
-  it('POST fails to sanitize masked CSRF-attack by not applying sanitization recursively', () => {
-    return frisby.post(API_URL + '/Feedbacks', {
-      headers: jsonHeader,
-      body: {
-        comment: 'The sanitize-html module up to at least version 1.4.2 has this issue: <<script>Foo</script>iframe src="javascript:alert(`xss`)">',
-        rating: 1,
-        captchaId: captchaId,
-        captcha: captchaAnswer
-      }
-    })
-      .expect('status', 201)
-      .expect('json', 'data', {
-        comment: 'The sanitize-html module up to at least version 1.4.2 has this issue: <iframe src="javascript:alert(`xss`)">'
-      })
-  })
-
-  it('POST feedback in another users name as anonymous user', () => {
-    return frisby.post(API_URL + '/Feedbacks', {
-      headers: jsonHeader,
-      body: {
-        comment: 'Lousy crap! You use sequelize 1.7.x? Welcome to SQL Injection-land, morons! As if that is not bad enough, you use z85/base85 and hashids for crypto? Even MD5 to hash passwords! Srsly?!?!',
-        rating: 1,
-        UserId: 3,
-        captchaId: captchaId,
-        captcha: captchaAnswer
-      }
-    })
-      .expect('status', 201)
-      .expect('header', 'content-type', /application\/json/)
-      .expect('json', 'data', {
-        UserId: 3
-      })
-  })
-
-  it('POST feedback in a non-existing users name as anonymous user fails with constraint error', () => {
-    return frisby.post(API_URL + '/Feedbacks', {
-      headers: jsonHeader,
-      body: {
-        comment: 'Pickle Rick says your express-jwt 0.1.3 has issues!',
-        rating: 0,
-        UserId: 4711,
-        captchaId: captchaId,
-        captcha: captchaAnswer
-      }
-    })
-      .expect('status', 500)
-      .expect('header', 'content-type', /application\/json/)
-      .then(({ json }) => {
-        expect(json.errors).toContain('SQLITE_CONSTRAINT: FOREIGN KEY constraint failed')
-      })
-  })
-
-  it('POST feedback is associated with current user', () => {
-    return frisby.post(REST_URL + '/user/login', {
-      headers: jsonHeader,
-      body: {
-        email: 'bjoern.kimminich@googlemail.com',
-        password: 'YmpvZXJuLmtpbW1pbmljaEBnb29nbGVtYWlsLmNvbQ=='
-      }
-    })
+    return frisby.get(REST_URL + '/captcha')
       .expect('status', 200)
+      .expect('header', 'content-type', /application\/json/)
       .then(({ json }) => {
         return frisby.post(API_URL + '/Feedbacks', {
-          headers: { 'Authorization': 'Bearer ' + json.authentication.token, 'content-type': 'application/json' },
+          headers: jsonHeader,
           body: {
-            comment: 'Stupid JWT secret "' + insecurity.defaultSecret + '" and being typosquatted by epilogue-js and ng2-bar-rating!',
-            rating: 5,
-            UserId: 4,
-            captchaId: captchaId,
-            captcha: captchaAnswer
+            comment: 'I am a harm<script>steal-cookie</script><img src="csrf-attack"/><iframe src="evil-content"></iframe>less comment.',
+            rating: 1,
+            captchaId: json.captchaId,
+            captcha: json.answer
           }
         })
           .expect('status', 201)
-          .expect('header', 'content-type', /application\/json/)
           .expect('json', 'data', {
-            UserId: 4
+            comment: 'I am a harmless comment.'
           })
       })
   })
 
-  it('POST feedback is associated with any passed user ID', () => {
-    return frisby.post(REST_URL + '/user/login', {
-      headers: jsonHeader,
-      body: {
-        email: 'bjoern.kimminich@googlemail.com',
-        password: 'YmpvZXJuLmtpbW1pbmljaEBnb29nbGVtYWlsLmNvbQ=='
-      }
-    })
+  it('POST fails to sanitize masked CSRF-attack by not applying sanitization recursively', () => {
+    return frisby.get(REST_URL + '/captcha')
       .expect('status', 200)
+      .expect('header', 'content-type', /application\/json/)
       .then(({ json }) => {
         return frisby.post(API_URL + '/Feedbacks', {
-          headers: { 'Authorization': 'Bearer ' + json.authentication.token, 'content-type': 'application/json' },
+          headers: jsonHeader,
           body: {
-            comment: 'Bender\'s choice award!',
-            rating: 5,
+            comment: 'The sanitize-html module up to at least version 1.4.2 has this issue: <<script>Foo</script>iframe src="javascript:alert(`xss`)">',
+            rating: 1,
+            captchaId: json.captchaId,
+            captcha: json.answer
+          }
+        })
+          .expect('status', 201)
+          .expect('json', 'data', {
+            comment: 'The sanitize-html module up to at least version 1.4.2 has this issue: <iframe src="javascript:alert(`xss`)">'
+          })
+      })
+  })
+
+  it('POST feedback in another users name as anonymous user', () => {
+    return frisby.get(REST_URL + '/captcha')
+      .expect('status', 200)
+      .expect('header', 'content-type', /application\/json/)
+      .then(({ json }) => {
+        return frisby.post(API_URL + '/Feedbacks', {
+          headers: jsonHeader,
+          body: {
+            comment: 'Lousy crap! You use sequelize 1.7.x? Welcome to SQL Injection-land, morons! As if that is not bad enough, you use z85/base85 and hashids for crypto? Even MD5 to hash passwords! Srsly?!?!',
+            rating: 1,
             UserId: 3,
-            captchaId: captchaId,
-            captcha: captchaAnswer
+            captchaId: json.captchaId,
+            captcha: json.answer
           }
         })
           .expect('status', 201)
@@ -150,55 +79,142 @@ describe('/api/Feedbacks', () => {
       })
   })
 
-  it('POST feedback can be created without actually supplying comment', () => {
-    return frisby.post(API_URL + '/Feedbacks', {
+  it('POST feedback in a non-existing users name as anonymous user fails with constraint error', () => {
+    return frisby.get(REST_URL + '/captcha')
+      .expect('status', 200)
+      .expect('header', 'content-type', /application\/json/)
+      .then(({ json }) => {
+        return frisby.post(API_URL + '/Feedbacks', {
+          headers: jsonHeader,
+          body: {
+            comment: 'Pickle Rick says your express-jwt 0.1.3 has issues!',
+            rating: 0,
+            UserId: 4711,
+            captchaId: json.captchaId,
+            captcha: json.answer
+          }
+        })
+          .expect('status', 500)
+          .expect('header', 'content-type', /application\/json/)
+          .then(({ json }) => {
+            expect(json.errors).toContain('SQLITE_CONSTRAINT: FOREIGN KEY constraint failed')
+          })
+      })
+  })
+
+  it('POST feedback is associated with current user', () => {
+    return frisby.timeout(10000).post(REST_URL + '/user/login', {
       headers: jsonHeader,
       body: {
-        rating: 1,
-        captchaId: captchaId,
-        captcha: captchaAnswer
+        email: 'bjoern.kimminich@googlemail.com',
+        password: 'YmpvZXJuLmtpbW1pbmljaEBnb29nbGVtYWlsLmNvbQ=='
       }
     })
-      .expect('status', 201)
+      .expect('status', 200)
+      .then(({ json: jsonLogin }) => {
+        return frisby.get(REST_URL + '/captcha')
+          .expect('status', 200)
+          .expect('header', 'content-type', /application\/json/)
+          .then(({ json }) => {
+            return frisby.post(API_URL + '/Feedbacks', {
+              headers: { 'Authorization': 'Bearer ' + jsonLogin.authentication.token, 'content-type': 'application/json' },
+              body: {
+                comment: 'Stupid JWT secret "' + insecurity.defaultSecret + '" and being typosquatted by epilogue-js and ng2-bar-rating!',
+                rating: 5,
+                UserId: 4,
+                captchaId: json.captchaId,
+                captcha: json.answer
+              }
+            })
+              .expect('status', 201)
+              .expect('header', 'content-type', /application\/json/)
+              .expect('json', 'data', {
+                UserId: 4
+              })
+          })
+      })
+  })
+
+  it('POST feedback is associated with any passed user ID', () => {
+    return frisby.timeout(10000).post(REST_URL + '/user/login', {
+      headers: jsonHeader,
+      body: {
+        email: 'bjoern.kimminich@googlemail.com',
+        password: 'YmpvZXJuLmtpbW1pbmljaEBnb29nbGVtYWlsLmNvbQ=='
+      }
+    })
+      .expect('status', 200)
+      .then(({ json: jsonLogin }) => {
+        return frisby.get(REST_URL + '/captcha')
+          .expect('status', 200)
+          .expect('header', 'content-type', /application\/json/)
+          .then(({ json }) => {
+            return frisby.post(API_URL + '/Feedbacks', {
+              headers: { 'Authorization': 'Bearer ' + jsonLogin.authentication.token, 'content-type': 'application/json' },
+              body: {
+                comment: 'Bender\'s choice award!',
+                rating: 5,
+                UserId: 3,
+                captchaId: json.captchaId,
+                captcha: json.answer
+              }
+            })
+              .expect('status', 201)
+              .expect('header', 'content-type', /application\/json/)
+              .expect('json', 'data', {
+                UserId: 3
+              })
+          })
+      })
+  })
+
+  it('POST feedback can be created without actually supplying comment', () => {
+    return frisby.get(REST_URL + '/captcha')
+      .expect('status', 200)
       .expect('header', 'content-type', /application\/json/)
-      .expect('json', 'data', {
-        comment: null,
-        rating: 1
+      .then(({ json }) => {
+        return frisby.post(API_URL + '/Feedbacks', {
+          headers: jsonHeader,
+          body: {
+            rating: 1,
+            captchaId: json.captchaId,
+            captcha: json.answer
+          }
+        })
+          .expect('status', 201)
+          .expect('header', 'content-type', /application\/json/)
+          .expect('json', 'data', {
+            comment: null,
+            rating: 1
+          })
       })
   })
 
   it('POST feedback cannot be created without actually supplying rating', () => {
-    return frisby.post(API_URL + '/Feedbacks', {
-      headers: jsonHeader,
-      body: {
-        captchaId: captchaId,
-        captcha: captchaAnswer
-      }
-    })
-      .expect('status', 400)
+    return frisby.get(REST_URL + '/captcha')
+      .expect('status', 200)
       .expect('header', 'content-type', /application\/json/)
-      .expect('jsonTypes', {
-        message: Joi.string()
-      })
       .then(({ json }) => {
-        expect(json.message.match(/notNull Violation: (Feedback\.)?rating cannot be null/))
+        return frisby.post(API_URL + '/Feedbacks', {
+          headers: jsonHeader,
+          body: {
+            captchaId: json.captchaId,
+            captcha: json.answer
+          }
+        })
+          .expect('status', 400)
+          .expect('header', 'content-type', /application\/json/)
+          .expect('jsonTypes', {
+            message: Joi.string()
+          })
+          .then(({ json }) => {
+            expect(json.message.match(/notNull Violation: (Feedback\.)?rating cannot be null/))
+          })
       })
   })
 })
 
 describe('/api/Feedbacks/:id', () => {
-  let captchaId, captchaAnswer
-
-  beforeAll(() => {
-    return frisby.get(REST_URL + '/captcha')
-      .expect('status', 200)
-      .expect('header', 'content-type', /application\/json/)
-      .then(({ json }) => {
-        captchaId = json.captchaId
-        captchaAnswer = json.answer
-      })
-  })
-
   it('GET existing feedback by id is forbidden via public API', () => {
     return frisby.get(API_URL + '/Feedbacks/1')
       .expect('status', 401)
@@ -237,21 +253,26 @@ describe('/api/Feedbacks/:id', () => {
   })
 
   it('DELETE existing feedback', () => {
-    return frisby.post(API_URL + '/Feedbacks', {
-      headers: jsonHeader,
-      body: {
-        comment: 'I will be gone soon!',
-        rating: 1,
-        captchaId: captchaId,
-        captcha: captchaAnswer
-      }
-    })
-      .expect('status', 201)
-      .expect('jsonTypes', 'data', { id: Joi.number() })
+    return frisby.get(REST_URL + '/captcha')
+      .expect('status', 200)
+      .expect('header', 'content-type', /application\/json/)
       .then(({ json }) => {
-        return frisby.del(API_URL + '/Feedbacks/' + json.data.id, { headers: authHeader })
-          .expect('status', 200)
-      }
-      )
+        return frisby.post(API_URL + '/Feedbacks', {
+          headers: jsonHeader,
+          body: {
+            comment: 'I will be gone soon!',
+            rating: 1,
+            captchaId: json.captchaId,
+            captcha: json.answer
+          }
+        })
+          .expect('status', 201)
+          .expect('jsonTypes', 'data', { id: Joi.number() })
+          .then(({ json }) => {
+            return frisby.del(API_URL + '/Feedbacks/' + json.data.id, { headers: authHeader })
+              .expect('status', 200)
+          }
+          )
+      })
   })
 })
