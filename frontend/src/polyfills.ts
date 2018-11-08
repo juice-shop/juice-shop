7964226faf6fb3b575cd3b3ce520aad5dc144cac
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2018-11-08 19:17:56 +0100

    Add HammerJS to polyfills

--- frontend/src/polyfills.ts
+++ frontend/src/polyfills.ts
@@ -75,3 +75,4 @@ import 'zone.js/dist/zone'  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+import 'hammerjs/hammer'
