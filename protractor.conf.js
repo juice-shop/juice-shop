168ec153d57e8952c7aabb894c3d880495dc16bf
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2018-11-08 13:20:41 +0100

    Fix code style in Protractor config

--- protractor.conf.js
+++ protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
   capabilities: {
     browserName: 'chrome',
     chromeOptions: {
-      args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
+      args: [ '--headless', '--disable-gpu', '--window-size=800,600' ]
     }
   },
 
