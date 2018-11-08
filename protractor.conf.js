aac854fc890ca0d87c3ea6fd8148bc5dd6302fc3
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2018-11-08 13:11:17 +0100

    Use headless Chrome for Protractor tests
    (see https://github.com/angular/protractor/blob/master/docs/browser-setup.md)

--- protractor.conf.js
+++ protractor.conf.js
@@ -10,7 +10,10 @@ exports.config = {
   ],
 
   capabilities: {
-    'browserName': 'chrome'
+    browserName: 'chrome',
+    chromeOptions: {
+      args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
+    }
   },
 
   baseUrl: 'http://localhost:3000',
