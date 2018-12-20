2ff4836b22f1f6020aa6f580d961e057910ee711
Supratik Das <rick.das08@gmail.com>
2018-12-20 13:44:26 +0530

    feat: add usernameXss challenge

--- routes/updateUserProfile.js
+++ routes/updateUserProfile.js
@@ -1,10 +1,20 @@
 const models = require('../models/index')
 const insecurity = require('../lib/insecurity')
+const utils = require('../lib/utils')
+const challenges = require('../data/datacache').challenges
 
 module.exports = function updateUserProfile () {
   return (req, res, next) => {
     const loggedInUser = insecurity.authenticatedUsers.get(req.cookies.token)
-    if (loggedInUser) {
+    req.body.username = decodeURIComponent(req.body.username)
+    if (utils.notSolved(challenges.usernameXssChallenge) && utils.contains(req.body.username, '<iframe src="javascript:alert(`xss`)">')) {
+      utils.solve(challenges.usernameXssChallenge)
+    }
+    var flag = true
+    if (req.body.username.includes('<script') || req.body.username.includes('onload')) {
+      flag = false
+    }
+    if (loggedInUser && flag) {
       models.User.findByPk(loggedInUser.data.id).then(user => {
         return user.updateAttributes({ username: req.body.username })
       }).catch(error => {
