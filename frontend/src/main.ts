9aad23f8bea6299ea099f414872c60836fb2ce87
aaryan10 <aaryanbudhiraja2@gmail.com>
2019-01-13 22:29:37 +0530

    Added logic to login

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,6 +5,16 @@ import { AppModule } from './app/app.module'
 import { environment } from './environments/environment'
 import 'hammerjs'
 
+export function pressEnterHandler (loginForm, loginFunc, isEnabled) {
+  document.getElementById(loginForm)
+    .addEventListener('keyup', function (event) {
+      event.preventDefault()
+      if (event.keyCode === 13 && isEnabled()) {
+        loginFunc()
+      }
+    })
+}
+
 if (environment.production) {
   enableProdMode()
 }
