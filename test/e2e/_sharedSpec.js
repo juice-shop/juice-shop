a29f29e8854be44b1aa415e57a8309554a7c89a6
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2019-01-14 12:46:49 +0100

    Fix check for presence of elements

--- test/e2e/_sharedSpec.js
+++ test/e2e/_sharedSpec.js
@@ -6,8 +6,8 @@ protractor.expect = {
       })
 
       it("challenge '" + context.challenge + "' should be solved on score board", () => {
-        expect(element(by.id(context.challenge + '.solved'))).toBeTruthy()
-        expect(element(by.id(context.challenge + '.notSolved'))).not.toBeTruthy()
+        expect(element(by.id(context.challenge + '.solved')).isPresent()).toBeTruthy()
+        expect(element(by.id(context.challenge + '.notSolved')).isPresent()).toBeFalsy()
       })
     })
   }
