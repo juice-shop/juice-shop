fd3b5301122aedde08d40f3a11d08bc43902fda3
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2019-01-23 14:10:16 +0100

    Enable oauth component unit test

--- frontend/src/app/oauth/oauth.component.spec.ts
+++ frontend/src/app/oauth/oauth.component.spec.ts
@@ -1,7 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 import { TranslateModule } from '@ngx-translate/core'
+import { MatCardModule } from '@angular/material/card'
+import { MatCheckboxModule } from '@angular/material/checkbox'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { CookieModule } from 'ngx-cookie'
+import { HttpClientModule } from '@angular/common/http'
+import { RouterTestingModule } from '@angular/router/testing'
 
 import { OAuthComponent } from './oauth.component'
+import { LoginComponent } from '../login/login.component'
 
 describe('OAuthComponent', () => {
   let component: OAuthComponent
@@ -9,9 +16,18 @@ describe('OAuthComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ OAuthComponent ],
+      declarations: [ OAuthComponent, LoginComponent ],
       imports: [
-        TranslateModule.forRoot()
+        RouterTestingModule.withRoutes([
+          { path: 'login', component: LoginComponent }
+        ]
+        ),
+        CookieModule.forRoot(),
+        TranslateModule.forRoot(),
+        MatCardModule,
+        MatFormFieldModule,
+        MatCheckboxModule,
+        HttpClientModule
       ]
     })
     .compileComponents()
@@ -23,7 +39,7 @@ describe('OAuthComponent', () => {
     fixture.detectChanges()
   })
 
-  xit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy()
   })
 })
