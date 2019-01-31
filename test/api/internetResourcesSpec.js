8eed0059ee6ce8b5f2eacd7d05e38e2a817d624b
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2019-01-31 09:31:15 +0100

    Apply HTML encoding to expected body content

--- test/api/internetResourcesSpec.js
+++ test/api/internetResourcesSpec.js
@@ -1,7 +1,7 @@
 const frisby = require('frisby')
 
 describe('Required Internet resource', () => {
-  xit('PasteBin paste (https://pastebin.com/90dUgd7s) for "DLP Failure Tier 1" challenge available', () => {
+  it('PasteBin paste (https://pastebin.com/90dUgd7s) for "DLP Failure Tier 1" challenge available', () => {
     return frisby.get('https://pastebin.com/90dUgd7s')
       .expect('status', 200)
       .expect('bodyContains', 'Hueteroneel')
@@ -11,13 +11,13 @@ describe('Required Internet resource', () => {
   it('PasteBin paste (https://pastebin.com/4U1V1UjU) for "DLP Failure Tier 2" challenge available', () => {
     return frisby.get('https://pastebin.com/4U1V1UjU')
       .expect('status', 200)
-      .expect('bodyContains', 'current=0Y8rMnww$*9VFYE%C2%A759-!Fg1L6t&6lB')
+      .expect('bodyContains', 'current=0Y8rMnww$*9VFYE%C2%A759-!Fg1L6t&amp;6lB')
   })
 
   it('GitHub issue (https://github.com/punkave/sanitize-html/issues/29) for "XSS Tier 4" challenge available', () => {
     return frisby.get('https://github.com/punkave/sanitize-html/issues/29')
       .expect('status', 200)
       .expect('bodyContains', 'Sanitization is not applied recursively')
-      .expect('bodyContains', 'I am not harmless: <<img src="csrf-attack"/>img src="csrf-attack"/> is sanitized to I am not harmless: <img src="csrf-attack"/>')
+      .expect('bodyContains', 'I am not harmless: &lt;&lt;img src=&quot;csrf-attack&quot;/&gt;img src=&quot;csrf-attack&quot;/&gt; is sanitized to I am not harmless: &lt;img src=&quot;csrf-attack&quot;/&gt;')
   })
 })
