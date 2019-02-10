b1f84088a923600be8a61a679612a0f32df0a146
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2019-02-10 20:20:30 +0100

    Added frontend component to enter totp token

---
+++ frontend/src/app/two-factor-auth-enter/two-factor-auth-enter.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TwoFactorAuthEnterComponent } from './two-factor-auth-enter.component';
+
+describe('TwoFactorAuthEnterComponent', () => {
+  let component: TwoFactorAuthEnterComponent;
+  let fixture: ComponentFixture<TwoFactorAuthEnterComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TwoFactorAuthEnterComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TwoFactorAuthEnterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
