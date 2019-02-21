fedc133baba869c78f29ea67411cc28ac76e69b4
Supratik Das <rick.das08@gmail.com>
2019-02-22 00:35:17 +0530

    transferred the functions to routes/recycles

--- routes/verify.js
+++ routes/verify.js
@@ -20,21 +20,6 @@ exports.forgedFeedbackChallenge = () => (req, res, next) => {
   next()
 }
 
-exports.sequelizeVulnerabilityChallenge = () => (req, res, next) => {
-  models.Recycle.findAll({
-    where: {
-      id: JSON.parse(req.params['id'])
-    }
-  }).then((Recycle) => {
-    return res.send(utils.queryResultToJson(Recycle))
-  })
-}
-
-exports.blockRecycleItems = () => (req, res, next) => {
-  let errMsg = { err: 'Sorry, this endpoint is not supported.' }
-  return res.send(utils.queryResultToJson(errMsg))
-}
-
 exports.captchaBypassChallenge = () => (req, res, next) => {
   /* jshint eqeqeq:false */
   if (utils.notSolved(challenges.captchaBypassChallenge)) {
