529b276d7f617ee2112a8aaaa4b2c32dc346f172
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2019-02-26 17:11:34 +0100

    Refactored 2fa verification code

--- routes/2fa.js
+++ routes/2fa.js
@@ -11,46 +11,42 @@ otplib.authenticator.options = {
   window: 1
 }
 
-function verify () {
-  return async (req, res, next) => {
-    const { tmpToken, totpToken } = req.body
+async function verify (req, res) {
+  const { tmpToken, totpToken } = req.body
 
-    try {
-      const { userId, type } = insecurity.verify(tmpToken)
+  try {
+    const { userId, type } = insecurity.verify(tmpToken)
 
-      if (type !== 'password_valid_needs_second_factor_token') {
-        throw new Error('Invalid token type')
-      }
-
-      const user = await models.User.findByPk(userId)
-
-      const isValid = otplib.authenticator.check(totpToken, user.totpSecret)
-
-      const plainUser = utils.queryResultToJson(user)
-
-      if (isValid) {
-        if (utils.notSolved(challenges.twoFactorAuthUnsafeSecretStorage) && user.email === 'J12934@' + config.get('application.domain')) {
-          utils.solve(challenges.twoFactorAuthUnsafeSecretStorage)
-        }
-
-        models.Basket.findOrCreate({ where: { userId }, defaults: {} })
-          .then(([basket, created]) => {
-            const token = insecurity.authorize(plainUser)
-            plainUser.bid = basket.id // keep track of original basket for challenge solution check
-            insecurity.authenticatedUsers.put(token, plainUser)
-            res.json({ authentication: { token, bid: basket.id, umail: user.email } })
-          }).catch(error => {
-            next(error)
-          })
-      } else {
-        res.status(401).send()
-      }
-    } catch (error) {
-      console.error('Failed to verify token identity')
-      console.error(error)
-      res.status(401).send()
+    if (type !== 'password_valid_needs_second_factor_token') {
+      throw new Error('Invalid token type')
     }
+
+    const user = await models.User.findByPk(userId)
+
+    const isValid = otplib.authenticator.check(totpToken, user.totpSecret)
+
+    const plainUser = utils.queryResultToJson(user)
+
+    if (!isValid) {
+      return res.status(401).send()
+    }
+      
+    if (utils.notSolved(challenges.twoFactorAuthUnsafeSecretStorage) && user.email === 'J12934@' + config.get('application.domain')) {
+      utils.solve(challenges.twoFactorAuthUnsafeSecretStorage)
+    }
+
+    const [ basket ] = await models.Basket.findOrCreate({ where: { userId }, defaults: {} })
+    
+    const token = insecurity.authorize(plainUser)
+    plainUser.bid = basket.id // keep track of original basket for challenge solution check
+    insecurity.authenticatedUsers.put(token, plainUser)
+    
+    res.json({ authentication: { token, bid: basket.id, umail: user.email } })
+  } catch (error) {
+    console.error('Failed to verify token identity')
+    res.status(401).send()
   }
 }
 
-module.exports.verify = verify
+
+module.exports.verify = () => verify
