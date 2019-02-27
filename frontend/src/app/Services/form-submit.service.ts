24cd7960512f754f9087342f161b2fa2fd87b338
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2019-02-27 14:12:40 +0100

    Import DOCUMENT from angular/common
    (see https://stackoverflow.com/questions/37521298/how-to-inject-document-in-angular-2-service)

--- frontend/src/app/Services/form-submit.service.ts
+++ frontend/src/app/Services/form-submit.service.ts
@@ -1,5 +1,5 @@
 import { Inject, Injectable } from '@angular/core'
-import { DOCUMENT } from '@angular/platform-browser'
+import { DOCUMENT } from '@angular/common'
 
 @Injectable({
   providedIn: 'root'
