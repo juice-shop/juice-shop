7ad202420528a49d0b3519376a9ab07ead64d2a4
Supratik Das <rick.das08@gmail.com>
2019-03-12 19:39:57 +0530

    add methods to handle jsonp response

--- routes/authenticatedUsers.js
+++ routes/authenticatedUsers.js
@@ -1,18 +1,56 @@
 const utils = require('../lib/utils')
 const insecurity = require('../lib/insecurity')
 const models = require('../models/index')
+const challenges = require('../data/datacache').challenges
 
 module.exports = function retrieveUserList () {
   return (req, res, next) => {
-    models.User.findAll().then(users => {
-      const usersWithLoginStatus = utils.queryResultToJson(users)
-      usersWithLoginStatus.data.forEach(user => {
-        user.token = insecurity.authenticatedUsers.tokenOf(user)
-        user.password = user.password ? user.password.replace(/./g, '*') : null
-      })
-      res.json(usersWithLoginStatus)
-    }).catch(error => {
-      next(error)
-    })
+    const loggedInUser = insecurity.authenticatedUsers.get(req.cookies.token)
+
+    let callbackFunc = req.query.callback
+
+    if (callbackFunc === 'getDetails') {
+      models.User.findAll().then(users => {
+        let referer = ''
+        if (req.headers.referer) {
+          referer = req.headers.referer
+        }
+        if (referer === 'thrift.juice-shop.com') {
+          if (utils.notSolved(challenges.jsonpChallenge)) {
+            utils.solve(challenges.jsonpChallenge)
+          }
+        }
+        const usersWithLoginStatus = utils.queryResultToJson(users)
+        usersWithLoginStatus.data.forEach(user => {
+          user.token = insecurity.authenticatedUsers.tokenOf(user)
+          user.password = user.password ? user.password.replace(/./g, '*') : null
+        })
+        const responseValue = 'getdetails(' + JSON.stringify(usersWithLoginStatus) + ')'
+        res.setHeader('content-type', 'text/javascript')
+        res.send(responseValue)
+      }).catch(error => {
+        next(error)
+    }) }
+    else if (callbackFunc === undefined) {
+       if (loggedInUser) {
+        models.User.findAll().then(users => {
+          const usersWithLoginStatus = utils.queryResultToJson(users)
+          usersWithLoginStatus.data.forEach(user => {
+            user.token = insecurity.authenticatedUsers.tokenOf(user)
+            user.password = user.password ? user.password.replace(/./g, '*') : null
+          })
+          const responseValue = 'getdetails(' + JSON.stringify(usersWithLoginStatus) + ')'
+          res.setHeader('content-type', 'text/javascript')
+          res.send(responseValue)
+        }).catch(error => {
+          next(error)
+       }) }
+      else {
+        let errMsg = { err: 'Your token is missing, method not allowed' }
+        return res.send(utils.queryResultToJson(errMsg))
+      }}
+    else {
+      next(new Error('Blocked illegal activity by ' + req.connection.remoteAddress))
+    }
   }
 }
