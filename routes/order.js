c63938b023af806e14f6a4135169ba2d44fb0fe6
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2019-03-25 00:24:36 +0100

    Fix comparison problem with coupon date due to type mismatch

--- routes/order.js
+++ routes/order.js
@@ -105,7 +105,7 @@ function calculateApplicableDiscount (basket, req) {
     const couponCode = couponData[0]
     const couponDate = couponData[1]
     const campaign = campaigns[couponCode]
-    if (campaign && couponDate === campaign.validOn) {
+    if (campaign && couponDate == campaign.cvalidOn) { // eslint-disable-line eqeqeq
       if (utils.notSolved(challenges.manipulateClockChallenge)) {
         utils.solve(challenges.manipulateClockChallenge)
       }
