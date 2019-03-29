019e83786399ff465168274b90c3ad75986454be
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2019-03-29 17:50:07 +0100

    Add validation for frontend dependencies
    (extension of #903)

--- server.js
+++ server.js
@@ -71,6 +71,7 @@ const config = require('config')
 
 errorhandler.title = `${config.get('application.name')} (Express ${utils.version('express')})`
 
+require('./lib/startup/validateDependencies')({ packageDir: './frontend' })
 require('./lib/startup/validatePreconditions')()
 require('./lib/startup/validateConfig')()
 require('./lib/startup/cleanupFtpFolder')()
