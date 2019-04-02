178746f7855197c63986ec22a6a11b11c5a1e820
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2019-04-02 16:51:35 +0200

    Prototype: Loading hints from an object definition
    Co-Authored-By: Jorge Estigarribia <jorgestiga@users.noreply.github.com>

--- frontend/src/hacking-instructor/index.ts
+++ frontend/src/hacking-instructor/index.ts
@@ -2,10 +2,55 @@
 //   return ''
 // }
 
-export function init () {
-  // loadInstructorModule('challenge_xss')
-  console.log('Hacking Instructor Init')
+interface HackingInstructorFileFormat {
+  challenges: ChallengeInstruction[]
+}
 
+interface ChallengeInstruction {
+  name: string
+  hints: ChallengeHint[]
+}
+
+interface ChallengeHint {
+  text: string,
+  page: string,
+  fixture: {
+    type: string,
+    value: string
+  }
+  position: string
+  hideAfterHint?: boolean
+}
+
+const challengeInstructions: HackingInstructorFileFormat = {
+  challenges: [
+    {
+      name: 'XSS Tier 1',
+      hints: [
+        {
+          text: 'Well, I prefer stripes, but let‘s do some cross site scripting! First, let‘s search for „wasp“ in the Search box at the top of the page!',
+          page: 'score-board',
+          fixture: {
+            type: 'id',
+            value: 'searchQuery'
+          },
+          position: 'right'
+        },
+        {
+          text: 'Irgendein Quatsch!',
+          page: 'score-board',
+          fixture: {
+            type: 'id',
+            value: 'search-result-heading'
+          },
+          position: 'right'
+        }
+      ]
+    }
+  ]
+}
+
+function loadHint (hint: ChallengeHint): HTMLElement {
   const elem = document.createElement('div')
   elem.id = 'hacking-instructor'
   elem.style.position = 'absolute'
@@ -19,9 +64,9 @@ export function init () {
   elem.style.lineHeight = '1.3'
   elem.style.overflow = ''
   elem.style.top = `8px`
-  elem.innerText = 'Well, I prefer stripes, but let‘s do some cross site scripting! First, let‘s search for „wasp“ in the Search box at the top of the page!'
+  elem.innerText = hint.text
 
-  const target = document.getElementById('searchQuery') as HTMLElement
+  const target = document.getElementById(hint.fixture.value) as HTMLElement
 
   const relAnchor = document.createElement('div')
   relAnchor.style.position = 'relative'
@@ -29,5 +74,21 @@ export function init () {
 
   if (target.parentElement) {
     target.parentElement.appendChild(relAnchor)
+  }
+
+  return relAnchor
+}
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+export async function init () {
+  console.log('Hacking Instructor Init')
+
+  for (const hint of challengeInstructions.challenges[0].hints) {
+    const element = loadHint(hint)
+    element.scrollIntoView()
+
+    await sleep(5000)
+    element.remove()
   }
 }
