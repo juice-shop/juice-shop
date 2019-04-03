3cd9a4109f9ab73fed2ed2e65b91cb09f6f93b71
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2019-04-03 16:25:43 +0200

    Make video and subtitles configurable

--- routes/videoHandler.js
+++ routes/videoHandler.js
@@ -7,7 +7,7 @@ const themes = require('../views/themes/themes').themes
 
 exports.getVideo = () => {
   return (req, res, next) => {
-    const path = 'frontend/src/assets/public/videos/JuiceShopJingle.mp4'
+    const path = videoPath()
     const stat = fs.statSync(path)
     const fileSize = stat.size
     const range = req.headers.range
@@ -67,10 +67,25 @@ exports.promotionVideo = () => {
 }
 
 function getSubsFromFile () {
-  try {
-    var data = fs.readFileSync('frontend/dist/frontend/assets/public/subtitles/jingleSubtitles.vtt', 'utf8')
-    return data.toString()
-  } catch (e) {
-    console.log('Error:', e.stack)
+  let subtitles = 'jingleSubtitles.vtt'
+  if (config && config.application && config.application.promotion && config.application.promotion.subtitles !== null) {
+    subtitles = config.application.promotion.subtitles
+    if (subtitles.substring(0, 4) === 'http') {
+      subtitles = decodeURIComponent(subtitles.substring(subtitles.lastIndexOf('/') + 1))
+    }
   }
+  const data = fs.readFileSync('frontend/dist/frontend/assets/public/subtitles/' + subtitles, 'utf8')
+  return data.toString()
+}
+
+function videoPath () {
+  if (config && config.application && config.application.promotion && config.application.promotion.video !== null) {
+    let video = config.application.promotion.video
+
+    if (video.substring(0, 4) === 'http') {
+      video = decodeURIComponent(video.substring(video.lastIndexOf('/') + 1))
+    }
+    return 'frontend/src/assets/public/videos/' + video
+  }
+  return 'frontend/src/assets/public/videos/JuiceShopJingle.mp4'
 }
