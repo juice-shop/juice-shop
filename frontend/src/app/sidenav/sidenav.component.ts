92019aaccca297c36350a2b334c87390095a4cc7
MarcRler <mr144@hdm-stuttgart.de>
2019-04-07 15:50:27 +0200

    refactor(forms): fix various styling issues
    - Last-Login-IP: Inserting a minimum width for the mat card item
     
    - Navbar: Correction of a shopping cart display error on small displays
    
    Sidenav: Since on small displays the toolbar with the reintroduced shopping cart was too full and the search has shifted over a part of the content, the account menu on small displays is now in the Sidenav. This should make the search work as usual again. The account itself is displayed in the Sidenav as a nested menu.
    
    -Two-Factor-Auth: Inserting a minimum width for the mat card item

--- frontend/src/app/sidenav/sidenav.component.ts
+++ frontend/src/app/sidenav/sidenav.component.ts
@@ -2,6 +2,9 @@ import { ChallengeService } from '../Services/challenge.service'
 import { Component, OnInit, EventEmitter, NgZone, Output }from '@angular/core'
 import { SocketIoService } from '../Services/socket-io.service'
 import { AdministrationService } from '../Services/administration.service'
+import { Router } from '@angular/router'
+import { UserService } from '../Services/user.service'
+import { CookieService } from 'ngx-cookie'
 
 @Component({
   selector: 'sidenav',
@@ -12,13 +15,21 @@ export class SidenavComponent implements OnInit {
 
   public applicationName = 'OWASP Juice Shop'
   public gitHubRibbon = true
+  public userEmail = ''
   public scoreBoardVisible: boolean = false
   public version: string = ''
+  public isExpanded = true
+  public showSubmenu: boolean = false
+  public isShowing = false
+  public isSubExpanded = true
+  public showSubSubmenu: boolean = false
+  public isSubShowing = false
 
   @Output() public sidenavToggle = new EventEmitter()
 
   constructor (private administrationService: AdministrationService, private challengeService: ChallengeService,
-    private ngZone: NgZone, private io: SocketIoService) { }
+    private ngZone: NgZone, private io: SocketIoService, private userService: UserService, private cookieService: CookieService,
+    private router: Router) { }
 
   ngOnInit () {
 
@@ -29,6 +40,20 @@ export class SidenavComponent implements OnInit {
     },(err) => console.log(err))
 
     this.getScoreBoardStatus()
+
+    if (localStorage.getItem('token')) {
+      this.getUserDetails()
+    } else {
+      this.userEmail = ''
+    }
+
+    this.userService.getLoggedInState().subscribe((isLoggedIn) => {
+      if (isLoggedIn) {
+        this.getUserDetails()
+      } else {
+        this.userEmail = ''
+      }
+    })
 
     this.ngZone.runOutsideAngular(() => {
       this.io.socket().on('challenge solved', () => {
@@ -41,11 +66,33 @@ export class SidenavComponent implements OnInit {
     return localStorage.getItem('token')
   }
 
+  logout () {
+      this.userService.saveLastLoginIp().subscribe((user: any) => { this.noop() }, (err) => console.log(err))
+      localStorage.removeItem('token')
+      this.cookieService.remove('token', { domain: document.domain })
+      sessionStorage.removeItem('bid')
+      this.userService.isLoggedIn.next(false)
+      this.router.navigate(['/'])
+    }
+
+  goToProfilePage () {
+      window.location.replace('/profile')
+    }
+
+  // tslint:disable-next-line:no-empty
+  noop () { }
+
   getScoreBoardStatus () {
     this.challengeService.find({ name: 'Score Board' }).subscribe((challenges: any) => {
       this.ngZone.run(() => {
         this.scoreBoardVisible = challenges[0].solved
       })
+    }, (err) => console.log(err))
+  }
+
+  getUserDetails () {
+    this.userService.whoAmI().subscribe((user: any) => {
+      this.userEmail = user.email
     }, (err) => console.log(err))
   }
 
