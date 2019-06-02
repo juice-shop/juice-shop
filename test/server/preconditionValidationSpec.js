cabd16321f2109189d33d4e66d07cc2f435c93f2
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2019-06-02 17:51:00 +0200

    Enable support for node.js 12.x
    (experimental!)

--- test/server/preconditionValidationSpec.js
+++ test/server/preconditionValidationSpec.js
@@ -11,12 +11,13 @@ describe('preconditionValidation', () => {
   describe('checkIfRunningOnSupportedNodeVersion', () => {
     const supportedVersion = require('./../../package.json').engines.node
 
-    it('should define the supported semver range as 8 - 11', () => {
-      expect(supportedVersion).to.equal('8 - 11')
+    it('should define the supported semver range as 8 - 12', () => {
+      expect(supportedVersion).to.equal('8 - 12')
       expect(semver.validRange(supportedVersion)).to.not.equal(null)
     })
 
     it('should accept a supported version', () => {
+      expect(checkIfRunningOnSupportedNodeVersion('12.3.0')).to.equal(true)
       expect(checkIfRunningOnSupportedNodeVersion('11.3.0')).to.equal(true)
       expect(checkIfRunningOnSupportedNodeVersion('10.12.0')).to.equal(true)
       expect(checkIfRunningOnSupportedNodeVersion('9.11.2')).to.equal(true)
@@ -24,7 +25,7 @@ describe('preconditionValidation', () => {
     })
 
     it('should fail for an unsupported version', () => {
-      expect(checkIfRunningOnSupportedNodeVersion('12.2.0')).to.equal(false)
+      expect(checkIfRunningOnSupportedNodeVersion('13.0.0')).to.equal(false)
       expect(checkIfRunningOnSupportedNodeVersion('7.10.1')).to.equal(false)
       expect(checkIfRunningOnSupportedNodeVersion('6.14.4')).to.equal(false)
       expect(checkIfRunningOnSupportedNodeVersion('4.9.1')).to.equal(false)
