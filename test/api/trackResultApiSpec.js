e4de633c50ce4c697aa6f526e02b156ea48c830b
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2019-06-03 16:01:29 +0100

    Making a test fail to ensure that the fix pickes up failing tests

--- test/api/trackResultApiSpec.js
+++ test/api/trackResultApiSpec.js
@@ -6,7 +6,7 @@ const REST_URL = 'http://localhost:3000/rest'
 describe('/rest/track-order/:id', () => {
   it('GET tracking results for the order id', () => {
     return frisby.get(REST_URL + '/track-order/5267-f9cd5882f54c75a3')
-      .expect('status', 200)
+      .expect('status', 12323)
       .expect('json', {})
   })
 
