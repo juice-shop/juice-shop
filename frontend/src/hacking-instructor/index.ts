3206a1e1e7ff1c675438f3a936a65696ea97aae7
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2019-06-04 22:00:44 +0100

    Added juice bot as the friendly hacking instructor

--- frontend/src/hacking-instructor/index.ts
+++ frontend/src/hacking-instructor/index.ts
@@ -34,8 +34,8 @@ function loadHint (hint: ChallengeHint): HTMLElement {
   elem.style.position = 'absolute'
   elem.style.zIndex = '20000'
   elem.style.backgroundColor = '#4472C4'
-  elem.style.maxWidth = '300px'
-  elem.style.minWidth = '250px'
+  elem.style.maxWidth = '400px'
+  elem.style.minWidth = hint.text.length > 100 ? '350px' : '250px'
   elem.style.padding = '16px'
   elem.style.borderRadius = '8px'
   elem.style.whiteSpace = 'initial'
@@ -43,7 +43,21 @@ function loadHint (hint: ChallengeHint): HTMLElement {
   elem.style.top = `24px`
   elem.style.cursor = 'pointer'
   elem.style.fontSize = '14px'
-  elem.innerText = hint.text
+  elem.style.display = 'flex'
+  elem.style.alignItems = 'center'
+
+  const picture = document.createElement('img')
+  picture.style.width = '64px'
+  picture.style.height = '64px'
+  picture.style.marginRight = '8px'
+  picture.src = '/assets/public/images/juice_bot.png'
+
+  const textBox = document.createElement('span')
+  textBox.style.flexGrow = '2'
+  textBox.innerText = hint.text
+
+  elem.appendChild(picture)
+  elem.appendChild(textBox)
 
   const relAnchor = document.createElement('div')
   relAnchor.style.position = 'relative'
@@ -75,7 +89,10 @@ export async function startHackingInstructorFor (challengeName: String): Promise
     }
     element.scrollIntoView()
 
-    await Promise.race([ hint.resolved(), waitForClick(element)])
+    await Promise.race([
+      hint.resolved(),
+      waitForClick(element)
+    ])
 
     element.remove()
   }
