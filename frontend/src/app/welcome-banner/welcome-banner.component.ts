5aa3f4acedea5e41473489055643ffb621351863
Martin Rock-Evans <martin.rockevans@gmail.com>
2019-06-05 21:19:20 +0100

    Starting to add a welcome SnackBar. Shows pop-up #1085

---
+++ frontend/src/app/welcome-banner/welcome-banner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-welcome-banner-component',
+    templateUrl: 'welcome-banner.component.html',
+    styles: [`
+    .example-pizza-party {
+      color: hotpink;
+    }
+  `],
+})
+export class WelcomeBannerComponent { }
