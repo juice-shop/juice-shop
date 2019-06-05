214b20f3647731aceacefd3026a56900c3789671
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2019-06-05 14:56:20 +0100

    Added proper type to the queried elements

--- frontend/src/hacking-instructor/helpers/helpers.ts
+++ frontend/src/hacking-instructor/helpers/helpers.ts
@@ -8,7 +8,7 @@ export function waitForInputToHaveValue (inputSelector: string, value: string) {
   return async () => {
     const password = document.querySelector(
       inputSelector
-    )
+    ) as HTMLInputElement
 
     while (true) {
       if (password.value === value) {
@@ -23,7 +23,7 @@ export function waitForInputToNotHaveValue (inputSelector: string, value: string
   return async () => {
     const password = document.querySelector(
       inputSelector
-    )
+    ) as HTMLInputElement
 
     while (true) {
       if (password.value !== value) {
