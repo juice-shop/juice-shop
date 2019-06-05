413b03816fbf2ce7e985dc302628ebc442a0431c
Martin Rock-Evans <martin.rockevans@gmail.com>
2019-06-05 22:12:06 +0100

    Getting the welcome banner content from tanslation

--- frontend/src/app/welcome-banner/welcome-banner.component.ts
+++ frontend/src/app/welcome-banner/welcome-banner.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core'
+import { Component, OnInit} from '@angular/core'
+import { ConfigurationService } from '../Services/configuration.service'
 
 @Component({
     selector: 'app-welcome-banner-component',
@@ -9,4 +10,18 @@ import { Component } from '@angular/core'
     }
   `],
 })
-export class WelcomeBannerComponent { }
+export class WelcomeBannerComponent implements OnInit {
+    public applicationName = 'OWASP Juice Shop'
+
+    constructor(private configurationService: ConfigurationService) { }
+
+    ngOnInit(): void {
+        this.configurationService.getApplicationConfiguration().subscribe((config) => {
+            if (config && config.application) {
+                if (config.application.name !== null) {
+                    this.applicationName = config.application.name
+                }
+            }
+        }, (err) => console.log(err))
+    }
+ }
