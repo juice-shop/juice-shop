2fee2192073a9467e93652fff675568ae5ce1992
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2019-06-06 23:28:40 +0200

    Give XSS exploits on admin page 10sec to actually occur
    [ci skip]

--- test/e2e/registerSpec.js
+++ test/e2e/registerSpec.js
@@ -26,7 +26,7 @@ describe('/#/register', () => {
       browser.waitForAngularEnabled(false)
       const EC = protractor.ExpectedConditions
       browser.get('/#/administration')
-      browser.wait(EC.alertIsPresent(), 5000, "'xss' alert is not present on /#/administration")
+      browser.wait(EC.alertIsPresent(), 10000, "'xss' alert is not present on /#/administration")
       browser.switchTo().alert().then(alert => {
         expect(alert.getText()).toEqual('xss')
         alert.accept()
