4772819a1b1272667125b614a6d2ff119b9f0610
agrawalarpit14 <agrawal.arpit14@gmail.com>
2019-07-18 17:18:10 +0530

    Order History route at /#/order-history

--- frontend/src/app/order-history/order-history.component.ts
+++ frontend/src/app/order-history/order-history.component.ts
@@ -5,8 +5,11 @@ import { BasketService } from '../Services/basket.service'
 import { ProductDetailsComponent } from '../product-details/product-details.component'
 import { MatDialog } from '@angular/material/dialog'
 import { Product } from '../Models/product.model'
+import { ProductService } from '../Services/product.service'
+import { Router } from '@angular/router'
 
 interface StrippedProduct {
+  id: number,
   name: string,
   price: number,
   quantity: number,
@@ -17,7 +20,8 @@ interface Order {
   orderId: string,
   totalPrice: number,
   bonus: number,
-  products: MatTableDataSource<StrippedProduct>
+  products: MatTableDataSource<StrippedProduct>,
+  delivered: boolean
 }
 
 @Component({
@@ -27,17 +31,25 @@ interface Order {
 })
 export class OrderHistoryComponent implements OnInit {
 
-  public tableColumns = ['product','price','quantity','total price']
+  public tableColumns = ['product', 'price', 'quantity', 'total price', 'review']
   public orders: Order[] = []
+  public emptyState: boolean = true
 
-  constructor (private dialog: MatDialog, private orderHistory: OrderHistoryService, private basketService: BasketService) { }
+  constructor (private router: Router, private dialog: MatDialog, private orderHistory: OrderHistoryService, private basketService: BasketService, private productService: ProductService) { }
 
   ngOnInit () {
     this.orderHistory.get().subscribe((orders) => {
+      orders = orders.reverse()
+      if (orders.length === 0) {
+        this.emptyState = true
+      } else {
+        this.emptyState = false
+      }
       for (const order of orders) {
         const products: StrippedProduct[] = []
         for (const product of order.products) {
           products.push({
+            id: product.id,
             name: product.name,
             price: product.price,
             quantity: product.quantity,
@@ -48,25 +60,43 @@ export class OrderHistoryComponent implements OnInit {
           orderId: order.orderId,
           totalPrice: order.totalPrice,
           bonus: order.bonus,
-          products: new MatTableDataSource<StrippedProduct>(products)
+          products: new MatTableDataSource<StrippedProduct>(products),
+          delivered: order.delivered
         })
       }
-      console.log(this.orders)
     })
   }
 
-  showDetail (element: Product) {
-    this.dialog.open(ProductDetailsComponent, {
-      width: '500px',
-      height: 'max-content',
-      data: {
-        productData: element
+  showDetail (id: number) {
+    this.productService.get(id).subscribe((product) => {
+      const element: Product = {
+        id: product.id,
+        name: product.name,
+        description: product.description,
+        image: product.image,
+        price: product.price,
+        points: Math.round(product.price / 10)
       }
+      this.dialog.open(ProductDetailsComponent, {
+        width: '500px',
+        height: 'max-content',
+        data: {
+          productData: element
+        }
+      })
     })
   }
 
   openConfirmationPDF (orderId) {
     const redirectUrl = this.basketService.hostServer + '/ftp/order_' + orderId + '.pdf'
     window.open(redirectUrl,'_blank')
+  }
+
+  trackOrder (orderId) {
+    this.router.navigate(['/track-result'], {
+      queryParams: {
+        id: orderId
+      }
+    })
   }
 }
