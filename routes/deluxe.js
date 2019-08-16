829482e650d20cc3dafb6a5612cfc55a673a5e29
bjoern.kimminich <bjoern.kimminich@kuehne-nagel.com>
2019-08-16 19:33:50 +0200

    Tidy up server side error/confirmation messages

--- routes/deluxe.js
+++ routes/deluxe.js
@@ -8,9 +8,9 @@ module.exports.upgradeToDeluxe = function upgradeToDeluxe () {
     }
     const user = await models.User.update({ role: insecurity.roles.deluxe }, { where: { id: req.body.UserId, role: insecurity.roles.customer } })
     if (user[0] !== 0) {
-      res.status(200).json({ status: 'success', data: { confirmation: 'Congratulations, you are now a deluxe customer.' } })
+      res.status(200).json({ status: 'success', data: { confirmation: 'Congratulations! You are now a deluxe member!' } })
     } else {
-      res.status(400).json({ status: 'error', error: 'Please try again.' })
+      res.status(400).json({ status: 'error', error: 'Something went wrong. Please try again!' })
     }
   }
 }
@@ -20,9 +20,9 @@ module.exports.deluxeMembershipStatus = function deluxeMembershipStatus () {
     if (insecurity.isCustomer(req)) {
       res.status(200).json({ status: 'success', data: { membershipCost: 49 } })
     } else if (insecurity.isDeluxe(req)) {
-      res.status(400).json({ status: 'error', error: 'Congratulations! You are a deluxe member.' })
+      res.status(400).json({ status: 'error', error: 'You are already a deluxe member!' })
     } else {
-      res.status(400).json({ status: 'error', error: 'You are not eligible for deluxe membership.' })
+      res.status(400).json({ status: 'error', error: 'You are not eligible for deluxe membership!' })
     }
   }
 }
