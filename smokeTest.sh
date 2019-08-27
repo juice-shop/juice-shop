723051e9f2e5d5a31fef61732d83130762897304
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2019-08-27 21:44:16 +0200

    Fix bash color for passed smoke test

--- smokeTest.sh
+++ smokeTest.sh
@@ -1,6 +1,6 @@
 sleep 5
 if curl http://localhost:3000 | grep -q '<app-root></app-root>'; then
-  echo -e "\e[42mSmoke test passed!\e[0m"
+  echo -e "\e[32mSmoke test passed!\e[0m"
   exit 0
 else
   echo -e "\e[31mSmoke test failed!\e[0m"
