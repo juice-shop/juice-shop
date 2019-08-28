554d5b973a6004218f4f5cf000af26245df8af71
bjoern.kimminich <bjoern.kimminich@kuehne-nagel.com>
2019-08-28 14:59:53 +0200

    Fall back to sleep 10sec due to incompatibility on Travis

--- smokeTest.sh
+++ smokeTest.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-if curl http://localhost:3000 --retry 4 --retry-connrefused | grep -q '<app-root></app-root>'; then
+sleep 10
+if curl http://localhost:3000 | grep -q '<app-root></app-root>'; then
   echo -e "\e[32mSmoke test passed!\e[0m"
   exit 0
 else
