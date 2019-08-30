d91cd5b103dec188bc51bb5bf8296016a01d4f1a
bjoern.kimminich <bjoern.kimminich@kuehne-nagel.com>
2019-08-30 19:37:22 +0200

    Simplify and prettify log output

--- test/smoke/smoke-test.sh
+++ test/smoke/smoke-test.sh
@@ -1,32 +1,42 @@
 #!/bin/sh
 
 printf "Waiting 20sec for %s to launch" "$1"
-for i in {1..20}; do
-  sleep 1;
-  printf "."
-done
-printf "\n"
+sleep 5
+printf "....."
+sleep 5
+printf "....."
+sleep 5
+printf "....."
+sleep 5
+printf ".....\n"
+printf "Running smoke tests...\n"
 
 EXIT=0
-if curl "$1" | grep -q '<app-root></app-root>'; then
+if curl "$1" -s | grep -q '<app-root></app-root>'; then
   printf "\033[0;32mIndex smoke test passed!\033[0m\n"
 else
   printf "\033[0;31mIndex smoke test failed!\033[0m\n"
   EXIT=$((EXIT+1))
 fi
 
-if curl "$1/api/Challenges" | grep -q '"status":"success"'; then
+if curl "$1/api/Challenges" -s | grep -q '"status":"success"'; then
   printf "\033[0;32mAPI smoke test passed!\033[0m\n"
 else
   printf "\033[0;31mAPI smoke test failed!\033[0m\n"
-  EXIT=$((EXIT+2))
+  EXIT=$((EXIT+1))
 fi
 
-if curl "$1/main-es2015.js" | grep -q 'this.applicationName="OWASP Juice Shop"'; then
+if curl "$1/main-es2015.js" -s | grep -q 'this.applicationName="OWASP Juice Shop"'; then
   printf "\033[0;32mAngular smoke test passed!\033[0m\n"
 else
   printf "\033[0;31mAngular smoke test failed!\033[0m\n"
-  EXIT=$((EXIT+4))
+  EXIT=$((EXIT+1))
 fi
 
+printf "Smoke tests exiting with code %s (" "$EXIT"
+if [ $EXIT -gt 0 ]; then
+  printf "\033[0;31mFAILED\033[0m)\n"
+else
+  printf "\033[0;32mSUCCESS\033[0m)\n"
+fi
 exit $EXIT
