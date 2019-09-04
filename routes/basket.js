75d91d3e251f1ef6cb62baa135b63028f82d7139
bjoern.kimminich <bjoern.kimminich@kuehne-nagel.com>
2019-09-04 15:41:38 +0200

    Apply product name i18n on basket, order and invoice

--- routes/basket.js
+++ routes/basket.js
@@ -15,6 +15,11 @@ module.exports = function retrieveBasket () {
             utils.solve(challenges.basketAccessChallenge)
           }
         }
+        if (basket.Products && basket.Products.length > 0) {
+          for (let i = 0; i < basket.Products.length; i++) {
+            basket.Products[i].name = req.__(basket.Products[i].name)
+          }
+        }
         res.json(utils.queryResultToJson(basket))
       }).catch(error => {
         next(error)
