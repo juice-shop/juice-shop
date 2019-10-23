419ea40b2ae2a1baccfddb8ef20f9a5ea7b1a678
bjoern.kimminich <bjoern.kimminich@kuehne-nagel.com>
2019-10-23 09:17:07 +0200

    Add option to create Memory via user definition YAML

--- data/datacreator.js
+++ data/datacreator.js
@@ -80,7 +80,7 @@ async function createUsers () {
   const users = await loadStaticData('users')
 
   await Promise.all(
-    users.map(async ({ username, email, password, customDomain, key, role, deletedFlag, profileImage, securityQuestion, feedback, address, card, totpSecret: totpSecret = '' }) => {
+    users.map(async ({ username, email, password, customDomain, key, role, memory, deletedFlag, profileImage, securityQuestion, feedback, address, card, totpSecret: totpSecret = '' }) => {
       try {
         const completeEmail = customDomain ? email : `${email}@${config.get('application.domain')}`
         const user = await models.User.create({
@@ -94,6 +94,7 @@ async function createUsers () {
         datacache.users[key] = user
         if (securityQuestion) await createSecurityAnswer(user.id, securityQuestion.id, securityQuestion.answer)
         if (feedback) await createFeedback(user.id, feedback.comment, feedback.rating)
+        if (memory) await createMemory(user.id, memory.imagePath, memory.caption)
         if (deletedFlag) await deleteUser(user.id)
         if (address) await createAddresses(user.id, address)
         if (card) await createCards(user.id, card)
@@ -400,6 +401,12 @@ function createAnonymousFeedback () {
 function createFeedback (UserId, comment, rating) {
   return models.Feedback.create({ UserId, comment, rating }).catch((err) => {
     logger.error(`Could not insert Feedback ${comment} mapped to UserId ${UserId}: ${err.message}`)
+  })
+}
+
+function createMemory (UserId, imagePath, caption) {
+  return models.Memory.create({ UserId, imagePath, caption }).catch((err) => {
+    logger.error(`Could not insert Memory ${caption} mapped to UserId ${UserId}: ${err.message}`)
   })
 }
 
