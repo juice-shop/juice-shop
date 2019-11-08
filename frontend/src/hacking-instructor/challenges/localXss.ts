e6afb64dcc5ef900bcc36cef71d1489410cf8055
bjoern.kimminich <bjoern.kimminich@kuehne-nagel.com>
2019-11-08 19:45:09 +0100

    Make all steps demanding input unskippable

--- frontend/src/hacking-instructor/challenges/localXss.ts
+++ frontend/src/hacking-instructor/challenges/localXss.ts
@@ -18,11 +18,13 @@ export const DomXssInstruction: ChallengeInstruction = {
     {
       text: "Let's start by searching for all products containing `owasp` in their name or description.",
       fixture: '#searchQuery',
+      unskippable: true,
       resolved: waitForInputToHaveValue('#searchQuery input', 'owasp')
     },
     {
       text: 'Now hit enter.',
       fixture: '#searchQuery',
+      unskippable: true,
       resolved: waitForElementsInnerHtmlToBe('#searchValue', 'owasp')
     },
     {
@@ -43,11 +45,13 @@ export const DomXssInstruction: ChallengeInstruction = {
     {
       text: 'Change your search value into `<h1>owasp` to see if we can inject HTML.',
       fixture: '#searchQuery',
+      unskippable: true,
       resolved: waitForInputToHaveValue('#searchQuery input', '<h1>owasp')
     },
     {
       text: 'Hit enter again.',
       fixture: '#searchQuery',
+      unskippable: true,
       resolved: waitForElementsInnerHtmlToBe('#searchValue', '<h1>owasp</h1>') // Browsers will autocorrect the unclosed tag.
     },
     {
@@ -63,11 +67,13 @@ export const DomXssInstruction: ChallengeInstruction = {
     {
       text: "Let's now try to inject JavaScript. Type `<script>alert(xss)</script>` into the search box now.",
       fixture: '#searchQuery',
+      unskippable: true,
       resolved: waitForInputToHaveValue('#searchQuery input', '<script>alert(xss)</script>')
     },
     {
       text: 'Hit enter again.',
       fixture: '#searchQuery',
+      unskippable: true,
       resolved: waitForElementsInnerHtmlToBe('#searchValue', '<script>alert(xss)</script>')
     },
     {
