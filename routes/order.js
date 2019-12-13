5d69220f31ac2fac469209b92bc514d33b271c53
bjoern.kimminich <bjoern.kimminich@kuehne-nagel.com>
2019-12-13 14:02:46 +0100

    Reinstate i18n but leave PDF-password protection off

--- routes/order.js
+++ routes/order.js
@@ -91,7 +91,7 @@ module.exports = function placeOrder () {
               bonus: itemBonus
             }
             basketProducts.push(product)
-            doc.text(BasketItem.quantity + 'x ' + req.__(name) + ' ea. ' + itemPrice + ' = ' + itemTotal + '¤')
+            doc.text(`${BasketItem.quantity}x ${req.__(name)} ${req.__('ea.')} ${itemPrice} = ${itemTotal}¤`)
             doc.moveDown()
             totalPrice += itemTotal
             totalPoints += itemBonus
@@ -101,7 +101,7 @@ module.exports = function placeOrder () {
           let discountAmount = 0
           if (discount > 0) {
             discountAmount = (totalPrice * (discount / 100)).toFixed(2)
-            doc.text(discount + '% discount from coupon: -' + discountAmount + '¤')
+            doc.text(`${discount}% ${req.__('discount from coupon')}: -${discountAmount}¤`)
             doc.moveDown()
             totalPrice -= discountAmount
           }
@@ -115,12 +115,12 @@ module.exports = function placeOrder () {
           }
           const deliveryAmount = insecurity.isDeluxe(req) ? deliveryMethod.deluxePrice : deliveryMethod.price
           totalPrice += deliveryAmount
-          doc.text('Delivery Price: ' + deliveryAmount.toFixed(2) + '¤')
+          doc.text(`${req.__('Delivery Price')}: ${deliveryAmount.toFixed(2)}¤`)
           doc.moveDown()
-          doc.font('Helvetica-Bold', 20).text('Total Price: ' + totalPrice.toFixed(2) + '¤')
+          doc.font('Helvetica-Bold', 20).text(`${req.__('Total Price')}: ${totalPrice.toFixed(2)}¤`)
           doc.moveDown()
-          doc.font('Helvetica-Bold', 15).text('Bonus Points Earned: ' + totalPoints)
-          doc.font('Times-Roman', 15).text('(The bonus points from this order will be added 1:1 to your wallet ¤-fund for future purchases!)')
+          doc.font('Helvetica-Bold', 15).text(`${req.__('Bonus Points Earned')}: ${totalPoints}`)
+          doc.font('Times-Roman', 15).text(`(${req.__('The bonus points from this order will be added 1:1 to your wallet ¤-fund for future purchases!')})`)
           doc.moveDown()
           doc.moveDown()
           doc.font('Times-Roman', 15).text('Thank you for your order!')
