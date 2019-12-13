388b9173c18cc0bfd043d349a2fa054ca1c4965f
bjoern.kimminich <bjoern.kimminich@kuehne-nagel.com>
2019-12-13 12:08:20 +0100

    Revert later i18n calls in order process
    (to verify impact on API tests running into HTTP header errors)

--- routes/order.js
+++ routes/order.js
@@ -116,15 +116,15 @@ module.exports = function placeOrder () {
           }
           const deliveryAmount = insecurity.isDeluxe(req) ? deliveryMethod.deluxePrice : deliveryMethod.price
           totalPrice += deliveryAmount
-          doc.text(`${req.__('Delivery Price')}: ${deliveryAmount.toFixed(2)}¤`)
+          doc.text(`Delivery Price: ${deliveryAmount.toFixed(2)}¤`)
           doc.moveDown()
-          doc.font('Helvetica-Bold', 20).text(`${req.__('Total Price')}: ${totalPrice.toFixed(2)}¤`)
+          doc.font('Helvetica-Bold', 20).text(`Total Price: ${totalPrice.toFixed(2)}¤`)
           doc.moveDown()
-          doc.font('Helvetica-Bold', 15).text(`${req.__('Bonus Points Earned')}: ${totalPoints}`)
-          doc.font('Times-Roman', 15).text(`(${req.__('The bonus points from this order will be added 1:1 to your wallet ¤-fund for future purchases!')}`)
+          doc.font('Helvetica-Bold', 15).text(`Bonus Points Earned: ${totalPoints}`)
+          doc.font('Times-Roman', 15).text(`(The bonus points from this order will be added 1:1 to your wallet ¤-fund for future purchases!`)
           doc.moveDown()
           doc.moveDown()
-          doc.font('Times-Roman', 15).text(req.__('Thank you for your order!'))
+          doc.font('Times-Roman', 15).text('Thank you for your order!')
           doc.moveDown()
           doc.image(path.join(__dirname, '../frontend/dist/frontend/assets/public/images/', logo), { width: 50 })
           doc.end()
