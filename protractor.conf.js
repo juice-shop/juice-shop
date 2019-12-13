8441bdeabc7972c15a3113816f6c1e8dbdad17ab
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2019-12-13 22:01:02 +0100

    Set cookie expiration on e2e startup

--- protractor.conf.js
+++ protractor.conf.js
@@ -44,9 +44,10 @@ exports.config = {
 
     // Get all banners out of the way
     browser.get('/#')
-    browser.manage().addCookie({ name: 'cookieconsent_status', value: 'dismiss' })
-    browser.manage().addCookie({ name: 'welcomebanner_status', value: 'dismiss' })
-    browser.get('/#')
+    let expiry = new Date()
+    expiry.setFullYear(expiry.getFullYear() + 1)
+    browser.manage().addCookie({ name: 'cookieconsent_status', value: 'dismiss', expiry })
+    browser.manage().addCookie({ name: 'welcomebanner_status', value: 'dismiss', expiry })
 
     // Ensure score board shows all challenges (by default only 1-star challenges are shown)
     browser.get('/#/score-board')
