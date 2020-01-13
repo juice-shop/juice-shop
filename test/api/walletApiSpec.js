8f6491282ed6e3afd95f3f827706d5a1a68c96db
bjoern.kimminich <bjoern.kimminich@kuehne-nagel.com>
2020-01-13 18:04:30 +0100

    Add initial API test for wallet route

---
+++ test/api/walletApiSpec.js
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2014-2020 Bjoern Kimminich.
+ * SPDX-License-Identifier: MIT
+ */
+
+const frisby = require('frisby')
+
+const API_URL = 'http://localhost:3000/api'
+const REST_URL = 'http://localhost:3000/rest'
+
+const jsonHeader = { 'content-type': 'application/json' }
+let authHeader
+
+beforeAll(() => {
+  return frisby.post(REST_URL + '/user/login', {
+    headers: jsonHeader,
+    body: {
+      email: 'jim@juice-sh.op',
+      password: 'ncc-1701'
+    }
+  })
+    .expect('status', 200)
+    .then(({ json }) => {
+      authHeader = { Authorization: 'Bearer ' + json.authentication.token, 'content-type': 'application/json' }
+    })
+})
+
+describe('/api/Wallets', () => {
+  it('GET wallet is forbidden via public API', () => {
+    return frisby.get(API_URL + '/Wallets')
+      .expect('status', 401)
+  })
+})
