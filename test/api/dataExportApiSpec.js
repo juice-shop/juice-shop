e790358c55090af332d56b4af6c30fb647645c1b
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2020-01-14 18:38:00 +0100

    Fix Memory API endpoint

--- test/api/dataExportApiSpec.js
+++ test/api/dataExportApiSpec.js
@@ -196,7 +196,7 @@ describe('/rest/user/data-export', () => {
     })
       .expect('status', 200)
       .then(({ json: jsonLogin }) => {
-        return frisby.post(API_URL + '/Memorys', {
+        return frisby.post(REST_URL + '/memories', {
           headers: {
             Authorization: 'Bearer ' + jsonLogin.authentication.token,
             'Content-Type': form.getHeaders()['content-type']
