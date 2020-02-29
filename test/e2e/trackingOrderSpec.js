c7c42c85ce7bd6020c1cd73a0962bc92f5f8ad6c
J12934 <13718901+J12934@users.noreply.github.com>
2020-02-29 13:08:38 -0500

    Delete tracking order page
    Order tracking is now only supposed to be accessed via the "Order History" page.
    
    This increases the challenge difficulty a bit, as it requires a bit more attention to detect

--- test/e2e/trackingOrderSpec.js
+++ test/e2e/trackingOrderSpec.js
@@ -6,22 +6,16 @@
 const utils = require('../../lib/utils')
 
 describe('/#/track-order', () => {
-  let orderId, trackButton
-
-  beforeEach(() => {
-    browser.get('/#/track-order')
-    orderId = element(by.id('orderId'))
-    trackButton = element(by.id('trackButton'))
-  })
-
   if (!utils.disableOnContainerEnv()) {
     describe('challenge "reflectedXss"', () => {
       it('Order Id should be susceptible to reflected XSS attacks', () => {
         const EC = protractor.ExpectedConditions
 
-        orderId.sendKeys('<iframe src="javascript:alert(`xss`)">')
-        trackButton.click()
-        browser.wait(EC.alertIsPresent(), 5000, "'xss' alert is not present on /#/track-order ")
+        browser.waitForAngularEnabled(false)
+        browser.get('/#/track-result?id=<iframe src="javascript:alert(`xss`)">')
+        browser.refresh()
+
+        browser.wait(EC.alertIsPresent(), 5000, "'xss' alert is not present on /#/track-result ")
         browser.switchTo().alert().then(alert => {
           expect(alert.getText()).toEqual('xss')
           alert.accept()
