531e32d9d84bb76b16c51b4dffbed1ce5a26f781
Scar26 <mmatty26@gmail.com>
2020-03-01 10:16:19 +0530

    fix forgedJWT e2e test
    Signed-off-by: Scar26 <mmatty26@gmail.com>

--- test/e2e/forgedJwtSpec.js
+++ test/e2e/forgedJwtSpec.js
@@ -15,7 +15,7 @@ describe('/', () => {
 
   describe('challenge "jwtForged"', () => {
     it('should accept a token HMAC-signed with public RSA key with email rsa_lord@juice-sh.op in the payload ', () => {
-      browser.executeScript('localStorage.setItem("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImVtYWlsIjoicnNhX2xvcmRAanVpY2Utc2gub3AifSwiaWF0IjoxNTA4NjM5NjEyLCJleHAiOjk5OTk5OTk5OTl9.dFeqI0EGsOecwi5Eo06dFUBtW5ziRljFgMWOCYeA8yw")')
+      browser.executeScript('localStorage.setItem("token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRhIjp7ImVtYWlsIjoicnNhX2xvcmRAanVpY2Utc2gub3AifSwiaWF0IjoxNTgzMDM3NzExfQ.gShXDT5TrE5736mpIbfVDEcQbLfteJaQUG7Z0PH8Xc8")')
       browser.get('/#/')
     })
 
