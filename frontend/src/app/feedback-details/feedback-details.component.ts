88b3199045ea9efc6656e5b0ab44aa0ee870de91
MarcRler <marc.ruettler@live.de>
2020-03-08 23:47:43 +0100

    refactored admin Section

---
+++ frontend/src/app/feedback-details/feedback-details.component.ts
@@ -0,0 +1,26 @@
+/*
+* Copyright (c) 2014-2020 Bjoern Kimminich.
+* SPDX-License-Identifier: MIT
+*/
+
+import { UserService } from '../Services/user.service'
+import { Component, OnInit, Inject } from '@angular/core'
+import { MAT_DIALOG_DATA } from '@angular/material/dialog'
+
+@Component({
+  selector: 'app-feedback-details',
+  templateUrl: './feedback-details.component.html',
+  styleUrls: ['./feedback-details.component.scss']
+})
+export class FeedbackDetailsComponent implements OnInit {
+
+  public feedback: any
+  public id: any
+  constructor (@Inject(MAT_DIALOG_DATA) public dialogData: any) { }
+
+  ngOnInit () {
+    this.feedback = this.dialogData.feedback
+    this.id = this.dialogData.id
+  }
+
+}
