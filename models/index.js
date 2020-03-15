83371299037e4eecdeea59fbe6662f380f3bb9c2
Scar26 <mmatty26@gmail.com>
2020-03-15 14:37:13 +0530

    increase retries on SQLite connection
    Signed-off-by: Scar26 <mmatty26@gmail.com>

--- models/index.js
+++ models/index.js
@@ -10,6 +10,13 @@ const sequelizeNoUpdateAttributes = require('sequelize-noupdate-attributes')
 const Sequelize = require('sequelize')
 const sequelize = new Sequelize('database', 'username', 'password', {
   dialect: 'sqlite',
+  retry: {
+    match: [
+      /SQLITE_BUSY/
+    ],
+    name: 'query',
+    max: 5
+  },
   storage: 'data/juiceshop.sqlite',
   logging: false,
   pool: {
