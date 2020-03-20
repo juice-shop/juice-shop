8a47e3893f88a9e57fa2561ab97cae1ffcb59577
mu71l473d <judoka_rene@hotmail.com>
2020-03-20 16:05:23 +0100

    new version

--- startshop.sh
+++ startshop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker stop $(docker ps -qa)
-docker run -d -e "NODE_ENV=ctf" -e "NODE_ENV=kiwa" -p 3000:3000 mu71l473d/kiwa-shop:v1.0.0
+docker run -d -e "NODE_ENV=kiwa" -p 3000:3000 mu71l473d/kiwa-shop:v1.0.0
 
