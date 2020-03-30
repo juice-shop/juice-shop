7988d14b7a019654cc77ed58675ff5946573d2ef
JuiceShopBot <61591748+JuiceShopBot@users.noreply.github.com>
2020-03-30 14:36:37 +0000

    Auto-fix linting issues
    Signed-off-by: JuiceShopBot <61591748+JuiceShopBot@users.noreply.github.com>

--- frontend/src/hacking-instructor/tutorialUnavailable.ts
+++ frontend/src/hacking-instructor/tutorialUnavailable.ts
@@ -13,19 +13,19 @@ export const TutorialUnavailableInstruction: ChallengeInstruction = {
   hints: [
     {
       text:
-        "😓 Sorry, this hacking challenge does not have a step-by-step tutorial (yet) ... 🧭 Can you find your own way to solve it?",
+        '😓 Sorry, this hacking challenge does not have a step-by-step tutorial (yet) ... 🧭 Can you find your own way to solve it?',
       fixture: 'app-navbar',
       resolved: waitInMs(15000)
     },
     {
       text:
-        "Do you want to contribute a tutorial for this challenge? [Check out our documentation](https://pwning.owasp-juice.shop/part3/tutorials.html) to learn how!",
+        'Do you want to contribute a tutorial for this challenge? [Check out our documentation](https://pwning.owasp-juice.shop/part3/tutorials.html) to learn how!',
       fixture: 'app-navbar',
       resolved: waitInMs(15000)
     },
     {
       text:
-        "And now: 👾 **GLHF** with this challenge! ",
+        'And now: 👾 **GLHF** with this challenge! ',
       fixture: 'app-navbar',
       resolved: waitInMs(10000)
     }
