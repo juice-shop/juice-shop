2c87d667254861a42f8000400f5d76df3f40ec5e
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2020-04-14 22:20:42 +0200

    Allow no external payment requests

--- server.js
+++ server.js
@@ -147,6 +147,11 @@ app.use(helmet.noSniff())
 app.use(helmet.frameguard())
 // app.use(helmet.xssFilter()); // = no protection from persisted XSS via RESTful API
 app.disable('x-powered-by')
+app.use(helmet.featurePolicy({
+  features: {
+    payment: ["'self'"]
+  }
+}))
 
 /* Remove duplicate slashes from URL which allowed bypassing subsequent filters */
 app.use((req, res, next) => {
