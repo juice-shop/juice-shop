dc4edacf0d7007c5bfedf36864f8e1b03c72aa26
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2020-06-28 19:23:46 +0200

    Remove hammer.js polyfills
    (might have to add back if broken animations are observed)

--- frontend/src/polyfills.ts
+++ frontend/src/polyfills.ts
@@ -80,4 +80,3 @@ import 'zone.js/dist/zone'  // Included with Angular CLI.
 /** *************************************************************************************************
  * APPLICATION IMPORTS
  */
-import 'hammerjs/hammer'
