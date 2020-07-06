802959ef1bed1dfa8ea8a2d55873a2e2a8be4f11
Scar26 <mmatty26@gmail.com>
2020-07-07 00:51:47 +0530

    fix linting
    Signed-off-by: Scar26 <mmatty26@gmail.com>

--- test/api/chatBotSpec.js
+++ test/api/chatBotSpec.js
@@ -65,7 +65,7 @@ describe('/chatbot', () => {
       email: `J12934@${config.get('application.domain')}`,
       password: '0Y8rMnww$*9VFYE§59-!Fg1L6t&6lB'
     })
-    bot.addUser('12345' ,'J12934')
+    bot.addUser('12345', 'J12934')
     const trainingData = JSON.parse(fs.readFileSync(`data/BotTrainingData/${config.get('application.chatBot.trainingData')}`, { encoding: 'utf8' }))
     const testCommand = trainingData.intents[0].question
     const testResponse = await bot.respond(testCommand, 12345)
