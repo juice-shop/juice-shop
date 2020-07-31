0d6b8da31e4a6e6989270b35d1292e0d20b06c7f
Scar26 <mmatty26@gmail.com>
2020-08-01 04:20:11 +0530

    linting fixes
    Signed-off-by: Scar26 <mmatty26@gmail.com>

--- frontend/src/app/Services/chatbot.service.ts
+++ frontend/src/app/Services/chatbot.service.ts
@@ -7,18 +7,16 @@ import { catchError, map } from 'rxjs/operators'
   providedIn: 'root'
 })
 export class ChatbotService {
+  private hostServer = environment.hostServer
+  private host = this.hostServer + '/rest/chatbot'
 
-    private hostServer = environment.hostServer
-    private host = this.hostServer + '/rest/chatbot'
+  constructor (private http: HttpClient) { }
 
-    constructor (private http: HttpClient) { }
+  getChatbotStatus () {
+    return this.http.get(this.host + '/status').pipe(map((response: any) => response), catchError(error => { throw error }))
+  }
 
-    getChatbotStatus () {
-      return this.http.get(this.host + '/status').pipe(map((response: any) => response), catchError(error => { throw error }))
-    }
-
-    getResponse (action, query) {
-      return this.http.post(this.host + '/respond', { action: action, query: query }).pipe(map((response: any) => response), catchError(error => { throw error }))
-    }
-    
+  getResponse (action, query) {
+    return this.http.post(this.host + '/respond', { action: action, query: query }).pipe(map((response: any) => response), catchError(error => { throw error }))
+  }
 }
