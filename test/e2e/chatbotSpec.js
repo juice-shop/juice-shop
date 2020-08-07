9a640a68530ae3302c65a1b6cf244e0a4903874c
Scar26 <mmatty26@gmail.com>
2020-08-07 06:38:13 +0530

    fix linting issues
    Signed-off-by: Scar26 <mmatty26@gmail.com>

--- test/e2e/chatbotSpec.js
+++ test/e2e/chatbotSpec.js
@@ -4,28 +4,27 @@
  */
 
 const config = require('config')
-const utils = require('../../lib/utils')
 
 describe('/chatbot', () => {
-    let username, submitButton, messageBox
-    protractor.beforeEach.login({ email: 'admin@' + config.get('application.domain'), password: 'admin123' })
+  let username, submitButton, messageBox
+  protractor.beforeEach.login({ email: 'admin@' + config.get('application.domain'), password: 'admin123' })
 
-    describe('challenge "killJuicy"', () => {
-        it('should be possible to kill the chatbot by setting the process to null', () => {
-            browser.waitForAngularEnabled(false)
-            browser.get(protractor.basePath + '/profile')
-            username = element(by.id('username'))
-            submitButton = element(by.id('submit'))
-            username.sendKeys('admin"); process=null; usersusers.addUser("1337", "test')
-            submitButton.click()
-            browser.get(protractor.basePath + '/chatbot')
-            browser.driver.sleep(5000)
-            browser.waitForAngularEnabled(true)
-            messageBox = element(by.id('message'))
-            messageBox.sendKeys('hi')
-            messageBox.sendKeys(Key.ENTER)
-            browser.driver.sleep(5000)
-            protractor.expect.challengeSolved({ challenge: 'Kill Juicy' })
-        })
+  describe('challenge "killJuicy"', () => {
+    it('should be possible to kill the chatbot by setting the process to null', () => {
+      browser.waitForAngularEnabled(false)
+      browser.get(protractor.basePath + '/profile')
+      username = element(by.id('username'))
+      submitButton = element(by.id('submit'))
+      username.sendKeys('admin"); process=null; usersusers.addUser("1337", "test')
+      submitButton.click()
+      browser.get(protractor.basePath + '/chatbot')
+      browser.driver.sleep(5000)
+      browser.waitForAngularEnabled(true)
+      messageBox = element(by.id('message'))
+      messageBox.sendKeys('hi')
+      messageBox.sendKeys(protractor.Key.ENTER)
+      browser.driver.sleep(5000)
+      protractor.expect.challengeSolved({ challenge: 'Kill Juicy' })
     })
+  })
 })
