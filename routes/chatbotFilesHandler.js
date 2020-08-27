2a286d3744ffa96935854d56ef0df1c3de1b200c
Scar26 <mmatty26@gmail.com>
2020-08-27 14:24:02 +0530

    linting fixes
    Signed-off-by: Scar26 <mmatty26@gmail.com>

--- routes/chatbotFilesHandler.js
+++ routes/chatbotFilesHandler.js
@@ -3,9 +3,9 @@ const config = require('config')
 const utils = require('../lib/utils')
 
 exports.avatar = () => {
-    return (req, res, next) => {
-        const filename = utils.extractFilename(config.get('application.chatBot.avatar'))
-        const avatar = fs.createReadStream(`frontend/dist/frontend/assets/public/images/${filename}`)
-        avatar.pipe(res)
-    }
+  return (req, res, next) => {
+    const filename = utils.extractFilename(config.get('application.chatBot.avatar'))
+    const avatar = fs.createReadStream(`frontend/dist/frontend/assets/public/images/${filename}`)
+    avatar.pipe(res)
+  }
 }
