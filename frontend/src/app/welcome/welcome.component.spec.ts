ba926694ed132eaf6c81b6ff9de62204154a7434
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2020-09-03 19:20:08 +0200

    Wrap relevant tests into async function calls

--- frontend/src/app/welcome/welcome.component.spec.ts
+++ frontend/src/app/welcome/welcome.component.spec.ts
@@ -8,7 +8,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { MatDialog, MatDialogModule } from '@angular/material/dialog'
 import { CookieService } from 'ngx-cookie-service'
 
-import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing'
+import { async, ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing'
 
 import { WelcomeComponent } from './welcome.component'
 import { of } from "rxjs";
@@ -22,9 +22,9 @@ fdescribe('WelcomeComponent', () => {
   let dialog: any
 
   beforeEach(async(() => {
-    configurationService = jasmine.createSpyObj('ConfigurationService',['getApplicationConfiguration'])
-    configurationService.getApplicationConfiguration.and.returnValue(of({ application: { } }))
-    dialog = jasmine.createSpyObj('MatDialog',['open'])
+    configurationService = jasmine.createSpyObj('ConfigurationService', ['getApplicationConfiguration'])
+    configurationService.getApplicationConfiguration.and.returnValue(of({application: {}}))
+    dialog = jasmine.createSpyObj('MatDialog', ['open'])
     dialog.open.and.returnValue(null)
 
     TestBed.configureTestingModule({
@@ -35,12 +35,12 @@ fdescribe('WelcomeComponent', () => {
       ],
       declarations: [WelcomeComponent],
       providers: [
-        { provide: ConfigurationService, useValue: configurationService },
-        { provide: MatDialog, useValue: dialog },
+        {provide: ConfigurationService, useValue: configurationService},
+        {provide: MatDialog, useValue: dialog},
         CookieService
       ]
     })
-    .compileComponents()
+      .compileComponents()
 
     cookieService = TestBed.inject(CookieService)
   }))
@@ -54,22 +54,22 @@ fdescribe('WelcomeComponent', () => {
     expect(component).toBeTruthy()
   })
 
-  it('should open the welcome banner dialog if configured to show on start' , () => {
-    configurationService.getApplicationConfiguration.and.returnValue(of({ application: { welcomeBanner: { showOnFirstStart: true } } }))
+  it('should open the welcome banner dialog if configured to show on start', fakeAsync(() => {
+    configurationService.getApplicationConfiguration.and.returnValue(of({application: {welcomeBanner: {showOnFirstStart: true}}}))
     component.ngOnInit()
     expect(dialog.open).toHaveBeenCalled()
-  })
+  }))
 
-  it('should not open the welcome banner dialog if configured to not show on start' , () => {
-    configurationService.getApplicationConfiguration.and.returnValue(of({ application: { welcomeBanner: { showOnFirstStart: false } } }))
+  it('should not open the welcome banner dialog if configured to not show on start', fakeAsync(() => {
+    configurationService.getApplicationConfiguration.and.returnValue(of({application: {welcomeBanner: {showOnFirstStart: false}}}))
     component.ngOnInit()
     expect(dialog.open).not.toHaveBeenCalled()
-  })
+  }))
 
-  it('should not open the welcome banner dialog if previously dismissed' , () => {
-    configurationService.getApplicationConfiguration.and.returnValue(of({ application: { welcomeBanner: { showOnFirstStart: true } } }))
+  it('should not open the welcome banner dialog if previously dismissed', fakeAsync(() => {
+    configurationService.getApplicationConfiguration.and.returnValue(of({application: {welcomeBanner: {showOnFirstStart: true}}}))
     cookieService.set('welcomebanner_status', 'dismiss')
     component.ngOnInit()
     expect(dialog.open).not.toHaveBeenCalled()
-  })
+  }))
 })
