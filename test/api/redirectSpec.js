9e5c4931c1130d6281b46e72bee0252914ded527
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2020-12-09 11:19:52 +0100

    Re-enable all redirect tests

--- test/api/redirectSpec.js
+++ test/api/redirectSpec.js
@@ -9,42 +9,42 @@ const config = require('config')
 const URL = 'http://localhost:3000'
 
 describe('/redirect', () => {
-  xit('GET redirected to https://github.com/bkimminich/juice-shop when this URL is passed as "to" parameter', () => {
+  it('GET redirected to https://github.com/bkimminich/juice-shop when this URL is passed as "to" parameter', () => {
     return frisby.get(URL + '/redirect?to=https://github.com/bkimminich/juice-shop', { redirect: 'manual' })
       .expect('status', 302)
   })
 
-  xit('GET redirected to https://blockchain.info/address/1AbKfgvw9psQ41NbLi8kufDQTezwG8DRZm when this URL is passed as "to" parameter', () => {
+  it('GET redirected to https://blockchain.info/address/1AbKfgvw9psQ41NbLi8kufDQTezwG8DRZm when this URL is passed as "to" parameter', () => {
     return frisby.get(URL + '/redirect?to=https://blockchain.info/address/1AbKfgvw9psQ41NbLi8kufDQTezwG8DRZm', { redirect: 'manual' })
       .expect('status', 302)
   })
 
-  xit('GET redirected to http://shop.spreadshirt.com/juiceshop when this URL is passed as "to" parameter', () => {
+  it('GET redirected to http://shop.spreadshirt.com/juiceshop when this URL is passed as "to" parameter', () => {
     return frisby.get(URL + '/redirect?to=http://shop.spreadshirt.com/juiceshop', { redirect: 'manual' })
       .expect('status', 302)
   })
 
-  xit('GET redirected to http://shop.spreadshirt.de/juiceshop when this URL is passed as "to" parameter', () => {
+  it('GET redirected to http://shop.spreadshirt.de/juiceshop when this URL is passed as "to" parameter', () => {
     return frisby.get(URL + '/redirect?to=http://shop.spreadshirt.de/juiceshop', { redirect: 'manual' })
       .expect('status', 302)
   })
 
-  xit('GET redirected to https://www.stickeryou.com/products/owasp-juice-shop/794 when this URL is passed as "to" parameter', () => {
+  it('GET redirected to https://www.stickeryou.com/products/owasp-juice-shop/794 when this URL is passed as "to" parameter', () => {
     return frisby.get(URL + '/redirect?to=https://www.stickeryou.com/products/owasp-juice-shop/794', { redirect: 'manual' })
       .expect('status', 302)
   })
 
-  xit('GET redirected to https://explorer.dash.org/address/Xr556RzuwX6hg5EGpkybbv5RanJoZN17kW when this URL is passed as "to" parameter', () => {
+  it('GET redirected to https://explorer.dash.org/address/Xr556RzuwX6hg5EGpkybbv5RanJoZN17kW when this URL is passed as "to" parameter', () => {
     return frisby.get(URL + '/redirect?to=https://explorer.dash.org/address/Xr556RzuwX6hg5EGpkybbv5RanJoZN17kW', { redirect: 'manual' })
       .expect('status', 302)
   })
 
-  xit('GET redirected to https://etherscan.io/address/0x0f933ab9fcaaa782d0279c300d73750e1311eae6 when this URL is passed as "to" parameter', () => {
+  it('GET redirected to https://etherscan.io/address/0x0f933ab9fcaaa782d0279c300d73750e1311eae6 when this URL is passed as "to" parameter', () => {
     return frisby.get(URL + '/redirect?to=https://etherscan.io/address/0x0f933ab9fcaaa782d0279c300d73750e1311eae6', { redirect: 'manual' })
       .expect('status', 302)
   })
 
-  xit('GET error message with information leakage when calling /redirect without query parameter', () => {
+  it('GET error message with information leakage when calling /redirect without query parameter', () => {
     return frisby.get(URL + '/redirect')
       .expect('status', 500)
       .expect('header', 'content-type', /text\/html/)
@@ -53,7 +53,7 @@ describe('/redirect', () => {
       .expect('bodyContains', '&#39;includes&#39; of undefined')
   })
 
-  xit('GET error message with information leakage when calling /redirect with unrecognized query parameter', () => {
+  it('GET error message with information leakage when calling /redirect with unrecognized query parameter', () => {
     return frisby.get(URL + '/redirect?x=y')
       .expect('status', 500)
       .expect('header', 'content-type', /text\/html/)
@@ -62,7 +62,7 @@ describe('/redirect', () => {
       .expect('bodyContains', '&#39;includes&#39; of undefined')
   })
 
-  xit('GET error message hinting at allowlist validation when calling /redirect with an unrecognized "to" target', () => {
+  it('GET error message hinting at allowlist validation when calling /redirect with an unrecognized "to" target', () => {
     return frisby.get(URL + '/redirect?to=whatever')
       .expect('status', 406)
       .expect('header', 'content-type', /text\/html/)
@@ -70,7 +70,7 @@ describe('/redirect', () => {
       .expect('bodyContains', 'Unrecognized target URL for redirect: whatever')
   })
 
-  xit('GET redirected to target URL in "to" parameter when a white-listed URL is part of the query string', () => {
+  it('GET redirected to target URL in "to" parameter when a white-listed URL is part of the query string', () => {
     return frisby.get(URL + '/redirect?to=/score-board?satisfyIndexOf=https://github.com/bkimminich/juice-shop')
       .expect('status', 200)
       .expect('header', 'content-type', /text\/html/)
