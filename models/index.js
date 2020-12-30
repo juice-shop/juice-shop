0304b132c54fb6dfc9c824e31547640b27e9cd40
shantanu69 <56212958+shantanu69@users.noreply.github.com>
2020-12-31 01:15:52 +0530

    updated sequelize to latest version

--- models/index.js
+++ models/index.js
@@ -27,7 +27,7 @@ const db = {}
 fs.readdirSync(__dirname)
   .filter(file => (file.indexOf('.') !== 0) && (file !== 'index.js'))
   .forEach(file => {
-    const model = sequelize.import(path.join(__dirname, file))
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
     db[model.name] = model
   })
 
