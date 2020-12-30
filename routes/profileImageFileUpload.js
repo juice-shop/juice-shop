cb850515f10d110b82dbd0e3c53a0dccc331e733
shantanu <56212958+cigar-galaxy82@users.noreply.github.com>
2020-12-30 14:10:18 +0530

    U (#1552)
    Update file-type dependency to 16.x
    
    (see #1440)

--- routes/profileImageFileUpload.js
+++ routes/profileImageFileUpload.js
@@ -11,33 +11,39 @@ const logger = require('../lib/logger')
 const fileType = require('file-type')
 
 module.exports = function fileUpload () {
-  return (req, res, next) => {
+  return async (req, res, next) => {
     const file = req.file
     const buffer = file.buffer
-    const uploadedFileType = fileType(buffer)
-    if (uploadedFileType !== null && utils.startsWith(uploadedFileType.mime, 'image')) {
-      const loggedInUser = insecurity.authenticatedUsers.get(req.cookies.token)
-      if (loggedInUser) {
-        fs.open(`frontend/dist/frontend/assets/public/images/uploads/${loggedInUser.data.id}.${uploadedFileType.ext}`, 'w', function (err, fd) {
-          if (err) logger.warn('Error opening file: ' + err.message)
-          fs.write(fd, buffer, 0, buffer.length, null, function (err) {
-            if (err) logger.warn('Error writing file: ' + err.message)
-            fs.close(fd, function () { })
-          })
-        })
-        models.User.findByPk(loggedInUser.data.id).then(user => {
-          return user.update({ profileImage: `assets/public/images/uploads/${loggedInUser.data.id}.${uploadedFileType.ext}` })
-        }).catch(error => {
-          next(error)
-        })
-        res.location(process.env.BASE_PATH + '/profile')
-        res.redirect(process.env.BASE_PATH + '/profile')
-      } else {
-        next(new Error('Blocked illegal activity by ' + req.connection.remoteAddress))
-      }
+    const uploadedFileType = await fileType.fromBuffer(buffer)
+
+    if (uploadedFileType === undefined) {
+      res.status(500)
+      next(new Error('Illegal file type'))
     } else {
-      res.status(415)
-      next(new Error(`Profile image upload does not accept this file type${uploadedFileType ? (': ' + uploadedFileType.mime) : '.'}`))
+      if (uploadedFileType !== null && utils.startsWith(uploadedFileType.mime, 'image')) {
+        const loggedInUser = insecurity.authenticatedUsers.get(req.cookies.token)
+        if (loggedInUser) {
+          fs.open(`frontend/dist/frontend/assets/public/images/uploads/${loggedInUser.data.id}.${uploadedFileType.ext}`, 'w', function (err, fd) {
+            if (err) logger.warn('Error opening file: ' + err.message)
+            fs.write(fd, buffer, 0, buffer.length, null, function (err) {
+              if (err) logger.warn('Error writing file: ' + err.message)
+              fs.close(fd, function () { })
+            })
+          })
+          models.User.findByPk(loggedInUser.data.id).then(user => {
+            return user.update({ profileImage: `assets/public/images/uploads/${loggedInUser.data.id}.${uploadedFileType.ext}` })
+          }).catch(error => {
+            next(error)
+          })
+          res.location(process.env.BASE_PATH + '/profile')
+          res.redirect(process.env.BASE_PATH + '/profile')
+        } else {
+          next(new Error('Blocked illegal activity by ' + req.connection.remoteAddress))
+        }
+      } else {
+        res.status(415)
+        next(new Error(`Profile image upload does not accept this file type${uploadedFileType ? (': ' + uploadedFileType.mime) : '.'}`))
+      }
     }
   }
 }
