2ec9439eaf36db71035243fa982ad345278716e1
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2021-01-08 18:55:56 +0100

    Switch to karma-coverage module for frontend tests

--- frontend/src/karma.conf.js
+++ frontend/src/karma.conf.js
@@ -14,13 +14,13 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
       dir: require('path').join(__dirname, '../../build/reports/coverage/frontend-tests'),
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
