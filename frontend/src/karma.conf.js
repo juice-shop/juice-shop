10a5f1810509aca66bbde59b467549c47594543e
shantanu <56212958+cigar-galaxy82@users.noreply.github.com>
2021-02-01 18:02:05 -0500

    Updated socket.io and Karma (#1577)
    * updated socket.io and socket.io-client
    
    * Fix LCOV and HTML reporter setup to follow plugin config options
    
    Co-authored-by: shantanu69 <56212958+shantanu69@users.noreply.github.com>
    Co-authored-by: Bjoern Kimminich <bjoern.kimminich@gmx.de>

--- frontend/src/karma.conf.js
+++ frontend/src/karma.conf.js
@@ -14,16 +14,18 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
       dir: require('path').join(__dirname, '../../build/reports/coverage/frontend-tests'),
-      reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
+      reporters: [
+        { type: 'html', subdir: '.' },
+        { type: 'lcovonly', subdir: '.' }
+      ]
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
