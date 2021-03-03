789ccc8ce40a07382f03c761c403f66c0641437a
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2021-03-03 15:58:50 +0100

    Access demo source file from root folder

--- routes/vulnCodeSnippet.js
+++ routes/vulnCodeSnippet.js
@@ -10,7 +10,7 @@ module.exports = function serveCodeSnippet () {
   return (req, res, next) => {
     const challenge = challenges[req.params.challenge]
     if (challenge) {
-      const source = fs.readFileSync('routes/login.js', 'utf8') // TODO Instead find file(s) containing "vuln-code-snippet start [challengeKey, otherChallengeKey, ...]"
+      const source = fs.readFileSync('./routes/login.js', 'utf8') // TODO Instead find file(s) containing "vuln-code-snippet start [challengeKey, otherChallengeKey, ...]"
       let snippet = source.match(`// vuln-code-snippet start [.*${challenge}.*](.|\\r\\n)*vuln-code-snippet end [.*${challenge}.*]`)
       if (snippet) {
         snippet = snippet[0]
@@ -24,7 +24,7 @@ module.exports = function serveCodeSnippet () {
           }
         }
 
-        snippet = snippet.replace(/.*\/\/ vuln-code-snippet hide-lin1e/g, '')
+        snippet = snippet.replace(/.*\/\/ vuln-code-snippet hide-line/g, '')
         snippet = snippet.replace(/\/\/ vuln-code-snippet .*/g, '')
         snippet = snippet.trim()
 
