796081602b1f7bfcb496abf5c71733fd1b264f71
Ziyang Li <paseaf@me.com>
2021-03-05 21:02:54 +0000

    Migrate /routes/*.js to .ts
    Signed-off-by: Ziyang Li <paseaf@me.com>

---
+++ routes/b2bOrder.ts
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2014-2021 Bjoern Kimminich.
+ * SPDX-License-Identifier: MIT
+ */
+
+const utils = require('../lib/utils')
+const security = require('../lib/insecurity')
+const safeEval = require('notevil')
+import vm = require('vm')
+const challenges = require('../data/datacache').challenges
+
+module.exports = function b2bOrder () {
+  return ({ body }, res, next) => {
+    if (!utils.disableOnContainerEnv()) {
+      const orderLinesData = body.orderLinesData || ''
+      try {
+        const sandbox = { safeEval, orderLinesData }
+        vm.createContext(sandbox)
+        vm.runInContext('safeEval(orderLinesData)', sandbox, { timeout: 2000 })
+        res.json({ cid: body.cid, orderNo: uniqueOrderNumber(), paymentDue: dateTwoWeeksFromNow() })
+      } catch (err) {
+        if (err.message && err.message.match(/Script execution timed out.*/)) {
+          utils.solveIf(challenges.rceOccupyChallenge, () => { return true })
+          res.status(503)
+          next(new Error('Sorry, we are temporarily not available! Please try again later.'))
+        } else {
+          utils.solveIf(challenges.rceChallenge, () => { return err.message === 'Infinite loop detected - reached max iterations' })
+          next(err)
+        }
+      }
+    } else {
+      res.json({ cid: body.cid, orderNo: uniqueOrderNumber(), paymentDue: dateTwoWeeksFromNow() })
+    }
+  }
+
+  function uniqueOrderNumber () {
+    return security.hash(new Date() + '_B2B')
+  }
+
+  function dateTwoWeeksFromNow () {
+    return new Date(new Date().getTime() + (14 * 24 * 60 * 60 * 1000)).toISOString()
+  }
+}
