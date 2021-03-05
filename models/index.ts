0143ec219178df4fa24e8cda417a4d470ae3924e
Ziyang Li <paseaf@me.com>
2021-03-05 20:52:22 +0000

    Migrate /models/*.js to .ts
    Signed-off-by: Ziyang Li <paseaf@me.com>

--- models/index.ts
+++ models/index.ts
@@ -4,7 +4,7 @@
  */
 
 /* jslint node: true */
-const fs = require('fs')
+import fs = require('fs')
 const path = require('path')
 const sequelizeNoUpdateAttributes = require('sequelize-notupdate-attributes')
 const Sequelize = require('sequelize')
@@ -25,7 +25,7 @@ sequelizeNoUpdateAttributes(sequelize)
 const db = {}
 
 fs.readdirSync(__dirname)
-  .filter(file => (file.indexOf('.') !== 0) && (file !== 'index.js'))
+  .filter(file => (file.match(/\.[jt]s$/)) && !file.includes('index.'))
   .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
     db[model.name] = model
