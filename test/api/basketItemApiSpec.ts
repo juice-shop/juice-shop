7c7226446e066249e1f65bfb4b1f659d7140b446
Ziyang Li <paseaf@me.com>
2021-03-05 20:37:29 +0000

    Migrate api tests (frisby) to TypeScript
    Signed-off-by: Ziyang Li <paseaf@me.com>

---
+++ test/api/basketItemApiSpec.ts
@@ -0,0 +1,267 @@
+/*
+ * Copyright (c) 2014-2021 Bjoern Kimminich.
+ * SPDX-License-Identifier: MIT
+ */
+
+import frisby = require('frisby')
+const config = require('config')
+
+const API_URL = 'http://localhost:3000/api'
+const REST_URL = 'http://localhost:3000/rest'
+
+const jsonHeader = { 'content-type': 'application/json' }
+let authHeader
+
+beforeAll(() => {
+  return frisby.post(REST_URL + '/user/login', {
+    headers: jsonHeader,
+    body: {
+      email: 'jim@' + config.get('application.domain'),
+      password: 'ncc-1701'
+    }
+  })
+    .expect('status', 200)
+    .then(({ json }) => {
+      authHeader = { Authorization: 'Bearer ' + json.authentication.token, 'content-type': 'application/json' }
+    })
+})
+
+describe('/api/BasketItems', () => {
+  it('GET all basket items is forbidden via public API', () => {
+    return frisby.get(API_URL + '/BasketItems')
+      .expect('status', 401)
+  })
+
+  it('POST new basket item is forbidden via public API', () => {
+    return frisby.post(API_URL + '/BasketItems', {
+      BasketId: 2,
+      ProductId: 1,
+      quantity: 1
+    })
+      .expect('status', 401)
+  })
+
+  it('GET all basket items', () => {
+    return frisby.get(API_URL + '/BasketItems', { headers: authHeader })
+      .expect('status', 200)
+  })
+
+  it('POST new basket item', () => {
+    return frisby.post(API_URL + '/BasketItems', {
+      headers: authHeader,
+      body: {
+        BasketId: 2,
+        ProductId: 2,
+        quantity: 1
+      }
+    })
+      .expect('status', 200)
+  })
+
+  it('POST new basket item with more than available quantity is forbidden', () => {
+    return frisby.post(API_URL + '/BasketItems', {
+      headers: authHeader,
+      body: {
+        BasketId: 2,
+        ProductId: 2,
+        quantity: 101
+      }
+    })
+      .expect('status', 400)
+  })
+
+  it('POST new basket item with more than allowed quantity is forbidden', () => {
+    return frisby.post(API_URL + '/BasketItems', {
+      headers: authHeader,
+      body: {
+        BasketId: 2,
+        ProductId: 1,
+        quantity: 6
+      }
+    })
+      .expect('status', 400)
+      .expect('json', 'error', 'You can order only up to 5 items of this product.')
+  })
+})
+
+describe('/api/BasketItems/:id', () => {
+  it('GET basket item by id is forbidden via public API', () => {
+    return frisby.get(API_URL + '/BasketItems/1')
+      .expect('status', 401)
+  })
+
+  it('PUT update basket item is forbidden via public API', () => {
+    return frisby.put(API_URL + '/BasketItems/1', {
+      quantity: 2
+    }, { json: true })
+      .expect('status', 401)
+  })
+
+  it('DELETE basket item is forbidden via public API', () => {
+    return frisby.del(API_URL + '/BasketItems/1')
+      .expect('status', 401)
+  })
+
+  it('GET newly created basket item by id', () => {
+    return frisby.post(API_URL + '/BasketItems', {
+      headers: authHeader,
+      body: {
+        BasketId: 2,
+        ProductId: 6,
+        quantity: 3
+      }
+    })
+      .expect('status', 200)
+      .then(({ json }) => {
+        return frisby.get(API_URL + '/BasketItems/' + json.data.id, { headers: authHeader })
+          .expect('status', 200)
+      })
+  })
+
+  it('PUT update newly created basket item', () => {
+    return frisby.post(API_URL + '/BasketItems', {
+      headers: authHeader,
+      body: {
+        BasketId: 2,
+        ProductId: 3,
+        quantity: 3
+      }
+    })
+      .expect('status', 200)
+      .then(({ json }) => {
+        return frisby.put(API_URL + '/BasketItems/' + json.data.id, {
+          headers: authHeader,
+          body: {
+            quantity: 20
+          }
+        })
+          .expect('status', 200)
+          .expect('json', 'data', { quantity: 20 })
+      })
+  })
+
+  it('PUT update basket ID of basket item is forbidden', () => {
+    return frisby.post(API_URL + '/BasketItems', {
+      headers: authHeader,
+      body: {
+        BasketId: 2,
+        ProductId: 8,
+        quantity: 8
+      }
+    })
+      .expect('status', 200)
+      .then(({ json }) => {
+        return frisby.put(API_URL + '/BasketItems/' + json.data.id, {
+          headers: authHeader,
+          body: {
+            BasketId: 42
+          }
+        })
+          .expect('status', 400)
+          .expect('json', { message: 'null: `BasketId` cannot be updated due `noUpdate` constraint', errors: [{ field: 'BasketId', message: '`BasketId` cannot be updated due `noUpdate` constraint' }] })
+      })
+  })
+
+  it('PUT update basket ID of basket item without basket ID', () => {
+    return frisby.post(API_URL + '/BasketItems', {
+      headers: authHeader,
+      body: {
+        ProductId: 8,
+        quantity: 8
+      }
+    })
+      .expect('status', 200)
+      .then(({ json }) => {
+        expect(json.data.BasketId).toBeUndefined()
+        return frisby.put(API_URL + '/BasketItems/' + json.data.id, {
+          headers: authHeader,
+          body: {
+            BasketId: 3
+          }
+        })
+          .expect('status', 200)
+          .expect('json', 'data', { BasketId: 3 })
+      })
+  })
+
+  it('PUT update product ID of basket item is forbidden', () => {
+    return frisby.post(API_URL + '/BasketItems', {
+      headers: authHeader,
+      body: {
+        BasketId: 2,
+        ProductId: 9,
+        quantity: 9
+      }
+    })
+      .expect('status', 200)
+      .then(({ json }) => {
+        return frisby.put(API_URL + '/BasketItems/' + json.data.id, {
+          headers: authHeader,
+          body: {
+            ProductId: 42
+          }
+        })
+          .expect('status', 400)
+          .expect('json', { message: 'null: `ProductId` cannot be updated due `noUpdate` constraint', errors: [{ field: 'ProductId', message: '`ProductId` cannot be updated due `noUpdate` constraint' }] })
+      })
+  })
+
+  it('PUT update newly created basket item with more than available quantity is forbidden', () => {
+    return frisby.post(API_URL + '/BasketItems', {
+      headers: authHeader,
+      body: {
+        BasketId: 2,
+        ProductId: 12,
+        quantity: 12
+      }
+    })
+      .expect('status', 200)
+      .then(({ json }) => {
+        return frisby.put(API_URL + '/BasketItems/' + json.data.id, {
+          headers: authHeader,
+          body: {
+            quantity: 100
+          }
+        })
+          .expect('status', 400)
+      })
+  })
+
+  it('PUT update basket item with more than allowed quantity is forbidden', () => {
+    return frisby.post(API_URL + '/BasketItems', {
+      headers: authHeader,
+      body: {
+        BasketId: 2,
+        ProductId: 1,
+        quantity: 1
+      }
+    })
+      .expect('status', 200)
+      .then(({ json }) => {
+        return frisby.put(API_URL + '/BasketItems/' + json.data.id, {
+          headers: authHeader,
+          body: {
+            quantity: 6
+          }
+        })
+          .expect('status', 400)
+          .expect('json', 'error', 'You can order only up to 5 items of this product.')
+      })
+  })
+
+  it('DELETE newly created basket item', () => {
+    return frisby.post(API_URL + '/BasketItems', {
+      headers: authHeader,
+      body: {
+        BasketId: 2,
+        ProductId: 10,
+        quantity: 10
+      }
+    })
+      .expect('status', 200)
+      .then(({ json }) => {
+        return frisby.del(API_URL + '/BasketItems/' + json.data.id, { headers: authHeader })
+          .expect('status', 200)
+      })
+  })
+})
---
+++ test/api/internetResourcesSpec.ts
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2014-2021 Bjoern Kimminich.
+ * SPDX-License-Identifier: MIT
+ */
+
+import frisby = require('frisby')
+
+describe('Required Internet resource', () => {
+  describe('PasteBin paste for "Leaked Unsafe Product" challenge available', () => {
+    it('for default configuration (https://pastebin.com/90dUgd7s)', () => {
+      return frisby.get('https://pastebin.com/90dUgd7s')
+        .expect('status', 200)
+        .expect('bodyContains', 'Hueteroneel')
+        .expect('bodyContains', 'this coupled with Eurogium Edule was sometimes found fatal')
+    })
+
+    xit('for 7MS configuration (https://pastebin.com/8SMbWPxc)', () => { // FIXME Test would need to confirm/bypass PasteBin SMART filter to retrieve content
+      return frisby.get('https://pastebin.com/8SMbWPxc')
+        .expect('status', 200)
+        .expect('bodyContains', 'TAYLOR SWIFT')
+        .expect('bodyContains', 'KATY PERRY')
+        .expect('bodyContains', '(Consider rivalries when picking song list for events as wrong combo could infuriate a mixed-fans audience, see https://www.nickiswift.com/2075/taylor-swifts-list-enemies)')
+    })
+
+    it('for BodgeIt Store configuration (https://pastebin.com/G47LrDr0)', () => {
+      return frisby.get('https://pastebin.com/G47LrDr0')
+        .expect('status', 200)
+        .expect('bodyContains', 'Mind Blank - one willing creature you touch is immune any effect')
+        .expect('bodyContains', 'They should seek out a Couatl, and undertake a quest to receive')
+        .expect('bodyContains', 'They could acquire the aid of a rakshasa, and willingly invoke its')
+        .expect('bodyContains', 'Note to self: Option (1) and (3) of the above should not be combined.')
+    })
+
+    xit('for Mozilla configuration (https://pastebin.com/t8jqE1y7)', () => { // FIXME Test would need to confirm/bypass PasteBin SMART filter to retrieve content
+      return frisby.get('https://pastebin.com/t8jqE1y7')
+        .expect('status', 200)
+        .expect('bodyContains', 'Fixed a bug that, when this plugin was installed together with both the')
+        .expect('bodyContains', 'JuiceNote')
+        .expect('bodyContains', 'Magische Firefox Suche')
+        .expect('bodyContains', 'plugins, lwt your browser throw a')
+        .expect('bodyContains', 'JuiceOverFlowError')
+        .expect('bodyContains', 'The problem can still occur post-fix but at least now less frequently!')
+    })
+
+    it('for All Day DevOps configuration (https://pastebin.com/RXrihEMS)', () => {
+      return frisby.get('https://pastebin.com/RXrihEMS')
+        .expect('status', 200)
+        .expect('bodyContains', 'The infamous 301 and 303 lasers.')
+        .expect('bodyContains', 'Cheap Chinese crap with no quality control')
+        .expect('bodyContains', 'permanent damage before you can blink your eye')
+    })
+  })
+
+  it('Comment on "Top 10 Fruits you probably dont know" blog post with PasteBin paste URL spoiler available', () => {
+    return frisby.get('https://listverse.disqus.com/top_20_fruits_you_probably_don039t_know/latest.rss')
+      .expect('status', 200)
+      .expect('bodyContains', 'Rippertuer Special Juice')
+      .expect('bodyContains', 'https://pastebin.com/90dUgd7s')
+  })
+
+  it('PasteBin paste (https://pastebin.com/4U1V1UjU) for "Leaked Access Logs" challenge available', () => {
+    return frisby.get('https://pastebin.com/4U1V1UjU')
+      .expect('status', 200)
+      .expect('bodyContains', 'current=0Y8rMnww$*9VFYE%C2%A759-!Fg1L6t&amp;6lB')
+  })
+
+  it('StackOverflow question "Less verbose access logs using expressjs/morgan" with log snippet and PasteBin paste URL spoiler available', () => {
+    return frisby.get('https://stackoverflow.com/questions/57061271/less-verbose-access-logs-using-expressjs-morgan')
+      .expect('status', 200)
+      .expect('bodyContains', '/rest/continue-code')
+      .expect('bodyContains', '/api/Challenges/?name=Score%20Board')
+      .expect('bodyContains', 'https://pastebin.com/4U1V1UjU')
+  })
+
+  it('GitHub issue (https://github.com/punkave/sanitize-html/issues/29) for "Server-side XSS Protection" challenge available', () => {
+    return frisby.get('https://github.com/punkave/sanitize-html/issues/29')
+      .expect('status', 200)
+      .expect('bodyContains', 'Sanitization is not applied recursively')
+      .expect('bodyContains', 'I am not harmless: &lt;&lt;img src=&quot;csrf-attack&quot;/&gt;img src=&quot;csrf-attack&quot;/&gt; is sanitized to I am not harmless: &lt;img src=&quot;csrf-attack&quot;/&gt;')
+  })
+})
