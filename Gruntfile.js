69bf39d8e299e1eba7c0c45b528d97b3865ee45f
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2021-03-20 07:38:28 +0100

    Add forgotten comma in folder name array

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,7 +47,7 @@ module.exports = function (grunt) {
               'data/static/**',
               'data/chatbot/.gitkeep',
               'encryptionkeys/**',
-              'frontend/dist/frontend/**'
+              'frontend/dist/frontend/**',
               'frontend/src/**/*.ts',
               'ftp/**',
               'i18n/.gitkeep',
