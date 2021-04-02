6e50583932461cdee0258459a586c4f1f900cbe3
the-pro <42973786+the-pro@users.noreply.github.com>
2021-04-02 23:58:09 +0530

    Added url checks to the image path

--- test/server/configExifSpec.js
+++ test/server/configExifSpec.js
@@ -1,5 +1,5 @@
 const ExifImage = require('exif').ExifImage
-let pathToImage = 'frontend/src/assets/public/images/products/3d_keychain.jpg'
+let pathToImage = 'frontend/src/assets/public/images/products/'
 const config = require('config')
 const chai = require('chai')
 const sinonChai = require('sinon-chai')
@@ -14,12 +14,18 @@ describe('forBlueprintChallenge', () => {
     it('should contain the exif data for the blueprint challenge', () => {
       products.forEach(product => {
         if (product.exifForBlueprintChallenge !== undefined) {
+          const url = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/
+          if (url.test(product.image)) {
+            pathToImage = product.image
+          } else {
+            pathToImage = pathToImage + product.image
+          }
           ExifImage({ image: path.resolve(pathToImage) }, function (error, exifData) {
             if (error) {
-              expect.fail("Could no read EXIF data")
+              expect.fail('Could no read EXIF data')
             }
             Object.getOwnPropertyNames(product.exifForBlueprintChallenge).forEach(property => {
-                expect(exifData.image[property]).to.equal(product.exifForBlueprintChallenge[property])
+              expect(exifData.image[property]).to.equal(product.exifForBlueprintChallenge[property])
             })
           })
         }
