ca46ef41de2e2cef1e1be7ea51bf87422a29d3ff
the-pro <42973786+the-pro@users.noreply.github.com>
2021-06-20 19:35:04 +0530

    Fixed the failing tests
    Signed-off-by: the-pro <42973786+the-pro@users.noreply.github.com>

--- frontend/src/app/Services/vuln-lines.service.spec.ts
+++ frontend/src/app/Services/vuln-lines.service.spec.ts
@@ -1,16 +1,20 @@
-import { TestBed } from '@angular/core/testing'
-
+import { TestBed,inject } from '@angular/core/testing'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { VulnLinesService } from './vuln-lines.service'
+
 
 describe('VulnLinesService', () => {
   let service: VulnLinesService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({})
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [VulnLinesService]
+    })
     service = TestBed.inject(VulnLinesService)
   })
 
-  it('should be created', () => {
+  it('should be created', inject([VulnLinesService], (service: VulnLinesService) => {
     expect(service).toBeTruthy()
-  })
+  }))
 })
