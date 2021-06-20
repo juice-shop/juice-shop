5e96478207fe515e16617c5a6bea3dad7b031720
the-pro <42973786+the-pro@users.noreply.github.com>
2021-06-20 19:42:00 +0530

    Fixed the lint in files
    Signed-off-by: the-pro <42973786+the-pro@users.noreply.github.com>

--- frontend/src/app/Services/vuln-lines.service.spec.ts
+++ frontend/src/app/Services/vuln-lines.service.spec.ts
@@ -1,17 +1,13 @@
-import { TestBed,inject } from '@angular/core/testing'
+import { TestBed, inject } from '@angular/core/testing'
 import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { VulnLinesService } from './vuln-lines.service'
 
-
 describe('VulnLinesService', () => {
-  let service: VulnLinesService
-
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [VulnLinesService]
     })
-    service = TestBed.inject(VulnLinesService)
   })
 
   it('should be created', inject([VulnLinesService], (service: VulnLinesService) => {
