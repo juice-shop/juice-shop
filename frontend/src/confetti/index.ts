9e17ec5e6ebcb9a0e3f42007825639ff5f645893
the-pro <42973786+the-pro@users.noreply.github.com>
2021-07-07 11:37:33 +0530

    Added confetti and removed the confetti component
    Signed-off-by: the-pro <42973786+the-pro@users.noreply.github.com>

--- frontend/src/confetti/index.ts
+++ frontend/src/confetti/index.ts
@@ -5,29 +5,16 @@ const timeout = (ms: number) => {
         setTimeout(resolve,ms)
     })
 }
-export const shootConfetti = (message: string) => {
+export const shootConfetti = () => {
     const canvas = document.createElement('canvas')
     canvas.style.position = "fixed"
     canvas.style.left = "0"
     canvas.style.right = "0"
     canvas.style.width = "100%"
     canvas.style.height = "100%"
-    canvas.style.backgroundColor = "rgba(62, 66, 62, 0.322)"
-
-    const dialogBox = document.createElement('div')
-    dialogBox.style.width = "40%"
-    dialogBox.style.height = "20%"
-    dialogBox.style.position = "fixed"
-    dialogBox.style.left = "30%"
-    dialogBox.style.top = "40%"
-    dialogBox.style.backgroundColor = "rgb(58, 241, 12)"
-    dialogBox.style.color = "black"
-    dialogBox.innerText = message
-    dialogBox.style.zIndex = "2001"
     canvas.style.zIndex = "2000"
 
     document.body.appendChild(canvas)
-    document.body.appendChild(dialogBox)
 
     const shoot = confetti.create(canvas,{
         resize: true
@@ -48,6 +35,5 @@ export const shootConfetti = (message: string) => {
 
     timeout(6000).then(() => {
         canvas.remove()
-        dialogBox.remove()
     })
 }
