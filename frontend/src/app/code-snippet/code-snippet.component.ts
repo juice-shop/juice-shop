9f76731e7a916d8b95512335bbd49fb21c8799d1
the-pro <42973786+the-pro@users.noreply.github.com>
2021-07-13 12:30:32 +0530

    Added a view for the code fixes component
    Signed-off-by: the-pro <42973786+the-pro@users.noreply.github.com>

--- frontend/src/app/code-snippet/code-snippet.component.ts
+++ frontend/src/app/code-snippet/code-snippet.component.ts
@@ -8,6 +8,7 @@ import { VulnLinesService, result } from '../Services/vuln-lines.service'
 import { Component, Inject, OnInit } from '@angular/core'
 
 import { MAT_DIALOG_DATA } from '@angular/material/dialog'
+import { FormControl } from '@angular/forms'
 
 enum ResultState {
   Undecided,
@@ -23,6 +24,8 @@ enum ResultState {
 export class CodeSnippetComponent implements OnInit {
   public snippet: CodeSnippet = null
   public selectedLines: number[]
+  public tab: FormControl = new FormControl(0)
+  public lock: ResultState = ResultState.Undecided
   public result: ResultState = ResultState.Undecided
 
   constructor (@Inject(MAT_DIALOG_DATA) public dialogData: any, private readonly codeSnippetService: CodeSnippetService, private readonly vulnLinesService: VulnLinesService) { }
@@ -39,17 +42,35 @@ export class CodeSnippetComponent implements OnInit {
     this.selectedLines = lines
   }
 
+  toggleTab = (event) => {
+    this.tab.setValue(event)
+    this.result = ResultState.Undecided
+  }
+
   checkLines = () => {
     this.vulnLinesService.check(this.dialogData.key, this.selectedLines).subscribe((verdict: result) => {
       if (verdict.verdict) {
         this.result = ResultState.Right
+        this.lock = ResultState.Right
         import('../../confetti').then(module => {
           module.shootConfetti()
         })
       } else {
         this.result = ResultState.Wrong
+        this.lock = ResultState.Wrong
       }
     })
+  }
+
+  lockIcon (): string {
+    switch (this.lock) {
+      case ResultState.Right:
+        return 'lock_open'
+      case ResultState.Wrong:
+        return 'lock'
+      case ResultState.Undecided:
+        return 'lock'
+    }
   }
 
   resultIcon (): string {
