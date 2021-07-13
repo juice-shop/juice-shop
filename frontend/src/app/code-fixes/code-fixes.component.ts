7cce7b0b8269e38bd7cbd17a9749a26377b5f52d
the-pro <42973786+the-pro@users.noreply.github.com>
2021-07-13 12:30:32 +0530

    Added a view for the code fixes component
    Signed-off-by: the-pro <42973786+the-pro@users.noreply.github.com>

---
+++ frontend/src/app/code-fixes/code-fixes.component.ts
@@ -0,0 +1,45 @@
+import { Component, Input, OnInit, OnChanges } from '@angular/core'
+import { DiffResults } from 'ngx-text-diff/lib/ngx-text-diff.model'
+import { Observable, Subject } from 'rxjs'
+
+export interface DiffContent {
+  leftContent: string
+  rightContent: string
+}
+
+@Component({
+  selector: 'app-code-fixes',
+  templateUrl: './code-fixes.component.html',
+  styleUrls: ['./code-fixes.component.scss']
+})
+export class CodeFixesComponent implements OnInit, OnChanges {
+  @Input('snippet')
+  public snippet: string = ''
+
+  content: DiffContent = {
+    leftContent: '',
+    rightContent: ''
+  }
+
+  options: any = {
+    lineNumbers: true,
+    mode: 'xml'
+  }
+
+  contentObservable: Subject<DiffContent> = new Subject<DiffContent>()
+  contentObservable$: Observable<DiffContent> = this.contentObservable.asObservable()
+
+  ngOnInit (): void {
+    this.content.leftContent = `${this.snippet}`
+    this.content.rightContent = `${this.snippet}`
+    this.contentObservable.next(this.content)
+  }
+
+  ngOnChanges () {
+
+  }
+
+  onCompareResults (diffResults: DiffResults) {
+    console.log('diffResults', diffResults)
+  }
+}
