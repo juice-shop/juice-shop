71a2537a967c43efc232942507b53a031f2a683c
the-pro <42973786+the-pro@users.noreply.github.com>
2021-07-18 15:21:34 +0530

    Added the backend to serve the code fixes
    Signed-off-by: the-pro <42973786+the-pro@users.noreply.github.com>

--- frontend/src/app/code-fixes/code-fixes.component.ts
+++ frontend/src/app/code-fixes/code-fixes.component.ts
@@ -1,45 +1,19 @@
-import { Component, Input, OnInit, OnChanges } from '@angular/core'
-import { DiffResults } from 'ngx-text-diff/lib/ngx-text-diff.model'
-import { Observable, Subject } from 'rxjs'
-
-export interface DiffContent {
-  leftContent: string
-  rightContent: string
-}
+import { Component, Input, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-code-fixes',
   templateUrl: './code-fixes.component.html',
   styleUrls: ['./code-fixes.component.scss']
 })
-export class CodeFixesComponent implements OnInit, OnChanges {
+export class CodeFixesComponent implements OnInit {
   @Input('snippet')
   public snippet: string = ''
 
-  content: DiffContent = {
-    leftContent: '',
-    rightContent: ''
-  }
-
-  options: any = {
-    lineNumbers: true,
-    mode: 'typescript'
-  }
-
-  contentObservable: Subject<DiffContent> = new Subject<DiffContent>()
-  contentObservable$: Observable<DiffContent> = this.contentObservable.asObservable()
+  @Input('fixes')
+  public fixes: string[]
 
   ngOnInit (): void {
-    this.content.leftContent = `${this.snippet}`
-    this.content.rightContent = `${this.snippet}`
-    this.contentObservable.next(this.content)
-  }
-
-  ngOnChanges () {
-
-  }
-
-  onCompareResults (diffResults: DiffResults) {
-    console.log('diffResults', diffResults)
+    this.snippet = this.snippet.replace(/(\r)/gm, '')
+    this.snippet += '\n'
   }
 }
