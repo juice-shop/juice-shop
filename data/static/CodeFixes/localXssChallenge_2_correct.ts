5019c39d50da7191d2c9d9390294e62a522a9cb4
the-pro <42973786+the-pro@users.noreply.github.com>
2021-07-24 13:11:20 +0530

    Added the checking of fix
    Signed-off-by: the-pro <42973786+the-pro@users.noreply.github.com>

---
+++ data/static/CodeFixes/localXssChallenge_2_correct.ts
@@ -0,0 +1,19 @@
+filterTable () {
+    let queryParam: string = this.route.snapshot.queryParams.q
+    if (queryParam) {
+      queryParam = queryParam.trim()
+      this.dataSource.filter = queryParam.toLowerCase()
+      this.searchValue = queryParam
+      this.gridDataSource.subscribe((result: any) => {
+        if (result.length === 0) {
+          this.emptyState = true
+        } else {
+          this.emptyState = false
+        }
+      })
+    } else {
+      this.dataSource.filter = ''
+      this.searchValue = undefined
+      this.emptyState = false
+    }
+  }
--- routes/vulnCodeFixes.ts
+++ routes/vulnCodeFixes.ts
@@ -1,7 +1,23 @@
 import { Request, Response, NextFunction } from 'express'
-const yaml = require('js-yaml')
 const fs = require('fs')
-const codeFixes = yaml.load(fs.readFileSync('data/static/CodeFixes.yml'))
+
+const FixesDir = 'data/static/CodeFixes'
+const files = fs.readdirSync(FixesDir)
+const CodeFixes: any = {}
+for (const file of files) {
+  const fix = fs.readFileSync(`${FixesDir}/${file}`).toString()
+  const metadata = file.split('_')
+  const key = metadata[0]
+  const number = metadata[1]
+  if (!CodeFixes[key]) {
+    CodeFixes[key] = {}
+    CodeFixes[key].fixes = []
+  }
+  CodeFixes[key].fixes.push(fix)
+  if (metadata.length === 3) {
+    CodeFixes[key].correct = parseInt(number, 10)
+  }
+}
 
 interface FixesRequestParams {
   key: string
@@ -12,23 +28,15 @@ interface VerdictRequestBody {
   selectedFix: number
 }
 
-const challenges: any = {}
-for (const challenge of codeFixes) {
-  challenges[challenge.key] = {
-    fixes: challenge.fixes,
-    correct: challenge.correct
-  }
-}
-
 export const serveCodeFixes = () => (req: Request<FixesRequestParams, {}, {}>, res: Response, next: NextFunction) => {
-  if (challenges[req.params.key] === undefined) {
+  if (CodeFixes[req.params.key] === undefined) {
     res.status(404).json({
       error: 'No fixes found for the snippet!'
     })
     return
   }
   res.status(200).json({
-    fixes: challenges[req.params.key].fixes
+    fixes: CodeFixes[req.params.key].fixes
   })
 }
 
@@ -36,14 +44,14 @@ export const checkCorrectFix = () => (req: Request<{}, {}, VerdictRequestBody>, 
   const challenge = req.body.key
   const selectedFix = req.body.selectedFix
 
-  if (challenges[challenge] === undefined) {
+  if (CodeFixes[challenge] === undefined) {
     res.status(404).json({
       error: 'No fixes found for the snippet!'
     })
     return
   }
 
-  if (selectedFix === challenges[challenge].correct) {
+  if (selectedFix === CodeFixes[challenge].correct) {
     res.status(200).json({
       verdict: true
     })
