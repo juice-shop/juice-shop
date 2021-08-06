784c49802bf12d6311920272ea3fa21e1b0d2954
the-pro <42973786+the-pro@users.noreply.github.com>
2021-08-06 21:51:42 +0530

    Added the shufling to the frontend
    Signed-off-by: the-pro <42973786+the-pro@users.noreply.github.com>

--- frontend/src/app/code-fixes/code-fixes.component.ts
+++ frontend/src/app/code-fixes/code-fixes.component.ts
@@ -16,13 +16,35 @@ export class CodeFixesComponent implements OnInit {
   public emitFix = new EventEmitter<number>()
 
   public selectedFix: number = 0
+  public randomFixes = []
+
+  shuffle () {
+    let index = 0
+    for (const fix of this.fixes) {
+      this.randomFixes.push({
+        fix: fix,
+        index: index
+      })
+      index++
+    }
+    let randomRotation = Math.random() * 100
+    while (randomRotation > 0) {
+      const end = this.randomFixes[this.randomFixes.length - 1]
+      for (let i = this.randomFixes.length - 1; i > 0; i--) {
+        this.randomFixes[i] = this.randomFixes[i - 1]
+      }
+      this.randomFixes[0] = end
+      randomRotation--
+    }
+  }
 
   ngOnInit (): void {
-
+    this.shuffle()
+    this.emitFix.emit(this.randomFixes[0].index)
   }
 
   changeFix (event: Event) {
     this.selectedFix = parseInt((event.target as HTMLSelectElement).value, 10)
-    this.emitFix.emit(this.selectedFix + 1)
+    this.emitFix.emit(this.randomFixes[this.selectedFix].index)
   }
 }
