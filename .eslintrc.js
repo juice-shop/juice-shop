bf85aae38e4d541f7fa324c39d3d2a2d8fdf3c6c
TuncerYukler <tunceryukler@gmail.com>
2021-09-10 22:32:48 -0400

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,6 +40,20 @@ module.exports = {
         '@typescript-eslint/restrict-template-expressions': 'off', // 466
         '@typescript-eslint/no-var-requires': 'off' // 502
       }
+    }  overrides: [
+    {
+      files: ['**/*.ts'],
+      parser: '@typescript-eslint/parser',
+      rules: {
+        'no-void': 'off', // conflicting with recommendation from @typescript-eslint/no-floating-promises
+        // FIXME warnings below this line need to be checked and fixed. Line end comments below are number of findings per rule on 24.03.2021
+        '@typescript-eslint/no-misused-promises': 'off', // 97
+        '@typescript-eslint/explicit-function-return-type': 'off', // 132
+        '@typescript-eslint/restrict-plus-operands': 'off', // 292
+        '@typescript-eslint/strict-boolean-expressions': 'off', // 388
+        '@typescript-eslint/restrict-template-expressions': 'off', // 466
+        '@typescript-eslint/no-var-requires': 'off' // 502
+      }
     }
   ]
 }
