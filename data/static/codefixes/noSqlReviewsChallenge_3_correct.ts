ce1ab3aa3315f5c4a4a33d12e49eeda1fd663cab
Stefan Denker <Stefan@dn-kr.de>
2021-09-29 14:28:53 +0200

    a valid review-id is a string, not a number
    Signed-off-by: Stefan Denker <Stefan@dn-kr.de>

--- data/static/codefixes/noSqlReviewsChallenge_3_correct.ts
+++ data/static/codefixes/noSqlReviewsChallenge_3_correct.ts
@@ -2,7 +2,7 @@ module.exports = function productReviews () {
   return (req, res, next) => {
     const user = security.authenticatedUsers.from(req)
 
-    if (typeof req.body.id !== 'number') {
+    if (typeof req.body.id !== 'string') {
       res.status(400).send()
       return
     }
