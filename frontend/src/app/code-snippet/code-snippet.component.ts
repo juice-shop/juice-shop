1b40b145a88e6f6f07b6867cef7ae4cc1a2472aa
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2021-10-07 15:35:07 +0200

    Display explanation of submitted code fix choice
    (if available from fix info YAML file)

--- frontend/src/app/code-snippet/code-snippet.component.ts
+++ frontend/src/app/code-snippet/code-snippet.component.ts
@@ -37,6 +37,7 @@ export class CodeSnippetComponent implements OnInit {
   public tab: FormControl = new FormControl(0)
   public lock: ResultState = ResultState.Undecided
   public result: ResultState = ResultState.Undecided
+  public explanation: string = null
   public solved: Solved = { findIt: false, fixIt: false }
 
   constructor (@Inject(MAT_DIALOG_DATA) public dialogData: any, private readonly codeSnippetService: CodeSnippetService, private readonly vulnLinesService: VulnLinesService, private readonly codeFixesService: CodeFixesService, private readonly challengeService: ChallengeService, private readonly cookieService: CookieService) { }
@@ -86,6 +87,7 @@ export class CodeSnippetComponent implements OnInit {
   checkFix = () => {
     this.codeFixesService.check(this.dialogData.key, this.selectedFix).subscribe((verdict) => {
       this.setVerdict(verdict.verdict)
+      this.explanation = verdict.explanation
     })
   }
 
