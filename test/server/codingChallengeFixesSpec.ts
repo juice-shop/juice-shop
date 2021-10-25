cdd33efcb197d14ac801236ca95642b7019580ee
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2021-10-25 16:49:40 +0200

    Prepare test case to check presence of info YAML files

--- test/server/codingChallengeFixesSpec.ts
+++ test/server/codingChallengeFixesSpec.ts
@@ -6,6 +6,7 @@
 import { retrieveChallengesWithCodeSnippet } from '../../routes/vulnCodeSnippet'
 import { readFixes } from '../../routes/vulnCodeFixes'
 import chai = require('chai')
+import fs from 'graceful-fs'
 const sinonChai = require('sinon-chai')
 const expect = chai.expect
 chai.use(sinonChai)
@@ -27,6 +28,12 @@ describe('codingChallengeFixes', () => {
     for (const challenge of codingChallenges) {
       const fixes = readFixes(challenge)
       expect(fixes.fixes.length, `Coding challenge ${challenge} does not have enough fix option files`).to.be.greaterThanOrEqual(3)
+    }
+  })
+
+  xit('should have an info YAML file for each coding challenge', async () => { // TODO Enable test once all coding challenges have an info file. Then also state it as a mandatory pieced in companion guide.
+    for (const challenge of codingChallenges) {
+      expect(fs.existsSync('./data/static/codefixes/' + challenge + '.info.yml'), `Coding challenge ${challenge} does not have an info YAML file`).to.equal(true)
     }
   })
 })
