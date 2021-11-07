18441958b1ffaaea431bcd40491719b6d71e059a
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2021-11-07 13:56:50 +0100

    Add steps until submission of "Find It" solution attempt

--- frontend/src/hacking-instructor/challenges/codingChallenges.ts
+++ frontend/src/hacking-instructor/challenges/codingChallenges.ts
@@ -29,6 +29,25 @@ export const CodingChallengesInstruction: ChallengeInstruction = {
         'All Coding Challenges take place in a modal dialog like this. They consist of two parts, one for finding and one for fixing the vulnerability in the code.',
       fixture: '#code-snippet',
       resolved: waitInMs(15000)
-    }
+    },
+    {
+      text:
+        'The code snippet below shows a part of the actual application source code retrieved in real-time.',
+      fixture: '#code-snippet',
+      resolved: waitInMs(10000)
+    },
+    {
+      text:
+        'You will always get a snippet that is involved in the security vulnerability or flaw behind the corresponding hacking challenge. In this case, you see the routing code that exposes all dialogs, including the supposedly "well-hidden" Score Board.',
+      fixture: '#code-snippet',
+      resolved: waitInMs(20000)
+    },
+    {
+      text:
+        'For the "Find It" part of this coding challenge, tick the 🔲 on all lines of code that you think are responsible for the exposure of the Score Board. Then click on "Submit".',
+      fixture: '#code-snippet',
+      unskippable: true,
+      resolved: waitForElementToGetClicked('#findItSubmitButton')
+    } // TODO Introduce helper to check for the right or wrong verdict of a code snippet
   ]
 }
