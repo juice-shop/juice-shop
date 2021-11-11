33a630dad85040f2f270c5fdb2316fc4492f6f9b
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2021-11-11 21:33:59 +0100

    Improve diversity of false fix options and add info file for "Reset Morty's Password" challenge

--- data/static/codefixes/resetPasswordMortyChallenge_1.ts
+++ data/static/codefixes/resetPasswordMortyChallenge_1.ts
@@ -1,7 +1,6 @@
 /* Rate limiting */
-  app.enable('trust proxy')
   app.use('/rest/user/reset-password', new RateLimit({
-    windowMs: 3 * 60 * 1000,
+    windowMs: 5 * 60 * 1000,
     max: 100,
     keyGenerator ({ headers, ip }) { return headers['X-Forwarded-For'] || ip },
   }))
--- data/static/codefixes/resetPasswordMortyChallenge_2.ts
+++ data/static/codefixes/resetPasswordMortyChallenge_2.ts
@@ -2,6 +2,6 @@
   app.enable('trust proxy')
   app.use('/rest/user/reset-password', new RateLimit({
     windowMs: 5 * 60 * 1000,
-    max: 10,
-    keyGenerator ({ headers, ip }) { return headers['X-Forwarded-For'] || ip },
+    max: 100,
+    keyGenerator ({ headers, ip }) { return headers['Forwarded'] || ip },
   }))
