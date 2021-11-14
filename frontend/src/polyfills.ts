e10a7f189253e9e82345cd00058d5164c0d45f1d
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2021-11-14 11:01:14 +0100

    Update to Angular 12
    (npx @angular/cli@12 update @angular/core@12 @angular/cli@12)

--- frontend/src/polyfills.ts
+++ frontend/src/polyfills.ts
@@ -74,7 +74,7 @@ import 'core-js/es7/reflect'
 /** *************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone' // Included with Angular CLI.
+import 'zone.js' // Included with Angular CLI.
 (window as any).global = window
 
 /** *************************************************************************************************
