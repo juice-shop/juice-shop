05b0083aa3307ee5dddefe84f16946068a001987
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2021-12-28 11:19:40 +0100

    Bump copyright notice to include 2022

--- test/files/decrypt_bruteforce.py
+++ test/files/decrypt_bruteforce.py
@@ -1,4 +1,4 @@
-#  Copyright (c) 2014-2021 Bjoern Kimminich & the OWASP Juice Shop contributors.
+#  Copyright (c) 2014-2022 Bjoern Kimminich & the OWASP Juice Shop contributors.
 #  SPDX-License-Identifier: MIT
 
 # Public Parameters
