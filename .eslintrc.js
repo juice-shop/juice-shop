b376d66a271e57b090978de6d0dff341c54d32aa
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2022-01-09 01:51:20 +0100

    Remove obsolete global ignore of promise lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,7 +34,6 @@ module.exports = {
       rules: {
         'no-void': 'off', // conflicting with recommendation from @typescript-eslint/no-floating-promises
         // FIXME warnings below this line need to be checked and fixed. Line end comments below are number of findings per rule on 24.03.2021
-        '@typescript-eslint/no-misused-promises': 'off', // 97
         '@typescript-eslint/explicit-function-return-type': 'off', // 132
         '@typescript-eslint/restrict-plus-operands': 'off', // 292
         '@typescript-eslint/strict-boolean-expressions': 'off', // 388
