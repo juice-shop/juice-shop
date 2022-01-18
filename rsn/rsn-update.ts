4a0b265d0f6bb6bf0b39041d824a2b37c5b168ed
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2022-01-18 14:02:07 +0100

    Clarify meaning of result output on console

--- rsn/rsn-update.ts
+++ rsn/rsn-update.ts
@@ -3,17 +3,10 @@ const colors = require('colors/safe')
 
 const keys = readFiles()
 checkDiffs(keys)
-  .then(({ okay, notOkay, data }) => {
+  .then(data => {
     console.log(('---------------------------------------'))
-    if (notOkay === 0) {
-      console.log(colors.green('All files passed'))
-    } else {
-      console.log(colors.green('Total files passed: ' + okay))
-      console.log(colors.red('Total files failed: ' + notOkay))
-
-      writeToFile(data)
-      console.log('All diffs are updated')
-    }
+    writeToFile(data)
+    console.log(colors.bold('All file diffs have been locked!') + ' Commit changed cache.json to git.')
   // seePatch('restfulXssChallenge_1_correct.ts')
   })
   .catch(err => {
