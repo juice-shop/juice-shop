7c8f71dbd77471ccf2a12301011f5f1bfc555467
kai <kai@gmail.com>
2022-02-09 22:35:28 +0700

    commit 1

---
+++ data/error.js
@@ -0,0 +1,6 @@
+console.log("calling dangerousFunction() with static string");
+var userInput = req.body.danger;
+lib.dangerousFunction("static input is fine"); // compliant
+console.log("calling dangerousFunction() with user input");
+lib.dangerousFunction(userInput); // non-compliant
+console.log("dangerousFunction() called successfully");
