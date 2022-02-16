95736df3cf331627db5bd382735e1cc68564d2d3
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2022-02-16 23:06:44 +0100

    Split off verbose RSN script for local use

--- rsn/rsn-verbose.ts
+++ rsn/rsn-verbose.ts
@@ -11,7 +11,9 @@ checkDiffs(keys)
       console.log(`${colors.green.bold('No new file diffs recognized since last lock!')} No action required.`)
     } else {
       console.log(`${colors.red.bold('New file diffs recognized since last lock!')} Double-check and amend listed files and lock new state with ${colors.bold('npm run rsn:update')}`)
+      console.log(`Be aware that diffs for the ${filesWithDiff.length} affected files below contain ${colors.bold('all changes')} including locked & cached ones! Compare carefully!`)
       console.log('---------------------------------------')
+      filesWithDiff.forEach(async file => await seePatch(file))
       process.exitCode = 1
     }
   })
