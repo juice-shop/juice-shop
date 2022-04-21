09ba1ceee4447ae08881ec0bbb6077450f710daa
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2022-04-21 11:49:04 +0200

    Fix some unreported TypeScript compiler errors

--- lib/startup/cleanupFtpFolder.ts
+++ lib/startup/cleanupFtpFolder.ts
@@ -4,16 +4,17 @@
  */
 
 import glob = require('glob')
+const utils = require('../utils')
 const path = require('path')
 const fs = require('fs-extra')
 const logger = require('../logger')
 
 const cleanupFtpFolder = () => {
-  glob(path.resolve('ftp/*.pdf'), (err, files) => {
+  glob(path.resolve('ftp/*.pdf'), (err: unknown, files: string[]) => {
     if (err != null) {
-      logger.warn('Error listing PDF files in /ftp folder: ' + err.message)
+      logger.warn('Error listing PDF files in /ftp folder: ' + utils.getErrorMessage(err))
     } else {
-      files.forEach(filename => {
+      files.forEach((filename: string) => {
         fs.remove(filename)
       })
     }
