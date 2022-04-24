4c9360f4cd4de7346033824ac4aedec1ecff4137
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2022-04-24 22:16:04 +0200

    Catch and log all unexpected deluxe membership errors

--- routes/deluxe.ts
+++ routes/deluxe.ts
@@ -14,39 +14,43 @@ const challenges = require('../data/datacache').challenges
 
 module.exports.upgradeToDeluxe = function upgradeToDeluxe () {
   return async (req: Request, res: Response, next: NextFunction) => {
-    const user = await UserModel.findOne({ where: { id: req.body.UserId, role: security.roles.customer } })
-    if (!user) {
-      res.status(400).json({ status: 'error', error: 'Something went wrong. Please try again!' })
-      return
-    }
-    if (req.body.paymentMode === 'wallet') {
-      const wallet = await WalletModel.findOne({ where: { UserId: req.body.UserId } })
-      if (wallet && wallet.balance < 49) {
-        res.status(400).json({ status: 'error', error: 'Insuffienct funds in Wallet' })
-        return
-      } else {
-        await WalletModel.decrement({ balance: 49 }, { where: { UserId: req.body.UserId } })
-      }
-    }
-
-    if (req.body.paymentMode === 'card') {
-      const card = await CardModel.findOne({ where: { id: req.body.paymentId | -1, UserId: req.body.UserId } })
-      if (!card || card.expYear < new Date().getFullYear() || (card.expYear === new Date().getFullYear() && card.expMonth - 1 < new Date().getMonth())) {
-        res.status(400).json({ status: 'error', error: 'Invalid Card' })
-        return
-      }
-    }
-
-    user.update({ role: security.roles.deluxe, deluxeToken: security.deluxeToken(user.email) })
-      .then(user => {
-        utils.solveIf(challenges.freeDeluxeChallenge, () => { return security.verify(utils.jwtFrom(req)) && req.body.paymentMode !== 'wallet' && req.body.paymentMode !== 'card' })
-        user = utils.queryResultToJson(user)
-        const updatedToken = security.authorize(user)
-        security.authenticatedUsers.put(updatedToken, user)
-        res.status(200).json({ status: 'success', data: { confirmation: 'Congratulations! You are now a deluxe member!', token: updatedToken } })
-      }).catch(() => {
+    try {
+      const user = await UserModel.findOne({ where: { id: req.body.UserId, role: security.roles.customer } })
+      if (!user) {
         res.status(400).json({ status: 'error', error: 'Something went wrong. Please try again!' })
-      })
+        return
+      }
+      if (req.body.paymentMode === 'wallet') {
+        const wallet = await WalletModel.findOne({ where: { UserId: req.body.UserId } })
+        if (wallet && wallet.balance < 49) {
+          res.status(400).json({ status: 'error', error: 'Insuffienct funds in Wallet' })
+          return
+        } else {
+          await WalletModel.decrement({ balance: 49 }, { where: { UserId: req.body.UserId } })
+        }
+      }
+
+      if (req.body.paymentMode === 'card') {
+        const card = await CardModel.findOne({ where: { id: req.body.paymentId, UserId: req.body.UserId } })
+        if (!card || card.expYear < new Date().getFullYear() || (card.expYear === new Date().getFullYear() && card.expMonth - 1 < new Date().getMonth())) {
+          res.status(400).json({ status: 'error', error: 'Invalid Card' })
+          return
+        }
+      }
+
+      user.update({ role: security.roles.deluxe, deluxeToken: security.deluxeToken(user.email) })
+        .then(user => {
+          utils.solveIf(challenges.freeDeluxeChallenge, () => { return security.verify(utils.jwtFrom(req)) && req.body.paymentMode !== 'wallet' && req.body.paymentMode !== 'card' })
+          user = utils.queryResultToJson(user)
+          const updatedToken = security.authorize(user)
+          security.authenticatedUsers.put(updatedToken, user)
+          res.status(200).json({ status: 'success', data: { confirmation: 'Congratulations! You are now a deluxe member!', token: updatedToken } })
+        }).catch(() => {
+          res.status(400).json({ status: 'error', error: 'Something went wrong. Please try again!' })
+        })
+    } catch (err: unknown) {
+      res.status(400).json({ status: 'error', error: 'Something went wrong: ' + utils.getErrorMessage(err) })
+    }
   }
 }
 
