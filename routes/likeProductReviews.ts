f8b71bc45c6813d1f97774cc206faa5da71fa1d8
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2022-05-02 21:57:29 +0200

    Prevent liking non-existing product reviews
    (addresses https://github.com/juice-shop/juice-shop/commit/fc0eb5bb85435a60d518508958a5c749efa87473#commitcomment-72498456)

--- routes/likeProductReviews.ts
+++ routes/likeProductReviews.ts
@@ -16,43 +16,47 @@ module.exports = function productReviews () {
     const id = req.body.id
     const user = security.authenticatedUsers.from(req)
     db.reviews.findOne({ _id: id }).then((review: Review) => {
-      const likedBy = review?.likedBy
-      if (!likedBy?.includes(user.data.email)) {
-        db.reviews.update(
-          { _id: id },
-          { $inc: { likesCount: 1 } }
-        ).then(
-          () => {
-            // Artificial wait for timing attack challenge
-            setTimeout(function () {
-              db.reviews.findOne({ _id: id }).then((review: Review) => {
-                const likedBy = review.likedBy
-                likedBy.push(user.data.email)
-                let count = 0
-                for (let i = 0; i < likedBy.length; i++) {
-                  if (likedBy[i] === user.data.email) {
-                    count++
-                  }
-                }
-                utils.solveIf(challenges.timingAttackChallenge, () => { return count > 2 })
-                db.reviews.update(
-                  { _id: id },
-                  { $set: { likedBy: likedBy } }
-                ).then(
-                  (result: any) => {
-                    res.json(result)
-                  }, (err: unknown) => {
-                    res.status(500).json(err)
-                  })
-              }, () => {
-                res.status(400).json({ error: 'Wrong Params' })
-              })
-            }, 150)
-          }, (err: unknown) => {
-            res.status(500).json(err)
-          })
+      if (!review) {
+        res.status(404).json({ error: 'Not found' })
       } else {
-        res.status(403).json({ error: 'Not allowed' })
+        const likedBy = review.likedBy
+        if (!likedBy.includes(user.data.email)) {
+          db.reviews.update(
+            { _id: id },
+            { $inc: { likesCount: 1 } }
+          ).then(
+            () => {
+              // Artificial wait for timing attack challenge
+              setTimeout(function () {
+                db.reviews.findOne({ _id: id }).then((review: Review) => {
+                  const likedBy = review.likedBy
+                  likedBy.push(user.data.email)
+                  let count = 0
+                  for (let i = 0; i < likedBy.length; i++) {
+                    if (likedBy[i] === user.data.email) {
+                      count++
+                    }
+                  }
+                  utils.solveIf(challenges.timingAttackChallenge, () => { return count > 2 })
+                  db.reviews.update(
+                    { _id: id },
+                    { $set: { likedBy: likedBy } }
+                  ).then(
+                    (result: any) => {
+                      res.json(result)
+                    }, (err: unknown) => {
+                      res.status(500).json(err)
+                    })
+                }, () => {
+                  res.status(400).json({ error: 'Wrong Params' })
+                })
+              }, 150)
+            }, (err: unknown) => {
+              res.status(500).json(err)
+            })
+        } else {
+          res.status(403).json({ error: 'Not allowed' })
+        }
       }
     }, () => {
       res.status(400).json({ error: 'Wrong Params' })
