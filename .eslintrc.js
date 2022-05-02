afa8206ac0f6336623e2202deb1610e1f29b4198
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2022-05-02 22:19:04 +0200

    Update number of knowingly suppressed ESLint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,13 +33,13 @@ module.exports = {
       parser: '@typescript-eslint/parser',
       rules: {
         'no-void': 'off', // conflicting with recommendation from @typescript-eslint/no-floating-promises
-        // FIXME warnings below this line need to be checked and fixed. Line end comments below are number of findings per rule on 24.03.2021
-        '@typescript-eslint/explicit-function-return-type': 'off', // 132
-        '@typescript-eslint/restrict-plus-operands': 'off', // 292
-        '@typescript-eslint/strict-boolean-expressions': 'off', // 388
-        '@typescript-eslint/restrict-template-expressions': 'off', // 466
-        '@typescript-eslint/no-var-requires': 'off', // 502
-        '@typescript-eslint/no-misused-promises': 'off' // 95
+        // FIXME warnings below this line need to be checked and fixed. Line end comments below are number of findings per rule on 02.05.2022
+        '@typescript-eslint/no-misused-promises': 'off', // 1
+        '@typescript-eslint/explicit-function-return-type': 'off', // 197
+        '@typescript-eslint/restrict-plus-operands': 'off', // 250
+        '@typescript-eslint/strict-boolean-expressions': 'off', // 337
+        '@typescript-eslint/restrict-template-expressions': 'off', // 395
+        '@typescript-eslint/no-var-requires': 'off' // 509
       }
     }
   ]
