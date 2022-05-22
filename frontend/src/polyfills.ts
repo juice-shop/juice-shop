331cf9dc9996620ce6707a4d27e5855247dffa4e
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2022-05-22 13:42:42 +0200

    Fix code-js path for polyfills
    (fixes #1815)

--- frontend/src/polyfills.ts
+++ frontend/src/polyfills.ts
@@ -44,7 +44,7 @@
 
 /** Evergreen browsers require these. */
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
-import 'core-js/es7/reflect'
+import 'core-js/es/reflect'
 
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
