b9cff8c0c0cf2ffe03f0fc40524f069fd570f340
ShubhamPalriwala <spalriwalau@gmail.com>
2022-05-31 20:25:02 +0530

    migrate: default files to ts

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -16,7 +16,7 @@
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
+export = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
