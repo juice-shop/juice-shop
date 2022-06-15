a5daa70c7126a7f4930df920958d8f7cb2cc97ae
ShubhamPalriwala <spalriwalau@gmail.com>
2022-06-15 13:45:44 +0530

    feat: cy.expectChallengeSolved command

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -14,7 +14,15 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import "./commands";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      expectChallengeSolved(value: { challenge: string }): void;
+    }
+  }
+}
