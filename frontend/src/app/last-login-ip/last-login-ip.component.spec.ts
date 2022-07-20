1bb101b122c95236faace995a89a2bc5df92b0c3
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2022-07-20 18:23:49 +0200

    Add tests for last login IP

--- frontend/src/app/last-login-ip/last-login-ip.component.spec.ts
+++ frontend/src/app/last-login-ip/last-login-ip.component.spec.ts
@@ -6,14 +6,23 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
 import { LastLoginIpComponent } from './last-login-ip.component'
 import { MatCardModule } from '@angular/material/card'
+import { DomSanitizer } from '@angular/platform-browser'
 
 describe('LastLoginIpComponent', () => {
   let component: LastLoginIpComponent
   let fixture: ComponentFixture<LastLoginIpComponent>
+  let sanitizer
 
   beforeEach(waitForAsync(() => {
+    sanitizer = jasmine.createSpyObj('DomSanitizer', ['bypassSecurityTrustHtml', 'sanitize'])
+    sanitizer.bypassSecurityTrustHtml.and.callFake((args: any) => args)
+    sanitizer.sanitize.and.returnValue({})
+
     TestBed.configureTestingModule({
       declarations: [LastLoginIpComponent],
+      providers: [
+        { provide: DomSanitizer, useValue: sanitizer }
+      ],
       imports: [
         MatCardModule
       ]
@@ -28,5 +37,24 @@ describe('LastLoginIpComponent', () => {
 
   it('should compile', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should log JWT parsing error to console', () => {
+    console.log = jasmine.createSpy('log')
+    localStorage.setItem('token', 'definitelyInvalidJWT')
+    component.ngOnInit()
+    expect(console.log).toHaveBeenCalled()
+  })
+
+  it('should set Last-Login IP from JWT as trusted HTML', () => {
+    localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Imxhc3RMb2dpbklwIjoiMS4yLjMuNCJ9fQ.RAkmdqwNypuOxv3SDjPO4xMKvd1CddKvDFYDBfUt3bg')
+    component.ngOnInit()
+    expect(sanitizer.bypassSecurityTrustHtml).toHaveBeenCalledWith('<small>1.2.3.4</small>')
+  })
+
+  it('should not set Last-Login IP if none is present in JWT', () => {
+    localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7fX0.bVBhvll6IaeR3aUdoOeyR8YZe2S2DfhGAxTGfd9enLw')
+    component.ngOnInit()
+    expect(component.lastLoginIp).toBe('?')
   })
 })
