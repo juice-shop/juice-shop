ff39820cf9b0ae36fbab96581f12e907ba04ab35
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2022-11-29 18:15:41 +0100

    Check presence of token before verifying
    (fixes #1928)

--- lib/insecurity.ts
+++ lib/insecurity.ts
@@ -53,7 +53,7 @@ exports.cutOffPoisonNullByte = (str: string) => {
 exports.isAuthorized = () => expressJwt({ secret: publicKey })
 exports.denyAll = () => expressJwt({ secret: '' + Math.random() })
 exports.authorize = (user = {}) => jwt.sign(user, privateKey, { expiresInMinutes: 60 * 5, algorithm: 'RS256' })
-const verify = (token: string) => jws.verify(token, publicKey)
+const verify = (token: string) => token ? jws.verify(token, publicKey) : false
 module.exports.verify = verify
 const decode = (token: string) => { return jws.decode(token).payload }
 module.exports.decode = decode
