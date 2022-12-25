948f22771c03f70c624b138a29d2030828a0262a
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2022-12-26 00:32:14 +0100

    Bump copyright notes to include 2023

--- routes/dataErasure.ts
+++ routes/dataErasure.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2022 Bjoern Kimminich & the OWASP Juice Shop contributors.
+ * Copyright (c) 2014-2023 Bjoern Kimminich & the OWASP Juice Shop contributors.
  * SPDX-License-Identifier: MIT
  */
 import express, { NextFunction, Request, Response } from 'express'
