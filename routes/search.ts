429fa5428262e8ae243d0b1dbd9f9165588d7f62
arnica-engineering <95713264+arnica-engineering@users.noreply.github.com>
2023-01-15 14:46:59 -0500

    refactor

--- routes/search.ts
+++ routes/search.ts
@@ -18,9 +18,9 @@ class ErrorWithParent extends Error {
 // vuln-code-snippet start unionSqlInjectionChallenge dbSchemaChallenge
 module.exports = function searchProducts () {
   return (req: Request, res: Response, next: NextFunction) => {
-    let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
-    criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
+    let criteria1: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
+    criteria1 = (criteria1.length <= 200) ? criteria1 : criteria1.substring(0, 200)
+    models.sequelize.query(`SELECT a,b,c FROM Products WHERE ((name LIKE '%${criteria1}%' OR description LIKE '%${criteria1}%') AND deletedAt IS NULL) ORDER BY name`) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
       .then(([products]: any) => {
         const dataString = JSON.stringify(products)
         if (challengeUtils.notSolved(challenges.unionSqlInjectionChallenge)) { // vuln-code-snippet hide-start
