fb47378f6ce2e88f6872d71ca56fc851901ce765
Timo Pagel <github@timo-pagel.de>
2023-01-18 09:07:06 +0100

    move logic to user.ts

--- data/static/codefixes/weakPasswordChallenge_1_correct.ts
+++ data/static/codefixes/weakPasswordChallenge_1_correct.ts
@@ -1,16 +1,9 @@
-export class LoginComponent implements OnInit {
-  public emailControl = new UntypedFormControl('', [Validators.required])
-
-  public passwordControl = new UntypedFormControl('', [
-    Validators.required,
-    Validators.minLength(8),
-    validatePasswordIsNotInTopOneMillionCommonPasswordsList()
-  ])
-
-  public hide = true
-  public user: any
-  public rememberMe: UntypedFormControl = new UntypedFormControl(false)
-  public error: any
-  public clientId = '1005568560502-6hm16lef8oh46hr2d98vf2ohlnj4nfhq.apps.googleusercontent.com'
-  public oauthUnavailable: boolean = true
-  public redirectUri: string = ''
+User.init(
+      password: {
+        type: DataTypes.STRING,
+        set (clearTextPassword) {
+          validatePasswordHasAtLeastTenChar(clearTextPassword)
+          validatePasswordIsNotInTopOneMillionCommonPasswordsList(clearTextPassword)
+          this.setDataValue('password', security.hash(clearTextPassword))
+        }
+      },
--- data/static/codefixes/weakPasswordChallenge_3.ts
+++ data/static/codefixes/weakPasswordChallenge_3.ts
@@ -1,20 +1,12 @@
-export class LoginComponent implements OnInit {
-  public emailControl = new UntypedFormControl('', [Validators.required])
-
-  public passwordControl = new UntypedFormControl('', [
-    Validators.required,
-    Validators.minLength(8),
-    validatePasswordHasAtLeastOneNumber(),
-    validatePasswordHasAtLeastOneSpecialChar(),
-    validatePasswordHasAtLeastOneUpperCaseChar(),
-    validatePasswordHasAtLeastOneLowerCaseChar(),
-    validatePasswordHasNoSpace(),
-  ])
-
-  public hide = true
-  public user: any
-  public rememberMe: UntypedFormControl = new UntypedFormControl(false)
-  public error: any
-  public clientId = '1005568560502-6hm16lef8oh46hr2d98vf2ohlnj4nfhq.apps.googleusercontent.com'
-  public oauthUnavailable: boolean = true
-  public redirectUri: string = ''
+User.init(
+      password: {
+        type: DataTypes.STRING,
+        set (clearTextPassword) {
+          validatePasswordHasAtLeastOneNumber(clearTextPassword)
+          validatePasswordHasAtLeastOneSpecialChar(clearTextPassword)
+          validatePasswordHasAtLeastOneUpperCaseChar(clearTextPassword)
+          validatePasswordHasAtLeastOneLowerCaseChar(clearTextPassword)
+          validatePasswordHasAtLeastTenChar(clearTextPassword)
+          this.setDataValue('password', security.hash(clearTextPassword))
+        }
+      },
