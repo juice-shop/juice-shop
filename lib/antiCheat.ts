5c14887d8eeec2edf310be4c7fa5e0809e90db2c
Soham Parate <sohamparate1905@gmail.com>
2023-04-20 15:35:39 +0530

    Consider coding challenges that have overlapping code snippets easier to solve (#1996)
    * consider solving identical coding challenges easier if a challenge that uses the same code snippet is already solved
    
    * import/export functions and objects for /lib/startup/identicalChallenges.ts
    
    * running lib/startup/identicalChallenges at startup
    
    * Fixed the code where a potential file system race condition could arise
    
    * fixed RSN issue with 'exposed metrics' challenge
    
    * migrating fileSniff function to a more generalized location and considering identical coding Challenges easier to solve
    
    * consider identical(overlapping coding) Challenges easier to solve
    
    * delete files that use outdated approach
    
    * Update vuln code snippet code to only run once and generate the entire cache from that run
    
    Previously the entire file-sniffs was run once per challenge and each run read the file multiple times
    
    * Move pervious fileSniff code to startup and use from anti cheat
    
    * Remove timer
    
    * remove unncessary comments
    
    * update rsn
    
    * moving coding challenges from lib/startup/ to lib/
    
    * code refactoring
    
    * Adjust error messages and change expected status codes to 404
    
    I don't think it's required to have a 412 status code for the case when the challenge exists but doesn't have snippets.
    No snippets wre found so 404 is correct
    
    Signed-off-by: Jannik Hollenbach <jannik@hollenbach.de>
    
    * Fix snippets in yaml files not being found
    
    Signed-off-by: Jannik Hollenbach <jannik@hollenbach.de>
    
    * Change fs to default to async in coding challenge lib file
    
    Signed-off-by: Jannik Hollenbach <jannik@hollenbach.de>
    
    * Handle potential failure of file read
    
    Signed-off-by: Jannik Hollenbach <jannik@hollenbach.de>
    
    * remove comments
    
    * Fixed error: Cannot find logger in codingChallenges
    
    * reduced timeFactor of solving identical Challenges by 20%
    
    ---------
    
    Signed-off-by: Jannik Hollenbach <jannik@hollenbach.de>
    Co-authored-by: Jannik Hollenbach <jannik@hollenbach.de>

--- lib/antiCheat.ts
+++ lib/antiCheat.ts
@@ -7,6 +7,7 @@ import config = require('config')
 import { retrieveCodeSnippet } from '../routes/vulnCodeSnippet'
 import { readFixes } from '../routes/vulnCodeFixes'
 import { Challenge } from '../data/types'
+import { getCodeChallenges } from './codingChallenges'
 const colors = require('colors/safe')
 const logger = require('./logger')
 
@@ -40,20 +41,29 @@ exports.calculateCheatScore = (challenge: Challenge) => {
   return cheatScore
 }
 
-exports.calculateFindItCheatScore = async (challenge: Challenge) => { // TODO Consider coding challenges with identical/overlapping snippets as easier once one of them has been solved
+exports.calculateFindItCheatScore = async (challenge: Challenge) => {
   const timestamp = new Date()
   let timeFactor = 0.001
   timeFactor *= (challenge.key === 'scoreBoardChallenge' && config.get('hackingInstructor.isEnabled') ? 0.5 : 1)
   let cheatScore = 0
 
-  const { snippet, vulnLines } = await retrieveCodeSnippet(challenge.key)
+  const codeSnippet = await retrieveCodeSnippet(challenge.key)
+  if (!codeSnippet) {
+    return 0
+  }
+  const { snippet, vulnLines } = codeSnippet
   timeFactor *= vulnLines.length
+  const identicalSolved = await checkForIdenticalSolvedChallenge(challenge)
+  if (identicalSolved) {
+    timeFactor = 0.8 * timeFactor
+  }
   const minutesExpectedToSolve = Math.ceil(snippet.length * timeFactor)
   const minutesSincePreviousSolve = (timestamp.getTime() - previous().timestamp.getTime()) / 60000
   cheatScore += Math.max(0, 1 - (minutesSincePreviousSolve / minutesExpectedToSolve))
 
   logger.info(`Cheat score for "Find it" phase of ${challenge.key === 'scoreBoardChallenge' && config.get('hackingInstructor.isEnabled') ? 'tutorial ' : ''}${colors.cyan(challenge.key)} solved in ${Math.round(minutesSincePreviousSolve)}min (expected ~${minutesExpectedToSolve}min): ${cheatScore < 0.33 ? colors.green(cheatScore) : (cheatScore < 0.66 ? colors.yellow(cheatScore) : colors.red(cheatScore))}`)
   solves.push({ challenge, phase: 'find it', timestamp, cheatScore })
+
   return cheatScore
 }
 
@@ -86,4 +96,33 @@ function isTrivial (challenge: Challenge) {
 
 function previous () {
   return solves[solves.length - 1]
+}
+
+const checkForIdenticalSolvedChallenge = async (challenge: Challenge): Promise<Boolean> => {
+  const codingChallenges = await getCodeChallenges()
+  if (!codingChallenges.has(challenge.key)) {
+    return false
+  }
+
+  const codingChallengesToCompareTo = codingChallenges.get(challenge.key)
+  if (!codingChallengesToCompareTo || !codingChallengesToCompareTo.snippet) {
+    return false
+  }
+  const snippetToCompareTo = codingChallengesToCompareTo.snippet
+
+  for (const [challengeKey, { snippet }] of codingChallenges.entries()) {
+    if (challengeKey === challenge.key) {
+      // don't compare to itself
+      continue
+    }
+
+    if (snippet === snippetToCompareTo) {
+      for (const solvedChallenges of solves) {
+        if (solvedChallenges.phase === 'find it') {
+          return true
+        }
+      }
+    }
+  }
+  return false
 }
