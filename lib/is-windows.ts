eb5b292cd35d9675dea6e123fbd4ff4345635be8
Jannik Hollenbach <jannik@hollenbach.de>
2023-05-06 19:16:02 +0200

    Remove process import
    Again problems in cypress...

--- lib/is-windows.ts
+++ lib/is-windows.ts
@@ -1,6 +1,5 @@
 // from https://github.com/jonschlinkert/is-windows MIT Licensed
 // inlined to avoid import problems in cypress
-import process from 'process'
 
 export default function isWindows () {
   return process && (process.platform === 'win32' || /^(msys|cygwin)$/.test(process.env.OSTYPE ?? ''))
