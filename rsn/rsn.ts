a4ce795ad247bc65d7f4058ee84315d382befeae
Jannik Hollenbach <jannik@hollenbach.de>
2023-05-06 19:09:49 +0200

    Update rsn for challenges which non relevant line changes

--- rsn/rsn.ts
+++ rsn/rsn.ts
@@ -8,9 +8,9 @@ checkDiffs(keys)
     const fileData = getDataFromFile()
     const filesWithDiff = checkData(data, fileData)
     if (filesWithDiff.length === 0) {
-      console.log(`${colors.green.bold('No new file diffs recognized since last lock!')} No action required.`)
+      console.log(`${colors.green(colors.bold('No new file diffs recognized since last lock!'))} No action required.`)
     } else {
-      console.log(`${colors.red.bold('New file diffs recognized since last lock!')} Double-check and amend listed files and lock new state with ${colors.bold('npm run rsn:update')}`)
+      console.log(`${colors.red(colors.bold('New file diffs recognized since last lock!'))} Double-check and amend listed files and lock new state with ${colors.bold('npm run rsn:update')}`)
       console.log('---------------------------------------')
       process.exitCode = 1
     }
