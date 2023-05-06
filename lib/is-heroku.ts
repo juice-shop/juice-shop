3a9a6683ab7fdd3f2fdcb9b7d9827e1d86a17dea
Jannik Hollenbach <jannik@hollenbach.de>
2023-05-06 18:47:51 +0200

    Update is-heroku to ensure cypress compatability

--- lib/is-heroku.ts
+++ lib/is-heroku.ts
@@ -1,8 +1,4 @@
 // from https://github.com/sindresorhus/is-heroku/tree/main MIT Licensed
 // inlined to avoid import problems in cypress
 
-import process from 'process'
-
-const isHeroku = 'HEROKU' in process.env || ('DYNO' in process.env && process.env.HOME === '/app')
-
-export default () => isHeroku
+export default () => 'HEROKU' in process.env || ('DYNO' in process.env && process.env.HOME === '/app')
