1c7d50cfb2ed6fabc80156fb6e473e41eda288d8
Elad Kohavi <eladkohavi@Elads-MacBook-Pro.local>
2023-05-29 16:38:52 +0300

    Changes from demo

--- data/mongodb.ts
+++ data/mongodb.ts
@@ -5,7 +5,15 @@
 
 // @ts-expect-error due to non-existing type definitions for MarsDB
 import MarsDB = require('marsdb')
-
+export const SONAR_REGEX = {
+  BUGS: /\[\d+ Bug[s]?\]/g,
+  VULNERABILITIES: /\[\d+ Vulnerabilit(?:ies|y)\]/g,
+  SECURITY_HOTSPOTS: /\[\d+ Security Hotspot[s]?\]/g,
+  CODE_SMELL: /\[\d+ Code Smell[s]?\]/g,
+  DUPLICATIONS: /\[(\d+(\.\d+)?|\.\d+)%\]/g,
+  COVERAGE: /\[(\d+(\.\d+)?|\.\d+)%\]/g,
+  RATING: /!\[([A-Z])\]/g,
+};
 const reviews = new MarsDB.Collection('posts')
 const orders = new MarsDB.Collection('orders')
 
