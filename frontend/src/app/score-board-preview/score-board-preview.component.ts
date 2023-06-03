1e12d9fbbdb2908b83835bb6dd5e87f95203a1bc
Jannik Hollenbach <jannik@hollenbach.de>
2023-06-03 13:03:15 +0200

    WIP category filtering

--- frontend/src/app/score-board-preview/score-board-preview.component.ts
+++ frontend/src/app/score-board-preview/score-board-preview.component.ts
@@ -2,8 +2,11 @@ import { Component, OnInit } from '@angular/core'
 import { ChallengeService } from '../Services/challenge.service'
 import { CodeSnippetService } from '../Services/code-snippet.service'
 import { DomSanitizer } from '@angular/platform-browser'
-import { forkJoin } from 'rxjs'
+import { combineLatest, forkJoin } from 'rxjs'
 import { EnrichedChallenge } from './types/EnrichedChallenge'
+import { uniq } from 'lodash'
+import { ActivatedRoute } from '@angular/router'
+
 
 @Component({
   selector: 'score-board-preview',
@@ -14,17 +17,56 @@ export class ScoreBoardPreviewComponent implements OnInit {
   public allChallenges: EnrichedChallenge[] = []
   public filteredChallenges: EnrichedChallenge[] = []
 
+  public categories = [];
+  public selectedCategory = new Set();
+
   constructor (
     private readonly challengeService: ChallengeService,
     private readonly codeSnippetService: CodeSnippetService,
-    private readonly sanitizer: DomSanitizer
+    private readonly sanitizer: DomSanitizer,
+    private readonly route: ActivatedRoute
   ) { }
 
+
+  public toggleCategorySelected (category: string) {
+    if (this.selectedCategory.has(category)) {
+      this.selectedCategory.delete(category)
+    } else {
+      this.selectedCategory.add(category)
+    }
+    this.filteredChallenges = this.filterChallenges(this.allChallenges)
+  }
+  public isAllCategoriesSelected () {
+    return (this.selectedCategory.size === 0)
+  }
+  public isCategorySelected (category: string) {
+    return this.selectedCategory.has(category)
+  }
+
+  public resetCategoryFilter () {
+    this.selectedCategory = new Set()
+    this.filteredChallenges = this.filterChallenges(this.allChallenges)
+  }
+
+  public filterChallenges (challenges: EnrichedChallenge[]): EnrichedChallenge[] {
+    return challenges.filter((challenge) => {
+      if (this.selectedCategory.size === 0) {
+        return true
+      }
+      return this.selectedCategory.has(challenge.category)
+    })
+  }
+
   ngOnInit () {
-    forkJoin([
+    console.time('ScoreBoardPreview - load challenges')
+    combineLatest([
       this.challengeService.find({ sort: 'name' }),
-      this.codeSnippetService.challenges()
-    ]).subscribe(([challenges, challengeKeysWithCodeChallenges]) => {
+      this.codeSnippetService.challenges(),
+      this.route.queryParams
+    ]).subscribe(([challenges, challengeKeysWithCodeChallenges, queryParams]) => {
+      console.timeEnd('ScoreBoardPreview - load challenges')
+
+      console.time('ScoreBoardPreview - transform challenges')
       const transformedChallenges = challenges.map((challenge) => {
         return {
           ...challenge,
@@ -35,7 +77,13 @@ export class ScoreBoardPreviewComponent implements OnInit {
         }
       })
       this.allChallenges = transformedChallenges
-      this.filteredChallenges = transformedChallenges
+      this.categories = uniq(transformedChallenges.map((challenge) => challenge.category))
+      this.filteredChallenges = this.filterChallenges(this.allChallenges)
+      console.timeEnd('ScoreBoardPreview - transform challenges')
     })
+  }
+  
+  getChallengeKey(index: number, challenge: EnrichedChallenge): string {
+    return challenge.key
   }
 }
