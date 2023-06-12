702dc8ce9d2733bdb5b25279d8f0bbb9372427f8
Jannik Hollenbach <jannik@hollenbach.de>
2023-06-12 21:19:01 +0200

    Add filter settings for difficulty, status and tags

--- frontend/src/app/score-board-preview/score-board-preview.component.ts
+++ frontend/src/app/score-board-preview/score-board-preview.component.ts
@@ -1,13 +1,12 @@
 import { combineLatest } from 'rxjs'
-import { Component, OnChanges, OnInit } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { DomSanitizer } from '@angular/platform-browser'
-import { ActivatedRoute } from '@angular/router'
 
 import { ChallengeService } from '../Services/challenge.service'
 import { CodeSnippetService } from '../Services/code-snippet.service'
 
 import { EnrichedChallenge } from './types/EnrichedChallenge'
-import { FilterSetting } from './types/FilterSetting'
+import { DEFAULT_FILTER_SETTING, FilterSetting } from './types/FilterSetting'
 
 @Component({
   selector: 'score-board-preview',
@@ -17,7 +16,7 @@ import { FilterSetting } from './types/FilterSetting'
 export class ScoreBoardPreviewComponent implements OnInit {
   public allChallenges: EnrichedChallenge[] = []
   public filteredChallenges: EnrichedChallenge[] = []
-  public filterSetting: FilterSetting = { categories: new Set() }
+  public filterSetting: FilterSetting = structuredClone(DEFAULT_FILTER_SETTING)
 
   constructor (
     private readonly challengeService: ChallengeService,
@@ -26,16 +25,43 @@ export class ScoreBoardPreviewComponent implements OnInit {
   ) { }
 
   onFilterSettingUpdate (filterSetting: FilterSetting) {
+    console.log('ScoreBoardPreview - filter setting update', filterSetting)
+    this.filterSetting = filterSetting
     this.filteredChallenges = ScoreBoardPreviewComponent.filterChallenges(this.allChallenges, filterSetting)
   }
 
   public static filterChallenges (challenges: EnrichedChallenge[], filterSetting: FilterSetting): EnrichedChallenge[] {
-    return challenges.filter((challenge) => {
-      if (filterSetting.categories.size === 0) {
+    return challenges
+      .filter((challenge) => {
+        if (filterSetting.categories.size === 0) {
+          return true
+        }
+        return filterSetting.categories.has(challenge.category)
+      })
+      .filter((challenge) => {
+        if (filterSetting.difficulties.length === 0) {
+          return true
+        }
+        return filterSetting.difficulties.includes(challenge.difficulty)
+      })
+      .filter((challenge) => {
+        if (filterSetting.tags.length === 0) {
+          return true
+        }
+        return challenge.tagList.some((tag) => filterSetting.tags.includes(tag))
+      })
+      .filter((challenge) => {
+        if (filterSetting.status === null) {
+          return true
+        }
+        if (filterSetting.status === 'solved') {
+          return challenge.solved
+        }
+        if (filterSetting.status === 'unsolved') {
+          return !challenge.solved
+        }
         return true
-      }
-      return filterSetting.categories.has(challenge.category)
-    })
+      })
   }
 
   ngOnInit () {
@@ -64,5 +90,11 @@ export class ScoreBoardPreviewComponent implements OnInit {
 
   getChallengeKey (index: number, challenge: EnrichedChallenge): string {
     return challenge.key
+  }
+
+  public reset () {
+    console.log('resetting filter settings')
+    this.filterSetting = structuredClone(DEFAULT_FILTER_SETTING)
+    this.filteredChallenges = ScoreBoardPreviewComponent.filterChallenges(this.allChallenges, this.filterSetting)
   }
 }
--- frontend/src/app/score-board-preview/types/FilterSetting.ts
+++ frontend/src/app/score-board-preview/types/FilterSetting.ts
@@ -1,4 +1,20 @@
 export interface FilterSetting {
     // categories to display. if empty, all categories are displayed
     categories: Set<string>
+
+    // difficulties to display. if empty, all difficulties are displayed
+    difficulties: Array<1|2|3|4|5|6>
+
+    // tags to display. if empty, all tags are displayed
+    tags: string[]
+
+    // status to display. if null, all status are displayed
+    status: 'solved' | 'unsolved' | null
 }
+
+export const DEFAULT_FILTER_SETTING: Readonly<FilterSetting> = Object.freeze({
+    categories: new Set<string>(),
+    difficulties: [],
+    tags: [],
+    status: null
+})
