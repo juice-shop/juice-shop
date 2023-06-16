69e1d1e1e4868812ba12afa875b105fcf037c303
Jannik Hollenbach <jannik@hollenbach.de>
2023-06-16 21:47:37 +0200

    Fix typescript for rsn:verbose

--- rsn/rsn-verbose.ts
+++ rsn/rsn-verbose.ts
@@ -8,9 +8,9 @@ checkDiffs(keys)
     const fileData = getDataFromFile()
     const filesWithDiff = checkData(data, fileData)
     if (filesWithDiff.length === 0) {
-      console.log(`${colors.green.bold('No new file diffs recognized since last lock!')} No action required.`)
+      console.log(`${colors.green(colors.bold('No new file diffs recognized since last lock!'))} No action required.`)
     } else {
-      console.log(`${colors.red.bold('New file diffs recognized since last lock!')} Double-check and amend listed files and lock new state with ${colors.bold('npm run rsn:update')}`)
+      console.log(`${colors.red(colors.bold('New file diffs recognized since last lock!'))} Double-check and amend listed files and lock new state with ${colors.bold('npm run rsn:update')}`)
       console.log(`Be aware that diffs for the ${filesWithDiff.length} affected files below contain ${colors.bold('all changes')} including locked & cached ones! Compare carefully!`)
       console.log('---------------------------------------')
       filesWithDiff.forEach(async file => await seePatch(file))
