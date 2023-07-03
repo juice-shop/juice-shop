2b25ed6fb4a2bdbd3d663a8384bbadfbede933f1
Jannik Hollenbach <jannik@hollenbach.de>
2023-07-03 21:43:02 +0200

    Remove commented out badges

--- frontend/src/app/score-board-preview/components/challenge-card/challenge-card.component.ts
+++ frontend/src/app/score-board-preview/components/challenge-card/challenge-card.component.ts
@@ -25,18 +25,4 @@ export class ChallengeCardComponent {
 
   public hasInstructions = hasInstructions
   public startHackingInstructorFor = startHackingInstructorFor
-
-  codingChallengeStatusColor () {
-    if (!this.challenge.hasCodingChallenge || !this.challenge.solved) {
-      return 'var(--theme-background-lighter)'
-    }
-    switch (this.challenge.codingChallengeStatus) {
-      case 0:
-        return 'var(--theme-background-lighter)'
-      case 1:
-        return 'var(--theme-accent-dark)'
-      case 2:
-        return 'var(--theme-accent)'
-    }
-  }
 }
