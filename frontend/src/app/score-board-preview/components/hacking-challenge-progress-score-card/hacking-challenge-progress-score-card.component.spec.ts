181b99fe4d45292be5a4969a378a175685e180c4
Jannik Hollenbach <jannik@hollenbach.de>
2023-07-05 21:15:26 +0200

    Add translation to new score-board

--- frontend/src/app/score-board-preview/components/hacking-challenge-progress-score-card/hacking-challenge-progress-score-card.component.spec.ts
+++ frontend/src/app/score-board-preview/components/hacking-challenge-progress-score-card/hacking-challenge-progress-score-card.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { HackingChallengeProgressScoreCardComponent } from './hacking-challenge-progress-score-card.component'
+import { TranslateModule } from '@ngx-translate/core'
 
 describe('HackingChallengeProgressScoreCardComponent', () => {
   let component: HackingChallengeProgressScoreCardComponent
@@ -8,6 +9,8 @@ describe('HackingChallengeProgressScoreCardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [TranslateModule.forRoot()],
+
       declarations: [HackingChallengeProgressScoreCardComponent]
     })
     .compileComponents()
