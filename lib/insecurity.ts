e9f772aa6b71fbdca2510ac1a0b80368bdd6702f
gitsaf1020 <70586046+gitsaf1020@users.noreply.github.com>
2023-07-26 09:31:28 -0500

    Update insecurity.ts
    Commented out the code line where Math.random() was used.

--- lib/insecurity.ts
+++ lib/insecurity.ts
@@ -52,7 +52,7 @@ export const cutOffPoisonNullByte = (str: string) => {
 }
 
 export const isAuthorized = () => expressJwt(({ secret: publicKey }) as any)
-export const denyAll = () => expressJwt({ secret: '' + Math.random() } as any)
+/* export const denyAll = () => expressJwt({ secret: '' + Math.random() } as any) */
 export const authorize = (user = {}) => jwt.sign(user, privateKey, { expiresIn: '6h', algorithm: 'RS256' })
 export const verify = (token: string) => token ? (jws.verify as ((token: string, secret: string) => boolean))(token, publicKey) : false
 export const decode = (token: string) => { return jws.decode(token).payload }
