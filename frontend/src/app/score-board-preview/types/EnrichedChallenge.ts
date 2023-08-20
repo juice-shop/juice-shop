9e3a1ff7257ce2f00e45b8d4d377ba5c1672647d
Jannik Hollenbach <jannik@hollenbach.de>
2023-08-20 14:31:55 +0200

    Extract difficulty stars component

--- frontend/src/app/score-board-preview/types/EnrichedChallenge.ts
+++ frontend/src/app/score-board-preview/types/EnrichedChallenge.ts
@@ -3,6 +3,5 @@ import { Challenge } from '../../Models/challenge.model'
 export interface EnrichedChallenge extends Challenge {
   originalDescription: string
   tagList: string[]
-  difficultyAsList: number[]
   hasCodingChallenge: boolean
 }
