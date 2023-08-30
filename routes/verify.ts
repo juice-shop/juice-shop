fef22cf5e94d7ebc986a2730c42080c6134af641
rishabhkeshan <rishabhkeshan@gmail.com>
2023-08-30 15:07:56 +0530

    feat: added 1 star challenge for fidning sandbox

--- routes/verify.ts
+++ routes/verify.ts
@@ -63,6 +63,7 @@ exports.passwordRepeatChallenge = () => (req: Request, res: Response, next: Next
 
 exports.accessControlChallenges = () => ({ url }: Request, res: Response, next: NextFunction) => {
   challengeUtils.solveIf(challenges.scoreBoardChallenge, () => { return utils.endsWith(url, '/1px.png') })
+  challengeUtils.solveIf(challenges.web3SandboxChallenge, () => { return utils.endsWith(url, '/11px.png') })
   challengeUtils.solveIf(challenges.adminSectionChallenge, () => { return utils.endsWith(url, '/19px.png') })
   challengeUtils.solveIf(challenges.tokenSaleChallenge, () => { return utils.endsWith(url, '/56px.png') })
   challengeUtils.solveIf(challenges.privacyPolicyChallenge, () => { return utils.endsWith(url, '/81px.png') })
