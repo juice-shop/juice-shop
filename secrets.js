d898c0a146a790f8fd3838cadd42fbce112290a8
mnanjala903 <143017705+mnanjala903@users.noreply.github.com>
2023-09-05 15:47:22 -0500

    Create secrets.js

---
+++ secrets.js
@@ -0,0 +1,4 @@
+//This file introduces a fake token into a variable
+var token1 = 'vStLjoQnxbkYNg.atlasv1.SJuX2rdMQChw3AvQ5QAsyJ8NencfZWsCGEee1LVzNxCcy9a0s5GMme3kFAGenWMLqMA'
+var token2 = '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'
+var token3 = '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'
