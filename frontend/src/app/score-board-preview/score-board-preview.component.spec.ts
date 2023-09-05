1dc9e96437691967e6732e3de1757c1b94b2f5ea
Jannik Hollenbach <jannik@hollenbach.de>
2023-09-05 21:19:18 +0200

    Add spinner when loading the initial challenge progress
    Signed-off-by: Jannik Hollenbach <jannik@hollenbach.de>

--- frontend/src/app/score-board-preview/score-board-preview.component.spec.ts
+++ frontend/src/app/score-board-preview/score-board-preview.component.spec.ts
@@ -1,3 +1,4 @@
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
 import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { RouterTestingModule } from '@angular/router/testing'
@@ -73,6 +74,7 @@ describe('ScoreBoardPreviewComponent', () => {
         TranslateModule.forRoot(),
         HttpClientTestingModule,
         RouterTestingModule,
+        MatProgressSpinnerModule,
         MatDialogModule,
         MatIconModule
       ],
