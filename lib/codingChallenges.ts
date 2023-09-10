1863fbfc719b889d2fa7eba9dacde907966f9c76
Björn Kimminich <bjoern.kimminich@gmx.de>
2023-09-11 01:52:16 +0200

    Find also web3 code snippets

--- lib/codingChallenges.ts
+++ lib/codingChallenges.ts
@@ -2,7 +2,7 @@ import fs from 'fs/promises'
 import path from 'path'
 import logger from './logger'
 
-export const SNIPPET_PATHS = Object.freeze(['./server.ts', './routes', './lib', './data', './frontend/src/app', './models'])
+export const SNIPPET_PATHS = Object.freeze(['./server.ts', './routes', './lib', './data', './data/static/web3-snippets', './frontend/src/app', './models'])
 
 interface FileMatch {
   path: string
