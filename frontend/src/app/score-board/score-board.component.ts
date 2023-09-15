9485d9e6a621daf363aa8e4ce57edb86cd53ff37
dw-omni <damian.watrach@omnilogy.pl>
2023-09-15 09:32:11 +0200

    Update score-board.component.ts

--- frontend/src/app/score-board/score-board.component.ts
+++ frontend/src/app/score-board/score-board.component.ts
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: MIT
  */
 
+
 import { MatTableDataSource } from '@angular/material/table'
 import { DomSanitizer } from '@angular/platform-browser'
 import { ChallengeService } from '../Services/challenge.service'
