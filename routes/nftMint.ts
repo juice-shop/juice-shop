9c88febac7fc0257929916020b0c019aeceb0e61
Björn Kimminich <bjoern.kimminich@gmx.de>
2023-09-22 00:05:29 +0200

    Wrap Smart Contract initialization into try-catch
    (prevents server crash if infura.io
     is unreachable, but results in retry
     every 1 second)

--- routes/nftMint.ts
+++ routes/nftMint.ts
@@ -3,16 +3,16 @@ import challengeUtils = require('../lib/challengeUtils')
 import * as utils from '../lib/utils'
 const nftABI = require('../data/static/contractABIs').nftABI
 const ethers = require('ethers')
-const customHttpProvider = new ethers.JsonRpcProvider('https://sepolia.infura.io/v3/0b88ff4d03a647b8a4649e9bfdf6644f')
 const challenges = require('../data/datacache').challenges
 const nftAddress = '0x41427790c94E7a592B17ad694eD9c06A02bb9C39'
-const contract = new ethers.Contract(nftAddress, nftABI, customHttpProvider)
 const addressesMinted = new Set()
 let isEventListenerCreated = false
 
 module.exports.nftMintListener = function nftMintListener () {
   return (req: Request, res: Response) => {
     try {
+      const customHttpProvider = new ethers.JsonRpcProvider('https://sepolia.infura.io/v3/0b88ff4d03a647b8a4649e9bfdf6644f')
+      const contract = new ethers.Contract(nftAddress, nftABI, customHttpProvider)
       if (!isEventListenerCreated) {
         contract.on('NFTMinted', (minter: string) => {
           if (!addressesMinted.has(minter)) {
@@ -23,7 +23,6 @@ module.exports.nftMintListener = function nftMintListener () {
       }
       res.status(200).json({ success: true, message: 'Event Listener Created' })
     } catch (error) {
-      console.log(error)
       res.status(500).json(utils.getErrorMessage(error))
     }
   }
@@ -41,7 +40,6 @@ module.exports.walletNFTVerify = function walletNFTVerify () {
         res.status(200).json({ success: false, message: 'Wallet did not mint the NFT', status: challenges.nftMintChallenge })
       }
     } catch (error) {
-      console.log(error)
       res.status(500).json(utils.getErrorMessage(error))
     }
   }
