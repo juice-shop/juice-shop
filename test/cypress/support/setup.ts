76dc3a9844e6898551aa194e1a1d4e4f90c01035
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2023-09-21 13:39:22 +0200

    Disable GPU usage on Chrome

--- test/cypress/support/setup.ts
+++ test/cypress/support/setup.ts
@@ -1,5 +1,20 @@
+import BrowserLaunchOptions = Cypress.BrowserLaunchOptions
+import Browser = Cypress.Browser
+
 beforeEach(() => {
   cy.setCookie('cookieconsent_status', 'dismiss')
   cy.setCookie('welcomebanner_status', 'dismiss')
   cy.setCookie('language', 'en')
 })
+
+module.exports = (on) => {
+  on('before:browser:launch', (browser: Browser, launchOptions: BrowserLaunchOptions) => {
+    console.log(launchOptions.args)
+
+    if (browser.name === 'chrome') {
+      launchOptions.args.push('--disable-gpu')
+    }
+
+    return launchOptions
+  })
+}
