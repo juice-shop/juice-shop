6e41ae26df374ce9d570fbedabd748ce25a5e358
Björn Kimminich <bjoern.kimminich@gmx.de>
2023-09-22 00:05:29 +0200

    Wrap Smart Contract initialization into try-catch
    (prevents server crash if infura.io
     is unreachable, but results in retry
     every 1 second)

--- routes/web3Wallet.ts
+++ routes/web3Wallet.ts
@@ -3,10 +3,8 @@ import challengeUtils = require('../lib/challengeUtils')
 import * as utils from '../lib/utils'
 const web3WalletABI = require('../data/static/contractABIs').web3WalletABI
 const ethers = require('ethers')
-const provider = new ethers.WebSocketProvider('wss://sepolia.infura.io/ws/v3/0b88ff4d03a647b8a4649e9bfdf6644f')
 const challenges = require('../data/datacache').challenges
 const web3WalletAddress = '0x413744D59d31AFDC2889aeE602636177805Bd7b0'
-const contract = new ethers.Contract(web3WalletAddress, web3WalletABI, provider)
 const walletsConnected = new Set()
 let isEventListenerCreated = false
 
@@ -15,6 +13,8 @@ module.exports.contractExploitListener = function contractExploitListener () {
     const metamaskAddress = req.body.walletAddress
     walletsConnected.add(metamaskAddress)
     try {
+      const provider = new ethers.WebSocketProvider('wss://sepolia.infura.io/ws/v3/0b88ff4d03a647b8a4649e9bfdf6644f')
+      const contract = new ethers.Contract(web3WalletAddress, web3WalletABI, provider)
       if (!isEventListenerCreated) {
         contract.on('ContractExploited', (exploiter: string) => {
           if (walletsConnected.has(exploiter)) {
@@ -26,7 +26,6 @@ module.exports.contractExploitListener = function contractExploitListener () {
       }
       res.status(200).json({ success: true, message: 'Event Listener Created' })
     } catch (error) {
-      console.log(error)
       res.status(500).json(utils.getErrorMessage(error))
     }
   }
