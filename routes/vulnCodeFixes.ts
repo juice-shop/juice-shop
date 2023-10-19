ab8627281b6674ac30910f6c3e95532fe6dd01c8
Bharat Arya <aryabharat33@gmail.com>
2023-10-19 09:07:41 +0530

    snyk fix

--- routes/vulnCodeFixes.ts
+++ routes/vulnCodeFixes.ts
@@ -67,7 +67,7 @@ export const serveCodeFixes = () => (req: Request<FixesRequestParams, Record<str
 }
 
 export const checkCorrectFix = () => async (req: Request<Record<string, unknown>, Record<string, unknown>, VerdictRequestBody>, res: Response, next: NextFunction) => {
-  const key = req.body.key
+  const key = req.body.key.replace(/^(\.\.(\/|\\|$))+/, '')
   const selectedFix = req.body.selectedFix
   const fixData = readFixes(key)
   if (fixData.fixes.length === 0) {
