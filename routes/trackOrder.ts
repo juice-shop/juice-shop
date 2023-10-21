e0cdd8304efdab810ecb070a6a2787901e29a530
ataseren <ata.seren@hotmail.com>
2023-10-21 10:47:45 +0000

    Update trackOrder.ts

--- routes/trackOrder.ts
+++ routes/trackOrder.ts
@@ -12,7 +12,7 @@ const db = require('../data/mongodb')
 
 module.exports = function trackOrder () {
   return (req: Request, res: Response) => {
-    const id = utils.disableOnContainerEnv() ? String(req.params.id).replace(/[^\w-]+/g, '') : req.params.id
+    const id = utils.disableOnContainerEnv() ? Number(req.params.id).replace(/[^\w-]+/g, '') : req.params.id
 
     challengeUtils.solveIf(challenges.reflectedXssChallenge, () => { return utils.contains(id, '<iframe src="javascript:alert(`xss`)">') })
     db.orders.find({ $where: `this.orderId === '${id}'` }).then((order: any) => {
