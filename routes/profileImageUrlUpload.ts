563ef2fc8d3a54893938f8ed8c1c73484000680f
Yuichi Tanaka <yuichielectric@github.com>
2023-12-04 22:29:57 +0900

    Server side request forgery

--- routes/profileImageUrlUpload.ts
+++ routes/profileImageUrlUpload.ts
@@ -15,8 +15,7 @@ const request = require('request')
 module.exports = function profileImageUrlUpload () {
   return (req: Request, res: Response, next: NextFunction) => {
     if (req.body.imageUrl !== undefined) {
-      // const url = req.body.imageUrl
-      const url = "https://github.com"
+      const url = req.body.imageUrl
       if (url.match(/(.)*solve\/challenges\/server-side(.)*/) !== null) req.app.locals.abused_ssrf_bug = true
       const loggedInUser = security.authenticatedUsers.get(req.cookies.token)
       if (loggedInUser) {
