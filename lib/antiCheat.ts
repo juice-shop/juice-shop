69e0e649013b8ecafc5d5ae118b6b25c4bb5a656
Björn Kimminich <bjoern.kimminich@gmx.de>
2023-12-07 23:17:35 +0100

    Change to more compact code style

--- lib/antiCheat.ts
+++ lib/antiCheat.ts
@@ -26,56 +26,16 @@ const trivialChallenges = ['errorHandlingChallenge', 'privacyPolicyChallenge', '
 const solves: Array<{ challenge: any, phase: string, timestamp: Date, cheatScore: number }> = [{ challenge: {}, phase: 'server start', timestamp: new Date(), cheatScore: 0 }] // seed with server start timestamp
 
 const preSolveInteractions: Array<{ challengeKey: any, urlFragments: string[], interactions: number }> = [
-  {
-    challengeKey: 'missingEncodingChallenge',
-    urlFragments: ['/assets/public/images/uploads/%F0%9F%98%BC-'],
-    interactions: 0
-  },
-  {
-    challengeKey: 'directoryListingChallenge',
-    urlFragments: ['/ftp'],
-    interactions: 0
-  },
-  {
-    challengeKey: 'easterEggLevelOneChallenge',
-    urlFragments: ['/ftp', '/ftp/eastere.gg'],
-    interactions: 0
-  },
-  {
-    challengeKey: 'easterEggLevelTwoChallenge',
-    urlFragments: ['/ftp', '/gur/qrif/ner/fb/shaal/gurl/uvq/na/rnfgre/rtt/jvguva/gur/rnfgre/rtt'],
-    interactions: 0
-  },
-  {
-    challengeKey: 'forgottenDevBackupChallenge',
-    urlFragments: ['/ftp', '/ftp/package.json.bak'],
-    interactions: 0
-  },
-  {
-    challengeKey: 'forgottenBackupChallenge',
-    urlFragments: ['/ftp', '/ftp/coupons_2013.md.bak'],
-    interactions: 0
-  },
-  {
-    challengeKey: 'loginSupportChallenge',
-    urlFragments: ['/ftp', '/ftp/incident-support.kdbx'],
-    interactions: 0
-  },
-  {
-    challengeKey: 'misplacedSignatureFileChallenge',
-    urlFragments: ['/ftp', '/ftp/suspicious_errors.yml'],
-    interactions: 0
-  },
-  {
-    challengeKey: 'recChallenge',
-    urlFragments: ['/api-docs', '/b2b/v2/orders'],
-    interactions: 0
-  },
-  {
-    challengeKey: 'rceOccupyChallenge',
-    urlFragments: ['/api-docs', '/b2b/v2/orders'],
-    interactions: 0
-  }
+  { challengeKey: 'missingEncodingChallenge', urlFragments: ['/assets/public/images/uploads/%F0%9F%98%BC-'], interactions: 0 },
+  { challengeKey: 'directoryListingChallenge', urlFragments: ['/ftp'], interactions: 0 },
+  { challengeKey: 'easterEggLevelOneChallenge', urlFragments: ['/ftp', '/ftp/eastere.gg'], interactions: 0 },
+  { challengeKey: 'easterEggLevelTwoChallenge', urlFragments: ['/ftp', '/gur/qrif/ner/fb/shaal/gurl/uvq/na/rnfgre/rtt/jvguva/gur/rnfgre/rtt'], interactions: 0 },
+  { challengeKey: 'forgottenDevBackupChallenge', urlFragments: ['/ftp', '/ftp/package.json.bak'], interactions: 0 },
+  { challengeKey: 'forgottenBackupChallenge', urlFragments: ['/ftp', '/ftp/coupons_2013.md.bak'], interactions: 0 },
+  { challengeKey: 'loginSupportChallenge', urlFragments: ['/ftp', '/ftp/incident-support.kdbx'], interactions: 0 },
+  { challengeKey: 'misplacedSignatureFileChallenge', urlFragments: ['/ftp', '/ftp/suspicious_errors.yml'], interactions: 0 },
+  { challengeKey: 'recChallenge', urlFragments: ['/api-docs', '/b2b/v2/orders'], interactions: 0 },
+  { challengeKey: 'rceOccupyChallenge', urlFragments: ['/api-docs', '/b2b/v2/orders'], interactions: 0 }
 ]
 
 exports.checkForPreSolveInteractions = () => ({ url }: Request, res: Response, next: NextFunction) => {
