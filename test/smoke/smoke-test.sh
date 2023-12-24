91bd1088ce1e934cc8c2db70d2973dc91f5d98da
Björn Kimminich <bjoern.kimminich@gmx.de>
2023-12-24 09:07:52 +0100

    Bump copyright to include year 2024

--- test/smoke/smoke-test.sh
+++ test/smoke/smoke-test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #
-# Copyright (c) 2014-2023 Bjoern Kimminich & the OWASP Juice Shop contributors.
+# Copyright (c) 2014-2024 Bjoern Kimminich & the OWASP Juice Shop contributors.
 # SPDX-License-Identifier: MIT
 #
 
