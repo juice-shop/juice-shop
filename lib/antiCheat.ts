2e366ad82819af08b11fe74555cd9de4985dfa00
Björn Kimminich <bjoern.kimminich@gmx.de>
2024-01-05 17:59:36 +0100

    Fix capping of final score

--- lib/antiCheat.ts
+++ lib/antiCheat.ts
@@ -69,7 +69,7 @@ export const calculateCheatScore = (challenge: Challenge) => {
     percentPrecedingInteraction = preSolveInteraction.interactions / (preSolveInteraction.urlFragments.length)
     const multiplierForMissingExpectedInteraction = 1 + Math.max(0, 1 - percentPrecedingInteraction) / 2
     cheatScore *= multiplierForMissingExpectedInteraction
-    cheatScore = Math.min(1, multiplierForMissingExpectedInteraction)
+    cheatScore = Math.min(1, cheatScore)
   }
 
   logger.info(`Cheat score for ${areCoupled(challenge, previous().challenge) ? 'coupled ' : (isTrivial(challenge) ? 'trivial ' : '')}${challenge.tutorialOrder ? 'tutorial ' : ''}${colors.cyan(challenge.key)} solved in ${Math.round(minutesSincePreviousSolve)}min (expected ~${minutesExpectedToSolve}min) with${config.get('challenges.showHints') ? '' : 'out'} hints allowed${percentPrecedingInteraction > -1 ? (' and ' + percentPrecedingInteraction * 100 + '% expected preceding URL interaction') : ''}: ${cheatScore < 0.33 ? colors.green(cheatScore.toString()) : (cheatScore < 0.66 ? colors.yellow(cheatScore.toString()) : colors.red(cheatScore.toString()))}`)
