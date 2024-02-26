61bc34214eb934ece695f1c94fd5a87eb00ff9b7
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2024-02-26 13:55:13 +0100

    Update typosquatting challenge target

--- test/api/feedbackApiSpec.ts
+++ test/api/feedbackApiSpec.ts
@@ -128,7 +128,7 @@ describe('/api/Feedbacks', () => {
             return frisby.post(API_URL + '/Feedbacks', {
               headers: { Authorization: 'Bearer ' + jsonLogin.authentication.token, 'content-type': 'application/json' },
               body: {
-                comment: 'Stupid JWT secret "' + security.defaultSecret + '" and being typosquatted by epilogue-js and anuglar2-qrcode!',
+                comment: 'Stupid JWT secret "' + security.defaultSecret + '" and being typosquatted by epilogue-js and ngy-cookie!',
                 rating: 5,
                 UserId: 4,
                 captchaId: json.captchaId,
