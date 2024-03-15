6ba551aa650c361bd057f2ce06749b6d7e722fb5
meelunae <git@eleuna.me>
2024-03-15 23:33:27 +0100

    Escaped : in URL in XSS challenge payload codeblock

--- test/cypress/e2e/search.spec.ts
+++ test/cypress/e2e/search.spec.ts
@@ -22,7 +22,7 @@ describe('/#/search', () => {
       cy.get('#searchQuery').click()
       cy.get('#mat-input-0')
         .type(
-          '<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/771984076&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>'
+          '<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https%3A//w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/771984076&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>'
         )
         .type('{enter}')
       cy.expectChallengeSolved({ challenge: 'Bonus Payload' })
