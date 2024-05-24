58725ae66d41eea9f201e2ab3d03b4d530916bde
PRAVEENKUMAR6130 <101183582+PRAVEENKUMAR6130@users.noreply.github.com>
2024-05-24 14:33:28 +0530

    Update updateProductReviews.ts

--- routes/updateProductReviews.ts
+++ routes/updateProductReviews.ts
@@ -14,8 +14,8 @@ const security = require('../lib/insecurity')
 module.exports = function productReviews () {
   return (req: Request, res: Response, next: NextFunction) => {
     const user = security.authenticatedUsers.from(req) // vuln-code-snippet vuln-line forgedReviewChallenge
-    db.reviews.update( // vuln-code-snippet neutral-line forgedReviewChallenge
-      { _id: req.body.id }, // vuln-code-snippet vuln-line noSqlReviewsChallenge forgedReviewChallenge
+    db.reviews.update( 
+      { _id: req.body._id }, 
       { $set: { message: req.body.message } },
       { multi: true } // vuln-code-snippet vuln-line noSqlReviewsChallenge
     ).then(
