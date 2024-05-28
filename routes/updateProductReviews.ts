8719ea78d6c12bf2006187667f35ed2068ea047a
mustafanaa <mustafa@backslash.security>
2024-05-28 21:38:18 +0300

    Update updateProductReviews.ts

--- routes/updateProductReviews.ts
+++ routes/updateProductReviews.ts
@@ -14,7 +14,7 @@ const security = require('../lib/insecurity')
 module.exports = function productReviews () {
   return (req: Request, res: Response, next: NextFunction) => {
     const user = security.authenticatedUsers.from(req) // vuln-code-snippet vuln-line forgedReviewChallenge
-    db.reviews.update( // vuln-code-snippet neutral-line forgedReviewChallenge
+    db.reviews.insert( // vuln-code-snippet neutral-line forgedReviewChallenge
       { _id: req.body.id }, // vuln-code-snippet vuln-line noSqlReviewsChallenge forgedReviewChallenge
       { $set: { message: req.body.message } },
       { multi: true } // vuln-code-snippet vuln-line noSqlReviewsChallenge
