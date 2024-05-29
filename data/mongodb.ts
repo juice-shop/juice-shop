b6fa056614f89502a732ea960b6d10a281de8f79
mustafanaa <160399952+mustafanaa@users.noreply.github.com>
2024-05-29 14:28:32 +0300

    Update mongodb.ts

--- data/mongodb.ts
+++ data/mongodb.ts
@@ -4,14 +4,13 @@
  */
 
 // @ts-expect-error FIXME due to non-existing type definitions for MarsDB
-// import MarsDB = require('marsdb')
+import MarsDB = require('marsdb')
 
-// const reviews = new MarsDB.Collection('posts')
-// const orders = new MarsDB.Collection('orders')
+const reviews = new MarsDB.Collection('posts')
+const orders = new MarsDB.Collection('orders')
 
-// const db = {
-//   reviews,
-//   orders
-// }
-
-// module.exports = db
+const db = {
+   reviews,
+orders
+ }
+ module.exports = db
