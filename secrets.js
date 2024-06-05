86e4c62f5efcdceb8c64e6ed42bf109250946e58
mcampora-ebsco <mcampora@ebsco.com>
2024-06-05 16:15:26 +0200

    test with my secrets

---
+++ secrets.js
@@ -0,0 +1,5 @@
+// secrets hardcoded in code to test GH secrets detection
+var americanExpress = "378282246310005";
+var visa = "4111111111111111";
+var password = "password";
+var token = "AKCp8ogq20EEZH5Gs7f83RSLPTB6npMoau2NZEKhKh5uNid4CNdBPaQe4RkWymobAkkcCcd9KB";
