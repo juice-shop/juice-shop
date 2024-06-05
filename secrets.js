5e4744ef4e9f93ae0432e8eaa244a730894d1e20
mcampora-ebsco <mcampora@ebsco.com>
2024-06-05 16:44:53 +0200

    introduced a codeql issue in my branch

--- secrets.js
+++ secrets.js
@@ -3,3 +3,6 @@ var americanExpress = "378282246310005";
 var visa = "4111111111111111";
 var password = "password";
 var token = "AKCp8ogq20EEZH5Gs7f83RSLPTB6npMoau2NZEKhKh5uNid4CNdBPaQe4RkWymobAkkcCcd9KB";
+
+req = { body: {}, headers: {} };
+req.headers = { authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRhIjp7ImVtYWlsIjoicnNhX2xvcmRAanVpY2Utc2gub3AifSwiaWF0IjoxNTgyMjIxNTc1fQ.ycFwtqh4ht4Pq9K5rhiPPY256F9YCTIecd4FHFuSEAg' };
