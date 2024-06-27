68d227c2a0cb178ee29382c106d28b73d255fbc1
r <r@rs-MacBook-Pro.local>
2024-06-26 23:53:12 -0700

    asd

--- routes/appVersion.ts
+++ routes/appVersion.ts
@@ -10,8 +10,27 @@ import * as utils from '../lib/utils'
 
 module.exports = function retrieveAppVersion () {
   return (_req: Request, res: Response) => {
-    res.json({
-      version: config.get('application.showVersionNumber') ? utils.version() : ''
-    })
+    // res.json({
+    //   version: config.get('application.showVersionNumber') ? utils.version() : ''
+    // })
+    const version = config.get('application.showVersionNumber') ? utils.version() : '';
+    const userInput = _req.query.input || ''; // Adding a user input from query parameter
+
+    // Introducing XSS vulnerability by directly reflecting user input
+    res.send(`
+      <html>
+        <body>
+          <h1>App Version: ${version}</h1>
+          <div>User Input: ${userInput}</div> <!-- XSS vulnerability here -->
+        </body>
+      </html>`)
   }
 }
+
+
+// module.exports = function retrieveAppVersion () {
+//   return (req, res) => {
+
+//     `);
+//   }
+// }
