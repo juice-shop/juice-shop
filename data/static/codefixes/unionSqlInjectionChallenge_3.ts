bf9720147236cc8fac769eab26a7379a78da16ce
Owen Pickford <owen.pickford@snyk.io>
2024-10-01 15:50:48 -0700

    Fix SQL Injection

--- data/static/codefixes/unionSqlInjectionChallenge_3.ts
+++ data/static/codefixes/unionSqlInjectionChallenge_3.ts
@@ -1,13 +1,15 @@
 module.exports = function searchProducts () {
   return (req: Request, res: Response, next: NextFunction) => {
-    let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
+    let criteria: any = req.query.q === 'undefined' ? '' : String(req.query.q)
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
     // only allow apple or orange related searches
     if (!criteria.startsWith("apple") || !criteria.startsWith("orange")) {
       res.status(400).send()
       return
     }
-    models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`)
+    models.sequelize.query('SELECT * FROM Products WHERE ((name LIKE ? OR description LIKE?) AND deletedAt IS NULL) ORDER BY name', {
+      replacements: ['%' + criteria + '%']
+    })
       .then(([products]) => {
         const dataString = JSON.stringify(products)
         for (let i = 0; i < products.length; i++) {
