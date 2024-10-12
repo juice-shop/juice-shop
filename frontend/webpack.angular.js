33bcf4daf64da03336596fa7eac0f8cfde161454
OhWellNevermind <aleksandrov.oleksii.wgm@gmail.com>
2024-10-12 16:13:39 +0300

    fixes for first two critical issues from fortify

--- frontend/webpack.angular.js
+++ frontend/webpack.angular.js
@@ -1,10 +1,12 @@
 // this file contains extras that should override angular's default configs
 
 const { CycloneDxWebpackPlugin } = require('@cyclonedx/webpack-plugin')
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   plugins: [
     // @see https://www.npmjs.com/package/@cyclonedx/webpack-plugin
+    new Dotenv(),
     new CycloneDxWebpackPlugin({
       outputLocation: '../bom', // The path is relative to webpack's overall output path,
       includeWellknown: false
