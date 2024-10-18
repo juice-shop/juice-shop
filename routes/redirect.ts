10a694f4eab78e5eb4039af52aa105bfd074767c
IvanKoshan2004 <ivan.koshangod@gmail.com>
2024-10-18 13:54:32 +0300

    fix open redirect vulnerability by closing it

--- routes/redirect.ts
+++ routes/redirect.ts
@@ -12,14 +12,22 @@ const security = require('../lib/insecurity')
 
 module.exports = function performRedirect () {
   return ({ query }: Request, res: Response, next: NextFunction) => {
-    const toUrl: string = query.to as string
-    if (security.isRedirectAllowed(toUrl)) {
-      challengeUtils.solveIf(challenges.redirectCryptoCurrencyChallenge, () => { return toUrl === 'https://explorer.dash.org/address/Xr556RzuwX6hg5EGpkybbv5RanJoZN17kW' || toUrl === 'https://blockchain.info/address/1AbKfgvw9psQ41NbLi8kufDQTezwG8DRZm' || toUrl === 'https://etherscan.io/address/0x0f933ab9fcaaa782d0279c300d73750e1311eae6' })
-      challengeUtils.solveIf(challenges.redirectChallenge, () => { return isUnintendedRedirect(toUrl) })
-      res.redirect(toUrl)
+    const to: string = query.to as string
+    const allowedRedirectUrlMap = {
+      github: 'https://github.com/juice-shop/juice-shop',
+      blockchain: 'https://blockchain.info/address/1AbKfgvw9psQ41NbLi8kufDQTezwG8DRZm', // vuln-code-snippet vuln-line redirectCryptoCurrencyChallenge
+      explorer: 'https://explorer.dash.org/address/Xr556RzuwX6hg5EGpkybbv5RanJoZN17kW', // vuln-code-snippet vuln-line redirectCryptoCurrencyChallenge
+      etherscan: 'https://etherscan.io/address/0x0f933ab9fcaaa782d0279c300d73750e1311eae6', // vuln-code-snippet vuln-line redirectCryptoCurrencyChallenge
+      spreadshirt: 'http://shop.spreadshirt.com/juiceshop',
+      spreadshirtDe: 'http://shop.spreadshirt.de/juiceshop',
+      stickeryou: 'https://www.stickeryou.com/products/owasp-juice-shop/794',
+      leanpub: 'http://leanpub.com/juice-shop'
+    }
+    if (allowedRedirectUrlMap[to]) {
+      res.redirect(allowedRedirectUrlMap[to]);
     } else {
       res.status(406)
-      next(new Error('Unrecognized target URL for redirect: ' + toUrl))
+      next(new Error('Unrecognized target URL map key for redirect: ' + to));
     }
   }
 }
