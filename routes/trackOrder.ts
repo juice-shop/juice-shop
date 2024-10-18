e943793a4d8a79baf706c1b94bddd6217d3bed17
Shakhvl23 <vladshah294@gmail.com>
2024-10-18 17:59:28 +0300

    Forgot to add this fix. Does the same thing as all other SQL fixes

--- routes/trackOrder.ts
+++ routes/trackOrder.ts
@@ -14,7 +14,7 @@ module.exports = function trackOrder () {
     const id = !utils.isChallengeEnabled(challenges.reflectedXssChallenge) ? String(req.params.id).replace(/[^\w-]+/g, '') : req.params.id
 
     challengeUtils.solveIf(challenges.reflectedXssChallenge, () => { return utils.contains(id, '<iframe src="javascript:alert(`xss`)">') })
-    db.ordersCollection.find({ $where: `this.orderId === '${id}'` }).then((order: any) => {
+    db.ordersCollection.find({ orderId:id }).then((order: any) => {
       const result = utils.queryResultToJson(order)
       challengeUtils.solveIf(challenges.noSqlOrdersChallenge, () => { return result.data.length > 1 })
       if (result.data[0] === undefined) {
