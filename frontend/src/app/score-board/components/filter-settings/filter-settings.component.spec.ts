3e28e69eac52e0cdba836088892ec2df0abac807
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2024-10-21 14:19:47 +0200

    Add tests for emitting filter changes

--- frontend/src/app/score-board/components/filter-settings/filter-settings.component.spec.ts
+++ frontend/src/app/score-board/components/filter-settings/filter-settings.component.spec.ts
@@ -98,4 +98,39 @@ describe('FilterSettingsComponent', () => {
     component.openAdditionalSettingsDialog()
     expect(dialog.open).toHaveBeenCalled()
   })
+
+  it('should emit difficulty filter change', () => {
+    spyOn(component.filterSettingChange, 'emit')
+    const difficulties = [1, 2, 3]
+    component.onDifficultyFilterChange([1, 2, 3])
+    expect(component.filterSettingChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({ difficulties }))
+  })
+
+  it('should emit status filter change', () => {
+    spyOn(component.filterSettingChange, 'emit')
+    const status = 'unsolved'
+    component.onStatusFilterChange(status)
+    expect(component.filterSettingChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({ status }))
+  })
+
+  it('should emit tag filter change', () => {
+    spyOn(component.filterSettingChange, 'emit')
+    const tags = ['tag1', 'tag2']
+    component.onTagFilterChange(tags)
+    expect(component.filterSettingChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({ tags }))
+  })
+
+  it('should emit category filter change', () => {
+    spyOn(component.filterSettingChange, 'emit')
+    const categories = ['category1', 'category2']
+    component.onCategoryFilterChange(categories)
+    expect(component.filterSettingChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({ categories }))
+  })
+
+  it('should emit search query filter change', () => {
+    spyOn(component.filterSettingChange, 'emit')
+    const searchQuery = 'query'
+    component.onSearchQueryFilterChange(searchQuery)
+    expect(component.filterSettingChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({ searchQuery }))
+  })
 })
