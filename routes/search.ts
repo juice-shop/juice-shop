dc5d7a3fadd771d8324237b18438237b1fb00b3c
Owen Pickford <owen.pickford@snyk.io>
2024-10-23 13:47:22 -0700

    Fix pdfkit

--- routes/search.ts
+++ routes/search.ts
@@ -14,7 +14,7 @@ module.exports = function searchProducts () {
   return (req: Request, res: Response, next: NextFunction) => {
     let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    models.sequelize.query('SELECT * FROM Products WHERE ((name LIKE? OR description LIKE?) AND deletedAt IS NULL) ORDER BY name', { replacements: ['%' + criteria + '%'] }) 
+    models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
       .then(([products]) => {
         const dataString = JSON.stringify(products)
         if (utils.notSolved(challenges.unionSqlInjectionChallenge)) { // vuln-code-snippet hide-start
