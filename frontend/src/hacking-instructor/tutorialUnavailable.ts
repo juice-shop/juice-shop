570d30187bb953bfe23075e0d932b6814a5a8bde
Jannik Hollenbach <jannik@hollenbach.de>
2024-11-19 19:56:31 +0100

    Fix incorrect / outdates documentation links
    Signed-off-by: Jannik Hollenbach <jannik@hollenbach.de>

--- frontend/src/hacking-instructor/tutorialUnavailable.ts
+++ frontend/src/hacking-instructor/tutorialUnavailable.ts
@@ -19,7 +19,7 @@ export const TutorialUnavailableInstruction: ChallengeInstruction = {
     },
     {
       text:
-        '✍️ Do you want to contribute a tutorial for this challenge? [Check out our documentation](https://pwning.owasp-juice.shop/part3/tutorials.html) to learn how! 🏫',
+        '✍️ Do you want to contribute a tutorial for this challenge? [Check out our documentation](https://pwning.owasp-juice.shop/companion-guide/latest/part5/tutorials.html) to learn how! 🏫',
       fixture: 'app-navbar',
       resolved: waitInMs(15000)
     },
