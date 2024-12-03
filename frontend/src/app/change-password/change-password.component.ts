d169877878d59768c58c9a41730162fa03a86333
Marcus <roycefum@oregonstate.edu>
2024-12-03 03:39:58 +0100

    register and change password hardened

--- frontend/src/app/change-password/change-password.component.ts
+++ frontend/src/app/change-password/change-password.component.ts
@@ -1,85 +1,125 @@
-/*
- * Copyright (c) 2014-2024 Bjoern Kimminich & the OWASP Juice Shop contributors.
- * SPDX-License-Identifier: MIT
- */
-
-import { type AbstractControl, UntypedFormControl, Validators } from '@angular/forms'
-import { UserService } from '../Services/user.service'
-import { Component } from '@angular/core'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSave } from '@fortawesome/free-solid-svg-icons'
-import { faEdit } from '@fortawesome/free-regular-svg-icons'
-import { FormSubmitService } from '../Services/form-submit.service'
-import { TranslateService } from '@ngx-translate/core'
-
-library.add(faSave, faEdit)
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { UntypedFormControl, Validators } from '@angular/forms';
+import { UserService } from '../Services/user.service';
+import { FormSubmitService } from '../Services/form-submit.service';
+import { TranslateService } from '@ngx-translate/core';
+import { commonPasswordValidator } from '../register/custom-password-validator';
 
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.component.html',
-  styleUrls: ['./change-password.component.scss']
+  styleUrls: ['./change-password.component.scss'],
 })
-export class ChangePasswordComponent {
-  public passwordControl: UntypedFormControl = new UntypedFormControl('', [Validators.required])
-  public newPasswordControl: UntypedFormControl = new UntypedFormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(40)])
-  public repeatNewPasswordControl: UntypedFormControl = new UntypedFormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(40), matchValidator(this.newPasswordControl)])
-  public error: any
-  public confirmation: any
+export class ChangePasswordComponent implements OnInit {
+  public passwordControl = new UntypedFormControl('', [Validators.required]); // Current password
+  public newPasswordControl = new UntypedFormControl('', [
+    Validators.required,
+    Validators.minLength(8),
+    Validators.maxLength(30),
+    commonPasswordValidator([
+      '12345678',
+      'password',
+      '123456789',
+      'qwertyuiop',
+      'letmein123',
+      'admin123',
+      'iloveyou',
+      'welcome1',
+      'sunshine',
+      'football',
+    ]), // Custom validator for common passwords
+  ]);
+  public repeatNewPasswordControl = new UntypedFormControl('', [
+    Validators.required,
+    matchValidator(this.newPasswordControl), // Match new password validator
+  ]);
 
-  constructor (private readonly userService: UserService, private readonly formSubmitService: FormSubmitService, private readonly translate: TranslateService) { }
+  public error: any;
+  public confirmation: any;
 
-  ngOnInit () {
-    this.formSubmitService.attachEnterKeyHandler('password-form', 'changeButton', () => { this.changePassword() })
+  constructor(
+    private cdr: ChangeDetectorRef,
+    private readonly userService: UserService,
+    private readonly formSubmitService: FormSubmitService,
+    private readonly translate: TranslateService
+  ) {}
+
+  ngOnInit(): void {
+    // Revalidate dynamically as the user types
+    this.newPasswordControl.valueChanges.subscribe(() => {
+      this.newPasswordControl.updateValueAndValidity({ onlySelf: true, emitEvent: false });
+    });
+
+    this.repeatNewPasswordControl.valueChanges.subscribe(() => {
+      this.repeatNewPasswordControl.updateValueAndValidity({ onlySelf: true, emitEvent: false });
+    });
+
+    // Ensure proper initialization
+    setTimeout(() => {
+      this.cdr.detectChanges();
+    });
+
+    this.formSubmitService.attachEnterKeyHandler('password-form', 'changeButton', () => {
+      this.changePassword();
+    });
   }
 
-  changePassword () {
-    if (localStorage.getItem('email')?.match(/support@.*/) && !this.newPasswordControl.value.match(/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,30}/)) {
-      console.error('Parola echipei de asistență nu respectă politica corporativă pentru conturile privilegiate! Vă rugăm să schimbați parola în consecință!')
+  changePassword(): void {
+    if (this.passwordControl.invalid || this.newPasswordControl.invalid || this.repeatNewPasswordControl.invalid) {
+      console.error('Form is invalid. Correct the errors before submitting.');
+      return;
     }
-    this.userService.changePassword({
+
+    const passwordData = {
       current: this.passwordControl.value,
       new: this.newPasswordControl.value,
-      repeat: this.repeatNewPasswordControl.value
-    }).subscribe((response: any) => {
-      this.error = undefined
-      this.translate.get('PASSWORD_SUCCESSFULLY_CHANGED').subscribe((passwordSuccessfullyChanged) => {
-        this.confirmation = passwordSuccessfullyChanged
-      }, (translationId) => {
-        this.confirmation = { error: translationId }
-      })
-      this.resetForm()
-    }, (error) => {
-      console.log(error)
-      this.error = error
-      this.confirmation = undefined
-      this.resetPasswords()
-    })
+      repeat: this.repeatNewPasswordControl.value,
+    };
+
+    this.userService.changePassword(passwordData).subscribe(
+      (response: any) => {
+        this.error = undefined;
+        this.translate.get('PASSWORD_SUCCESSFULLY_CHANGED').subscribe(
+          (successMessage) => {
+            this.confirmation = successMessage;
+          },
+          (translationId) => {
+            this.confirmation = { error: translationId };
+          }
+        );
+        this.resetForm();
+      },
+      (error) => {
+        console.error(error);
+        this.error = error;
+        this.confirmation = undefined;
+        this.resetPasswords();
+      }
+    );
   }
 
-  resetForm () {
-    this.passwordControl.setValue('')
-    this.resetPasswords()
+  resetForm(): void {
+    this.passwordControl.setValue('');
+    this.resetPasswords();
   }
 
-  resetPasswords () {
-    this.passwordControl.markAsPristine()
-    this.passwordControl.markAsUntouched()
-    this.newPasswordControl.setValue('')
-    this.newPasswordControl.markAsPristine()
-    this.newPasswordControl.markAsUntouched()
-    this.repeatNewPasswordControl.setValue('')
-    this.repeatNewPasswordControl.markAsPristine()
-    this.repeatNewPasswordControl.markAsUntouched()
+  resetPasswords(): void {
+    [this.passwordControl, this.newPasswordControl, this.repeatNewPasswordControl].forEach((control) => {
+      control.setValue('');
+      control.markAsPristine();
+      control.markAsUntouched();
+    });
   }
 }
 
-function matchValidator (newPasswordControl: AbstractControl) {
-  return function matchOtherValidate (repeatNewPasswordControl: UntypedFormControl) {
-    const password = newPasswordControl.value
-    const passwordRepeat = repeatNewPasswordControl.value
-    if (password !== passwordRepeat) {
-      return { notSame: true }
+// Custom validator to match passwords
+function matchValidator(newPasswordControl: UntypedFormControl) {
+  return (repeatNewPasswordControl: UntypedFormControl) => {
+    const password = newPasswordControl.value;
+    const repeatPassword = repeatNewPasswordControl.value;
+    if (password !== repeatPassword) {
+      return { notSame: true };
     }
-    return null
-  }
+    return null;
+  };
 }
--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,110 +1,139 @@
-/*
- * Copyright (c) 2014-2024 Bjoern Kimminich & the OWASP Juice Shop contributors.
- * SPDX-License-Identifier: MIT
- */
-
-import { SecurityAnswerService } from '../Services/security-answer.service'
-import { UserService } from '../Services/user.service'
-import { type AbstractControl, UntypedFormControl, Validators } from '@angular/forms'
-import { Component, NgZone, type OnInit } from '@angular/core'
-import { SecurityQuestionService } from '../Services/security-question.service'
-import { Router } from '@angular/router'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { MatSnackBar } from '@angular/material/snack-bar'
-
-import { faExclamationCircle, faUserPlus } from '@fortawesome/free-solid-svg-icons'
-import { FormSubmitService } from '../Services/form-submit.service'
-import { SnackBarHelperService } from '../Services/snack-bar-helper.service'
-import { TranslateService } from '@ngx-translate/core'
-import { type SecurityQuestion } from '../Models/securityQuestion.model'
-
-import { HttpClient } from '@angular/common/http';
-import { customPasswordValidator, loadCommonPasswords } from './custom-password-validator';
-
-library.add(faUserPlus, faExclamationCircle)
+import { Component, NgZone, OnInit, ChangeDetectorRef } from '@angular/core';
+import { UntypedFormControl, Validators } from '@angular/forms';
+import { SecurityQuestionService } from '../Services/security-question.service';
+import { UserService } from '../Services/user.service';
+import { SecurityAnswerService } from '../Services/security-answer.service';
+import { Router } from '@angular/router';
+import { FormSubmitService } from '../Services/form-submit.service';
+import { SnackBarHelperService } from '../Services/snack-bar-helper.service';
+import { TranslateService } from '@ngx-translate/core';
+import { commonPasswordValidator } from './custom-password-validator';
+import { SecurityQuestion } from '../Models/securityQuestion.model';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-  public emailControl: UntypedFormControl = new UntypedFormControl('', [Validators.required, Validators.email, customPasswordValidator()])
-  public passwordControl: UntypedFormControl = new UntypedFormControl('', [Validators.required, customPasswordValidator()])
-  public repeatPasswordControl: UntypedFormControl = new UntypedFormControl('', [Validators.required, matchValidator(this.passwordControl)])
-  public securityQuestionControl: UntypedFormControl = new UntypedFormControl('', [Validators.required])
-  public securityAnswerControl: UntypedFormControl = new UntypedFormControl('', [Validators.required])
-  public securityQuestions!: SecurityQuestion[]
-  public selected?: number
-  public error: string | null = null
+  public passwordVisible: boolean = false; // Password visibility toggle
+  public emailControl: UntypedFormControl = new UntypedFormControl('', [Validators.required, Validators.email]);
+  public passwordControl: UntypedFormControl = new UntypedFormControl('', [
+    Validators.required,
+    Validators.minLength(8),
+    Validators.maxLength(30),
+    commonPasswordValidator([
+      '12345678',
+      'password',
+      '123456789',
+      'qwertyuiop',
+      'letmein123',
+      'admin123',
+      'iloveyou',
+      'welcome1',
+      'sunshine',
+      'football',
+    ]), // Custom common password validator
+  ]);
+  public repeatPasswordControl: UntypedFormControl = new UntypedFormControl('', [
+    Validators.required,
+    matchValidator(this.passwordControl),
+  ]);
+  public securityQuestionControl: UntypedFormControl = new UntypedFormControl('', [Validators.required]);
+  public securityAnswerControl: UntypedFormControl = new UntypedFormControl('', [Validators.required]);
+  public securityQuestions!: SecurityQuestion[];
+  public selected?: number;
+  public error: string | null = null;
+  public checkingCommonness: boolean = false;
 
-  constructor (private readonly securityQuestionService: SecurityQuestionService,
+  constructor(
+    private cdr: ChangeDetectorRef,
+    private readonly securityQuestionService: SecurityQuestionService,
     private readonly userService: UserService,
     private readonly securityAnswerService: SecurityAnswerService,
     private readonly router: Router,
     private readonly formSubmitService: FormSubmitService,
     private readonly translateService: TranslateService,
-    private readonly snackBar: MatSnackBar,
     private readonly snackBarHelperService: SnackBarHelperService,
-    private readonly ngZone: NgZone, 
-    private http: HttpClient) {}
+    private readonly ngZone: NgZone
+  ) {}
 
+  ngOnInit(): void {
+    // Ensure the password controls are revalidated dynamically
+    this.passwordControl.valueChanges.subscribe(() => {
+      this.passwordControl.updateValueAndValidity({ onlySelf: true, emitEvent: false });
+    });
 
-  ngOnInit () {
-    this.securityQuestionService.find(null).subscribe((securityQuestions: any) => {
-      this.securityQuestions = securityQuestions
-    }, (err) => { console.log(err) })
+    this.repeatPasswordControl.valueChanges.subscribe(() => {
+      this.repeatPasswordControl.updateValueAndValidity({ onlySelf: true, emitEvent: false });
+    });
 
-    this.formSubmitService.attachEnterKeyHandler('registration-form', 'registerButton', () => { this.save() })
-    loadCommonPasswords(this.http);
+    // Force update for proper initialization
+    setTimeout(() => {
+      this.cdr.detectChanges();
+    });
+
+    this.securityQuestionService.find(null).subscribe(
+      (securityQuestions: SecurityQuestion[]) => {
+        this.securityQuestions = securityQuestions;
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+
+    this.formSubmitService.attachEnterKeyHandler('registration-form', 'registerButton', () => {
+      this.save();
+    });
   }
 
-  save () {
+  save(): void {
     const user = {
       email: this.emailControl.value,
       password: this.passwordControl.value,
       passwordRepeat: this.repeatPasswordControl.value,
-      securityQuestion: this.securityQuestions.find((question) => question.id === this.securityQuestionControl.value),
-      securityAnswer: this.securityAnswerControl.value
-    }
+      securityQuestion: this.securityQuestions.find(
+        (question) => question.id === this.securityQuestionControl.value
+      ),
+      securityAnswer: this.securityAnswerControl.value,
+    };
 
-    this.userService.save(user).subscribe((response: any) => {
-      this.securityAnswerService.save({
-        UserId: response.id,
-        answer: this.securityAnswerControl.value,
-        SecurityQuestionId: this.securityQuestionControl.value
-      }).subscribe(() => {
-        this.ngZone.run(async () => await this.router.navigate(['/login']))
-        this.snackBarHelperService.open('CONFIRM_REGISTER')
-      })
-    }, (err) => {
-      console.log(err)
-      if (err.error?.errors) {
-        const error = err.error.errors[0]
-        if (error.message) {
-          // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
-          this.error = error.message[0].toUpperCase() + error.message.slice(1)
-        } else {
-          this.error = error
+    this.userService.save(user).subscribe(
+      (response: any) => {
+        this.securityAnswerService
+          .save({
+            UserId: response.id,
+            answer: this.securityAnswerControl.value,
+            SecurityQuestionId: this.securityQuestionControl.value,
+          })
+          .subscribe(() => {
+            this.ngZone.run(async () => await this.router.navigate(['/login']));
+            this.snackBarHelperService.open('CONFIRM_REGISTER');
+          });
+      },
+      (err) => {
+        console.error(err);
+        if (err.error?.errors) {
+          const error = err.error.errors[0];
+          if (error.message) {
+            this.error = error.message[0].toUpperCase() + error.message.slice(1);
+          } else {
+            this.error = error;
+          }
         }
       }
-    })
+    );
   }
 }
 
-// custom password validator function
-
-
-
-
-
-function matchValidator (passwordControl: AbstractControl) {
-  return function matchOtherValidate (repeatPasswordControl: UntypedFormControl) {
-    const password = passwordControl.value
-    const passwordRepeat = repeatPasswordControl.value
-    if (password !== passwordRepeat) {
-      return { notSame: true }
+// Custom validator to match passwords
+function matchValidator(passwordControl: UntypedFormControl) {
+  return (repeatPasswordControl: UntypedFormControl) => {
+    const password = passwordControl.value;
+    const repeatPassword = repeatPasswordControl.value;
+    if (password !== repeatPassword) {
+      return { notSame: true };
     }
-    return null
-  }
+    return null;
+  };
 }
