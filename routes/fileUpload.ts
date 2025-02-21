d3e22888cc1859a8564ac3d1358477b1043e1145
Zaid Arif <zaid.zrf@gmail.com>
2025-02-21 22:05:13 +0530

    switched completely to CommonJS

--- routes/fileUpload.ts
+++ routes/fileUpload.ts
@@ -3,33 +3,32 @@
  * SPDX-License-Identifier: MIT
  */
 
-import os from 'os'
-import fs = require('fs')
-import challengeUtils = require('../lib/challengeUtils')
-import { type NextFunction, type Request, type Response } from 'express'
-import path from 'path'
-import * as utils from '../lib/utils'
-import { challenges } from '../data/datacache'
+const os = require('os')
+const fs = require('fs')
+const challengeUtils = require('../lib/challengeUtils')
+const path = require('path')
+const utils = require('../lib/utils')
+const { challenges } = require('../data/datacache')
 
 const libxml = require('libxmljs')
 const vm = require('vm')
 const unzipper = require('unzipper')
 
-function ensureFileIsPassed ({ file }: Request, res: Response, next: NextFunction) {
+function ensureFileIsPassed ({ file }: { file: any }, res: any, next: any) {
   if (file != null) {
     next()
   }
 }
 
-function handleZipFileUpload ({ file }: Request, res: Response, next: NextFunction) {
+function handleZipFileUpload ({ file }: { file: any }, res: any, next: any) {
   if (utils.endsWith(file?.originalname.toLowerCase(), '.zip')) {
     if (((file?.buffer) != null) && utils.isChallengeEnabled(challenges.fileWriteChallenge)) {
       const buffer = file.buffer
       const filename = file.originalname.toLowerCase()
       const tempFile = path.join(os.tmpdir(), filename)
-      fs.open(tempFile, 'w', function (err, fd) {
+      fs.open(tempFile, 'w', function (err: any, fd: any) {
         if (err != null) { next(err) }
-        fs.write(fd, buffer, 0, buffer.length, null, function (err) {
+        fs.write(fd, buffer, 0, buffer.length, null, function (err: any) {
           if (err != null) { next(err) }
           fs.close(fd, function () {
             fs.createReadStream(tempFile)
@@ -39,7 +38,7 @@ function handleZipFileUpload ({ file }: Request, res: Response, next: NextFuncti
                 const absolutePath = path.resolve('uploads/complaints/' + fileName)
                 challengeUtils.solveIf(challenges.fileWriteChallenge, () => { return absolutePath === path.resolve('ftp/legal.md') })
                 if (absolutePath.includes(path.resolve('.'))) {
-                  entry.pipe(fs.createWriteStream('uploads/complaints/' + fileName).on('error', function (err) { next(err) }))
+                  entry.pipe(fs.createWriteStream('uploads/complaints/' + fileName).on('error', function (err: any) { next(err) }))
                 } else {
                   entry.autodrain()
                 }
@@ -54,14 +53,14 @@ function handleZipFileUpload ({ file }: Request, res: Response, next: NextFuncti
   }
 }
 
-function checkUploadSize ({ file }: Request, res: Response, next: NextFunction) {
+function checkUploadSize ({ file }: { file: any }, res: any, next: any) {
   if (file != null) {
     challengeUtils.solveIf(challenges.uploadSizeChallenge, () => { return file?.size > 100000 })
   }
   next()
 }
 
-function checkFileType ({ file }: Request, res: Response, next: NextFunction) {
+function checkFileType ({ file }: { file: any }, res: any, next: any) {
   const fileType = file?.originalname.substr(file.originalname.lastIndexOf('.') + 1).toLowerCase()
   challengeUtils.solveIf(challenges.uploadTypeChallenge, () => {
     return !(fileType === 'pdf' || fileType === 'xml' || fileType === 'zip')
@@ -69,7 +68,7 @@ function checkFileType ({ file }: Request, res: Response, next: NextFunction) {
   next()
 }
 
-function handleXmlUpload ({ file }: Request, res: Response, next: NextFunction) {
+function handleXmlUpload ({ file }: { file: any }, res: any, next: any) {
   if (utils.endsWith(file?.originalname.toLowerCase(), '.xml')) {
     challengeUtils.solveIf(challenges.deprecatedInterfaceChallenge, () => { return true })
     if (((file?.buffer) != null) && utils.isChallengeEnabled(challenges.deprecatedInterfaceChallenge)) { // XXE attacks in Docker/Heroku containers regularly cause "segfault" crashes
@@ -82,7 +81,7 @@ function handleXmlUpload ({ file }: Request, res: Response, next: NextFunction) 
         challengeUtils.solveIf(challenges.xxeFileDisclosureChallenge, () => { return (utils.matchesEtcPasswdFile(xmlString) || utils.matchesSystemIniFile(xmlString)) })
         res.status(410)
         next(new Error('B2B customer complaints via file upload have been deprecated for security reasons: ' + utils.trunc(xmlString, 400) + ' (' + file.originalname + ')'))
-      } catch (err: any) { // TODO: Remove any
+      } catch (err: any) {
         if (utils.contains(err.message, 'Script execution timed out')) {
           if (challengeUtils.notSolved(challenges.xxeDosChallenge)) {
             challengeUtils.solve(challenges.xxeDosChallenge)
