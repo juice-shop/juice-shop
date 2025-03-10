a4f5968a74ae87812dee2d6c86b761c41fe62025
Jannik Hollenbach <jannik.hollenbach@owasp.org>
2025-03-10 20:33:18 +0100

    Fix typescript warning

--- frontend/src/app/deluxe-user/deluxe-user.component.ts
+++ frontend/src/app/deluxe-user/deluxe-user.component.ts
@@ -25,7 +25,7 @@ import { NgIf } from '@angular/common'
 
 export class DeluxeUserComponent implements OnInit {
   public membershipCost: number = 0
-  public error: string = undefined
+  public error?: string = undefined
   public applicationName = 'OWASP Juice Shop'
   public logoSrc: string = 'assets/public/images/JuiceShop_Logo.png'
 
