3a0acc9410c67b595e7e76c3f81e955a8a9ff925
Jannik Hollenbach <jannik.hollenbach@owasp.org>
2025-03-22 22:49:56 +0100

    Update codefixes properly

--- data/static/codefixes/resetPasswordMortyChallenge_1.ts
+++ data/static/codefixes/resetPasswordMortyChallenge_1.ts
@@ -1,5 +1,5 @@
 /* Rate limiting */
-  app.use('/rest/user/reset-password', new RateLimit({
+  app.use('/rest/user/reset-password', rateLimit({
     windowMs: 5 * 60 * 1000,
     max: 100,
     keyGenerator ({ headers, ip }) { return headers['X-Forwarded-For'] ?? ip }
