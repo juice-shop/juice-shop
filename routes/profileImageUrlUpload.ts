2115dc7c3ffb1630f6050b35370a3b02830bc17f
Jannik Hollenbach <jannik.hollenbach@owasp.org>
2025-03-22 20:16:52 +0100

    Migrate webhook to use fetch too
    Signed-off-by: Jannik Hollenbach <jannik.hollenbach@owasp.org>

--- routes/profileImageUrlUpload.ts
+++ routes/profileImageUrlUpload.ts
@@ -4,36 +4,46 @@
  */
 
 import fs from 'fs'
+import { fetch } from 'undici'
+import { promisify } from 'util'
+import { pipeline } from 'stream'
 import { type Request, type Response, type NextFunction } from 'express'
-import logger from '../lib/logger'
 
 import { UserModel } from '../models/user'
 import * as utils from '../lib/utils'
+import logger from '../lib/logger'
+
 const security = require('../lib/insecurity')
-const request = require('request')
 
 module.exports = function profileImageUrlUpload () {
-  return (req: Request, res: Response, next: NextFunction) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
     if (req.body.imageUrl !== undefined) {
       const url = req.body.imageUrl
       if (url.match(/(.)*solve\/challenges\/server-side(.)*/) !== null) req.app.locals.abused_ssrf_bug = true
       const loggedInUser = security.authenticatedUsers.get(req.cookies.token)
       if (loggedInUser) {
-        const imageRequest = request
-          .get(url)
-          .on('error', function (err: unknown) {
-            UserModel.findByPk(loggedInUser.data.id).then(async (user: UserModel | null) => { return await user?.update({ profileImage: url }) }).catch((error: Error) => { next(error) })
-            logger.warn(`Error retrieving user profile image: ${utils.getErrorMessage(err)}; using image link directly`)
-          })
-          .on('response', function (res: Response) {
-            if (res.statusCode === 200) {
-              const ext = ['jpg', 'jpeg', 'png', 'svg', 'gif'].includes(url.split('.').slice(-1)[0].toLowerCase()) ? url.split('.').slice(-1)[0].toLowerCase() : 'jpg'
-              imageRequest.pipe(fs.createWriteStream(`frontend/dist/frontend/assets/public/images/uploads/${loggedInUser.data.id}.${ext}`))
-              UserModel.findByPk(loggedInUser.data.id).then(async (user: UserModel | null) => { return await user?.update({ profileImage: `/assets/public/images/uploads/${loggedInUser.data.id}.${ext}` }) }).catch((error: Error) => { next(error) })
-            } else UserModel.findByPk(loggedInUser.data.id).then(async (user: UserModel | null) => { return await user?.update({ profileImage: url }) }).catch((error: Error) => { next(error) })
-          })
+        try {
+          const response = await fetch(url)
+          if (!response.ok || !response.body) {
+            throw new Error('url returned a non-OK status code or an empty body')
+          }
+          const ext = ['jpg', 'jpeg', 'png', 'svg', 'gif'].includes(url.split('.').slice(-1)[0].toLowerCase()) ? url.split('.').slice(-1)[0].toLowerCase() : 'jpg'
+          const streamPipeline = promisify(pipeline)
+          await streamPipeline(response.body, fs.createWriteStream(`frontend/dist/frontend/assets/public/images/uploads/${loggedInUser.data.id}.${ext}`))
+          await UserModel.findByPk(loggedInUser.data.id).then(async (user: UserModel | null) => { return await user?.update({ profileImage: `/assets/public/images/uploads/${loggedInUser.data.id}.${ext}` }) }).catch((error: Error) => { next(error) })
+        } catch (error) {
+          try {
+            const user = await UserModel.findByPk(loggedInUser.data.id)
+            await user?.update({ profileImage: url })
+            logger.warn(`Error retrieving user profile image: ${utils.getErrorMessage(error)}; using image link directly`)
+          } catch (error) {
+            next(error)
+            return
+          }
+        }
       } else {
         next(new Error('Blocked illegal activity by ' + req.socket.remoteAddress))
+        return
       }
     }
     res.location(process.env.BASE_PATH + '/profile')
