ae7aff8f2140eac0e0b71390056442881d3630ba
Jannik Hollenbach <jannik.hollenbach@owasp.org>
2025-03-23 15:19:58 +0100

    Update data erasure insecurity to import
    Signed-off-by: Jannik Hollenbach <jannik.hollenbach@owasp.org>

--- routes/dataErasure.ts
+++ routes/dataErasure.ts
@@ -10,15 +10,13 @@ import { PrivacyRequestModel } from '../models/privacyRequests'
 import { SecurityAnswerModel } from '../models/securityAnswer'
 import * as challengeUtils from '../lib/challengeUtils'
 import { challenges } from '../data/datacache'
+import * as security from '../lib/insecurity'
 import { UserModel } from '../models/user'
-
-const insecurity = require('../lib/insecurity')
 
 const router = express.Router()
 
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
-router.get('/', async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  const loggedInUser = insecurity.authenticatedUsers.get(req.cookies.token)
+router.get('/', async (req: Request, res: Response, next: NextFunction) => {
+  const loggedInUser = security.authenticatedUsers.get(req.cookies.token)
   if (!loggedInUser) {
     next(new Error('Blocked illegal activity by ' + req.socket.remoteAddress))
     return
@@ -54,7 +52,7 @@ interface DataErasureRequestParams {
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
 router.post('/', async (req: Request<Record<string, unknown>, Record<string, unknown>, DataErasureRequestParams>, res: Response, next: NextFunction): Promise<void> => {
-  const loggedInUser = insecurity.authenticatedUsers.get(req.cookies.token)
+  const loggedInUser = security.authenticatedUsers.get(req.cookies.token)
   if (!loggedInUser) {
     next(new Error('Blocked illegal activity by ' + req.socket.remoteAddress))
     return
