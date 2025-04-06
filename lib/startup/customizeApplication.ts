c93575905003c1115fb72b33f3c800d68c08fc81
Jannik Hollenbach <jannik.hollenbach@owasp.org>
2025-04-06 20:48:47 +0200

    Fix themes not getting applied properly
    Signed-off-by: Jannik Hollenbach <jannik.hollenbach@owasp.org>

--- lib/startup/customizeApplication.ts
+++ lib/startup/customizeApplication.ts
@@ -92,9 +92,9 @@ const customizeTitle = () => {
 }
 
 const customizeTheme = () => {
-  const bodyClass = '"mat-app-background ' + config.get<string>('application.theme') + '-theme"'
+  const bodyClass = '"mat-app-background mat-typography ' + config.get<string>('application.theme') + '-theme"'
   replace({
-    regex: /"mat-app-background .*-theme"/,
+    regex: /"mat-app-background mat-typography .*-theme"/,
     replacement: bodyClass,
     paths: ['frontend/dist/frontend/index.html'],
     recursive: false,
