3623da41591b19e2c0c8f2b9877f30f917ce11ea
anasteshap <anasteshap@gmail.com>
2025-04-14 00:43:19 +0300

    fix: sql injection

--- routes/search.ts
+++ routes/search.ts
@@ -20,7 +20,13 @@ module.exports = function searchProducts () {
   return (req: Request, res: Response, next: NextFunction) => {
     let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
+    models.sequelize.query(
+        `SELECT * FROM Products WHERE ((name :searchTerm ? OR description LIKE :searchTerm) AND deletedAt IS NULL) ORDER BY name`,
+        {
+            replacements: { searchTerm: `%${criteria}%` },
+            type: models.sequelize.QueryTypes.SELECT
+            }
+        ) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
       .then(([products]: any) => {
         const dataString = JSON.stringify(products)
         if (challengeUtils.notSolved(challenges.unionSqlInjectionChallenge)) { // vuln-code-snippet hide-start
