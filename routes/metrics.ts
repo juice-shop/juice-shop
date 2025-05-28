ec7d0f7f7ad8847b59eefaa05e46d78853b617e5
Sven Kirschbaum <sven@kirschbaum.me>
2025-05-28 23:44:27 +0200

    feat: Make ignored useragents for /metrics route configurable

--- routes/metrics.ts
+++ routes/metrics.ts
@@ -67,7 +67,8 @@ export function serveMetrics () {
   return async (req: Request, res: Response, next: NextFunction) => {
     challengeUtils.solveIf(challenges.exposedMetricsChallenge, () => {
       const userAgent = req.headers['user-agent'] ?? ''
-      return !userAgent.includes('Prometheus')
+      const ignoredUserAgents = config.get<string[]>('challenges.metricsIgnoredUserAgents')
+      return !ignoredUserAgents.some((ignoredUserAgent) => userAgent.includes(ignoredUserAgent))
     })
     res.set('Content-Type', register.contentType)
     res.end(await register.metrics())
