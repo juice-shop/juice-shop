0d5c646f71817f44174d534ff9fab89b15c3ce8b
Kanasin Amornkittisarn <50575341+gameblezz@users.noreply.github.com>
2025-06-04 12:45:39 +0700

    Add files via upload
    This file is IAC and have vuln

---
+++ provider_azurerm.tf
@@ -0,0 +1,19 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+resource "azurerm_mssql_server" "example" {
+  name                         = "example-sqlserver"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "sqladminuser"
+  administrator_login_password = "H@Sh1CoR3!"
+
+  public_network_access_enabled = true  # <-- This will trigger the IaC security rule
+}
