d47b50b28a3bab070b65c06659ee35c978833d92
stevebuko-semgrep <stephen@semgrep.com>
2025-06-04 11:43:40 -0400

    Update dbSchemaChallenge_1.ts
    fixed security vulnerability in line 10

--- data/static/codefixes/dbSchemaChallenge_1.ts
+++ data/static/codefixes/dbSchemaChallenge_1.ts
@@ -2,7 +2,12 @@ export function searchProducts () {
   return (req: Request, res: Response, next: NextFunction) => {
     let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    models.sequelize.query("SELECT * FROM Products WHERE ((name LIKE '%"+criteria+"%' OR description LIKE '%"+criteria+"%') AND deletedAt IS NULL) ORDER BY name")
+    const like_criteria = '%' + criteria + '%'
+    models.sequelize.query(
+      "SELECT * FROM Products WHERE ((name LIKE :like_criteria OR description LIKE :like_criteria) AND deletedAt IS NULL) ORDER BY name",
+      { replacements: { like_criteria } }
+    )
+      //models.sequelize.query("SELECT * FROM Products WHERE ((name LIKE '%"+criteria+"%' OR description LIKE '%"+criteria+"%') AND deletedAt IS NULL) ORDER BY name")
       .then(([products]: any) => {
         const dataString = JSON.stringify(products)
         for (let i = 0; i < products.length; i++) {
