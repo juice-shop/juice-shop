0c100c83541a453828e6c0c2da836b9e5a350fea
Vulna <dmitry.ovchinnikov@cycode.com>
2025-06-10 16:28:36 +0200

    Remove 1 violation

--- routes/updateProductReviews.ts
+++ routes/updateProductReviews.ts
@@ -15,7 +15,6 @@ export function updateProductReviews () {
   return (req: Request, res: Response, next: NextFunction) => {
     const user = security.authenticatedUsers.from(req) // vuln-code-snippet vuln-line forgedReviewChallenge
     db.reviewsCollection.update( // vuln-code-snippet neutral-line forgedReviewChallenge
-      { _id: req.body.id }, // vuln-code-snippet vuln-line noSqlReviewsChallenge forgedReviewChallenge
       { $set: { message: req.body.message } },
       { multi: true } // vuln-code-snippet vuln-line noSqlReviewsChallenge
     ).then(
