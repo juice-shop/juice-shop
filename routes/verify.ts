58dc4f3da7c4283759b236c1c38a47ad2a6e984a
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2025-06-11 14:38:04 +0200

    Add new "Leaked API Key" challenge

--- routes/verify.ts
+++ routes/verify.ts
@@ -164,6 +164,9 @@ export const databaseRelatedChallenges = () => (req: Request, res: Response, nex
   if (challengeUtils.notSolved(challenges.csafChallenge)) {
     csafChallenge()
   }
+  if (challengeUtils.notSolved(challenges.leakedApiKeyChallenge)) {
+    leakedApiKeyChallenge()
+  }
   next()
 }
 
@@ -407,6 +410,26 @@ function csafChallenge () {
     throw new Error('Unable to get data for known vulnerabilities. Please try again')
   })
 }
+
+function leakedApiKeyChallenge () {
+  FeedbackModel.findAndCountAll({ where: { comment: { [Op.like]: '%6PPi37DBxP4lDwlriuaxP15HaDJpsUXY5TspVmie%' } } }
+  ).then(({ count }: { count: number }) => {
+    if (count > 0) {
+      challengeUtils.solve(challenges.leakedApiKeyChallenge)
+    }
+  }).catch(() => {
+    throw new Error('Unable to get data for known vulnerabilities. Please try again')
+  })
+  ComplaintModel.findAndCountAll({ where: { message: { [Op.like]: '%6PPi37DBxP4lDwlriuaxP15HaDJpsUXY5TspVmie%' } } }
+  ).then(({ count }: { count: number }) => {
+    if (count > 0) {
+      challengeUtils.solve(challenges.leakedApiKeyChallenge)
+    }
+  }).catch(() => {
+    throw new Error('Unable to get data for known vulnerabilities. Please try again')
+  })
+}
+
 
 function dangerousIngredients () {
   return config.get<ProductConfig[]>('products')
