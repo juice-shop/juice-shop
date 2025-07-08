6ee0eaca9058d97d1e45349ae65f75cfc6e24d80
SokoP Urasoko <sokop@snyk.io>
2025-07-08 15:24:12 +0900

    mod param

--- frontend/src/app/search-result/search-result.component.ts
+++ frontend/src/app/search-result/search-result.component.ts
@@ -156,7 +156,7 @@ export class SearchResultComponent implements OnDestroy, AfterViewInit {
         this.io.socket().emit('verifyLocalXssChallenge', queryParam)
       }) // vuln-code-snippet hide-end
       this.dataSource.filter = queryParam.toLowerCase()
-      this.searchValue = this.sanitizer.bypassSecurityTrustHtml(queryParam) // vuln-code-snippet vuln-line localXssChallenge xssBonusChallenge
+      this.searchValue = this.sanitizer.bypassSecurityTrustHtml(queryParam)
       this.gridDataSource.subscribe((result: any) => {
         if (result.length === 0) {
           this.emptyState = true
