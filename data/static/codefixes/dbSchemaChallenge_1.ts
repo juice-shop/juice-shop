d57c2288fc64d0a1357727ee13aa46e75b5c2f0e
kira-offgrid <kira@offgridsec.com>
2025-08-22 14:23:52 +0000

    fix: javascript.sequelize.security.audit.sequelize-injection-express.express-sequelize-injection-data-static-codefixes-dbSchemaChallenge_1.ts

--- data/static/codefixes/dbSchemaChallenge_1.ts
+++ data/static/codefixes/dbSchemaChallenge_1.ts
@@ -2,7 +2,7 @@ export function searchProducts () {
   return (req: Request, res: Response, next: NextFunction) => {
     let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    models.sequelize.query("SELECT * FROM Products WHERE ((name LIKE '%"+criteria+"%' OR description LIKE '%"+criteria+"%') AND deletedAt IS NULL) ORDER BY name")
+    models.sequelize.query("SELECT * FROM Products WHERE ((name LIKE '%", { replacements: { userInput: criteria+"%' OR description LIKE '%"+criteria+"%' }, type: sequelize.QueryTypes.SELECT }) AND deletedAt IS NULL) ORDER BY name")
       .then(([products]: any) => {
         const dataString = JSON.stringify(products)
         for (let i = 0; i < products.length; i++) {
