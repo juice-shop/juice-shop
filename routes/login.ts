76aa3b12efdf1156911d9289e1d5c160cbfad9aa
Alonsoterrazas <alonsoterrazas81@gmail.com>
2025-09-21 18:10:59 -0600

    initial commit

--- routes/login.ts
+++ routes/login.ts
@@ -30,7 +30,7 @@ export function login () {
   }
 
   return (req: Request, res: Response, next: NextFunction) => {
-    verifyPreLoginChallenges(req) // vuln-code-snippet hide-line
+    verifyPreLoginChallenges(req) // vuln-code-snippet hide-
     models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${security.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model: UserModel, plain: true }) // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
       .then((authenticatedUser) => { // vuln-code-snippet neutral-line loginAdminChallenge loginBenderChallenge loginJimChallenge
         const user = utils.queryResultToJson(authenticatedUser)
