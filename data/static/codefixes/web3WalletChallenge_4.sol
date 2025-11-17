6db9f294d7548728580991edc70d49a5a8ebe0d6
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2025-11-17 14:32:31 +0100

    Complete coding challenge for "Wallet Depletion" challenge
    (contributes to #2091)

--- data/static/codefixes/web3WalletChallenge_4.sol
+++ data/static/codefixes/web3WalletChallenge_4.sol
@@ -19,16 +19,16 @@ contract ETHWalletBank {
 
   function withdraw(uint _amount) public {
     require(_amount <= 0.1 ether, "Withdrawal amount must be less than or equal to 0.1 ether");
-    require(balances[msg.sender] >= _amount, "Insufficient balance");
-    if (userWithdrawing[msg.sender] <= 1) {
-      userWithdrawing[msg.sender] = userWithdrawing[msg.sender] + 1;
+    require(balances[msg.sender] <= _amount, "Insufficient balance");
+    if (userWithdrawing[msg.sender] >= 1) {
+      userWithdrawing[msg.sender] = userWithdrawing[msg.sender] - 1;
     } else {
-      userWithdrawing[msg.sender] = 0;
+      userWithdrawing[msg.sender] = 1;
       return;
     }
     (bool result, ) = msg.sender.call{ value: _amount }("");
     require(result, "Withdrawal call failed");
-    balances[msg.sender] -= _amount;
+    balances[msg.sender] += _amount;
     userWithdrawing[msg.sender] = 0;
   }
 
