87332fa16c0ba2ec44e710ce041b95098504d0ca
abhay-codes07 <abhaysingh0293@gmail.com>
2025-12-01 16:12:02 +0530

    style: lint fixes

--- frontend/src/app/score-board/components/challenge-card/challenge-card.component.spec.ts
+++ frontend/src/app/score-board/components/challenge-card/challenge-card.component.spec.ts
@@ -1,65 +1,56 @@
-import { type ComponentFixture, TestBed } from '@angular/core/testing'
-
-import { ChallengeCardComponent } from './challenge-card.component'
-import { type Config } from 'src/app/Services/configuration.service'
+import { Component, Input, type OnInit, inject } from '@angular/core'
+import { EnrichedChallenge } from '../../types/EnrichedChallenge'
+import { Config } from 'src/app/Services/configuration.service'
 import { TranslateModule } from '@ngx-translate/core'
 import { MatIconModule } from '@angular/material/icon'
-import { MatTooltipModule } from '@angular/material/tooltip'
+import { MatTooltip } from '@angular/material/tooltip'
+import { NgClass } from '@angular/common'
+import { DifficultyStarsComponent } from '../difficulty-stars/difficulty-stars.component'
+import { SnackBarHelperService } from 'src/app/Services/snack-bar-helper.service'
 
-describe('ChallengeCard', () => {
-  let component: ChallengeCardComponent
-  let fixture: ComponentFixture<ChallengeCardComponent>
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [TranslateModule.forRoot(), MatIconModule, MatTooltipModule, ChallengeCardComponent]
-    })
-      .compileComponents()
-
-    fixture = TestBed.createComponent(ChallengeCardComponent)
-    component = fixture.componentInstance
-
-    component.challenge = {
-      category: 'foobar',
-      name: 'my name',
-      mitigationUrl: 'https://owasp.example.com',
-      hasCodingChallenge: true,
-      description: 'lorem ipsum',
-      tagList: ['Easy']
-    } as any
-
-    component.applicationConfiguration = {
-      ctf: {
-        showFlagsInNotifications: true
-      },
-      challenges: {
-        codingChallengesEnabled: 'solved'
-      },
-      hackingInstructor: {
-        isEnabled: true
-      }
-    } as Config
-
-    fixture.detectChanges()
-  })
-
-  it('should create', () => {
-    expect(component).toBeTruthy()
-  })
-
-  it('should not show a mitigation link when challenge has it but is not solved', () => {
-    component.challenge.solved = false
-    component.challenge.mitigationUrl = 'https://owasp.example.com'
-    fixture.detectChanges()
-    expect(fixture.nativeElement.querySelector('[aria-label="Vulnerability mitigation link"]'))
-      .toBeFalsy()
-  })
-
-  it('should show a mitigation link when challenge has it and is solved', () => {
-    component.challenge.solved = true
-    component.challenge.mitigationUrl = 'https://owasp.example.com'
-    fixture.detectChanges()
-    expect(fixture.nativeElement.querySelector('[aria-label="Vulnerability mitigation link"]'))
-      .toBeTruthy()
-  })
+@Component({
+  selector: 'challenge-card',
+  templateUrl: './challenge-card.component.html',
+  styleUrls: ['./challenge-card.component.scss'],
+  imports: [DifficultyStarsComponent, MatTooltip, MatIconModule, NgClass, TranslateModule]
 })
+export class ChallengeCardComponent implements OnInit {
+  private readonly snackBarHelperService = inject(SnackBarHelperService)
+
+  @Input()
+  public challenge: EnrichedChallenge
+
+  @Input()
+  public openCodingChallengeDialog: (challengeKey: string) => void
+
+  @Input()
+  public repeatChallengeNotification: (challengeKey: string) => void
+
+  @Input()
+  public unlockHint: (hintId: number) => void
+
+  @Input()
+  public applicationConfiguration: Config
+
+  public hasInstructions: (challengeName: string) => boolean = () => false
+  public startHackingInstructorFor: (challengeName: string) => Promise<void> = async () => {}
+
+  async ngOnInit () {
+    const { hasInstructions, startHackingInstructorFor } = await import('../../../../hacking-instructor')
+    this.hasInstructions = hasInstructions
+    this.startHackingInstructorFor = startHackingInstructorFor
+  }
+
+  copyPayload (event: MouseEvent) {
+    const target = event.target as HTMLElement
+    const codeElement = target.closest('code')
+    if (codeElement) {
+      const text = codeElement.innerText
+      if (navigator.clipboard) {
+        navigator.clipboard.writeText(text).then(() => {
+          this.snackBarHelperService.open('COPY_SUCCESS', 'confirmBar')
+        })
+      }
+    }
+  }
+}
