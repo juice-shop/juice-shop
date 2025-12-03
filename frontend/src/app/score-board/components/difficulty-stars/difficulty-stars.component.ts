00d445c61ff0a7bea92596ab7fa2fe0d1cff1e1c
Agrim Rai <agrimrai99@gmail.com>
2025-12-03 18:08:03 +0530

    accessible difficulty stars component changes in score-board

--- frontend/src/app/score-board/components/difficulty-stars/difficulty-stars.component.ts
+++ frontend/src/app/score-board/components/difficulty-stars/difficulty-stars.component.ts
@@ -1,12 +1,18 @@
-import { Component, Input } from '@angular/core'
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core'
 
 @Component({
   selector: 'difficulty-stars',
   templateUrl: './difficulty-stars.component.html',
   styleUrls: ['./difficulty-stars.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: []
 })
 export class DifficultyStarsComponent {
-  @Input()
-    difficulty: 1 | 2 | 3 | 4 | 5 | 6
+  // difficulty value between 0 and maxDifficulty
+  readonly difficulty = input.required<number>()
+  readonly maxDifficulty = input(6)
+
+  readonly stars = computed(() => Array.from({ length: this.maxDifficulty() }, (_, i) => i + 1))
+
+  readonly ariaLabel = computed(() => `Difficulty ${this.difficulty()} of ${this.maxDifficulty()}`)
 }
