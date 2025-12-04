d6f05f4b462bdad86076d6ab25881a6e496a3b42
Tenju Paul <tenjupaul@gmail.com>
2025-12-03 22:36:53 -0800

    Update register.component.ts

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -97,3 +97,40 @@ function matchValidator (passwordControl: AbstractControl) {
     return null
   }
 }
+
+function test7a(p1) {
+    if(p1 == null) {
+        return;
+    }
+    // In JavaScript null == undefined.
+    if(p1 == undefined) {
+        // Defect: dead code
+        return;
+    }
+    safe_process(p1);
+}
+
+function test7b(p1) {
+    if(p1 === null) {
+        return;
+    }
+    // But null !== undefined.
+    if(p1 === undefined) {
+        // No defect
+        return;
+    }
+    safe_process(p1);
+}
+
+function adapted_angular_code_example(parentElement) {
+  var isRoot = isMatchingElement(parentElement, $rootElement);
+  var state = isRoot ? rootAnimateState : parentElement.data(NG_ANIMATE_STATE);
+  var result = state && (!!state.disabled || state.totalActive > 0);
+  if(isRoot || result) {
+    return result;
+  }
+  if(isRoot) {
+    // Defect: dead code
+    return true;
+  }
+}
