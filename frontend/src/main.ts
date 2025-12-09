9815e6708ae03bf60c6bf686a673d4ad08877d42
Jannik Hollenbach <jannik.hollenbach@owasp.org>
2025-12-09 21:58:48 +0100

    Remove unused import

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,7 +4,6 @@
  */
 
 import { enableProdMode, importProvidersFrom } from '@angular/core'
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 
 import { environment } from './environments/environment'
 import { AppComponent } from './app/app.component'
