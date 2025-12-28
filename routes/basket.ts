1aa897267111ceea8ba7036ad23ee7f02aaa9123
rajeshuchil <rajeshuchil789@gmail.com>
2025-12-29 02:10:04 +0530

    replace callback pyramid with async/await
    Signed-off-by: rajeshuchil <rajeshuchil789@gmail.com>

--- routes/basket.ts
+++ routes/basket.ts
@@ -13,24 +13,24 @@ import * as security from '../lib/insecurity'
 import { challenges } from '../data/datacache'
 
 export function retrieveBasket () {
-  return (req: Request, res: Response, next: NextFunction) => {
-    const id = req.params.id
-    BasketModel.findOne({ where: { id }, include: [{ model: ProductModel, paranoid: false, as: 'Products' }] })
-      .then((basket: BasketModel | null) => {
-        /* jshint eqeqeq:false */
-        challengeUtils.solveIf(challenges.basketAccessChallenge, () => {
-          const user = security.authenticatedUsers.from(req)
-          return user && id && id !== 'undefined' && id !== 'null' && id !== 'NaN' && user.bid && user?.bid != parseInt(id, 10) // eslint-disable-line eqeqeq
-        })
-        if (((basket?.Products) != null) && basket.Products.length > 0) {
-          for (let i = 0; i < basket.Products.length; i++) {
-            basket.Products[i].name = req.__(basket.Products[i].name)
-          }
-        }
-
-        res.json(utils.queryResultToJson(basket))
-      }).catch((error: Error) => {
-        next(error)
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const id = req.params.id
+      const basket = await BasketModel.findOne({ where: { id }, include: [{ model: ProductModel, paranoid: false, as: 'Products' }] })
+      /* jshint eqeqeq:false */
+      challengeUtils.solveIf(challenges.basketAccessChallenge, () => {
+        const user = security.authenticatedUsers.from(req)
+        return user && id && id !== 'undefined' && id !== 'null' && id !== 'NaN' && user.bid && user?.bid != parseInt(id, 10) // eslint-disable-line eqeqeq
       })
+      if (((basket?.Products) != null) && basket.Products.length > 0) {
+        for (let i = 0; i < basket.Products.length; i++) {
+          basket.Products[i].name = req.__(basket.Products[i].name)
+        }
+      }
+
+      res.json(utils.queryResultToJson(basket))
+    } catch (error) {
+      next(error)
+    }
   }
 }
