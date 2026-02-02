68b5f75c6b5eca10cfacae55ef75883b5b96fa24
Henrique Cabral <hocabral37@gmail.com>
2026-02-02 11:53:55 -0300

    Triggering Aikido PR Checks

--- routes/profileImageUrlUpload.ts
+++ routes/profileImageUrlUpload.ts
@@ -17,7 +17,7 @@ export function profileImageUrlUpload () {
   return async (req: Request, res: Response, next: NextFunction) => {
     if (req.body.imageUrl !== undefined) {
       const url = req.body.imageUrl
-      if (url.match(/(.)*solve\/challenges\/server-side(.)*/) !== null) req.app.locals.abused_ssrf_bug = true
+      if (url.match(/(.)*solve\/challenges\/server-side(.)*/) != null) req.app.locals.abused_ssrf_bug = true
       const loggedInUser = security.authenticatedUsers.get(req.cookies.token)
       if (loggedInUser) {
         try {
