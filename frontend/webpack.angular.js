0543c00612c0e1ba2122830d03f2d113b9061c3e
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2026-02-04 10:53:21 +0100

    Run webpack analysis during build
    (creates ./frontend/dist/frontend/bundle-report.html)

--- frontend/webpack.angular.js
+++ frontend/webpack.angular.js
@@ -1,13 +1,21 @@
 // this file contains extras that should override angular's default configs
 
 const { CycloneDxWebpackPlugin } = require('@cyclonedx/webpack-plugin')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 module.exports = {
   plugins: [
     // @see https://www.npmjs.com/package/@cyclonedx/webpack-plugin
     new CycloneDxWebpackPlugin({
-      outputLocation: '../bom', // The path is relative to webpack's overall output path,
+      outputLocation: '../bom',
       includeWellknown: false
+    }),
+    // @see https://www.npmjs.com/package/webpack-bundle-analyzer
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+      reportFilename: 'bundle-report.html',
+      defaultSizes: 'gzip'
     })
   ]
 }
