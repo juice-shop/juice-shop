bbf43a396e17add2ec070a845a8962d7ff5f156e
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2026-02-04 11:12:01 +0100

    Make webpack analyzer plugin optional

--- frontend/webpack.angular.js
+++ frontend/webpack.angular.js
@@ -1,21 +1,32 @@
 // this file contains extras that should override angular's default configs
 
 const { CycloneDxWebpackPlugin } = require('@cyclonedx/webpack-plugin')
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+
+let BundleAnalyzerPlugin
+try {
+  BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+} catch (e) {
+  BundleAnalyzerPlugin = null
+}
+
+const plugins = [
+  // @see https://www.npmjs.com/package/@cyclonedx/webpack-plugin
+  new CycloneDxWebpackPlugin({
+    outputLocation: '../bom',
+    includeWellknown: false
+  })
+]
+
+if (BundleAnalyzerPlugin) {
+  // @see https://www.npmjs.com/package/webpack-bundle-analyzer
+  plugins.push(new BundleAnalyzerPlugin({
+    analyzerMode: 'static',
+    openAnalyzer: false,
+    reportFilename: 'bundle-report.html',
+    defaultSizes: 'gzip'
+  }))
+}
 
 module.exports = {
-  plugins: [
-    // @see https://www.npmjs.com/package/@cyclonedx/webpack-plugin
-    new CycloneDxWebpackPlugin({
-      outputLocation: '../bom',
-      includeWellknown: false
-    }),
-    // @see https://www.npmjs.com/package/webpack-bundle-analyzer
-    new BundleAnalyzerPlugin({
-      analyzerMode: 'static',
-      openAnalyzer: false,
-      reportFilename: 'bundle-report.html',
-      defaultSizes: 'gzip'
-    })
-  ]
+  plugins
 }
