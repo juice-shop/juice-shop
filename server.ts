baed7dfaaf16dd5583ef059982ba951af893989a
Jannik Hollenbach <jannik.hollenbach@owasp.org>
2026-02-17 22:38:26 +0100

    Serve frontend assests locally instead of loading them from thrid party CDNs
    Closes #3079
    
    Signed-off-by: Jannik Hollenbach <jannik.hollenbach@owasp.org>

--- server.ts
+++ server.ts
@@ -285,6 +285,13 @@ restoreOverwrittenFilesWithOriginals().then(() => {
   /* Swagger documentation for B2B v2 endpoints */
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
+  /* Serve vendor dependencies locally instead of from CDN */
+  app.use('/vendor/cookieconsent', express.static(path.resolve('node_modules/cookieconsent/build')))
+  app.use('/vendor/jquery', express.static(path.resolve('node_modules/jquery/dist')))
+  app.use('/vendor/material-design-lite', express.static(path.resolve('node_modules/material-design-lite/dist')))
+  app.use('/vendor/material-icons', express.static(path.resolve('node_modules/material-icons/iconfont')))
+  app.use('/vendor/fontsource-roboto', express.static(path.resolve('node_modules/@fontsource/roboto')))
+
   app.use(express.static(path.resolve('frontend/dist/frontend')))
   app.use(cookieParser('kekse'))
   // vuln-code-snippet end directoryListingChallenge accessLogDisclosureChallenge
