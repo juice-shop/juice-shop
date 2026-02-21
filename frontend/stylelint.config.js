186d91ff9bee2682f95477851620983fc1fdc294
ayu3456 <coaderayush1309@gmail.com>
2026-02-22 03:24:00 +0530

    chore: fix CI/CD errors (stylelint and linting)

--- frontend/stylelint.config.js
+++ frontend/stylelint.config.js
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2014-2026 Bjoern Kimminich & the OWASP Juice Shop contributors.
- * SPDX-License-Identifier: MIT
- */
-
 module.exports = {
   extends: 'stylelint-config-sass-guidelines',
   plugins: [
