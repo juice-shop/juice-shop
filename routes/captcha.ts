019770c1d50ea111679f922faaaa22934cff6b69
AnnaPolishkarova <113057862+AnnaPolishkarova@users.noreply.github.com>
2026-03-07 01:48:23 +0300

    fix: remove hardcoded secrets, fix SQL injection, replace eval

--- routes/captcha.ts
+++ routes/captcha.ts
@@ -20,7 +20,8 @@ export function captchas () {
     const secondOperator = operators[Math.floor((Math.random() * 3))]
 
     const expression = firstTerm.toString() + firstOperator + secondTerm.toString() + secondOperator + thirdTerm.toString()
-    const answer = eval(expression).toString() // eslint-disable-line no-eval
+//     const answer = eval(expression).toString() // eslint-disable-line no-eval
+    const answer = Function('"use strict"; return (' + expression + ')')().toString()
 
     const captcha = {
       captchaId,
