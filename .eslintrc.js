c106ce3c49c220a1662365dc973edbffad24ed38
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2026-03-16 14:49:32 +0100

    Add counts of violations and files affected by turned-off rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,13 +36,13 @@ module.exports = {
       files: ['**/*.ts'],
       parser: '@typescript-eslint/parser',
       rules: {
-        'no-void': 'off', // conflicting with recommendation from @typescript-eslint/no-floating-promises
+        'no-void': 'off', // 37 violations in 12 files (checked 2026-03-16) - conflicting with recommendation from @typescript-eslint/no-floating-promises
         // FIXME warnings below this line need to be checked and fixed.
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/strict-boolean-expressions': 'off',
-        '@typescript-eslint/no-var-requires': 'off',
-        '@typescript-eslint/no-misused-promises': 'off',
-        '@typescript-eslint/no-unsafe-argument': 'off'
+        '@typescript-eslint/explicit-function-return-type': 'off', // 394 violations in 126 files (checked 2026-03-16)
+        '@typescript-eslint/strict-boolean-expressions': 'off', // 243 violations in 74 files (checked 2026-03-16)
+        '@typescript-eslint/no-var-requires': 'off', // 1 violation in 1 file (checked 2026-03-16)
+        '@typescript-eslint/no-misused-promises': 'off', // 54 violations in 3 files (checked 2026-03-16)
+        '@typescript-eslint/no-unsafe-argument': 'off' // 333 violations in 55 files (checked 2026-03-16)
       }
     }
   ]
