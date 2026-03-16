0544be6ba88d19c2cb2e245a6e4dc7ef5dd5cdd7
Moustapha Mbaye <mbayemustaf2000@gmail.com>
2026-03-16 17:01:05 +0000

    feat: configuration de SonarCloud et du pipeline CI/CD

--- app.ts
+++ app.ts
@@ -7,6 +7,8 @@ async function app () {
   const { default: validateDependencies } = await import('./lib/startup/validateDependenciesBasic')
   await validateDependencies()
 
+  
+
   const server = await import('./server')
   await server.start()
 }
