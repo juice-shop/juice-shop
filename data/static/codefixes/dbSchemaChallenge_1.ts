3f6cc697667f8bd687d63038b8b477c3d3ee060d
shulgan <romashulgan@icloud.com>
2026-03-19 11:40:11 +0300

    dbSchemaChallenge injection fixed

--- data/static/codefixes/dbSchemaChallenge_1.ts
+++ data/static/codefixes/dbSchemaChallenge_1.ts
@@ -1,17 +1,38 @@
-export function searchProducts () {
-  return (req: Request, res: Response, next: NextFunction) => {
-    let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
-    criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    models.sequelize.query("SELECT * FROM Products WHERE ((name LIKE '%"+criteria+"%' OR description LIKE '%"+criteria+"%') AND deletedAt IS NULL) ORDER BY name")
-      .then(([products]: any) => {
-        const dataString = JSON.stringify(products)
-        for (let i = 0; i < products.length; i++) {
-          products[i].name = req.__(products[i].name)
-          products[i].description = req.__(products[i].description)
+import { Request, Response, NextFunction } from 'express'
+import { QueryTypes } from 'sequelize'
+
+export function searchProducts() {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      let criteria = req.query.q === 'undefined' ? '' : String(req.query.q ?? '')
+      criteria = criteria.length <= 200 ? criteria : criteria.substring(0, 200)
+
+      const likeCriteria = `%${criteria}%`
+
+      const products = await models.sequelize.query(
+        `
+          SELECT *
+          FROM Products
+          WHERE (
+            (name LIKE :criteria OR description LIKE :criteria)
+            AND deletedAt IS NULL
+          )
+          ORDER BY name
+        `,
+        {
+          replacements: { criteria: likeCriteria },
+          type: QueryTypes.SELECT
         }
-        res.json(utils.queryResultToJson(products))
-      }).catch((error: ErrorWithParent) => {
-        next(error.parent)
-      })
+      )
+
+      for (let i = 0; i < products.length; i++) {
+        products[i].name = req.__(products[i].name)
+        products[i].description = req.__(products[i].description)
+      }
+
+      res.json(utils.queryResultToJson(products))
+    } catch (error: any) {
+      next(error.parent ?? error)
+    }
   }
 }
