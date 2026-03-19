f735b744701e53d3d3e1a59ca927566730f9b3d5
Mikhail <polyakov.mihail@skbkontur.ru>
2026-03-20 00:03:55 +0300

    fix: SQL Injection, Hardcoded Secrets vulnerabilities

--- data/static/codefixes/unionSqlInjectionChallenge_3.ts
+++ data/static/codefixes/unionSqlInjectionChallenge_3.ts
@@ -2,12 +2,8 @@ export function searchProducts () {
   return (req: Request, res: Response, next: NextFunction) => {
     let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    // only allow apple or orange related searches
-    if (!criteria.startsWith("apple") || !criteria.startsWith("orange")) {
-      res.status(400).send()
-      return
-    }
-    models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`)
+    models.sequelize.query("SELECT * FROM Products WHERE ((name LIKE :criteria OR description LIKE :criteria) AND deletedAt IS NULL) ORDER BY name",
+      { replacements: { criteria: `%${criteria}%` } })
       .then(([products]: any) => {
         const dataString = JSON.stringify(products)
         for (let i = 0; i < products.length; i++) {
