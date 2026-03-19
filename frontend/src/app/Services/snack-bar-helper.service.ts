0eeb71db9ecdd1990d31cc8aed6b34efb9adffcb
JuiceShopBot <61591748+JuiceShopBot@users.noreply.github.com>
2026-03-19 16:39:11 +0000

    Auto-fix linting issues
    Signed-off-by: JuiceShopBot <61591748+JuiceShopBot@users.noreply.github.com>

--- frontend/src/app/Services/snack-bar-helper.service.ts
+++ frontend/src/app/Services/snack-bar-helper.service.ts
@@ -9,33 +9,33 @@ export class SnackBarHelperService {
   private readonly translateService = inject(TranslateService)
   private readonly snackBar = inject(MatSnackBar)
 
-  private isSnackbarOpen = false;
+  private isSnackbarOpen = false
 
   open(message: string, cssClass?: string) {
-    if (this.isSnackbarOpen) return;
+    if (this.isSnackbarOpen) return
 
     this.translateService.get(message).subscribe({
       next: (translatedMessage) => {
-        this.showSnack(translatedMessage, cssClass);
+        this.showSnack(translatedMessage, cssClass)
       },
       error: () => {
-        this.showSnack(message, cssClass);
+        this.showSnack(message, cssClass)
       }
-    });
+    })
   }
 
   private showSnack(message: string, cssClass?: string) {
-    this.isSnackbarOpen = true;
+    this.isSnackbarOpen = true
 
     const snackRef = this.snackBar.open(message, 'X', {
       duration: 5000,
       verticalPosition: 'top',
       horizontalPosition: 'right',
       panelClass: [cssClass || '', 'mat-body', 'custom-snackbar']
-    });
+    })
 
     snackRef.afterDismissed().subscribe(() => {
-      this.isSnackbarOpen = false;
-    });
+      this.isSnackbarOpen = false
+    })
   }
 }
