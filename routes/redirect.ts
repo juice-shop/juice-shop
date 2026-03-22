d2610177ee0d9668e6c1e0e7a85a3e7de6e48467
vyacheslav <vitslava5@gmail.com>
2026-03-22 15:56:09 +0300

    express-open-redirect routes/redirect.ts

--- routes/redirect.ts
+++ routes/redirect.ts
@@ -13,9 +13,25 @@ import * as utils from '../lib/utils'
 export function performRedirect () {
   return ({ query }: Request, res: Response, next: NextFunction) => {
     const toUrl: string = query.to as string
+
+    const allowedHosts = ['github.com', 'blockchain.info', 'explorer.dash.org', 'etherscan.io']
+
+    let parsedUrl: URL
+    try {
+      parsedUrl = new URL(toUrl)
+    } catch {
+      res.status(406)
+      next(new Error('Invalid URL for redirect: ' + toUrl))
+      return
+    }
+
+    if (!allowedHosts.includes(parsedUrl.hostname)) {
+      res.status(406)
+      next(new Error('Unrecognized target URL for redirect: ' + toUrl))
+      return
+    }
+
     if (security.isRedirectAllowed(toUrl)) {
-      challengeUtils.solveIf(challenges.redirectCryptoCurrencyChallenge, () => { return toUrl === 'https://explorer.dash.org/address/Xr556RzuwX6hg5EGpkybbv5RanJoZN17kW' || toUrl === 'https://blockchain.info/address/1AbKfgvw9psQ41NbLi8kufDQTezwG8DRZm' || toUrl === 'https://etherscan.io/address/0x0f933ab9fcaaa782d0279c300d73750e1311eae6' })
-      challengeUtils.solveIf(challenges.redirectChallenge, () => { return isUnintendedRedirect(toUrl) })
       res.redirect(toUrl)
     } else {
       res.status(406)
