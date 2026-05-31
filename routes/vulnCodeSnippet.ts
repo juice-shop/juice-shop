3259e2297a32b42d34b258b852c3205d8a9d47a6
Jannik Hollenbach <jannik.hollenbach@owasp.org>
2026-05-31 17:46:05 +0200

    Move vuln code snippet endpoint to use non-blocking fs
    Signed-off-by: Jannik Hollenbach <jannik.hollenbach@owasp.org>

--- routes/vulnCodeSnippet.ts
+++ routes/vulnCodeSnippet.ts
@@ -5,7 +5,7 @@
 
 import { type NextFunction, type Request, type Response } from 'express'
 import yaml from 'js-yaml'
-import fs from 'node:fs'
+import fs from 'node:fs/promises'
 
 import { getCodeChallenges } from '../lib/codingChallenges'
 import * as challengeUtils from '../lib/challengeUtils'
@@ -86,8 +86,8 @@ export const checkVulnLines = () => async (req: Request<Record<string, unknown>,
   const selectedLines: number[] = req.body.selectedLines
   const verdict = getVerdict(vulnLines, neutralLines, selectedLines)
   let hint
-  if (fs.existsSync('./data/static/codefixes/' + key + '.info.yml')) {
-    const codingChallengeInfos = yaml.load(fs.readFileSync('./data/static/codefixes/' + key + '.info.yml', 'utf8'))
+  if (await fs.stat('./data/static/codefixes/' + key + '.info.yml')) {
+    const codingChallengeInfos = yaml.load(await fs.readFile('./data/static/codefixes/' + key + '.info.yml', { encoding: 'utf8' }))
     if (codingChallengeInfos?.hints) {
       if (accuracy.getFindItAttempts(key) > codingChallengeInfos.hints.length) {
         if (vulnLines.length === 1) {
