5d2e89fd3aa6ac32764d594d61a02ca28cda5ddc
Sandeep <sandeep@WD365082200goldlinuxvpsNAxBU7d4k2.test.com>
2026-07-14 18:32:25 +0100

    fix: remediate 2 vulnerability(ies) in routes/videoHandler.ts
    Fixed vulnerabilities:
    - javascript.lang.security.audit.unknown-value-with-script-tag.unknown-value-with-script-tag
      Cannot determine what 'subs' is and it is used with a '<script>' tag. This could be susceptible to cross-site scripting (XSS). Ensure 'subs' is not externally controlled, or sanitize this data.

--- routes/videoHandler.ts
+++ routes/videoHandler.ts
@@ -67,7 +67,7 @@ export const promotionVideo = () => {
       template = template.replace(/_primDark_/g, theme.primDark)
       const pug = (await import('pug')).default
       const fn = pug.compile(template)
-      let compiledTemplate = fn()
+      let compiledTemplate = fn(); compiledTemplate = compiledTemplate.replace('<script id="subtitle"></script>', '<script id="subtitle" type="text/vtt" data-label="English" data-lang="en">' + sanitizeSubs(subs) + '</script>');
       compiledTemplate = compiledTemplate.replace('<script id="subtitle"></script>', '<script id="subtitle" type="text/vtt" data-label="English" data-lang="en">' + subs + '</script>')
       res.send(compiledTemplate)
     })
@@ -77,7 +77,7 @@ export const promotionVideo = () => {
   }
 }
 
-function getSubsFromFile () {
+function sanitizeSubs(subs) { return entities.encode(subs); } function getSubsFromFile () {
   const subtitles = config.get<string>('application.promotion.subtitles') ?? 'owasp_promo.vtt'
   const data = fs.readFileSync('frontend/dist/frontend/assets/public/videos/' + subtitles, 'utf8')
   return data.toString()
