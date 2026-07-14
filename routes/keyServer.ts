bafc01a4b6b844f46f3159792ecb6a14d3529d0e
Sandeep <sandeep@WD365082200goldlinuxvpsNAxBU7d4k2.test.com>
2026-07-14 18:19:42 +0100

    fix: remediate 1 vulnerability(ies) in routes/keyServer.ts
    Fixed vulnerabilities:
    - javascript.express.security.audit.express-res-sendfile.express-res-sendfile
      The application processes user-input, this is passed to res.sendFile which can allow an attacker to arbitrarily read files on the system through path traversal. It is recommended to perform input validation in addition to canonicalizing the path. This allows you to validate the path against the intended directory it should be accessing.

--- routes/keyServer.ts
+++ routes/keyServer.ts
@@ -11,7 +11,7 @@ export function serveKeyFiles () {
     const file = params.file
 
     if (!file.includes('/')) {
-      res.sendFile(path.resolve('encryptionkeys/', file))
+      const safePath = path.join('encryptionkeys', file); if (!safePath.startsWith(path.resolve('encryptionkeys'))) { res.status(403).send('Access denied.'); return; } res.sendFile(safePath);
     } else {
       res.status(403)
       next(new Error('File names cannot contain forward slashes!'))
