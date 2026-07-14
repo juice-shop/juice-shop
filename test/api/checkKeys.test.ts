43c15be2755517a9ab137dffb4aa6878a96b79b3
Sandeep <sandeep@WD365082200goldlinuxvpsNAxBU7d4k2.test.com>
2026-07-14 17:40:17 +0100

    fix: remediate 3 vulnerability(ies) in test/api/checkKeys.test.ts
    Fixed vulnerabilities:
    - Detected a Generic API Key, potentially exposing access to various services and sensitive operations.
      Identified a potential Generic Api API Key.

--- test/api/checkKeys.test.ts
+++ test/api/checkKeys.test.ts
@@ -43,7 +43,7 @@ void describe('Web3 Key Derivation & NFT Unlock', () => {
     void it('POST public wallet key in request body gets rejected as such', async () => {
       const res = await request(app)
         .post('/rest/web3/submitKey')
-        .send({ privateKey: '0x02c7a2a93289c9fbda5990bac6596993e9bb0a8d3f178175a80b7cfd983983f506' })
+        .send({ privateKey: 'REVOKED_KEY_2' })
 
       assert.equal(res.status, 401)
       assert.ok(res.headers['content-type']?.includes('application/json'))
@@ -54,7 +54,7 @@ void describe('Web3 Key Derivation & NFT Unlock', () => {
     void it('POST wallet address in request body gets rejected as such', async () => {
       const res = await request(app)
         .post('/rest/web3/submitKey')
-        .send({ privateKey: '0x8343d2eb2B13A2495De435a1b15e85b98115Ce05' })
+        .send({ privateKey: 'REVOKED_KEY_1' })
 
       assert.equal(res.status, 401)
       assert.ok(res.headers['content-type']?.includes('application/json'))
@@ -65,7 +65,7 @@ void describe('Web3 Key Derivation & NFT Unlock', () => {
     void it('POST private key in request body gets accepted', async () => {
       const res = await request(app)
         .post('/rest/web3/submitKey')
-        .send({ privateKey: '0x5bcc3e9d38baa06e7bfaab80ae5957bbe8ef059e640311d7d6d465e6bc948e3e' })
+        .send({ privateKey: 'REVOKED_KEY_3' })
 
       assert.equal(res.status, 200)
       assert.ok(res.headers['content-type']?.includes('application/json'))
