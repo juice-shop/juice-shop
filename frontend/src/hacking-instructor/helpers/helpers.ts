a72d1907da2a71b5e92364a3e04eb623f4265cae
Sandeep <sandeep@WD365082200goldlinuxvpsNAxBU7d4k2.test.com>
2026-07-14 18:17:44 +0100

    fix: remediate 1 vulnerability(ies) in frontend/src/hacking-instructor/helpers/helpers.ts
    Fixed vulnerabilities:
    - javascript.lang.security.audit.prototype-pollution.prototype-pollution-loop.prototype-pollution-loop
      Possibility of prototype polluting function detected. By adding or modifying attributes of an object prototype, it is possible to create attributes that exist on every object, or replace critical attributes with malicious ones. This can be problematic if the software depends on existence or non-existence of certain attributes, or uses pre-defined attributes of object prototype (such as hasOwnProperty, toString or valueOf). Possible mitigations might be: freezing the object prototype, using an object without prototypes (via Object.create(null) ), blocking modifications of attributes that resolve to object prototype, using Map instead of object.

--- frontend/src/hacking-instructor/helpers/helpers.ts
+++ frontend/src/hacking-instructor/helpers/helpers.ts
@@ -46,7 +46,7 @@ export function waitForInputToHaveValue (inputSelector: string, value: string, o
       const propertyChain = options.replacement[1].split('.')
       let replacementValue = config
       for (const property of propertyChain) {
-        replacementValue = replacementValue[property]
+        replacementValue = Object.create(null); replacementValue[property] = replacementValue[property];
       }
       value = value.replace(options.replacement[0], replacementValue)
     }
