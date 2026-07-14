4eab42f94006da49b18adf5bb063d03f24970f11
Sandeep <sandeep@WD365082200goldlinuxvpsNAxBU7d4k2.test.com>
2026-07-14 17:39:08 +0100

    fix: remediate 1 vulnerability(ies) in frontend/src/app/faucet/faucet.component.ts
    Fixed vulnerabilities:
    - Detected a Generic API Key, potentially exposing access to various services and sensitive operations.
      Identified a potential Generic Api API Key.

--- frontend/src/app/faucet/faucet.component.ts
+++ frontend/src/app/faucet/faucet.component.ts
@@ -31,7 +31,7 @@ const client = createClient({
 const { ethereum } = window
 
 const nftAddress = '0x41427790c94E7a592B17ad694eD9c06A02bb9C39'
-const BeeTokenAddress = '0x36435796Ca9be2bf150CE0dECc2D8Fab5C4d6E13'
+const BeeTokenAddress = '<REDACTED>';
 const BeeFaucetAddress = '0x860e3616aD0E0dEDc23352891f3E10C4131EA5BC'
 
 @Component({
