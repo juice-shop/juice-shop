82b1adbf39ec550d740fa407d39009414e090639
Sandeep <sandeep@WD365082200goldlinuxvpsNAxBU7d4k2.test.com>
2026-07-14 17:59:47 +0100

    fix: remediate 1 vulnerability(ies) in test/cypress/e2e/login.spec.ts
    Fixed vulnerabilities:
    - Detected a Generic API Key, potentially exposing access to various services and sensitive operations.
      Identified a potential Generic Api API Key.

--- test/cypress/e2e/login.spec.ts
+++ test/cypress/e2e/login.spec.ts
@@ -123,7 +123,7 @@ describe('/#/login', () => {
         }
       )
 
-      cy.task<string>('GenerateAuthenticator', 'IFTXE3SPOEYVURT2MRYGI52TKJ4HC3KH').then(
+      cy.task<string>('GenerateAuthenticator', 'YOUR_NEW_GENERATED_TOTP_SECRET').then(
         (totpToken: string) => {
           void cy.get('#totpToken').type(totpToken)
           void cy.get('#totpSubmitButton').click()
