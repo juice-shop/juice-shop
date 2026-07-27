7393e485d86426a1c749a274685d8d8e14e10598
Stefan Paraschiv <77758442+Stefan2320@users.noreply.github.com>
2026-07-27 18:01:53 +0300

    Add dummy configuration file with API keys

---
+++ config/dummy.js
@@ -0,0 +1,7 @@
+const config = {
+  awsAccessKeyId: "AKDDIOSFODNN7EXAMPLE",
+  awsSecretAccessKey: "wJalrXUtnFEMI/K7MDENG/bPxRfiCFEXAMPLEKOY",
+  apiKey: "sk_live_51H8qABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789eeff",
+};
+
+module.exports = config;
