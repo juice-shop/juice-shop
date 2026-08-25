e90d9341374382ff0eb6ddfcf33856242ce362a3
Ubuntu <ubuntu@ip-172-31-17-45.ap-south-1.compute.internal>
2026-08-25 11:30:55 +0000

    fix: remediate 1 vulnerability(ies) in data/static/codefixes/unionSqlInjectionChallenge_2_correct.ts
    Fixed vulnerabilities:
    - Unused assignments should be removed
      Remove this useless assignment to variable "dataString".

--- data/static/codefixes/unionSqlInjectionChallenge_2_correct.ts
+++ data/static/codefixes/unionSqlInjectionChallenge_2_correct.ts
@@ -6,7 +6,6 @@ export function searchProducts () {
         `SELECT * FROM Products WHERE ((name LIKE '%:criteria%' OR description LIKE '%:criteria%') AND deletedAt IS NULL) ORDER BY name`,
         { replacements: { criteria } }
       ).then(([products]: any) => {
-        const dataString = JSON.stringify(products)
         for (let i = 0; i < products.length; i++) {
           products[i].name = req.__(products[i].name)
           products[i].description = req.__(products[i].description)
