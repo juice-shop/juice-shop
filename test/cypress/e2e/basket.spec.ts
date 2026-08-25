3b5885116c915a61dbede8fa7aff630b45c4f087
Ubuntu <ubuntu@ip-172-31-17-45.ap-south-1.compute.internal>
2026-08-25 11:00:25 +0000

    fix: remediate 2 vulnerability(ies) in test/cypress/e2e/basket.spec.ts
    Fixed vulnerabilities:
    - Hard-coded passwords are security-sensitive
      Review this potentially hard-coded password.

--- test/cypress/e2e/basket.spec.ts
+++ test/cypress/e2e/basket.spec.ts
@@ -1,7 +1,7 @@
 describe('/#/basket', () => {
   describe('as admin', () => {
     beforeEach(() => {
-      cy.login({ email: 'admin', password: 'admin123' })
+      cy.login({ email: 'admin', password: Cypress.env('ADMIN_PASSWORD') })
     })
 
     describe('challenge "negativeOrderChallenge"', () => {
@@ -73,7 +73,7 @@ describe('/#/basket', () => {
 
   describe('as jim', () => {
     beforeEach(() => {
-      cy.login({ email: 'jim', password: 'ncc-1701' })
+      cy.login({ email: 'jim', password: Cypress.env('JIM_PASSWORD') })
     })
     describe('challenge "manipulateClockChallenge"', () => {
       it('should be possible to enter WMNSDY2019 coupon & place order with this expired coupon', () => {
