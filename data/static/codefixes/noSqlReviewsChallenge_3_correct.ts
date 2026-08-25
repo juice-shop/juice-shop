d60e10187e562f4386524b45ce6e3084be6632c4
Ubuntu <ubuntu@ip-172-31-17-45.ap-south-1.compute.internal>
2026-08-25 11:32:12 +0000

    fix: remediate 1 vulnerability(ies) in data/static/codefixes/noSqlReviewsChallenge_3_correct.ts
    Fixed vulnerabilities:
    - Unused assignments should be removed
      Remove this useless assignment to variable "dataString".

--- data/static/codefixes/noSqlReviewsChallenge_3_correct.ts
+++ data/static/codefixes/noSqlReviewsChallenge_3_correct.ts
@@ -1,6 +1,5 @@
 export function updateProductReviews () {
   return (req: Request, res: Response, next: NextFunction) => {
-    const user = security.authenticatedUsers.from(req)
 
     if (typeof req.body.id !== 'string') {
       res.status(400).send()
