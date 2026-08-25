ab4a8669ee5510d4661cb523864d70783f106a21
Ubuntu <ubuntu@ip-172-31-17-45.ap-south-1.compute.internal>
2026-08-25 11:20:46 +0000

    fix: remediate 3 vulnerability(ies) in data/static/codefixes/redirectCryptoCurrencyChallenge_3_correct.ts
    Fixed vulnerabilities:
    - Using clear-text protocols is security-sensitive
      Using http protocol is insecure. Use https instead.

--- data/static/codefixes/redirectCryptoCurrencyChallenge_3_correct.ts
+++ data/static/codefixes/redirectCryptoCurrencyChallenge_3_correct.ts
@@ -1,9 +1,9 @@
 export const redirectAllowlist = new Set([
   'https://github.com/juice-shop/juice-shop',
-  'http://shop.spreadshirt.com/juiceshop',
-  'http://shop.spreadshirt.de/juiceshop',
+  'https://shop.spreadshirt.com/juiceshop',
+  'https://shop.spreadshirt.de/juiceshop',
   'https://www.stickeryou.com/products/owasp-juice-shop/794',
-  'http://leanpub.com/juice-shop'
+  'https://leanpub.com/juice-shop'
 ])
 
 export const isRedirectAllowed = (url: string) => {
