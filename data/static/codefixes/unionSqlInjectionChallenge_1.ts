66db7477cdf60b7adb9db6727ce296a9dfaebe40
Ubuntu <ubuntu@ip-172-31-17-45.ap-south-1.compute.internal>
2026-08-25 11:30:41 +0000

    fix: remediate 1 vulnerability(ies) in data/static/codefixes/unionSqlInjectionChallenge_1.ts
    Fixed vulnerabilities:
    - Unused assignments should be removed
      Remove this useless assignment to variable "dataString".

--- data/static/codefixes/unionSqlInjectionChallenge_1.ts
+++ data/static/codefixes/unionSqlInjectionChallenge_1.ts
@@ -5,7 +5,7 @@ export function searchProducts () {
     criteria.replace(/"|'|;|and|or/i, "")
     models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`)
       .then(([products]: any) => {
-        const dataString = JSON.stringify(products)
+        
         for (let i = 0; i < products.length; i++) {
           products[i].name = req.__(products[i].name)
           products[i].description = req.__(products[i].description)
