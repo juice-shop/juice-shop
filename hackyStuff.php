3081adfbcba718db2a125fe60a9dd318a873f20b
Ernest <ernest@semgrep.com>
2026-08-28 09:06:19 +0100

    cool beans

---
+++ hackyStuff.php
@@ -0,0 +1,37 @@
+<?php
+
+if( isset( $_POST[ 'Submit' ]  ) ) {
+	// Get input
+	$target = trim($_REQUEST[ 'ip' ]);
+
+	// Set blacklist
+	$substitutions = array(
+		'||' => '',
+		'&'  => '',
+		';'  => '',
+		'| ' => '',
+		'-'  => '',
+		'$'  => '',
+		'('  => '',
+		')'  => '',
+		'`'  => '',
+	);
+
+	// Remove any of the characters in the array (blacklist).
+	$target = str_replace( array_keys( $substitutions ), $substitutions, $target );
+
+	// Determine OS and execute the ping command.
+	if( stristr( php_uname( 's' ), 'Windows NT' ) ) {
+		// Windows
+		$cmd = shell_exec( 'ping  ' . $target );
+	}
+	else {
+		// *nix
+		$cmd = shell_exec( 'ping  -c 4 ' . $target );
+	}
+
+	// Feedback for the end user
+	$html .= "<pre>{$cmd}</pre>";
+}
+
+?>
