6805f199bd7eecefc8541926fc63950f21b3c10d
Maciej Majerczyk <maciej@devarmor.com>
2026-09-21 09:31:16 +0200

    test-1

--- routes/angular.ts
+++ routes/angular.ts
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2014-2026 Bjoern Kimminich & the OWASP Juice Shop contributors.
- * SPDX-License-Identifier: MIT
+ * SPDX-License-Identifier: MIT test
  */
 
 import path from 'node:path'
